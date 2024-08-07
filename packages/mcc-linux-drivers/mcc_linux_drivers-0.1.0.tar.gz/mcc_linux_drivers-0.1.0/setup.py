f8fd7296e957ff4246b38a4f930b8e7d75482880
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-07 16:03:36 +0000

    Add package mcc-linux-drivers/0.1.0/mcc_linux_drivers-0.1.0.tar.gz

---
+++ packages/mcc-linux-drivers/mcc_linux_drivers-0.1.0.tar.gz/mcc_linux_drivers-0.1.0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="mcc_linux_drivers", 
+    version="0.1.0",  
+    author='W. Jasper',
+    author_email="your_email@example.com",
+    description='Linux Drivers for MCC',
+    url='https://github.com/wjasper/Linux_Drivers', 
+    packages=find_packages(),
+    python_requires='>=3.6',
+)
