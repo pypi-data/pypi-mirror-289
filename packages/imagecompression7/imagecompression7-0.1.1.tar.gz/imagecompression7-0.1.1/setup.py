84224a65a898750fbb498ab1ea66172d492c5abf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-07 14:04:25 +0000

    Add package imagecompression7/0.1.1/imagecompression7-0.1.1.tar.gz

---
+++ packages/imagecompression7/imagecompression7-0.1.1.tar.gz/imagecompression7-0.1.1/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='imagecompression7',
+    version='0.1.1',
+    packages=find_packages(),
+    install_requires=[
+        'numpy',
+        'scipy',
+        'pillow'
+    ],
+    description='Image compression using REIS algorithms',
+    author='Katie Wen-Ling Kuo',
+    author_email='katie20030705@gmail.com',
+    license='MIT',
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3'
+    ],
+    python_requires='>=3.6',
+)
