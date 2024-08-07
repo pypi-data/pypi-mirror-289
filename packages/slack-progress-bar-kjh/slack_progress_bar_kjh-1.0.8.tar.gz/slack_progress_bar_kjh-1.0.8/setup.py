80628630ce1e57da443435c1c516637fb3734d89
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-07 14:03:43 +0000

    Add package slack-progress-bar-kjh/1.0.8/slack_progress_bar_kjh-1.0.8.tar.gz

---
+++ packages/slack-progress-bar-kjh/slack_progress_bar_kjh-1.0.8.tar.gz/slack_progress_bar_kjh-1.0.8/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding='UTF-8') as fh:
+    long_description = fh.read()
+
+setup(
+    name="slack_progress_bar_kjh",
+    version="1.0.8",
+    author="jaehyeong.kim",
+    author_email="rlawogud970301@gmail.com",
+    description="Modify from Michael Lizzi's slack_progress_bar. Thank you.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/JaeHeong/slack-progress-bar_kjh",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.9",
+    install_requires=["slack_sdk"],
+)
