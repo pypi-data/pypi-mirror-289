3d88f1867faa6289a63072ad7ba728a1960e4628
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-07 04:03:02 +0000

    Add package artd-service/1.0.5/artd-service-1.0.5.tar.gz

---
+++ packages/artd-service/artd-service-1.0.5.tar.gz/artd-service-1.0.5/artd_service/data/service_data.py
@@ -0,0 +1,7 @@
+from django.utils.translation import gettext_lazy as _
+SERVICES = (
+    (1,"parking", _("Parking"), "https://yopandyop.s3.us-east-2.amazonaws.com/images/services/parking-area.png"),
+    (2, "bar", _("Bar"), "https://yopandyop.s3.us-east-2.amazonaws.com/images/services/cocktail.png"),
+    (3, "restaurant", _("Restaurant"), "https://yopandyop.s3.us-east-2.amazonaws.com/images/services/restaurant.png"),
+    (4, "club", _("Club"), "https://yopandyop.s3.us-east-2.amazonaws.com/images/services/club.png"),
+)
