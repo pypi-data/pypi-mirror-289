afbcb03405c21842409ed08eb1a89070935f1bdd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-07 16:03:41 +0000

    Add package invenio-pages/4.1.0/invenio_pages-4.1.0-py2.py3-none-any.whl

---
+++ packages/invenio-pages/invenio_pages-4.1.0-py2.py3-none-any.whl/invenio_pages/__init__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+#
+# This file is part of Invenio.
+# Copyright (C) 2015-2024 CERN.
+#
+# Invenio is free software; you can redistribute it and/or modify it
+# under the terms of the MIT License; see LICENSE file for more details.
+
+"""Static pages module for Invenio."""
+
+from .ext import InvenioPages, InvenioPagesREST
+from .records.models import PageList, PageModel
+
+__version__ = "4.1.0"
+
+__all__ = ("__version__", "InvenioPages", "InvenioPagesREST", "PageModel", "PageList")
