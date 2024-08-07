aab4a79e0764c2805d64d003ded71e56eb9820b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-07 16:03:48 +0000

    Add package pytorch-lightning/2.4.0/pytorch-lightning-2.4.0.tar.gz

---
+++ packages/pytorch-lightning/pytorch-lightning-2.4.0.tar.gz/pytorch-lightning-2.4.0/src/pytorch_lightning/demos/__init__.py
@@ -0,0 +1,2 @@
+from pytorch_lightning.demos.lstm import LightningLSTM, SequenceSampler, SimpleLSTM  # noqa: F401
+from pytorch_lightning.demos.transformer import LightningTransformer, Transformer, WikiText2  # noqa: F401
