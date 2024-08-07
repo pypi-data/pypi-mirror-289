24707d39966cbb76035f3ae20da1d6fa67f0f599
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-07 15:04:33 +0000

    Add package redpic/0.11.4/redpic-0.11.4.tar.gz

---
+++ packages/redpic/redpic-0.11.4.tar.gz/redpic-0.11.4/redpic/beam/__init__.py
@@ -0,0 +1,6 @@
+from redpic.beam.astra import AstraBeam
+from redpic.beam.base import BaseBeam
+from redpic.beam.radial_uniform import RadialUniformBeam
+from redpic.beam.uniform import UniformBeam
+
+__all__ = ["BaseBeam", "UniformBeam", "RadialUniformBeam", "AstraBeam"]
---
+++ packages/redpic/redpic-0.11.4.tar.gz/redpic-0.11.4/setup.py
@@ -0,0 +1,37 @@
+from setuptools import find_packages, setup
+
+from redpic.core import config
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+with open("requirements/base.txt", "r", encoding="utf-8") as fh:
+    required = fh.read().splitlines()
+
+setup(
+    name=config.NAME,
+    version=config.VERSION,
+    author=config.AUTHOR,
+    author_email=config.AUTHOR_EMAIL,
+    description=config.DOC,
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url=config.URL,
+    packages=find_packages(),
+    install_requires=required,
+    classifiers=[
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Intended Audience :: Science/Research",
+        "Topic :: Scientific/Engineering :: Physics",
+    ],
+    project_urls={
+        "Bug Reports": "https://github.com/fuodorov/redpic/issues",
+        "PyPi": "https://pypi.org/project/redpic/",
+        "Documentation": "https://fuodorov.github.io/redpic/",
+        "Source": "https://github.com/fuodorov/redpic/",
+    },
+)
