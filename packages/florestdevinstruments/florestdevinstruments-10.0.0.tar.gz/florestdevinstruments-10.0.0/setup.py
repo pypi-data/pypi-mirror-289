d6b81ed66fa75ac43e396e6fe5359a83b96bc658
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-07 16:03:37 +0000

    Add package florestdevinstruments/10.0.0/florestdevinstruments-10.0.0.tar.gz

---
+++ packages/florestdevinstruments/florestdevinstruments-10.0.0.tar.gz/florestdevinstruments-10.0.0/setup.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+
+
+setup(name='florestdevinstruments', version='10.0.0', description='Личная библиотека Флореста, написанная на Python.', long_description=f'Волшебная библиотека Флореста, созданная и работающая на Python.\nВ библиотеке очень много функций для взаимодействия с ИИ, PyGames, TXT файлами, API и много чего еще!\nСкачивай библиотеку с помощью `pip` и наслаждайся!\nМои социальные сети: https://telegra.ph/Socseti-Floresta-06-28', long_description_content_type='text/markdown', packages=['florestdevinstruments'], author='florestdev4185', author_email='florestone4185@internet.ru', install_requires=['g4f', 'MukeshAPI', 'pypresence', 'curl_cffi', 'pygame', 'pygame_gui', 'vk_api', 'pyautogui', 'googletrans', 'rcon', 'discord', 'smtplib', 'email', 'PyTelegramBotAPI'], url='https://telegra.ph/Socseti-Floresta-06-28', python_requires='>=3.8.9')
