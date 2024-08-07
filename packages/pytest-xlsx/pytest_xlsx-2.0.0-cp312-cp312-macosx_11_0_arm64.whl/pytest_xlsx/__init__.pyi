e24dd3ba454a4cd7ed6ca33583c7a417c737241c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-07 14:03:22 +0000

    Add package pytest-xlsx/2.0.0/pytest_xlsx-2.0.0-cp312-cp312-macosx_11_0_arm64.whl

---
+++ packages/pytest-xlsx/pytest_xlsx-2.0.0-cp312-cp312-macosx_11_0_arm64.whl/pytest_xlsx/__init__.pyi
@@ -0,0 +1,4 @@
+from .file import XlsxItem as XlsxItem
+from .templates import add_filters as add_filters, add_globals as add_globals, add_locals as add_locals, env_globals as env_globals, item_locals as item_locals
+
+__all__ = ['XlsxItem', 'env_globals', 'item_locals', 'add_filters', 'add_globals', 'add_locals']
