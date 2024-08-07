cd8ba059189137292a9e1ffbc0ed8037323fe0a5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-07 03:04:00 +0000

    Add package chemcloud/0.12.0/chemcloud-0.12.0-py3-none-any.whl

---
+++ packages/chemcloud/chemcloud-0.12.0-py3-none-any.whl/chemcloud/config.py
@@ -0,0 +1,25 @@
+from __future__ import annotations  # Can remove once we drop Python 3.9 support
+
+from pathlib import Path
+from typing import Optional
+
+from pydantic_settings import BaseSettings
+
+
+class Settings(BaseSettings):
+    """Base Settings configuration. Do not instantiate directly Use settings object on
+    module.
+    """
+
+    chemcloud_username: Optional[str] = None
+    chemcloud_password: Optional[str] = None
+    chemcloud_base_directory: Path = Path.home() / ".chemcloud"
+    chemcloud_credentials_file: str = "credentials"
+    chemcloud_access_token_expiration_buffer: int = 15
+    chemcloud_domain: str = "https://chemcloud.mtzlab.com"
+    chemcloud_api_version_prefix: str = "/api/v2"
+    chemcloud_credentials_profile: str = "default"
+    tcfe_keywords: str = "tcfe:keywords"
+
+
+settings = Settings()
