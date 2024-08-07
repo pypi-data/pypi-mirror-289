747795488824d49923378b3bf20b716d173fdaf7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-07 14:03:38 +0000

    Add package dxrsubscriber/0.0.0/dxrsubscriber-0.0.0-py3-none-any.whl

---
+++ packages/dxrsubscriber/dxrsubscriber-0.0.0-py3-none-any.whl/dxrsubscriber/__init__.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+# @Time    : 2024/8/7 8:54
+# @Author  : wangqinggang
+# @Email   : wqg1993@qq.com
+# @File    : __init__.py
+__version__ = "0.0.0"
+from .subscriber import DxrSubscriber
