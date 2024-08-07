8d8c2112a10ab03278f2c7f5d7b1e579c768ed1b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-07 14:03:19 +0000

    Add package joonmyung/1.5.12/joonmyung-1.5.12.tar.gz

---
+++ packages/joonmyung/joonmyung-1.5.12.tar.gz/joonmyung-1.5.12/models/SA/MHSA.py
@@ -0,0 +1,37 @@
+import torch.nn as nn
+class MHSA(nn.Module):
+    def __init__(self, dim, num_heads=8, qkv_bias=False, attn_drop=0., proj_drop=0.,
+                 talking_head=False):
+        super().__init__()
+        self.num_heads = num_heads
+        head_dim = dim // num_heads
+        self.scale = head_dim ** -0.5
+
+        self.qkv = nn.Linear(dim, dim * 3, bias=qkv_bias)
+        self.attn_drop = nn.Dropout(attn_drop)
+        self.proj = nn.Linear(dim, dim)
+        self.proj_drop = nn.Dropout(proj_drop)
+
+
+        self.proj_l = nn.Linear(num_heads, num_heads) if talking_head else nn.Identity()
+        self.proj_w = nn.Linear(num_heads, num_heads) if talking_head else nn.Identity()
+
+
+    def forward(self, x):
+        B, N, C = x.shape
+        qkv = self.qkv(x).reshape(B, N, 3, self.num_heads, C // self.num_heads).permute(2, 0, 3, 1, 4)
+        q, k, v = qkv[0], qkv[1], qkv[2]   # make torchscript happy (cannot use tensor as tuple)
+
+        attn = (q @ k.transpose(-2, -1)) * self.scale # (B, H, T, D)
+        # attn = self.proj_l(attn.permute(0, 2, 3, 1)).permute(0, 3, 1, 2)
+        attn = self.proj_l(attn.transpose(1, -1)).transpose(1, -1)
+
+        attn = attn.softmax(dim=-1)
+        attn = self.attn_drop(attn)
+        # attn = self.proj_w(attn.permute(0, 2, 3, 1)).permute(0, 3, 1, 2)
+        attn = self.proj_w(attn.transpose(1, -1)).transpose(1, -1)
+
+        x = (attn @ v).transpose(1, 2).reshape(B, N, C)
+        x = self.proj(x)
+        x = self.proj_drop(x)
+        return x
