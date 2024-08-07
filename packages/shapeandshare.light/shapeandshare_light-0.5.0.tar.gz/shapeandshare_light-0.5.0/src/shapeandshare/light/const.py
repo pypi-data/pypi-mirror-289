ff93dbebfc791f2fbbf333184b07014c056fc0df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-07 14:03:19 +0000

    Add package shapeandshare.light/0.5.0/shapeandshare_light-0.5.0.tar.gz

---
+++ packages/shapeandshare.light/shapeandshare_light-0.5.0.tar.gz/shapeandshare_light-0.5.0/src/shapeandshare/light/const.py
@@ -0,0 +1,19 @@
+import pygame
+
+# Predefined some colors
+BLUE: tuple[int, int, int] = (0, 0, 255)
+RED: tuple[int, int, int] = (255, 0, 0)
+GREEN: tuple[int, int, int] = (0, 255, 0)
+BLACK: tuple[int, int, int] = (0, 0, 0)
+WHITE: tuple[int, int, int] = (255, 255, 255)
+
+# dimensions=(20, 20) 20x15=300, 20x15=300
+
+WINDOW_WIDTH: int = 1200
+WINDOW_HEIGHT: int = 600
+
+FPS: int = 60
+FramePerSec = pygame.time.Clock()
+
+
+PRODUCT_NAME: str = "light"
