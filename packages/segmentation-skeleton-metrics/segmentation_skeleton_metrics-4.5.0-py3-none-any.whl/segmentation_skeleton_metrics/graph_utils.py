b3045adb486edb9b7134250815fa6aa7c62036ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-07 03:03:00 +0000

    Add package segmentation-skeleton-metrics/4.5.0/segmentation_skeleton_metrics-4.5.0-py3-none-any.whl

---
+++ packages/segmentation-skeleton-metrics/segmentation_skeleton_metrics-4.5.0-py3-none-any.whl/segmentation_skeleton_metrics/graph_utils.py
@@ -0,0 +1,258 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Aug 15 12:00:00 2023
+
+@author: Anna Grim
+@email: anna.grim@alleninstitute.org
+
+"""
+from random import sample
+
+import networkx as nx
+import numpy as np
+from scipy.spatial.distance import euclidean as get_dist
+
+from segmentation_skeleton_metrics import utils
+
+
+# --- Update graph structure ---
+def remove_edge(graph, i, j):
+    """
+    Remove the edge "(i,j)" from "graph".
+
+    Parameters
+    ----------
+    graph : networkx.Graph
+        Graph to edited.
+    i : int
+        Node part of edge to be removed.
+    j : int
+        Node part of edge to be removed.
+
+    Returns
+    -------
+    graph : networkx.Graph
+        Graph with edge removed.
+
+    """
+    graph.remove_edge(i, j)
+    return graph
+
+
+def delete_nodes(graph, delete_label, return_cnt=False):
+    """
+    Deletes nodes in "graph" whose label is identical to "delete_label".
+
+    Parameters
+    ----------
+    graph : networkx.Graph
+        Graph to be searched and edited.
+    delete_label : int
+        Label to be removed from graph.
+    return : bool, optional
+        Indication of whether to return the number of nodes deleted from
+        "graph". The default is False.
+
+    Returns
+    -------
+    graph : networkx.Graph
+        Updated graph.
+
+    """
+    # Find nodes matching delete_marker
+    delete_nodes = []
+    for i in graph.nodes:
+        label = graph.nodes[i]["label"]
+        if label == delete_label:
+            delete_nodes.append(i)
+
+    # Count deleted edges (if applicable)
+    if return_cnt:
+        subgraph = graph.subgraph(delete_nodes)
+        cnt = subgraph.number_of_edges()
+
+    # Update graph
+    graph.remove_nodes_from(delete_nodes)
+    if return_cnt:
+        return graph, cnt
+    else:
+        return graph
+
+
+# -- Labeling Nodes --
+def get_coord(graph, i):
+    """
+    Gets xyz image coordinates of node "i".
+
+    Parameters
+    ----------
+    graph : networkx.Graph
+        Graph to be queried.
+    i : int
+        Node of "graph".
+
+    Returns
+    -------
+    tuple
+        The xyz image coordinates of node "i".
+
+    """
+    return tuple(graph.nodes[i]["xyz"])
+
+
+def upd_labels(graph, nodes, label):
+    """
+    Updates the label of each node in "nodes" with "label".
+
+    Parameters
+    ----------
+    graph : networkx.Graph
+        Graph to be updated.
+    nodes : list
+        List of nodes to be updated.
+    label : int
+        New label of each node in "nodes".
+
+    Returns
+    -------
+    graph : networkx.Graph
+        Updated graph.
+
+    """
+    for i in nodes:
+        graph.nodes[i].update({"label": label})
+    return graph
+
+
+def store_labels(graph):
+    """
+    Iterates over all nodes in "graph" and stores the label and node id in
+    a dictionary called "label_to_node".
+
+    Parameters
+    ----------
+    graph : networkx.Graph
+        Graph to be updated
+
+    Returns
+    -------
+    label_to_node : dict
+        Dictionary that stores the label and node id.
+
+    """
+    label_to_node = dict()
+    for i in graph.nodes:
+        label = graph.nodes[i]["label"]
+        if label in label_to_node.keys():
+            label_to_node[label].add(i)
+        else:
+            label_to_node[label] = set([i])
+    return label_to_node
+
+
+# -- eval tools --
+def count_splits(graph):
+    """
+    Counts the number of splits in "graph".
+
+    Parameters
+    ----------
+    graph : networkx.Graph
+        Graph to be evaluated.
+
+    Returns
+    -------
+    int
+        Number of splits in "graph".
+
+    """
+    return max(len(list(nx.connected_components(graph))) - 1, 0)
+
+
+def compute_run_lengths(graph):
+    """
+    Computes the path length of each connected component in "graph".
+
+    Parameters
+    ----------
+    graph : networkx.Graph
+        Graph to be parsed.
+
+    Returns
+    -------
+    run_lengths : numpy.ndarray
+        Array containing run lengths of each connected component in "graph".
+
+    """
+    run_lengths = []
+    if graph.number_of_nodes():
+        for nodes in nx.connected_components(graph):
+            subgraph = graph.subgraph(nodes)
+            run_lengths.append(compute_run_length(subgraph))
+    else:
+        run_lengths.append(0)
+    return np.array(run_lengths)
+
+
+def compute_run_length(graph, apply=True):
+    """
+    Computes path length of graph.
+
+    Parameters
+    ----------
+    graph : networkx.Graph
+        Graph to be parsed.
+
+    Returns
+    -------
+    path_length : float
+        Path length of graph.
+
+    """
+    path_length = 0
+    for i, j in nx.dfs_edges(graph):
+        xyz_1 = graph.nodes[i]["xyz"]
+        xyz_2 = graph.nodes[j]["xyz"]
+        if apply:
+            xyz_1 = utils.to_world(xyz_1)
+            xyz_2 = utils.to_world(xyz_2)
+        path_length += get_dist(xyz_1, xyz_2)
+    return path_length
+
+
+# -- miscellaneous --
+def sample_leaf(graph):
+    """
+    Samples leaf node from "graph".
+
+    Parameters
+    ----------
+    graph : networkx.Graph
+        Graph to be sampled from.
+
+    Returns
+    -------
+    int
+        Leaf node of "graph"
+
+    """
+    leafs = [i for i in graph.nodes if graph.degree[i] == 1]
+    return sample(leafs, 1)[0]
+
+
+def sample_node(graph):
+    """
+    Samples a node from "graph".
+
+    Parameters
+    ----------
+    graph : networkx.Graph
+        Graph to be sampled from.
+
+    Returns
+    -------
+    int
+        Node.
+
+    """
+    return sample(list(graph.nodes), 1)[0]
---
+++ packages/segmentation-skeleton-metrics/segmentation_skeleton_metrics-4.5.0-py3-none-any.whl/segmentation_skeleton_metrics/swc_utils.py
@@ -0,0 +1,460 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed June 5 16:00:00 2023
+
+@author: Anna Grim
+@email: anna.grim@alleninstitute.org
+
+"""
+
+from concurrent.futures import (
+    ProcessPoolExecutor,
+    ThreadPoolExecutor,
+    as_completed,
+)
+from io import BytesIO, StringIO
+from zipfile import ZipFile
+
+import networkx as nx
+import numpy as np
+from google.cloud import storage
+
+from segmentation_skeleton_metrics import graph_utils as gutils
+from segmentation_skeleton_metrics import utils
+
+ANISOTROPY = [0.748, 0.748, 1.0]
+
+
+# -- read --
+def init_valid_labels(swc_paths, min_size, anisotropy=[1.0, 1.0, 1.0]):
+    """
+    Reads swc files and extracts the xyz coordinates.
+
+    Paramters
+    ---------
+    swc_paths : list or dict
+        If swc files are on local machine, list of paths to swc files where
+        each file corresponds to a neuron in the prediction. If swc files are
+        on cloud, then dict with keys "bucket_name" and "path".
+    min_size : int
+        Threshold on the number of nodes stored in an swc file. Only swc files
+        with more than "min_size" nodes are stored in "swc_coords".
+   anisotropy : list[float], optional
+       Image to world scaling factors applied to xyz coordinates to account
+        for anisotropy of the microscope.
+
+    Returns
+    -------
+    dict
+        Dictionary where each item is an swc_id and an array of the xyz
+        coordinates read from cooresponding swc file.
+
+    """
+    if type(swc_paths) is list:
+        return set(parse_local_paths(swc_paths, min_size, anisotropy).keys())
+    elif type(swc_paths) is dict:
+        return set(parse_cloud_paths(swc_paths, min_size, anisotropy).keys())
+    else:
+        return None
+
+
+def parse_local_paths(swc_paths, min_size, anisotropy):
+    """
+    Reads swc files from local machine and extracts the xyz coordinates.
+    --> hard coded
+
+    Paramters
+    ---------
+    swc_paths : list or dict
+        If swc files are on local machine, list of paths to swc files where
+        each file corresponds to a neuron in the prediction. If swc files are
+        on cloud, then dict with keys "bucket_name" and "path".
+    min_size : int
+        Threshold on the number of nodes stored in an swc file. Only swc files
+        with more than "min_size" nodes are stored in "swc_coords".
+   anisotropy : list[float], optional
+       Image to world scaling factors applied to xyz coordinates to account
+        for anisotropy of the microscope.
+
+    Returns
+    -------
+    dict
+        Dictionary that maps an swc_id to the the xyz coordinates read from
+        that swc file.
+
+    """
+    graphs = dict()
+    for path in swc_paths:
+        content = utils.read_txt(path)
+        if len(content) > min_size:
+            key = int(utils.get_id(path))
+            graphs[key] = get_coords(content, anisotropy)
+    return graphs
+
+
+def parse_local_zip(zip_path, min_size, anisotropy=[1.0, 1.0, 1.0]):
+    """
+    Reads swc files from a zip stored on the local machine and extracts the
+    xyz coordinates. Note this routine is hard coded for computing projected
+    run length.
+
+    Paramters
+    ---------
+    swc_paths : list or dict
+        If swc files are on local machine, list of paths to swc files where
+        each file corresponds to a neuron in the prediction. If swc files are
+        on cloud, then dict with keys "bucket_name" and "path".
+    min_size : int
+        Threshold on the number of nodes stored in an swc file. Only swc files
+        with more than "min_size" nodes are stored in "swc_coords".
+   anisotropy : list[float], optional
+       Image to world scaling factors applied to xyz coordinates to account
+        for anisotropy of the microscope.
+
+    Returns
+    -------
+    dict
+        Dictionary that maps an swc_id to the the xyz coordinates read from
+        that swc file.
+
+    """
+    graphs = dict()
+    anisotropy = [1.0 / val for val in ANISOTROPY]  # hard coded
+    with ZipFile(zip_path, "r") as zip_file:
+        files = zip_file.namelist()
+        for f in [f for f in files if f.endswith(".swc")]:
+            content = utils.read_zip(zip_file, f).splitlines()
+            if len(content) > min_size:
+                key = int(utils.get_id(f))
+                graphs[key] = to_graph(content, anisotropy=anisotropy)
+                graphs[key].graph["filename"] = f
+    return graphs
+
+
+def parse_cloud_paths(cloud_dict, min_size, anisotropy):
+    """
+    Reads swc files from a GCS bucket and extracts the xyz coordinates.
+
+    Parameters
+    ----------
+    cloud_dict : dict
+        Dictionary where keys are "bucket_name" and "path".
+    min_size : int
+        Threshold on the number of nodes stored in an swc file. Only swc files
+        with more than "min_size" nodes are stored in "swc_coords".
+   anisotropy : list[float], optional
+       Image to world scaling factors applied to xyz coordinates to account
+        for anisotropy of the microscope.
+
+    Returns
+    -------
+    dict
+        Dictionary that maps an swc_id to the the xyz coordinates read from
+        that swc file.
+
+    """
+    # Initializations
+    bucket = storage.Client().bucket(cloud_dict["bucket_name"])
+    zip_paths = utils.list_gcs_filenames(bucket, cloud_dict["path"], ".zip")
+    print("Downloading predicted swc files from cloud...")
+    print("# zip files:", len(zip_paths))
+
+    # Main
+    cnt = 1
+    chunk_size = len(zip_paths) * 0.02
+    t0, t1 = utils.init_timers()
+    with ProcessPoolExecutor() as executor:
+        # Assign processes
+        processes = []
+        for i, path in enumerate(zip_paths):
+            zip_content = bucket.blob(path).download_as_bytes()
+            processes.append(
+                executor.submit(download, zip_content, min_size, anisotropy)
+            )
+            if i >= cnt * chunk_size:
+                cnt, t1 = utils.report_progress(
+                    i, len(zip_paths), chunk_size, cnt, t0, t1
+                )
+
+        # Store results
+        swc_coords = dict()
+        for i, process in enumerate(as_completed(processes)):
+            swc_coords.update(process.result())
+    return swc_coords
+
+
+def download(zip_content, min_size, anisotropy):
+    """
+    Downloads the content from each swc file contained in the zip file at
+    "zip_path".
+
+    Parameters
+    ----------
+    zip_content : ...
+        content of a zip file.
+    min_size : int
+        Threshold on the number of nodes stored in an swc file. Only swc files
+        with more than "min_size" nodes are stored in "swc_coords".
+   anisotropy : list[float], optional
+       Image to world scaling factors applied to xyz coordinates to account
+        for anisotropy of the microscope.
+
+    Returns
+    -------
+    dict
+        Dictionary that maps an swc_id to the the xyz coordinates read from
+        that swc file.
+
+    """
+    with ZipFile(BytesIO(zip_content)) as zip_file:
+        with ThreadPoolExecutor() as executor:
+            # Assign threads
+            threads = [
+                executor.submit(
+                    parse_gcs_zip, zip_file, f, min_size, anisotropy
+                )
+                for f in utils.list_files_in_zip(zip_content)
+            ]
+
+            # Process results
+            swc_coords = dict()
+            for thread in as_completed(threads):
+                swc_coords.update(thread.result())
+    return swc_coords
+
+
+def parse_gcs_zip(zip_file, path, min_size, anisotropy):
+    """
+    Reads swc file stored at "path" which points to a file in a GCS bucket.
+
+    Parameters
+    ----------
+    zip_file : ZipFile
+        Zip containing swc file to be read.
+    path : str
+        Path to swc file to be read.
+    min_size : int
+        Threshold on the number of nodes stored in an swc file. Only swc files
+        with more than "min_size" nodes are stored in "swc_coords".
+    anisotropy : list[float], optional
+       Image to world scaling factors applied to xyz coordinates to account
+        for anisotropy of the microscope.
+
+    Returns
+    -------
+    dict
+        Dictionary that maps an swc_id to the the xyz coordinates read from
+        that swc file.
+
+    """
+    content = utils.read_zip(zip_file, path).splitlines()
+    if len(content) > min_size:
+        return {int(utils.get_id(path)): get_coords(content, anisotropy)}
+    else:
+        return dict()
+
+
+def get_coords(content, anisotropy):
+    """
+    Gets the xyz coords from the an swc file that has been read and stored as
+    "content".
+
+    Parameters
+    ----------
+    content : list[str]
+        Entries in swc file where each entry is the text string from an swc.
+    anisotropy : list[float]
+        Image to world scaling factors applied to xyz coordinates to account
+        for anisotropy of the microscope.
+
+    Returns
+    -------
+    numpy.ndarray
+        xyz coords from an swc file.
+
+    """
+    coords_list = []
+    offset = [0, 0, 0]
+    for line in content:
+        if line.startswith("# OFFSET"):
+            parts = line.split()
+            offset = read_xyz(parts[2:5], anisotropy, offset)
+        if not line.startswith("#"):
+            parts = line.split()
+            coords_list.append(read_xyz(parts[2:5], anisotropy, offset))
+    return np.array(coords_list)
+
+
+def read_xyz(xyz, anisotropy, offset):
+    """
+    Reads the xyz coordinates from an swc file, then transforms the
+    coordinates with respect to "anisotropy" and "offset".
+
+    Parameters
+    ----------
+    xyz : str
+        xyz coordinate stored in a str.
+    anisotropy : list[float]
+        Image to real-world coordinates scaling factors applied to "xyz".
+    offset : list[int]
+        Offset of xyz coordinates in swc file.
+
+    Returns
+    -------
+    numpy.ndarray
+        xyz coordinates of an entry from an swc file.
+
+    """
+    xyz = [float(xyz[i]) + offset[i] for i in range(3)]
+    return np.array([xyz[i] * anisotropy[i] for i in range(3)], dtype=int)
+
+
+# -- write --
+def save(path, xyz_1, xyz_2, color=None):
+    """
+    Writes an swc file.
+
+    Parameters
+    ----------
+    path : str
+        Path on local machine that swc file will be written to.
+    xyz_1 : ...
+        ...
+    xyz_2 : ...
+        ...
+    color : str, optional
+        Color of nodes. The default is None.
+
+    Returns
+    -------
+    None.
+
+    """
+    with open(path, "w") as f:
+        # Preamble
+        if color is not None:
+            f.write("# COLOR " + color)
+        else:
+            f.write("# id, type, z, y, x, r, pid")
+        f.write("\n")
+
+        # Entries
+        f.write(make_entry(1, -1, xyz_1))
+        f.write("\n")
+        f.write(make_entry(2, 1, xyz_2))
+
+
+def make_entry(node_id, parent_id, xyz):
+    """
+    Makes an entry to be written in an swc file.
+
+    Parameters
+    ----------
+    graph : networkx.Graph
+        Graph that "node_id" and "parent_id" belong to.
+    node_id : int
+        Node that entry corresponds to.
+    parent_id : int
+         Parent of node "node_id".
+    xyz : ...
+        xyz coordinate of "node_id".
+
+    Returns
+    -------
+    entry : str
+        Entry to be written in an swc file.
+
+    """
+    x, y, z = tuple(utils.to_world(xyz))
+    entry = f"{node_id} 2 {x} {y} {z} 8 {parent_id}"
+    return entry
+
+
+def to_zipped_swc(zip_writer, graph, color=None):
+    with StringIO() as text_buffer:
+        # Preamble
+        n_entries = 0
+        node_to_idx = dict()
+        if color:
+            text_buffer.write("# COLOR " + color)
+        text_buffer.write("# id, type, z, y, x, r, pid")
+
+        # Write entries
+        r = 5 if color else 3
+        for i, j in nx.dfs_edges(graph):
+            # Special Case: Root
+            x, y, z = tuple(utils.to_world(graph.nodes[i]["xyz"]))
+            if n_entries == 0:
+                parent = -1
+                node_to_idx[i] = 1
+                text_buffer.write("\n" + f"1 2 {x} {y} {z} {r} {parent}")
+                n_entries += 1
+
+            # General Case
+            node = n_entries + 1
+            parent = node_to_idx[i]
+            node_to_idx[j] = n_entries + 1
+            text_buffer.write("\n" + f"{node} 2 {x} {y} {z} {r} {parent}")
+            n_entries += 1
+
+        # Finish
+        zip_writer.writestr(graph.graph["filename"], text_buffer.getvalue())
+
+
+# -- conversions --
+def to_graph(content, anisotropy=[1.0, 1.0, 1.0]):
+    """
+    Reads an swc file and builds an undirected graph from it.
+
+    Parameters
+    ----------
+    path : str
+        Path to swc file to be read.
+    anisotropy : list[float], optional
+        Image to real-world coordinates scaling factors for (x, y, z) that is
+        applied to swc files. The default is [1.0, 1.0, 1.0].
+
+    Returns
+    -------
+    networkx.Graph
+        Graph built from an swc file.
+
+    """
+    # Build Gaph
+    graph = nx.Graph()
+    offset = [0, 0, 0]
+    for line in content:
+        if line.startswith("# OFFSET"):
+            parts = line.split()
+            offset = read_xyz(parts[2:5], anisotropy)
+        if not line.startswith("#"):
+            parts = line.split()
+            child = int(parts[0])
+            parent = int(parts[-1])
+            xyz = read_xyz(parts[2:5], anisotropy, offset=offset)
+            graph.add_node(child, xyz=xyz)
+            if parent != -1:
+                graph.add_edge(parent, child)
+
+    # Set graph-level attributes
+    graph.graph["initial_number_of_edges"] = graph.number_of_edges()
+    graph.graph["initial_run_length"] = gutils.compute_run_length(graph)
+    return graph
+
+
+def to_voxels(xyz):
+    """
+    Converts coordinates from world to voxels.
+
+    Parameters
+    ----------
+    xyz : numpy.ndarray
+        Coordinate to be converted.
+
+    Returns
+    -------
+    tuple
+        Converted coordinates.
+
+    """
+    return tuple([xyz[i] * 1.0 / ANISOTROPY[i] for i in range(3)])
