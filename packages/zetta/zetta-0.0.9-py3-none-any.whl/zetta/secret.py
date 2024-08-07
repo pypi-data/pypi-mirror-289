c5d8080b30c288f6487a9f8777527213240dee93
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-07 14:04:26 +0000

    Add package zetta/0.0.9/zetta-0.0.9-py3-none-any.whl

---
+++ packages/zetta/zetta-0.0.9-py3-none-any.whl/zetta/secret.py
@@ -0,0 +1,8 @@
+def list():
+    print("Listing secrets...")
+
+def get(key):
+    print(f"Getting secret {key}...")
+
+def set(key, value):
+    print(f"Setting secret {key} to {value}...")
---
+++ packages/zetta/zetta-0.0.9-py3-none-any.whl/zetta_version/__version_generated.py
@@ -0,0 +1,4 @@
+# Copyright @ZettaBlock Labs 2024
+
+# Note: Reset this value to -1 whenever you make a minor `0.X` release of the client.
+build_number = 9  # git: 0c07caf
