1af295d26f7746ea56003dda156b2cd1275b7002
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-07 16:03:46 +0000

    Add package minet/2.0.8/minet-2.0.8.tar.gz

---
+++ packages/minet/minet-2.0.8.tar.gz/minet-2.0.8/minet/scrape/__init__.py
@@ -0,0 +1,25 @@
+# =============================================================================
+# Minet Scrape Module
+# =============================================================================
+#
+# Module exposing utilities related to minet's scraping DSL.
+#
+from minet.scrape.classes.definition import scrape, DefinitionScraper, validate
+from minet.scrape.soup import WonderfulSoup
+from minet.scrape.regex import (
+    extract_encodings_from_xml,
+    extract_canonical_link,
+    extract_javascript_relocation,
+    extract_meta_refresh,
+)
+
+__all__ = [
+    "scrape",
+    "DefinitionScraper",
+    "validate",
+    "WonderfulSoup",
+    "extract_encodings_from_xml",
+    "extract_canonical_link",
+    "extract_javascript_relocation",
+    "extract_meta_refresh",
+]
