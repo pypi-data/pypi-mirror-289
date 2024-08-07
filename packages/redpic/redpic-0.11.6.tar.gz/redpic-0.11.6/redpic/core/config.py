77a0047a79370dda48a1b296e89f349bc769feea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-07 16:03:31 +0000

    Add package redpic/0.11.6/redpic-0.11.6.tar.gz

---
+++ packages/redpic/redpic-0.11.6.tar.gz/redpic-0.11.6/redpic/core/config.py
@@ -0,0 +1,35 @@
+from logging import config as logging_config
+
+from redpic.core.logger import LOGGING
+
+logging_config.dictConfig(LOGGING)
+
+NAME = "redpic"
+
+VERSION = "0.11.6"
+
+AUTHOR = "Vyacheslav Fedorov"
+
+AUTHOR_EMAIL = "slava@fuodorov.ru"
+
+DOC = "Relativistic Difference Scheme Particle-in-Cell code (REDPIC)"
+
+URL = "https://github.com/fuodorov/redpic"
+
+DISABLE_JIT = False
+
+DISABLE_NOPYTHON = False
+
+DISABLE_PARALLEL = False
+
+DISABLE_FASTMATH = False
+
+DISABLE_CACHE = False
+
+DISABLE_NOGIL = False
+
+ENABLE_CUDA = False
+
+CUDA_THREADS_PER_BLOCK = 1024
+
+DEFAULT_TRACK_SAVE_N_FILES = 20
---
+++ packages/redpic/redpic-0.11.6.tar.gz/redpic-0.11.6/redpic/solver/__init__.py
@@ -0,0 +1,10 @@
+from redpic.solver.base import BaseSimulation
+from redpic.solver.red import REDSimulation
+
+Simulation = REDSimulation
+
+__all__ = [
+    "BaseSimulation",
+    "Simulation",
+    "REDSimulation",
+]
