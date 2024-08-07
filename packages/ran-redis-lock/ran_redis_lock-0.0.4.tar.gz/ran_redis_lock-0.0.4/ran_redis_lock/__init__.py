7182a51e7cb1ef505d48868f7e893edac60731ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-07 03:02:47 +0000

    Add package ran-redis-lock/0.0.4/ran_redis_lock-0.0.4.tar.gz

---
+++ packages/ran-redis-lock/ran_redis_lock-0.0.4.tar.gz/ran_redis_lock-0.0.4/ran_redis_lock/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+*------------------------------
+｜@Project   : ran_redis_lock 
+｜@ Author   : ran <|-<
+｜@ Time     : 2024/8/7 1:34
+｜@ File     : __init__.py.py
+｜@ Software : PyCharm
+*------------------------------
+"""
