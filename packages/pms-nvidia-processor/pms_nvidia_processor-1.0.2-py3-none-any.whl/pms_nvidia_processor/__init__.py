44a462ecde99d2ffafa7f9523599489839e7bafe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-07 15:03:51 +0000

    Add package pms-nvidia-processor/1.0.2/pms_nvidia_processor-1.0.2-py3-none-any.whl

---
+++ packages/pms-nvidia-processor/pms_nvidia_processor-1.0.2-py3-none-any.whl/pms_nvidia_processor/__init__.py
@@ -0,0 +1,6 @@
+from ._dpir_processor import DPIRProcessor
+from ._dru_rbpn_sr_f3_processor import DRURBPNSRF3Processor
+from ._dru_rbpn_sr_f5_processor import DRURBPNSRF5Processor
+from ._dru_asm_sr_f3_processor import DRUASMSRF3Processor
+
+__version__ = "1.0.2"
---
+++ packages/pms-nvidia-processor/pms_nvidia_processor-1.0.2-py3-none-any.whl/pms_nvidia_processor/_dependency.py
@@ -0,0 +1,11 @@
+from typing import List, Optional, Tuple, Iterable, Literal, Dict, Any
+from dataclasses import dataclass
+import os
+import time
+import uvloop
+import asyncio
+import numpy as np
+from loguru import logger
+import cv2
+import pms_tensorrt as TRT
+from pms_inference_engine import IEngineProcessor, EngineIOData, register
