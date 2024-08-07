bdf4e5588e3fbcc8dbd0aa6260805f414ecda326
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-07 03:02:48 +0000

    Add package ffsim/0.0.44/ffsim-0.0.44.tar.gz

---
+++ packages/ffsim/ffsim-0.0.44.tar.gz/ffsim-0.0.44/docs/conf.py
@@ -0,0 +1,53 @@
+# (C) Copyright IBM 2023.
+#
+# This code is licensed under the Apache License, Version 2.0. You may
+# obtain a copy of this license in the LICENSE.txt file in the root directory
+# of this source tree or at http://www.apache.org/licenses/LICENSE-2.0.
+#
+# Any modifications or derivative works of this code must retain this
+# copyright notice, and modified files need to carry a notice indicating
+# that they have been altered from the originals.
+
+"""Sphinx configuration."""
+
+import importlib.metadata
+
+project = "ffsim"
+copyright = "2023, IBM"
+author = "Kevin J. Sung"
+release = importlib.metadata.version("ffsim")
+
+extensions = [
+    "sphinx.ext.napoleon",
+    "sphinx.ext.autodoc",
+    "sphinx.ext.autosummary",
+    "sphinx.ext.mathjax",
+    "sphinx.ext.viewcode",
+    "sphinx.ext.extlinks",
+    "sphinx_autodoc_typehints",
+    "myst_parser",
+    "nbsphinx",
+]
+
+exclude_patterns = ["_build", "**.ipynb_checkpoints"]
+
+# HTML output options
+html_theme = "furo"
+html_theme_options = {
+    "footer_icons": [
+        {
+            "name": "GitHub",
+            "url": "https://github.com/qiskit-community/ffsim",
+            "html": """
+                <svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 16 16">
+                    <path fill-rule="evenodd" d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0 0 16 8c0-4.42-3.58-8-8-8z"></path>
+                </svg>
+            """,  # noqa: E501
+            "class": "",
+        },
+    ],
+}
+
+# nbsphinx options (for tutorials)
+nbsphinx_timeout = 300
+nbsphinx_execute = "always"
