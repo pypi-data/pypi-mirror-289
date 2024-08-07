52f1a4db9af78fc4ae357fb6d6a2c5e0a1d79bd2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-07 15:04:23 +0000

    Add package YuChulYANG/1.0.0/YuChulYANG-1.0.0.tar.gz

---
+++ packages/YuChulYANG/YuChulYANG-1.0.0.tar.gz/YuChulYANG-1.0.0/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="YuChulYANG", # Replace with your own username
+    version="1.0.0",
+    author="YuChul YANG",
+    author_email="yuchulyang@naver.com",
+    description="A small example package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://pipy.org/project/YuChulYANG/",
+    packages=setuptools.find_packages(),
+#    classifiers=[
+#        "Programming Language :: Python :: 3",
+#        "License :: ",
+#        "Operating System :: OS",
+#    ],
+#    python_requires='>=3.6',
+)
