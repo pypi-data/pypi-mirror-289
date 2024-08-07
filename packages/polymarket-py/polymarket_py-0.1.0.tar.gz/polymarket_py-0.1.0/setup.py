58ae7853a9bf9839b6b7fd8a4e8711c22309900d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-07 03:02:46 +0000

    Add package polymarket-py/0.1.0/polymarket_py-0.1.0.tar.gz

---
+++ packages/polymarket-py/polymarket_py-0.1.0.tar.gz/polymarket_py-0.1.0/setup.py
@@ -0,0 +1,30 @@
+
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="polymarket-py",  
+    version="0.1.0",
+    description="A package to export historical odds for an outcome of a market on Polymarket",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="Matt Maximo",
+    author_email="matt@pioneerdigital.org",
+    url="https://github.com/MattMaximo/polymarket-py",
+    packages=find_packages(),
+    install_requires=[
+        "requests",
+        "pandas",
+        "pytz"
+
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    keywords=["polymarket", "crypto", "data", "api", "exporter", "market", "odds"],
+    python_requires='>=3.6',
+)
