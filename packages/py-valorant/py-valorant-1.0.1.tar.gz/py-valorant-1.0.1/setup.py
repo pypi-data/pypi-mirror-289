46cfb7ccc4218954f2e4c38b4cc6db53f809e683
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-07 14:03:56 +0000

    Add package py-valorant/1.0.1/py-valorant-1.0.1.tar.gz

---
+++ packages/py-valorant/py-valorant-1.0.1.tar.gz/py-valorant-1.0.1/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+with open('README.md','r') as f:
+    description = f.read()
+
+setup(
+	name='py-valorant',
+	version='1.0.1',
+    description='A Python Wrapper for valorant-api.com',
+	author='Guimx',
+	url='https://github.com/UnaPepsi/valorant-api-python',
+	install_requires=['requests','aiohttp'],
+    keywords=['valorant','api','valorant-api','valorant-api.com','py-valorant'],
+    license='MIT',
+    long_description=description,
+    long_description_content_type='text/markdown'
+)
