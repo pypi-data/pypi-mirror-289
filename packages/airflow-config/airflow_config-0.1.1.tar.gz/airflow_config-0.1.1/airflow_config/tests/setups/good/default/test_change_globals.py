2d81da96303a7faae94b138987880e9e7147d41e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-07 03:04:00 +0000

    Add package airflow-config/0.1.1/airflow_config-0.1.1.tar.gz

---
+++ packages/airflow-config/airflow_config-0.1.1.tar.gz/airflow_config-0.1.1/airflow_config/tests/setups/good/default/test_change_globals.py
@@ -0,0 +1,10 @@
+from unittest.mock import patch
+
+from airflow_config import load_config
+
+
+def test_config_and_owners_change_via_global():
+    with patch("airflow_config.configuration.base._get_calling_dag") as m1:
+        m1.return_value = __file__
+        conf = load_config("config", overrides=["+default_args=owner"])
+        assert conf.default_args.owner == "test"
