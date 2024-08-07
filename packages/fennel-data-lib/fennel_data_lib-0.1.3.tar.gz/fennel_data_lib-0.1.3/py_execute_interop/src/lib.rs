9378f595698b4dfea9be32f29a79783893b81ba3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-07 15:04:39 +0000

    Add package fennel-data-lib/0.1.3/fennel_data_lib-0.1.3.tar.gz

---
+++ packages/fennel-data-lib/fennel_data_lib-0.1.3.tar.gz/fennel_data_lib-0.1.3/py_execute_interop/src/lib.rs
@@ -0,0 +1,55 @@
+use bytes::Bytes;
+use ipc_channel::ipc;
+use serde::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct LookupRequest {
+    pub resp_channel: ipc::IpcSender<PythonResult>,
+
+    // lookup request
+    pub branch: String,
+    pub dataset_name: String,
+    pub df_keys: Bytes,
+    pub series_ts: Bytes,
+
+    pub cols: Vec<String>,
+
+    pub use_asof: bool,
+
+    // timestamp as of the request origination
+    pub ts_micros: u64,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub enum LookupResult {
+    Ok((Bytes, Vec<bool>)),
+    Err(String),
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct SecretRequest {
+    pub resp_channel: ipc::IpcSender<PythonResult>,
+
+    // secret name
+    pub name: String,
+    // timestamp as of the request origination
+    pub ts_micros: u64,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub enum SecretResult {
+    Ok(String),
+    Err(String),
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub enum PythonRequest {
+    DatasetLookup(LookupRequest),
+    Secret(SecretRequest),
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub enum PythonResult {
+    DatasetLookup(LookupResult),
+    Secret(SecretResult),
+}
