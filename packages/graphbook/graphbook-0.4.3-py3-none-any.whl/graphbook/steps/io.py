fe7e69b3a84b805bc3f5372fdbf69c8e9b1e099b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-07 14:04:29 +0000

    Add package graphbook/0.4.3/graphbook-0.4.3-py3-none-any.whl

---
+++ packages/graphbook/graphbook-0.4.3-py3-none-any.whl/graphbook/steps/io.py
@@ -0,0 +1,45 @@
+from .base import Step, SourceStep, Note, StepOutput
+import json
+
+
+class LoadJSONL(SourceStep):
+    RequiresInput = False
+    Parameters = {
+        "jsonl_path": {
+            "type": "resource",
+        }
+    }
+    Outputs = ["out"]
+    Category = "IO/Import"
+
+    def __init__(self, id, logger, jsonl_path, start_from=0):
+        super().__init__(id, logger)
+        self.jsonl_path = jsonl_path
+        self.start_from = start_from
+
+    def load(self) -> StepOutput:
+        with open(self.jsonl_path, "r") as f:
+            data = [json.loads(line) for line in f][self.start_from :]
+        notes = [Note(entry) for entry in data]
+        return {"out": notes}
+
+
+class DumpJSONL(Step):
+    RequiresInput = True
+    Parameters = {
+        "jsonl_path": {
+            "type": "resource",
+        }
+    }
+    Outputs = ["out"]
+    Category = "IO/Export"
+
+    def __init__(self, id, logger, jsonl_path):
+        super().__init__(id, logger)
+        self.jsonl_path = jsonl_path
+
+    def on_after_items(self, note: Note):
+        with open(self.jsonl_path, "a") as f:
+            note_entry = note.items
+            json.dump(note_entry, f)
+            f.write("\n")
