b488fd16bb5ce10c87c3d2adb127ec2bdda1dffc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-07 04:02:57 +0000

    Add package consigue/1.0/consigue-1.0-py3-none-any.whl

---
+++ packages/consigue/consigue-1.0-py3-none-any.whl/consigue/readme_consigue.py
@@ -0,0 +1,11 @@
+
+print("\
+Consigue es una libreria pensada para manejar \n\
+el desarrollo de software en forma de sistema \n\
+de tiempos, buscando cambiar un poco el paradigma \n\
+tradicional de programacion. \n\
+\n\
+la version 1.0 sera el comienzo de esta libreria \n\
+\n\
+")
+
