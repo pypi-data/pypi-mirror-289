91da01091ae6e485849a78d59205cdfabe2caac1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-07 15:03:49 +0000

    Add package finestock/0.4.5.2/finestock-0.4.5.2-py3-none-any.whl

---
+++ packages/finestock/finestock-0.4.5.2-py3-none-any.whl/finestock/model/price.py
@@ -0,0 +1,27 @@
+from dataclasses import dataclass, field
+from typing import List
+
+@dataclass(frozen=True)
+class Price:
+    workday: str
+    code: str
+    price: float
+    open: float
+    high: float
+    low: float
+    close: float
+    volume: int
+    volume_amt: int
+
+@dataclass(frozen=True)
+class Hoga:
+    price: int
+    qty: int
+
+@dataclass(frozen=True)
+class OrderBook:
+    code: str
+    total_buy: int
+    total_sell: int
+    buy: List[Hoga] = field(default_factory=list)
+    sell: List[Hoga] = field(default_factory=list)
