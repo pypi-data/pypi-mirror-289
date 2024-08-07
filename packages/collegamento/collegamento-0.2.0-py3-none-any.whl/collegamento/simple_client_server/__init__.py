23b33c4db3fe4dec81cbcbe53287a8417a2ccfb3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-07 14:03:20 +0000

    Add package collegamento/0.2.0/collegamento-0.2.0-py3-none-any.whl

---
+++ packages/collegamento/collegamento-0.2.0-py3-none-any.whl/collegamento/simple_client_server/__init__.py
@@ -0,0 +1,9 @@
+from .client import SimpleClient, SimpleServer  # noqa: F401, E402
+from .misc import (  # noqa: F401, E402
+    USER_FUNCTION,
+    CollegamentoError,
+    Request,
+    RequestQueueType,
+    Response,
+    ResponseQueueType,
+)
