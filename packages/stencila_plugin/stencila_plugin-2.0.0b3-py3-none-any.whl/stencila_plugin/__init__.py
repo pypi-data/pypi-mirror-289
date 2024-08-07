c9686db461e19dede1f993fb99b831ee97fd8372
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-07 14:03:48 +0000

    Add package stencila_plugin/2.0.0b3/stencila_plugin-2.0.0b3-py3-none-any.whl

---
+++ packages/stencila_plugin/stencila_plugin-2.0.0b3-py3-none-any.whl/stencila_plugin/__init__.py
@@ -0,0 +1,13 @@
+from .kernel import Kernel
+from .model import Model, ModelTask, ModelOutput
+from .plugin import Plugin, structure, unstructure
+
+__all__ = [
+    "Kernel",
+    "Model",
+    "ModelTask",
+    "ModelOutput",
+    "Plugin",
+    "structure",
+    "unstructure",
+]
