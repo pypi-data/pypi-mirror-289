f9d3c3bf737c53e950d4a19a1a681217c7abd68b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-07 15:03:32 +0000

    Add package vizdxp/0.2.1/vizdxp-0.2.1.tar.gz

---
+++ packages/vizdxp/vizdxp-0.2.1.tar.gz/vizdxp-0.2.1/vizdxp/__main__.py
@@ -0,0 +1,9 @@
+import sys
+from streamlit.web import cli as stcli
+import os
+
+target_path = os.path.join(os.path.dirname(__file__), 'app.py')
+
+if __name__ == '__main__':
+    sys.argv = ["streamlit", "run", target_path, "--global.developmentMode=false"]
+    sys.exit(stcli.main())
