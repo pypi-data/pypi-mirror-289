f0bd2ae405f5f9c085314aa6d496c8707195fb21
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-07 15:03:57 +0000

    Add package fluxutils/1.2.0/fluxutils-1.2.0.tar.gz

---
+++ packages/fluxutils/fluxutils-1.2.0.tar.gz/fluxutils-1.2.0/fluxutils/__init__.py
@@ -0,0 +1,11 @@
+"""
+    fluxutils
+    ~~~~~~~~~
+
+    FluxUtils is a simple yet customizable and versatile module designed to provide various utilities that enhance the efficiency and performance of your programs. Whether you are a seasoned developer or just starting out, FluxUtils offers tools that help streamline your workflow and improve code management.
+"""
+
+from . import log, decorators, cli
+
+__version__ = "1.2.0"
+__all__ = ["log", "decorators", "cli"]
