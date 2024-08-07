3e2e78d891ffcafa0a225dd93cf20b64de356555
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-07 16:03:19 +0000

    Add package ml-management/0.1.6/ml-management-0.1.6.tar.gz

---
+++ packages/ml-management/ml-management-0.1.6.tar.gz/ml-management-0.1.6/ML_management/model/model_type_to_methods_map.py
@@ -0,0 +1,37 @@
+"""Map supported model types to their unique abstract functions."""
+from enum import Enum
+
+from ML_management.model.patterns.evaluatable_model import EvaluatableModel
+from ML_management.model.patterns.gradient_model import GradientModel
+from ML_management.model.patterns.model_pattern import Model
+from ML_management.model.patterns.model_with_losses import ModelWithLosses
+from ML_management.model.patterns.retrainable_model import RetrainableModel
+from ML_management.model.patterns.target_layer import TargetLayer
+from ML_management.model.patterns.torch_model import TorchModel
+from ML_management.model.patterns.trainable_model import TrainableModel
+
+
+class ModelMethodName(str, Enum):
+    """Map supported model function names to infer jsonschemas."""
+
+    train_function = "train_function"
+    predict_function = "predict_function"
+    finetune_function = "finetune_function"
+    get_nn_module = "get_nn_module"
+    evaluate_function = "evaluate_function"
+    get_target_layer = "get_target_layer"
+    get_losses = "get_losses"
+    get_grad = "get_grad"
+
+
+# link model pattern to it abstract functions
+model_pattern_to_methods = {
+    Model: [ModelMethodName.predict_function],
+    TrainableModel: [ModelMethodName.train_function],
+    RetrainableModel: [ModelMethodName.finetune_function],
+    TorchModel: [ModelMethodName.get_nn_module],
+    TargetLayer: [ModelMethodName.get_target_layer],
+    EvaluatableModel: [ModelMethodName.evaluate_function],
+    ModelWithLosses: [ModelMethodName.get_losses],
+    GradientModel: [ModelMethodName.get_grad],
+}
