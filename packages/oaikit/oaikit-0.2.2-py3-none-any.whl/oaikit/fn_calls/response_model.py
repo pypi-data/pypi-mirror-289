159005a785211b28a2a5b1105311df37c66a7a23
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-08 06:05:01 +0000

    Add package oaikit/0.2.2/oaikit-0.2.2-py3-none-any.whl

---
+++ packages/oaikit/oaikit-0.2.2-py3-none-any.whl/oaikit/fn_calls/response_model.py
@@ -0,0 +1,24 @@
+from typing import Any
+from abc import ABC, abstractmethod
+
+from instructor import OpenAISchema
+
+__all__ = ["ResponseModelOAI"]
+
+
+class OAISchema(OpenAISchema):
+    @property
+    def tool(self) -> dict:
+        """ Retorna la tool en el formato que OpenAI espera."""
+        return {"type": "function", "function": self.openai_schema}
+
+# TODO: Quizás crear distintas categorías o tipos de tools, clasificadores, checkeadores,...
+class ResponseModelOAI(OAISchema, ABC):
+    @classmethod
+    @abstractmethod
+    def fn_name(cls) -> str:
+        ...
+    
+    @abstractmethod
+    def execute(self, *args, **kwargs) -> Any:
+        ...
