f8b40fb35f789f3cdbd054211dd8793b1fb96fff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-08 05:06:15 +0000

    Add package proxystr/2.0.2/proxystr-2.0.2-py3-none-any.whl

---
+++ packages/proxystr/proxystr-2.0.2-py3-none-any.whl/proxystr/__init__.py
@@ -0,0 +1,4 @@
+from .extended_proxy import Proxy
+from .proxy import ProxyPattern, PlaywrightProxySettings
+from .check import check_proxy, check_proxies, acheck_proxy, acheck_proxies
+from .utils import read_proxies
