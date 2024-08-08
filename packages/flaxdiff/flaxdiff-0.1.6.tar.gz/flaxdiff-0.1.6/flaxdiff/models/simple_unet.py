cc917d9d1c42843f7d5370951e782c4903cd8fce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-08 03:04:03 +0000

    Add package flaxdiff/0.1.6/flaxdiff-0.1.6.tar.gz

---
+++ packages/flaxdiff/flaxdiff-0.1.6.tar.gz/flaxdiff-0.1.6/flaxdiff/models/simple_unet.py
@@ -0,0 +1,205 @@
+import jax
+import jax.numpy as jnp
+from flax import linen as nn
+from flax.typing import Dtype, PrecisionLike
+from typing import Dict, Callable, Sequence, Any, Union, Optional
+import einops
+from .common import kernel_init, ConvLayer, Downsample, Upsample, FourierEmbedding, TimeProjection
+from .attention import TransformerBlock
+
+class Unet(nn.Module):
+    output_channels:int=3
+    emb_features:int=64*4,
+    feature_depths:list=[64, 128, 256, 512],
+    attention_configs:list=[{"heads":8}, {"heads":8}, {"heads":8}, {"heads":8}],
+    num_res_blocks:int=2,
+    num_middle_res_blocks:int=1,
+    activation:Callable = jax.nn.swish
+    norm_groups:int=8
+    dtype: Optional[Dtype] = None
+    precision: PrecisionLike = None
+
+    @nn.compact
+    def __call__(self, x, temb, textcontext):
+        # print("embedding features", self.emb_features)
+        temb = FourierEmbedding(features=self.emb_features)(temb)
+        temb = TimeProjection(features=self.emb_features)(temb)
+        
+        _, TS, TC = textcontext.shape
+        
+        # print("time embedding", temb.shape)
+        feature_depths = self.feature_depths
+        attention_configs = self.attention_configs
+
+        conv_type = up_conv_type = down_conv_type = middle_conv_type = "conv"
+        # middle_conv_type = "separable"
+        
+        x = ConvLayer(
+            conv_type,
+            features=self.feature_depths[0],
+            kernel_size=(3, 3),
+            strides=(1, 1),
+            kernel_init=kernel_init(1.0),
+            dtype=self.dtype,
+            precision=self.precision
+        )(x)
+        downs = [x]
+
+        # Downscaling blocks
+        for i, (dim_out, attention_config) in enumerate(zip(feature_depths, attention_configs)):
+            dim_in = x.shape[-1]
+            # dim_in = dim_out
+            for j in range(self.num_res_blocks):
+                x = ResidualBlock(
+                    down_conv_type,
+                    name=f"down_{i}_residual_{j}",
+                    features=dim_in,
+                    kernel_init=kernel_init(1.0),
+                    kernel_size=(3, 3),
+                    strides=(1, 1),
+                    activation=self.activation,
+                    norm_groups=self.norm_groups,
+                    dtype=self.dtype,
+                    precision=self.precision
+                )(x, temb)
+                if attention_config is not None and j == self.num_res_blocks - 1:   # Apply attention only on the last block
+                    x = TransformerBlock(heads=attention_config['heads'], dtype=attention_config.get('dtype', jnp.float32),
+                                        dim_head=dim_in // attention_config['heads'],
+                                        use_flash_attention=attention_config.get("flash_attention", True),
+                                        use_projection=attention_config.get("use_projection", False),
+                                        use_self_and_cross=attention_config.get("use_self_and_cross", True),
+                                        precision=attention_config.get("precision", self.precision),
+                                        only_pure_attention=True,
+                                        name=f"down_{i}_attention_{j}")(x, textcontext)
+                # print("down residual for feature level", i, "is of shape", x.shape, "features", dim_in)
+                downs.append(x)
+            if i != len(feature_depths) - 1:
+                # print("Downsample", i, x.shape)
+                x = Downsample(
+                    features=dim_out,
+                    scale=2,
+                    activation=self.activation,
+                    name=f"down_{i}_downsample",
+                    dtype=self.dtype,
+                    precision=self.precision
+                )(x)
+
+        # Middle Blocks
+        middle_dim_out = self.feature_depths[-1]
+        middle_attention = self.attention_configs[-1]
+        for j in range(self.num_middle_res_blocks):
+            x = ResidualBlock(
+                middle_conv_type,
+                name=f"middle_res1_{j}",
+                features=middle_dim_out,
+                kernel_init=kernel_init(1.0),
+                kernel_size=(3, 3),
+                strides=(1, 1),
+                activation=self.activation,
+                norm_groups=self.norm_groups,
+                dtype=self.dtype,
+                precision=self.precision
+            )(x, temb)
+            if middle_attention is not None and j == self.num_middle_res_blocks - 1:   # Apply attention only on the last block
+                x = TransformerBlock(heads=middle_attention['heads'], dtype=middle_attention.get('dtype', jnp.float32), 
+                                    dim_head=middle_dim_out // middle_attention['heads'],
+                                    use_flash_attention=middle_attention.get("flash_attention", True),
+                                    use_linear_attention=False,
+                                    use_projection=middle_attention.get("use_projection", False),
+                                    use_self_and_cross=False,
+                                    precision=attention_config.get("precision", self.precision),
+                                    only_pure_attention=True,
+                                    name=f"middle_attention_{j}")(x, textcontext)
+            x = ResidualBlock(
+                middle_conv_type,
+                name=f"middle_res2_{j}",
+                features=middle_dim_out,
+                kernel_init=kernel_init(1.0),
+                kernel_size=(3, 3),
+                strides=(1, 1),
+                activation=self.activation,
+                norm_groups=self.norm_groups,
+                dtype=self.dtype,
+                precision=self.precision
+            )(x, temb)
+
+        # Upscaling Blocks
+        for i, (dim_out, attention_config) in enumerate(zip(reversed(feature_depths), reversed(attention_configs))):
+            # print("Upscaling", i, "features", dim_out)
+            for j in range(self.num_res_blocks):
+                x = jnp.concatenate([x, downs.pop()], axis=-1)
+                # print("concat==> ", i, "concat", x.shape)
+                # kernel_size = (1 + 2 * (j + 1), 1 + 2 * (j + 1))
+                kernel_size = (3, 3)
+                x = ResidualBlock(
+                    up_conv_type,# if j == 0 else "separable",
+                    name=f"up_{i}_residual_{j}",
+                    features=dim_out,
+                    kernel_init=kernel_init(1.0),
+                    kernel_size=kernel_size,
+                    strides=(1, 1),
+                    activation=self.activation,
+                    norm_groups=self.norm_groups,
+                    dtype=self.dtype,
+                    precision=self.precision
+                )(x, temb)
+                if attention_config is not None and j == self.num_res_blocks - 1:   # Apply attention only on the last block
+                    x = TransformerBlock(heads=attention_config['heads'], dtype=attention_config.get('dtype', jnp.float32), 
+                                        dim_head=dim_out // attention_config['heads'],
+                                        use_flash_attention=attention_config.get("flash_attention", True),
+                                        use_projection=attention_config.get("use_projection", False),
+                                        use_self_and_cross=attention_config.get("use_self_and_cross", True),
+                                        precision=attention_config.get("precision", self.precision),
+                                        only_pure_attention=True,
+                                        name=f"up_{i}_attention_{j}")(x, textcontext)
+            # print("Upscaling ", i, x.shape)
+            if i != len(feature_depths) - 1:
+                x = Upsample(
+                    features=feature_depths[-i],
+                    scale=2,
+                    activation=self.activation,
+                    name=f"up_{i}_upsample",
+                    dtype=self.dtype,
+                    precision=self.precision
+                )(x)
+
+        # x = nn.GroupNorm(8)(x)
+        x = ConvLayer(
+            conv_type,
+            features=self.feature_depths[0],
+            kernel_size=(3, 3),
+            strides=(1, 1),
+            kernel_init=kernel_init(0.0),
+            dtype=self.dtype,
+            precision=self.precision
+        )(x)
+    
+        x = jnp.concatenate([x, downs.pop()], axis=-1)
+
+        x = ResidualBlock(
+            conv_type,
+            name="final_residual",
+            features=self.feature_depths[0],
+            kernel_init=kernel_init(1.0),
+            kernel_size=(3,3),
+            strides=(1, 1),
+            activation=self.activation,
+            norm_groups=self.norm_groups,
+            dtype=self.dtype,
+            precision=self.precision
+        )(x, temb)
+
+        x = nn.GroupNorm(self.norm_groups)(x)
+        x = self.activation(x)
+
+        noise_out = ConvLayer(
+            conv_type,
+            features=self.output_channels,
+            kernel_size=(3, 3),
+            strides=(1, 1),
+            # activation=jax.nn.mish
+            kernel_init=kernel_init(0.0),
+            dtype=self.dtype,
+            precision=self.precision
+        )(x)
+        return noise_out#, attentions
