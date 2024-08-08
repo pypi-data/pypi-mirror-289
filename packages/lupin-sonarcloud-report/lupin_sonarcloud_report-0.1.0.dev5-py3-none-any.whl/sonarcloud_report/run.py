4cc8f456105558f2f3db813de45137a57dea0e5c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-08 04:04:25 +0000

    Add package lupin-sonarcloud-report/0.1.0.dev5/lupin_sonarcloud_report-0.1.0.dev5-py3-none-any.whl

---
+++ packages/lupin-sonarcloud-report/lupin_sonarcloud_report-0.1.0.dev5-py3-none-any.whl/sonarcloud_report/run.py
@@ -0,0 +1,36 @@
+import os
+
+import typer
+
+from sonarcloud_report.generate_sonarcloud_project_report import (
+    generate_sonarcloud_project_report_file,
+)
+
+
+cli = typer.Typer()
+
+
+def version_callback(value: bool):
+    if value:
+        typer.echo("0.1.0.dev5")
+        raise typer.Exit()
+
+
+@cli.callback()
+def main(
+    version: bool = typer.Option(
+        None, "--version", callback=version_callback, is_eager=True
+    )
+):
+    pass
+
+
+@cli.command()
+def generate_report():
+    typer.echo("SonarCloud report generation started")
+    generate_sonarcloud_project_report_file(
+        project_name=os.environ.get("CI_PROJECT_NAME"),
+        commit_id="2cb96b25c2f2adedae964075c1743073df0a35c6",
+        sonar_token=os.environ.get("SONAR_TOKEN"),
+    )
+    typer.echo("SonarCloud report generation finished")
