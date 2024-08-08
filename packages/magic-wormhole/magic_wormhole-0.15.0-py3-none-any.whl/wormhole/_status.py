7f609da9f2f1f7ef783f662fbb5be0e9d36b44d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-08 14:04:23 +0000

    Add package magic-wormhole/0.15.0/magic_wormhole-0.15.0-py3-none-any.whl

---
+++ packages/magic-wormhole/magic_wormhole-0.15.0-py3-none-any.whl/wormhole/_status.py
@@ -0,0 +1,73 @@
+from attrs import frozen
+
+
+@frozen
+class Disconnected:
+    pass
+
+
+@frozen
+class Connecting:
+    url: str
+
+
+@frozen
+class Connected:
+    url: str
+
+
+@frozen
+class NoPeer:
+    pass
+
+
+@frozen
+class ConnectedPeer:
+    pass  # are more details relevant?
+
+
+# XXX union types need python 3.10 or later
+ConnectionStatus = Disconnected | Connecting | Connected
+PeerConnection = NoPeer | ConnectedPeer
+
+
+@frozen
+class WormholeStatus(object):
+    """
+    Represents the current status of a wormhole for use by the outside
+    """
+
+    # are we connected to the Mailbox Server
+    mailbox_connection: ConnectionStatus = Disconnected()
+
+    # XXX there isn't really this for pure "wormholes" -- only the
+    # file-transfer v1 Transit or Dilation know about "connection to a
+    # real peer"
+    # we believe we have communication with our peer
+    peer_connection: PeerConnection = NoPeer()
+
+    # there's the notion of "we have a mailbox", separate from the
+    # above; worth revealing?
+
+
+@frozen
+class DilationStatus(object):
+    """
+    Represents the current status of a Dilated wormhole
+    """
+
+    # current Dilation phase (ever increasing, aka "generation")
+    phase: int = -1;
+
+    # are we connected to the Mailbox Server
+    mailbox: ConnectionStatus = Disconnected()
+
+    # we believe we have communication with our peer
+    peer_connection: PeerConnection = NoPeer()
+
+    # there's the notion of "we have a mailbox", separate from the
+    # above; worth revealing?
+
+
+# worth having an Interface for "there is a new status"? it's just a
+# callable that takes a WormholeStatus ...
