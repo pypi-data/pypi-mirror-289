66072747d9925b8049ffb3a56e097d3941a12676
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-08 03:04:03 +0000

    Add package Jarvislib/2.8.5/jarvislib-2.8.5.tar.gz

---
+++ packages/Jarvislib/jarvislib-2.8.5.tar.gz/jarvislib-2.8.5/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='Jarvislib',
+      version='2.8.5',
+      description='Импорт функции: from Jarvislib import potok. Запуск главного цикла: okno()',
+      packages=['Jarvislib'],
+      author_email='babadey.pl@gmail.com',
+      zip_safe=False)
