d76602e1530683c3e6887e35672e170cf9eb4bee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-08 03:03:25 +0000

    Add package py-abu/1.0.7/py_abu-1.0.7.tar.gz

---
+++ packages/py-abu/py_abu-1.0.7.tar.gz/py_abu-1.0.7/setup.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2024/8/7 18:36
+# @Author  : Chris
+# @Email   : 10512@qq.com
+# @File    : setup.py
+# @Software: PyCharm
+
+from setuptools import setup, find_packages
+
+# with open("./requirements.txt", encoding="utf-8") as fp:
+#     install_requires = [str(requirement) for requirement in parse_requirements(fp)]
+
+setup(
+    name='py-abu',
+    version='1.0.7',
+    description='abu API',
+    long_description='Private API for abu',
+    author='Chris',
+    author_email='10512@qq.com',
+    url='https://github.com/ChrisYP/abu',
+    license='MIT',
+    packages=find_packages(),
+    install_requires=[
+        # list your dependencies here, for example:
+        # 'numpy',
+    ],
+    platforms=["all"],
+    include_package_data=True,
+    zip_safe=False,
+    keywords='abu',
+)
