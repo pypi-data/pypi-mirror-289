4f7ad549df6eb936eff70140815c7440c4747685
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-08 04:05:30 +0000

    Add package tma-authenticator/0.0.2.3/tma-authenticator-0.0.2.3.tar.gz

---
+++ packages/tma-authenticator/tma-authenticator-0.0.2.3.tar.gz/tma-authenticator-0.0.2.3/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='tma-authenticator',
+    version='0.0.2.3',
+    description='Verifying telegram user token.',
+    author='Ivan Kochelorov',
+    packages=find_packages(include=['tma_authenticator']),
+    install_requires=['pydantic', 'fastapi', 'bson'],
+)
