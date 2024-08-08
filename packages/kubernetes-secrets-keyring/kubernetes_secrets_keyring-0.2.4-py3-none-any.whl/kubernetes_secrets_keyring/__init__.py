630841ff76ef12e732855be006b306f406ecf904
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-08 05:05:00 +0000

    Add package kubernetes-secrets-keyring/0.2.4/kubernetes_secrets_keyring-0.2.4-py3-none-any.whl

---
+++ packages/kubernetes-secrets-keyring/kubernetes_secrets_keyring-0.2.4-py3-none-any.whl/kubernetes_secrets_keyring/__init__.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+"""
+Import KubernetesSecretsKeyring here so easily importable from this module:
+from kubernetes_secrets_keyring import KubernetesSecretsKeyring
+"""
+
+from .k8s_keyring_backend import KubernetesSecretsKeyring
