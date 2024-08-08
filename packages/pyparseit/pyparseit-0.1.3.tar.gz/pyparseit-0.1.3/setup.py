a164367b7e59b7eff7275b79bcd098c140f2bcb7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-08 05:03:56 +0000

    Add package pyparseit/0.1.3/pyparseit-0.1.3.tar.gz

---
+++ packages/pyparseit/pyparseit-0.1.3.tar.gz/pyparseit-0.1.3/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='pyparseit',
+    version='0.1.3',
+    packages=find_packages(),
+    install_requires=[],
+    entry_points={
+        'console_scripts': [
+            'pyparseit=pyparseit.cli:main',
+        ],
+    },
+    author='uladkaminski',
+    author_email='i@uladkaminski.com',
+    description='A library to parse Markdown and extract code snippets',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    url='https://github.com/uladkaminski/pyparseit',
+)
