22045a9e340bcdf4d16f08c319b83b92497e91a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-08 16:05:14 +0000

    Add package pygo-API/0.1.2/pygo_api-0.1.2.tar.gz

---
+++ packages/pygo-API/pygo_api-0.1.2.tar.gz/pygo_api-0.1.2/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='pygo_API',
+    version='0.1.2',
+    packages=find_packages(),
+    install_requires=[
+        "requests>=2.0"
+    ],
+    description='A simple tool to utilize the YGOProDeck API as a Python module',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/Yeet195/pygo_API',
+    license='MIT',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+)
