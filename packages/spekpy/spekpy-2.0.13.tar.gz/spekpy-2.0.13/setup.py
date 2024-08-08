fb8d9f228e754c97d16c1c8c88d647f78bb105ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-08 17:04:00 +0000

    Add package spekpy/2.0.13/spekpy-2.0.13.tar.gz

---
+++ packages/spekpy/spekpy-2.0.13.tar.gz/spekpy-2.0.13/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(name='spekpy',
+      version='2.0.13',
+      description='A Python software toolkit for modelling the x-ray spectra from x-ray tubes',
+      url='https://bitbucket.org/spekpy/spekpy_release',
+      author='Gavin Poludniowski & Robert Bujila',
+      author_email='gpoludniowski@gmail.com',
+      license='MIT License',
+      install_requires=['scipy','matplotlib'],
+      packages=['spekpy'],
+      zip_safe=False,
+      package_data={'spekpy':[r'data/tables/*.dat', r'data/matl_def/*.comp', r'data/matl_usr/*.comp', r'data/state_usr/*.state', r'data/state_def/*.state',r'data/tables/advanced/*.npz']},
+      include_package_data=True)
