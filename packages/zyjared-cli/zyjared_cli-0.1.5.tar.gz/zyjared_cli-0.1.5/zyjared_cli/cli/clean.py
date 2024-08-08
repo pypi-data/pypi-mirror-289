79bd27001f4f648e1cc33f9257eeec73fef4d7d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-08 16:05:26 +0000

    Add package zyjared-cli/0.1.5/zyjared_cli-0.1.5.tar.gz

---
+++ packages/zyjared-cli/zyjared_cli-0.1.5.tar.gz/zyjared_cli-0.1.5/zyjared_cli/cli/clean.py
@@ -0,0 +1,79 @@
+import typer
+from typing import List, Optional, Annotated
+from zyjared_fs import clean_directory
+from zyjared_color import Color
+from .app import app
+from ..helpers.config import resolve_config
+from ..utils.timing import measure_time
+from ..helpers.log import log
+
+
+@app.command()
+def clean(
+    dirpath: Annotated[
+        str,
+        typer.Argument(
+            show_default=False,
+            help="Specify the directory to clean up.",
+        )
+    ] = None,
+    include: Annotated[
+        List[str],
+        typer.Option(
+            '-i',
+            '--include',
+            show_default=False,
+            help="Regular expression to include files.",
+        )
+    ] = None,
+    exclude: Annotated[
+        Optional[List[str]],
+        typer.Option(
+            '-e',
+            '--exclude',
+            help="Regular expression to exclude files",
+        )
+    ] = []
+):
+    """
+    Clean up files in a directory with specified include.
+    """
+
+    config = resolve_config(
+        cli='clean',
+        dirpath=dirpath,
+        include=include,
+        exclude=exclude
+    )
+
+    if not config['dirpath'] or not config['include']:
+        log(
+            'clean',
+            'no files to clean up',
+            dirpath=config['dirpath'],
+            include=config['include'],
+        )
+        raise typer.Exit(1)
+
+    measure = measure_time(
+        lambda: clean_directory(
+            config['dirpath'],
+            config['include'],
+            config['exclude']
+        ),
+        unit='ms'
+    )
+
+    if (len(measure['result'])):
+        log(
+            'clean',
+            'success',
+            time=measure['duration'],
+            removed=measure['result'],
+        )
+    else:
+        log(
+            'clean',
+            'fail',
+            time=measure['duration'],
+        )
