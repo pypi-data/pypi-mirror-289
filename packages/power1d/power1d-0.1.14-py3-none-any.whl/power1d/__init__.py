f610312ca73bf41d964e6804bbd0fdebd1f205b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-08 07:04:09 +0000

    Add package power1d/0.1.14/power1d-0.1.14-py3-none-any.whl

---
+++ packages/power1d/power1d-0.1.14-py3-none-any.whl/power1d/__init__.py
@@ -0,0 +1,41 @@
+'''
+power1d: numerical power estimates for one-dimensional continua
+
+Pataky TC (2017) Power1D: A Python toolbox for numerical
+power estimates in experiments involving one-dimensional
+continua. PeerJ Computer Science 3: e125.
+DOI: 10.7717/peerj-cs.125
+https://peerj.com/articles/cs-125/
+
+
+Copyright (C) 2024  Todd Pataky
+'''
+
+
+__version__ = '0.1.14 (2024-08-08)'
+
+
+__all__ = ['geom', 'models', 'noise', 'roi', 'stats']
+
+
+
+from . import data
+from . import geom
+from . import io
+from . import models
+from . import noise
+from . import prob
+from . import random
+from . import roi
+from . import stats
+
+
+DataSample          = models.DataSample
+Experiment          = models.Experiment
+ExperimentSimulator = models.ExperimentSimulator
+RegionOfInterest    = roi.RegionOfInterest
+
+
+
+
+
