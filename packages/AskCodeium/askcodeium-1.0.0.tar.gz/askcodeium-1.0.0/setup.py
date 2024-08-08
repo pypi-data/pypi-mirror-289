c9294f00fc861d9a657c2e90dcf71d023239a2af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-08 04:04:33 +0000

    Add package AskCodeium/1.0.0/askcodeium-1.0.0.tar.gz

---
+++ packages/AskCodeium/askcodeium-1.0.0.tar.gz/askcodeium-1.0.0/setup.py
@@ -0,0 +1,21 @@
+# pip install setuptools wheel twine
+# python setup.py sdist bdist_wheel
+# twine upload dist/*
+from setuptools import setup, find_packages
+
+
+with open('./docs/pypi_docs.md', 'r') as f:
+    description = f.read()
+
+setup(
+    name='AskCodeium',
+    version='1.0.0',
+    packages=find_packages(),
+    install_requires=[
+        # Add dependencies here.
+        # e.g. 'numpy>=1.11.1'
+        'selenium>=4.6.0',
+    ],
+    long_description=description,
+    long_description_content_type='text/markdown',
+)
