cb52ddec65fba0cbc81785e9b841f2b1c37765f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-08 15:04:24 +0000

    Add package bode-logger/0.0.5/bode_logger-0.0.5.tar.gz

---
+++ packages/bode-logger/bode_logger-0.0.5.tar.gz/bode_logger-0.0.5/src/bode_logger/gunicorn_logger/gunicorn_logger.py
@@ -0,0 +1,11 @@
+class GunicornLogger(glogging.Logger):
+    """Custom logger for Gunicorn log messages."""
+
+    def setup(self, cfg):
+        """Configure Gunicorn application logging configuration."""
+        super().setup(cfg)
+        # fix annoying gunicorn wanting to implement its own logging
+        self.error_log.propagate = True
+        self.access_log.propagate = True
+        [self.error_log.removeHandler(handler) for handler in self.error_log.handlers]
+        [self.error_log.removeHandler(handler) for handler in self.access_log.handlers]
