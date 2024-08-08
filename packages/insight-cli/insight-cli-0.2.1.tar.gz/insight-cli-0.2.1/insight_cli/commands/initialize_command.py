40691c28abfe5c377965bf5376fb86ccbe448c3d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-08 16:05:23 +0000

    Add package insight-cli/0.2.1/insight-cli-0.2.1.tar.gz

---
+++ packages/insight-cli/insight-cli-0.2.1.tar.gz/insight-cli-0.2.1/insight_cli/commands/initialize_command.py
@@ -0,0 +1,30 @@
+from pathlib import Path
+
+from .base.command import Command
+from insight_cli.repository import Repository, InvalidRepositoryError
+from insight_cli.utils import Color
+
+
+class InitializeCommand(Command):
+    def __init__(self):
+        super().__init__(
+            flags=["-i", "--initialize"],
+            description="initializes the current directory as an insight repository",
+        )
+
+    def execute(self):
+        try:
+            repository = Repository(Path(""))
+
+            if repository.is_valid:
+                repository.reinitialize()
+                terminal_output = f"Reinitialized existing insight repository in {repository.path.resolve()}"
+
+            else:
+                repository.initialize()
+                terminal_output = f"Initialized insight repository in {repository.path.resolve()}"
+                
+            print(Color.green(terminal_output))
+
+        except InvalidRepositoryError as e:
+            print(Color.red(e))
