54f7a3d9b1af16dd384fcb19200fbe21757a4cd3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-08 07:04:01 +0000

    Add package its-a-trial/1.0.1/its_a_trial-1.0.1-py3-none-any.whl

---
+++ packages/its-a-trial/its_a_trial-1.0.1-py3-none-any.whl/my_first_trial_model/model.py
@@ -0,0 +1,34 @@
+import tensorflow as tf
+from tensorflow.keras.preprocessing.image import ImageDataGenerator, image
+import numpy as np
+
+class YourModel:
+    def __init__(self):
+        self.model = self.build_model()
+
+    def build_model(self):
+        cnn = tf.keras.models.Sequential()
+        cnn.add(tf.keras.layers.MaxPool2D(pool_size=2, strides=2))
+        cnn.add(tf.keras.layers.Conv2D(filters=32, kernel_size=3, activation='relu'))
+        cnn.add(tf.keras.layers.MaxPool2D(pool_size=2, strides=2))
+        cnn.add(tf.keras.layers.Flatten())
+        cnn.add(tf.keras.layers.Dense(units=128, activation='relu'))
+        cnn.add(tf.keras.layers.Dense(units=1, activation='sigmoid'))
+        cnn.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+        return cnn
+
+    def train(self, train_data_dir, val_data_dir, target_size=(64, 64), batch_size=32, epochs=50):
+        data_gen = ImageDataGenerator(rescale=1./255, shear_range=0.2, zoom_range=0.2, horizontal_flip=True, validation_split=0.2)
+        
+        train_data_gen = data_gen.flow_from_directory(train_data_dir, target_size=target_size, batch_size=batch_size, class_mode='binary', subset='training', shuffle=True)
+        val_data_gen = data_gen.flow_from_directory(val_data_dir, target_size=target_size, batch_size=batch_size, class_mode='binary', subset='validation', shuffle=True)
+        
+        self.model.fit(x=train_data_gen, validation_data=val_data_gen, epochs=epochs)
+
+    def predict(self, image_path, target_size=(64, 64)):
+        test_image = image.load_img(image_path, target_size=target_size)
+        test_image = image.img_to_array(test_image)
+        test_image = np.expand_dims(test_image, axis=0)
+        result = self.model.predict(test_image)
+        
+        return 'Negative' if result[0][0] == 1 else 'Positive'
