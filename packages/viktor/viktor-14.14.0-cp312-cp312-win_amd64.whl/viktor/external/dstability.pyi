5d3cfea55842eb4ea93852710759b9624528d85e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-08 04:04:50 +0000

    Add package viktor/14.14.0/viktor-14.14.0-cp312-cp312-win_amd64.whl

---
+++ packages/viktor/viktor-14.14.0-cp312-cp312-win_amd64.whl/viktor/external/dstability.pyi
@@ -0,0 +1,8 @@
+from ..core import File as File
+from .external_program import ExternalProgram as ExternalProgram
+from _typeshed import Incomplete
+
+class DStabilityAnalysis(ExternalProgram):
+    input_file: Incomplete
+    def __init__(self, input_file: File) -> None: ...
+    def get_output_file(self, extension: str = '.stix') -> File | None: ...
