01cbae17407e0c34006c9bf4d2a31250c337278d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-08 07:03:48 +0000

    Add package library-entity/1.0.1/library_entity-1.0.1.tar.gz

---
+++ packages/library-entity/library_entity-1.0.1.tar.gz/library_entity-1.0.1/library_entity/__init__.py
@@ -0,0 +1,4 @@
+from .country import Country
+from .department import Department
+from .city import City
+from .user import User
---
+++ packages/library-entity/library_entity-1.0.1.tar.gz/library_entity-1.0.1/setup.py
@@ -0,0 +1,21 @@
+# setup.py
+from setuptools import setup, find_packages
+
+setup(
+    name='library_entity',
+    version='1.0.1',
+    packages=find_packages(),
+    install_requires=['sqlalchemy'],  # Lista de dependencias
+    author='goallteam',
+    author_email='kenan249@gmail.com',
+    description='Libreria de entidades para goallteam',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/kenan249/library-entity',  # Repositorio de GitHub u otro
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.9',
+)
