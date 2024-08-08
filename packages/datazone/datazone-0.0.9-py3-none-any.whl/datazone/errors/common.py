6deae8a1608fa6029b0a30f29507fe6eb99e22fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-08 14:04:28 +0000

    Add package datazone/0.0.9/datazone-0.0.9-py3-none-any.whl

---
+++ packages/datazone/datazone-0.0.9-py3-none-any.whl/datazone/errors/common.py
@@ -0,0 +1,29 @@
+from datazone.errors.base import DatazoneError
+
+
+class DatazoneAuthError(DatazoneError):
+    message = "Datazone Authentication Error. You can login using `datazone auth login`."
+
+
+class DatazoneServiceError(DatazoneError):
+    message = "Datazone Service Error"
+
+
+class DatazoneServiceNotAccessibleError(DatazoneError):
+    message = "Datazone Service is not accessible."
+
+
+class DatazoneProfileNotFoundError(DatazoneError):
+    message = "Datazone Profile not found."
+
+
+class DatazoneConfigParseError(DatazoneError):
+    message = "Datazone Config Parse Error."
+
+
+class DatazoneConfigFileNotExistError(DatazoneError):
+    message = "Datazone Config file does not exist."
+
+
+class DatazoneNotLoggedInError(DatazoneError):
+    message = "You are not logged in. Please login using `datazone auth login`."
