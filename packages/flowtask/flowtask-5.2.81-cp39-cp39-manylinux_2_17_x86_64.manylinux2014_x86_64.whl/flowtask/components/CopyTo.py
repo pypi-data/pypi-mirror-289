f6ba499af915cda5f2b2a95872dc118d7961b6ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-08 14:04:02 +0000

    Add package flowtask/5.2.81/flowtask-5.2.81-cp39-cp39-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/flowtask/flowtask-5.2.81-cp39-cp39-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/flowtask/components/CopyTo.py
@@ -0,0 +1,8 @@
+from .abstract import DtComponent
+
+
+class CopyTo(DtComponent):
+    """CopyTo.
+
+    Abstract Class for Copying (saving) a Pandas Dataframe onto a Resource (example: Copy to PostgreSQL).
+    """
