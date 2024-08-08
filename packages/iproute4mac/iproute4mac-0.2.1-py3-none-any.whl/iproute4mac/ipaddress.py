9a6f42588d48e15ab6d3a88ee34334f230406cd1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-08 14:04:23 +0000

    Add package iproute4mac/0.2.1/iproute4mac-0.2.1-py3-none-any.whl

---
+++ packages/iproute4mac/iproute4mac-0.2.1-py3-none-any.whl/iproute4mac/ipaddress.py
@@ -0,0 +1,146 @@
+import iproute4mac.ifconfig as ifconfig
+
+from iproute4mac.utils import *
+
+
+def usage():
+    stderr("""\
+Usage: ip address {add|change|replace} IFADDR dev IFNAME [ LIFETIME ]
+                                                      [ CONFFLAG-LIST ]
+       ip address del IFADDR dev IFNAME [mngtmpaddr]
+       ip address {save|flush} [ dev IFNAME ] [ scope SCOPE-ID ]
+                            [ to PREFIX ] [ FLAG-LIST ] [ label LABEL ] [up]
+       ip address [ show [ dev IFNAME ] [ scope SCOPE-ID ] [ master DEVICE ]
+                         [ nomaster ]
+                         [ type TYPE ] [ to PREFIX ] [ FLAG-LIST ]
+                         [ label LABEL ] [up] [ vrf NAME ]
+                         [ proto ADDRPROTO ] ]
+       ip address {showdump|restore}
+IFADDR := PREFIX | ADDR peer PREFIX
+          [ broadcast ADDR ] [ anycast ADDR ]
+          [ label IFNAME ] [ scope SCOPE-ID ] [ metric METRIC ]
+          [ proto ADDRPROTO ]
+SCOPE-ID := [ host | link | global | NUMBER ]
+FLAG-LIST := [ FLAG-LIST ] FLAG
+FLAG  := [ permanent | dynamic | secondary | primary |
+           [-]tentative | [-]deprecated | [-]dadfailed | temporary |
+           CONFFLAG-LIST ]
+CONFFLAG-LIST := [ CONFFLAG-LIST ] CONFFLAG
+CONFFLAG  := [ home | nodad | mngtmpaddr | noprefixroute | autojoin ]
+LIFETIME := [ valid_lft LFT ] [ preferred_lft LFT ]
+LFT := forever | SECONDS
+ADDRPROTO := [ NAME | NUMBER ]
+TYPE := { amt | bareudp | bond | bond_slave | bridge | bridge_slave |
+          dsa | dummy | erspan | geneve | gre | gretap | gtp | hsr |
+          ifb | ip6erspan | ip6gre | ip6gretap | ip6tnl |
+          ipip | ipoib | ipvlan | ipvtap |
+          macsec | macvlan | macvtap | netdevsim |
+          netkit | nlmon | pfcp | rmnet | sit | team | team_slave |
+          vcan | veth | vlan | vrf | vti | vxcan | vxlan | wwan |
+          xfrm | virt_wifi }""")
+    exit(-1)
+
+
+def get_ifconfig_links(argv, usage=usage):
+    res = ifconfig.exec("-v", "-L", "-a")
+    links = ifconfig.parse(res)
+    while argv:
+        opt = argv.pop(0)
+        if strcmp(opt, "to"):
+            # to = next_arg(argv)
+            # get_prefix(to, OPTION["preferred_family"])
+            do_notimplemented()
+        elif strcmp(opt, "scope"):
+            scope = next_arg(argv)
+            if scope not in ("link", "host", "global", "all") and not scope.isdigit():
+                invarg('invalid "scope"', scope)
+            if scope == "all":
+                continue
+            do_notimplemented()
+        elif strcmp(opt, "up"):
+            links = [link for link in links if ("flags" in link and "UP" in link["flags"])]
+        # TODO: elif get_filter(opt):
+        elif strcmp(opt, "label"):
+            # label = next_opt(argv)
+            do_notimplemented()
+        elif strcmp(opt, "group"):
+            group = next_arg(argv)
+            do_notimplemented()
+            invarg('Invalid "group" value', group)
+        elif strcmp(opt, "master"):
+            master = next_arg(argv)
+            if not any(link["ifname"] == master for link in links):
+                invarg("Device does not exist", master)
+            links = [link for link in links if ("master" in link and link["master"] == master)]
+        elif strcmp(opt, "vrf"):
+            vrf = next_arg(argv)
+            if not any(link["ifname"] == vrf for link in links):
+                invarg("Not a valid VRF name", vrf)
+            # if not is_vrf(vrf):
+            #     invarg('Not a valid VRF name', vrf)
+            # links = [link for link in links if ('master' in link and link['master'] == vrf)]
+            # FIXME: https://wiki.netunix.net/freebsd/network/vrf/
+            do_notimplemented([opt])
+        elif strcmp(opt, "nomaster"):
+            links = [link for link in links if "master" not in link]
+        elif strcmp(opt, "type"):
+            kind = next_arg(argv)
+            if kind.endswith("_slave"):
+                kind = kind.replace("_slave", "")
+                links = [link for link in links if recurse_in(link, ["linkinfo", "info_slave_kind"], kind)]
+            else:
+                links = [link for link in links if recurse_in(link, ["linkinfo", "info_kind"], kind)]
+        else:
+            if strcmp(opt, "dev"):
+                opt = next_arg(argv)
+            elif matches(opt, "help"):
+                usage()
+            links = [link for link in links if link["ifname"] == opt]
+            if not links:
+                stderr(f'Device "{opt}" does not exist.')
+                exit(-1)
+
+    if not OPTION["show_details"]:
+        delete_keys(links, "linkinfo")
+
+    return links
+
+
+def ipaddr_list(argv):
+    links = get_ifconfig_links(argv)
+    if OPTION["preferred_family"] in (AF_INET, AF_INET6, AF_MPLS, AF_BRIDGE):
+        family = family_name(OPTION["preferred_family"])
+        links = [link for link in links if "addr_info" in link and any(addr["family"] == family for addr in link["addr_info"])]
+    ifconfig.dumps(links)
+
+    return EXIT_SUCCESS
+
+
+def do_ipaddr(argv):
+    if not argv:
+        return ipaddr_list(argv)
+
+    cmd = argv.pop(0)
+    if matches(cmd, "add"):
+        return do_notimplemented()
+    elif matches(cmd, "change", "chg"):
+        return do_notimplemented()
+    elif matches(cmd, "replace"):
+        return do_notimplemented()
+    elif matches(cmd, "delete"):
+        return do_notimplemented()
+    elif matches(cmd, "show", "lst", "list"):
+        return ipaddr_list(argv)
+    elif matches(cmd, "flush"):
+        return do_notimplemented()
+    elif matches(cmd, "save"):
+        return do_notimplemented()
+    elif matches(cmd, "showdump"):
+        return do_notimplemented()
+    elif matches(cmd, "restore"):
+        return do_notimplemented()
+    elif matches(cmd, "help"):
+        return usage()
+
+    stderr(f'Command "{cmd}" is unknown, try "ip address help".')
+    exit(-1)
---
+++ packages/iproute4mac/iproute4mac-0.2.1-py3-none-any.whl/iproute4mac/netstat.py
@@ -0,0 +1,152 @@
+from iproute4mac.utils import *
+
+
+"""
+https://docs.freebsd.org/en/books/handbook/advanced-networking/#routeflags
+"""
+RTF_PROTO1 = "1"  # Protocol specific routing flag #1
+RTF_PROTO2 = "2"  # Protocol specific routing flag #2
+RTF_PROTO3 = "3"  # Protocol specific routing flag #3
+RTF_BLACKHOLE = "B"  # Just discard packets (during updates)
+RTF_BROADCAST = "b"  # The route represents a broadcast address
+RTF_CLONING = "C"  # Generate new routes on use
+RTF_PRCLONING = "c"  # Protocol-specified generate new routes on use
+RTF_DYNAMIC = "D"  # Created dynamically (by redirect)
+RTF_GATEWAY = "G"  # Destination requires forwarding by intermediary
+RTF_HOST = "H"  # Host entry (net otherwise)
+RTF_IFSCOPE = "I"  # Route is associated with an interface scope
+RTF_IFREF = "i"  # Route is holding a reference to the interface
+RTF_LLINFO = "L"  # Valid protocol to link address translation
+RTF_MODIFIED = "M"  # Modified dynamically (by redirect)
+RTF_MULTICAST = "m"  # The route represents a multicast address
+RTF_REJECT = "R"  # Host or net unreachable
+RTF_ROUTER = "r"  # Host is a default router
+RTF_STATIC = "S"  # Manually added
+RTF_UP = "U"  # Route usable
+RTF_WASCLONED = "W"  # Route was generated as a result of cloning
+RTF_XRESOLVE = "X"  # External daemon translates proto to link address
+RTF_PROXY = "Y"  # Proxying; cloned routes will not be scoped
+RTF_GLOBAL = "g"  # Route to a destination of the global internet (policy hint)
+
+
+def exec(*argv):
+    return shell("netstat", *argv)
+
+
+def dumps(routes):
+    if OPTION["json"]:
+        print(json_dumps(routes, OPTION["pretty"]))
+        return
+
+    # if not routes:
+    #    return
+
+    for route in routes:
+        if OPTION["show_details"] or "type" in route:
+            stdout(route["type"] if "type" in route else "unicast", " ")
+        stdout(route["dst"])
+        if "gateway" in route:
+            stdout(f" via {route['gateway']}")
+        if "dev" in route:
+            stdout(f" dev {route['dev']}")
+        if "protocol" in route:
+            stdout(f" proto {route['protocol']}")
+        if "scope" in route:
+            stdout(f" scope {route['scope']}")
+        if "prefsrc" in route:
+            stdout(f" src {route['src']}")
+        stdout(end="\n")
+
+
+class netstatRegEx:
+    _route = re.compile(
+        rf"(?P<dst>(?:default|{IPV4ADDR}|{IPV6ADDR}))(?:%\w+)?(?:/(?P<prefix>\d+))?"
+        rf"\s+(?P<gateway>{IPV4ADDR}|{IPV6ADDR}|{LLADDR}|link#\d+)(?:%\w+)?"
+        r"\s+(?P<flags>\w+)"
+        r"\s+(?P<dev>\w+)"
+        r"\s+(?P<expire>\S+)?"
+    )
+
+    def __init__(self, line):
+        self.route = self._route.match(line)
+
+
+def parse(res):
+    routes = []
+    for line in iter(res.split("\n")):
+        match = netstatRegEx(line)
+
+        if match.route:
+            debug(f"Found route: {line.strip()}")
+            dst, prefix, gateway, flags, dev, expire = match.route.groups()
+
+            if RTF_WASCLONED in flags:
+                debug("Skip cloned rotue")
+                continue
+            if RTF_PROXY in flags:
+                debug("Skip proxy rotue")
+                continue
+
+            if prefix:
+                dst = f"{dst}/{prefix}"
+            dst = Prefix(dst)
+
+            scope = None
+            if re.match(LLADDR, gateway):
+                if not OPTION["show_details"]:
+                    debug("Skip host rotue")
+                    continue
+                gateway = None
+            elif gateway.startswith("link#"):
+                scope = "link"
+                gateway = None
+            else:
+                gateway = Prefix(gateway)
+                if dst.family == AF_UNSPEC:
+                    dst.family = gateway.family
+
+            if OPTION["preferred_family"] != AF_UNSPEC and dst.family != OPTION["preferred_family"]:
+                debug("Skip unmatched IP version rotue")
+                continue
+
+            # protocol
+            if RTF_STATIC in flags:
+                protocol = "static"
+            elif any(flag in flags for flag in (RTF_DYNAMIC, RTF_MODIFIED)):
+                protocol = "redirect"
+            else:
+                protocol = "kernel"
+
+            # scope
+            if RTF_HOST in flags:
+                scope = "host"
+            elif OPTION["show_details"]:
+                scope = "global"
+
+            # address type
+            if RTF_BLACKHOLE in flags:
+                addr_type = "blackhole"
+            elif RTF_BROADCAST in flags:
+                addr_type = "broadcast"
+            elif RTF_MULTICAST in flags:
+                addr_type = "multicast"
+            elif OPTION["show_details"]:
+                addr_type = "unicast"
+            else:
+                addr_type = None
+
+            route = {
+                "type": addr_type,
+                "dst": dst,
+                "gateway": gateway,
+                "dev": dev,
+                "protocol": protocol,
+                "scope": scope,
+                "expire": int(expire) if expire and expire.isdigit() else None,
+                "flags": [],
+            }
+            routes.append({k: v for k, v in route.items() if v is not None})
+        elif line:
+            debug(f'Unparsed line "{line.strip()}"')
+
+    return routes
