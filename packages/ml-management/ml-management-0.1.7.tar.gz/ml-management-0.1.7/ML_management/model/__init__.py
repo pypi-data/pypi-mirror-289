7347e9938fb8632c69fb84eabb37f84648d45760
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-08 17:03:59 +0000

    Add package ml-management/0.1.7/ml-management-0.1.7.tar.gz

---
+++ packages/ml-management/ml-management-0.1.7.tar.gz/ml-management-0.1.7/ML_management/model/__init__.py
@@ -0,0 +1,17 @@
+from .patterns.evaluatable_model import EvaluatableModel
+from .patterns.gradient_model import GradientModel
+from .patterns.model_pattern import Model
+from .patterns.model_with_losses import ModelWithLosses
+from .patterns.retrainable_model import RetrainableModel
+from .patterns.target_layer import TargetLayer
+from .patterns.torch_model import TorchModel
+from .patterns.trainable_model import TrainableModel
+
+EvaluatableModel = EvaluatableModel
+GradientModel = GradientModel
+Model = Model
+ModelWithLosses = ModelWithLosses
+RetrainableModel = RetrainableModel
+TargetLayer = TargetLayer
+TorchModel = TorchModel
+TrainableModel = TrainableModel
