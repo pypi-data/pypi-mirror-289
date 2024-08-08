adc5bcc1ad853f4a162ae8bad0f1686c5187209f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-08 07:04:39 +0000

    Add package django-autotask/1.6.1/django-autotask-1.6.1.tar.gz

---
+++ packages/django-autotask/django-autotask-1.6.1.tar.gz/django-autotask-1.6.1/djautotask/utils.py
@@ -0,0 +1,19 @@
+from django.conf import settings
+
+
+class DjautotaskSettings:
+
+    def get_settings(self):
+        request_settings = {
+            'timeout': 30.0,
+            'batch_size': 50,
+            'max_attempts': 3,
+            'keep_completed_hours': 8,
+            'batch_query_size': 400,
+            'queue_sync_filter': [],
+        }
+
+        if hasattr(settings, 'DJAUTOTASK_CONF_CALLABLE'):
+            request_settings.update(settings.DJAUTOTASK_CONF_CALLABLE())
+
+        return request_settings
