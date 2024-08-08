01333620cafc77a64a61d49bddf4e3d3a1f68d57
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-08 16:05:23 +0000

    Add package timer-tool/1.0.0/timer-tool-1.0.0.tar.gz

---
+++ packages/timer-tool/timer-tool-1.0.0.tar.gz/timer-tool-1.0.0/setup.py
@@ -0,0 +1,20 @@
+import timer,sys,os
+from setuptools import setup
+
+setup(
+    name='timer-tool',
+    version="1.0.0",
+    description=desc,
+    author=timer.__author__,
+    author_email=timer.__email__,
+    url="https://github.com/qfcy/Python/blob/main/timer.py",
+    py_modules=['timer'],
+    keywords=["timer","performance","analysis","计时器","性能"],
+    classifiers=[
+        'Programming Language :: Python',
+        "Natural Language :: Chinese (Simplified)",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Topic :: Software Development :: Testing",
+        "Topic :: Software Development :: Bug Tracking"
+    ],
+)
