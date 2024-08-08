1a37fea346e862787d2ff7247156a6bc7d8a131a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-08 04:03:32 +0000

    Add package jelver-cli/1.0.4/jelver_cli-1.0.4-py3-none-any.whl

---
+++ packages/jelver-cli/jelver_cli-1.0.4-py3-none-any.whl/enums.py
@@ -0,0 +1,21 @@
+""" File to house Enums """
+
+from enum import Enum
+
+
+class BrowserType(Enum):
+    """
+    Enum for browsers
+    """
+    CHROMIUM = 'CHROMIUM'
+    FIREFOX = 'FIREFOX'
+    WEBKIT = 'WEBKIT'
+    SAFARI = 'SAFARI'
+    EDGE = 'EDGE'
+
+
+class CaseType(Enum):
+    """
+    Class to house different CaseTypes
+    """
+    ROUTE = 'ROUTE'
