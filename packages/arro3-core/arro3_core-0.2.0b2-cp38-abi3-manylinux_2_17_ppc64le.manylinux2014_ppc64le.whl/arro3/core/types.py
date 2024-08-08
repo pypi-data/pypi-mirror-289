eb727dc5144973cdc4c5be502c0da84cef36f9da
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-08 06:05:02 +0000

    Add package arro3-core/0.2.0b2/arro3_core-0.2.0b2-cp38-abi3-manylinux_2_17_ppc64le.manylinux2014_ppc64le.whl

---
+++ packages/arro3-core/arro3_core-0.2.0b2-cp38-abi3-manylinux_2_17_ppc64le.manylinux2014_ppc64le.whl/arro3/core/types.py
@@ -0,0 +1,23 @@
+from __future__ import annotations
+
+from typing import Protocol, Tuple
+
+
+class ArrowSchemaExportable(Protocol):
+    """A C-level reference to an Arrow Schema or Field."""
+
+    def __arrow_c_schema__(self) -> object: ...
+
+
+class ArrowArrayExportable(Protocol):
+    """A C-level reference to an Arrow Array or RecordBatch."""
+
+    def __arrow_c_array__(
+        self, requested_schema: object | None = None
+    ) -> Tuple[object, object]: ...
+
+
+class ArrowStreamExportable(Protocol):
+    """A C-level reference to an Arrow RecordBatchReader, Table, or ChunkedArray."""
+
+    def __arrow_c_stream__(self, requested_schema: object | None = None) -> object: ...
