3d442e0c3c40e18d60259db3e2875dbeac10abc3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-08 04:04:06 +0000

    Add package viktor/14.14.0/viktor-14.14.0-cp311-cp311-manylinux2014_x86_64.whl

---
+++ packages/viktor/viktor-14.14.0-cp311-cp311-manylinux2014_x86_64.whl/viktor/__init__.pyi
@@ -0,0 +1,3 @@
+from .__version__ import __version__ as __version__
+from .core import Color as Color, File as File, InitialEntity as InitialEntity, ParamsFromFile as ParamsFromFile, UserMessage as UserMessage, ViktorController as ViktorController, progress_message as progress_message
+from .errors import UserError as UserError
