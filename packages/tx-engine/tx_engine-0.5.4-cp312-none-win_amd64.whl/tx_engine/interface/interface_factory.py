15c9e56f2774dd50c8d7a2ff4b1497d4f1f8bd64
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-08 05:04:04 +0000

    Add package tx-engine/0.5.4/tx_engine-0.5.4-cp312-none-win_amd64.whl

---
+++ packages/tx-engine/tx_engine-0.5.4-cp312-none-win_amd64.whl/tx_engine/interface/interface_factory.py
@@ -0,0 +1,22 @@
+from .blockchain_interface import ConfigType
+from .mock_interface import MockInterface
+from .woc_interface import WoCInterface
+
+
+INTERFACE_MAPPING = {
+    "mock": MockInterface,
+    "woc": WoCInterface,
+}
+
+
+class InterfaceFactory:
+    """ A class for creating interfaces to the BSV blockchain """
+
+    def set_config(self, config: ConfigType) -> MockInterface | WoCInterface:
+        interface_type = config['interface_type']
+        interface = INTERFACE_MAPPING[interface_type]()  # type: ignore[abstract]
+        interface.set_config(config)
+        return interface  # type: ignore[return-value]
+
+
+interface_factory = InterfaceFactory()
