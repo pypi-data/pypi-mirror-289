36c2bbdd5c5607eb211af2598deaef26a0453bbd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-08 03:03:35 +0000

    Add package pytilkit/1.0.1/pytilkit-1.0.1.tar.gz

---
+++ packages/pytilkit/pytilkit-1.0.1.tar.gz/pytilkit-1.0.1/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+from pathlib import Path
+
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+setup(
+    name='pytilkit',
+    version='1.0.1',
+    author='Srijal Dutta',
+    author_email='srijaldutta.official+pytilkit@gmail.com',
+    description='A utility package for various functionalities including math operations, JSON handling, and RSA encryption',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/yourusername/pyutilkit',
+    packages=find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+    install_requires=[
+        "cryptography"
+    ],
+)
