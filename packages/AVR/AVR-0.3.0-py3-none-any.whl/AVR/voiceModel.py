c9c04307f127fe660173927df42b602f56404153
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-08 17:04:01 +0000

    Add package AVR/0.3.0/AVR-0.3.0-py3-none-any.whl

---
+++ packages/AVR/AVR-0.3.0-py3-none-any.whl/AVR/voiceModel.py
@@ -0,0 +1,12 @@
+import torch
+import torch.nn as nn
+
+class getModelVoice(nn.Module):
+    def __init__(self):
+        pass
+ 
+    def forward(self):
+        pass
+
+    def initialize_weights(self):
+        pass
