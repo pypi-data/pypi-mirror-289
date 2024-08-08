7fbaec9b856d855e7d92b0870cd4b35deaba6265
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-08 03:03:44 +0000

    Add package my-library-2802/0.1/my_library_2802-0.1.tar.gz

---
+++ packages/my-library-2802/my_library_2802-0.1.tar.gz/my_library_2802-0.1/setup.py
@@ -0,0 +1,10 @@
+
+from setuptools import setup, find_packages
+
+setup(
+    name='my_library_2802',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[],
+    test_suite='my_library_2802.tests',
+)
