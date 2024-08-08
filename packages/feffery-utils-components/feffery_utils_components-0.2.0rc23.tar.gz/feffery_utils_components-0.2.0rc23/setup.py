785af286d783a607f12a1c838bd657c9dfb2b4de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-08 14:04:25 +0000

    Add package feffery-utils-components/0.2.0rc23/feffery_utils_components-0.2.0rc23.tar.gz

---
+++ packages/feffery-utils-components/feffery_utils_components-0.2.0rc23.tar.gz/feffery_utils_components-0.2.0rc23/setup.py
@@ -0,0 +1,31 @@
+import json
+import os
+from setuptools import setup
+
+
+with open('package.json') as f:
+    package = json.load(f)
+
+package_name = package["name"].replace(" ", "_").replace("-", "_")
+
+setup(
+    name=package_name,
+    version=package["version"],
+    author=package['author'],
+    author_email=package['email'],
+    packages=[package_name],
+    include_package_data=True,
+    package_data={
+        'feffery_antd_components': ['*']
+    },
+    license=package['license'],
+    description=package.get('description', package_name),
+    install_requires=[
+        'dash>=2.10.2, !=2.16.0'
+    ],
+    classifiers=[
+        'Framework :: Dash',
+    ],
+    homepage='https://github.com/CNFeffery/feffery-utils-components',
+    url='https://github.com/CNFeffery/feffery-utils-components'
+)
