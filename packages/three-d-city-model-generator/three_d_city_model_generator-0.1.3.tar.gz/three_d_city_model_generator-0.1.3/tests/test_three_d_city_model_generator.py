4f8d02e7b9f8e4561fbcc1534907bb7bba3524d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-08 03:03:27 +0000

    Add package three-d-city-model-generator/0.1.3/three_d_city_model_generator-0.1.3.tar.gz

---
+++ packages/three-d-city-model-generator/three_d_city_model_generator-0.1.3.tar.gz/three_d_city_model_generator-0.1.3/tests/test_three_d_city_model_generator.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+"""Tests for `three_d_city_model_generator` package."""
+
+
+import unittest
+
+from three_d_city_model_generator import three_d_city_model_generator
+
+
+class TestThree_d_city_model_generator(unittest.TestCase):
+    """Tests for `three_d_city_model_generator` package."""
+
+    def setUp(self):
+        """Set up test fixtures, if any."""
+
+    def tearDown(self):
+        """Tear down test fixtures, if any."""
+
+    def test_000_something(self):
+        """Test something."""
