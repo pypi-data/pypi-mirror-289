2590c810a225bf2e88acc9d7f9df066c5d6a45ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-08 16:04:39 +0000

    Add package lsst-pipe-base/27.2024.3200/lsst_pipe_base-27.2024.3200-py3-none-any.whl

---
+++ packages/lsst-pipe-base/lsst_pipe_base-27.2024.3200-py3-none-any.whl/lsst/pipe/base/__init__.py
@@ -0,0 +1,39 @@
+# The graphBuilder module is deprecated, but we still export its symbols for
+# backwards compatibility.
+import warnings
+
+from . import automatic_connection_constants, connectionTypes, pipeline_graph, pipelineIR, utils
+from ._dataset_handle import *
+
+# Symbols from _datasetQueryConstraints are exported from
+# all_dimensions_quantum_graph_builder, since that's the only place they are
+# used.
+from ._instrument import *
+from ._observation_dimension_packer import *
+from ._quantumContext import *
+from ._status import *
+from ._task_metadata import *
+from .config import *
+from .connections import *
+from .executionButlerBuilder import *
+from .graph import *
+
+with warnings.catch_warnings():
+    warnings.simplefilter("ignore", FutureWarning)
+    from .graphBuilder import *
+del warnings
+
+from .pipeline import *
+
+# We import the main PipelineGraph type and the module (above), but we don't
+# lift all symbols to package scope.
+from .pipeline_graph import PipelineGraph
+from .pipelineTask import *
+from .struct import *
+from .task import *
+from .taskFactory import *
+from .version import *
+
+# quantum_graph_builder, all_dimensions_quantum_graph_builder,
+# quantum_graph_skeleton, and prerequisite_helper symbols are intentionally not
+# lifted to package scope.
