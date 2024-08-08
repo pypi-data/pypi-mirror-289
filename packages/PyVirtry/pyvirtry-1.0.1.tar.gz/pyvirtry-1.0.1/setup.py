df3e64aeb7d0541709ac5971504de8916861e15c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-08 05:05:09 +0000

    Add package PyVirtry/1.0.1/pyvirtry-1.0.1.tar.gz

---
+++ packages/PyVirtry/pyvirtry-1.0.1.tar.gz/pyvirtry-1.0.1/setup.py
@@ -0,0 +1,43 @@
+from setuptools import setup, find_packages
+
+with open('README.md', encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name="PyVirtry",
+    version="1.0.1",
+    long_description = long_description,
+    long_description_content_type='text/markdown',
+    author="powerjsv",
+    author_email="powerjsv12@gmail.com",
+    url="https://github.com/powerjsv/jsv_package_test",
+    install_requires=[
+        "pip==23.3.1",
+        "accelerate==0.25.0",
+        "tqdm==4.66.1",
+        "transformers==4.36.2",
+        "diffusers==0.25.0",
+        "einops==0.7.0",
+        "bitsandbytes==0.39.0",
+        "scipy==1.11.1",
+        "opencv-python",
+        "transformers",
+        "huggingface_hub",
+        "fvcore",
+        "omegaconf",
+        "av",
+        "onnxruntime"
+    ],
+    packages=find_packages(exclude=[]),
+    include_package_data=True,
+    keywords=["vton", "powerjsv", "toy project", "pypi"],
+    python_requires=">=3.10",
+    package_data={
+        'nstm': ['configs/**/*']
+    },
+    zip_safe=False,
+    classifiers=[
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+    ],
+)
