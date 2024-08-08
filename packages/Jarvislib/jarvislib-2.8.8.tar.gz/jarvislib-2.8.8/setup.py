98b63493e097179e64a79daa0e6d402e93a78bf9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-08 04:03:57 +0000

    Add package Jarvislib/2.8.8/jarvislib-2.8.8.tar.gz

---
+++ packages/Jarvislib/jarvislib-2.8.8.tar.gz/jarvislib-2.8.8/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='Jarvislib',
+      version='2.8.8',
+      description='Импорт функции: from Jarvislib import status. Запуск главного цикла: statuss()',
+      packages=['Jarvislib'],
+      author_email='babadey.pl@gmail.com',
+      zip_safe=False)
