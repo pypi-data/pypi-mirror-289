f13e475c0151ef157a78f66b56c462c93b13d844
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-08 04:04:37 +0000

    Add package autocode-py/0.0.1.post7/autocode_py-0.0.1.post7.tar.gz

---
+++ packages/autocode-py/autocode_py-0.0.1.post7.tar.gz/autocode_py-0.0.1.post7/setup.py
@@ -0,0 +1,44 @@
+from pathlib import Path
+
+from setuptools import setup
+
+this_directory: Path = Path(__file__).parent
+long_description: str = (this_directory / "README.md").read_text()
+
+setup(
+    name='autocode-py',
+    version='0.0.1.post7',
+    author='muazhari',
+    url='https://github.com/muazhari/autocode',
+    description='autocode: Auto Code Improvement by Metrics Optimization.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    packages=['autocode'],
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Topic :: Software Development',
+        'Topic :: Scientific/Engineering',
+    ],
+    install_requires=[
+        'pymoo',
+        'pydantic_settings',
+        'fastapi',
+        'dependency-injector',
+        'ray',
+        'fastapi',
+        'matplotlib<3.9.0',
+        'sqlmodel',
+        'dill',
+        'streamlit',
+        'numpy<2',
+        'python-on-whales',
+        'uvicorn',
+        'langchain',
+        'langchain-openai>=0.1.21rc1',
+        'langgraph',
+    ],
+)
