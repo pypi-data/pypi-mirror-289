c34b78dab8767bc0d3751542dc490a4aff09f634
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-08 04:04:45 +0000

    Add package llvmpym/0.0.5/llvmpym-0.0.5-cp312-abi3-win_amd64.whl

---
+++ packages/llvmpym/llvmpym-0.0.5-cp312-abi3-win_amd64.whl/llvmpym/llvmpym_ext/target.pyi
@@ -0,0 +1,5 @@
+import llvmpym_ext
+
+
+class TargetData(llvmpym_ext.PymTargetDataObject):
+    """TargetData"""
---
+++ packages/llvmpym/llvmpym-0.0.5-cp312-abi3-win_amd64.whl/llvmpym/llvmpym_ext/utils.pyi
@@ -0,0 +1,4 @@
+import llvmpym_ext.core
+
+
+def parse_assembly(isam: str) -> llvmpym_ext.core.Module: ...
