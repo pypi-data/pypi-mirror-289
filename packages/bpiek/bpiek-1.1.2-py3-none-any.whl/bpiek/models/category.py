af0451c4cc875d3d1b93938d8653a659181466b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-08 14:04:05 +0000

    Add package bpiek/1.1.2/bpiek-1.1.2-py3-none-any.whl

---
+++ packages/bpiek/bpiek-1.1.2-py3-none-any.whl/bpiek/models/category.py
@@ -0,0 +1,13 @@
+from typing import Any
+from pydantic import BaseModel, ConfigDict, Field
+from pydantic.types import UUID1
+
+
+class Category(BaseModel):
+    slug: str  # Слаг категории
+    name: str  # Название категории
+    url: str  # Относительный адрес категории
+    apiUrl: str | None = Field(
+        default=None
+    )  # Ссылка на скачивание файла с содержимым категории
+    childs: list["Category"] | None = Field(default=[])
