d5c2d4f50d334063a6f438219b6df97eb2325198
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-08 15:04:20 +0000

    Add package loe-simp-app-fw/2.7.5/loe_simp_app_fw-2.7.5-py3-none-any.whl

---
+++ packages/loe-simp-app-fw/loe_simp_app_fw-2.7.5-py3-none-any.whl/loe_simp_app_fw/__init__.py
@@ -0,0 +1,32 @@
+from .logger import Logger
+from .config import BaseConfig, FrameworkConfig, NotInitialized
+from .cacher import CacheCorrupt, CacheExpired, CacheMiss, CacheNotFound, GlobalCacheManager
+from .csvnia import CSVReader, CSVWriter
+from .notebook import isNotebook
+from .start import main as init_repo
+from .request_handler import RequestError, RequestHandler, RetryCounter
+from .exit import Register
+
+# Register exit sequence in order
+Register.register()
+
+__all__ = [
+    "Logger", 
+    "CacheCorrupt",
+    "CacheExpired",
+    "CacheMiss",
+    "CacheNotFound",
+    "GlobalCacheManager",
+    "CSVReader",
+    "CSVWriter",
+    "FrameworkConfig",
+    "NotInitialized",
+    "BaseConfig",
+    "isNotebook",
+    "init_repo",
+    "RequestHandler",
+    "RequestError",
+    "RetryCounter",
+    ]
+
+__version__ = "2.7.5"
