63b862272416f0fbf9d8d54306df45fe374afa01
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-08 15:04:20 +0000

    Add package loe-simp-app-fw/2.7.5/loe_simp_app_fw-2.7.5.tar.gz

---
+++ packages/loe-simp-app-fw/loe_simp_app_fw-2.7.5.tar.gz/loe_simp_app_fw-2.7.5/src/loe_simp_app_fw/logger/__init__.py
@@ -0,0 +1,9 @@
+from .frontend import Logger
+from .model import LogLevels
+from .model import Exceptions as LoggerExceptions
+
+__all__ = [
+    "Logger",
+    "LogLevels",
+    "LoggerExceptions",
+]
