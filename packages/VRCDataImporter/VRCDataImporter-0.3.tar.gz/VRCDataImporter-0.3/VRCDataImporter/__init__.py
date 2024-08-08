93bfccdc6925ee9ee3dea45c005a65a8212a6db5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-08 16:04:30 +0000

    Add package VRCDataImporter/0.3/VRCDataImporter-0.3.tar.gz

---
+++ packages/VRCDataImporter/VRCDataImporter-0.3.tar.gz/VRCDataImporter-0.3/VRCDataImporter/__init__.py
@@ -0,0 +1,3 @@
+from .main import (RawLogLine, read_log_file_without_further_formatting, get_tags_in_order,
+                   get_formatted_arrays_in_order, get_raw_strings_in_order, segregate_by_tag_to_dict,
+                   segregate_by_tag_to_dataframe_raw_string, segregate_by_tag_to_dataframe_arrays)
