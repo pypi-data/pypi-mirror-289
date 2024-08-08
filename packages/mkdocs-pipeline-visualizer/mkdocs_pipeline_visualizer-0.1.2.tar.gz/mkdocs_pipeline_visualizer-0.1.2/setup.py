5153862ba0612a850ce90d63d696c0656e0ac849
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-08 07:05:04 +0000

    Add package mkdocs-pipeline-visualizer/0.1.2/mkdocs_pipeline_visualizer-0.1.2.tar.gz

---
+++ packages/mkdocs-pipeline-visualizer/mkdocs_pipeline_visualizer-0.1.2.tar.gz/mkdocs_pipeline_visualizer-0.1.2/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="mkdocs-pipeline-visualizer",
+    version="0.1.2",
+    description="generate MD file from tekton pipelines and tasks, intended to be used together with mkdocs-techdocs-core",
+    long_description="# not-yet",
+    long_description_content_type="text/markdown",
+    url="https://github.com/obegron/mkdocs-pipeline-visualizer",
+    packages=find_packages(),
+    author='Christer Grönblad',
+    install_requires=['mkdocs'],
+    entry_points={
+        'mkdocs.plugins': [
+            'pipeline-visualizer = src.visualizer:PipelineVisualizer'
+        ]
+    }
+)
