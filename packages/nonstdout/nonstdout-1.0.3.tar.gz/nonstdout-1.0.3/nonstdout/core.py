b38c1565d0df1f0d4f32931deaba52f3301a4246
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-08 15:04:23 +0000

    Add package nonstdout/1.0.3/nonstdout-1.0.3.tar.gz

---
+++ packages/nonstdout/nonstdout-1.0.3.tar.gz/nonstdout-1.0.3/nonstdout/core.py
@@ -0,0 +1,163 @@
+import time
+from colorama import *
+from getpass import getpass
+
+def clear() -> None: 
+    '''
+Clears the terminal screen.
+    '''
+    print("\033[H\033[2J\033[3J", end="")
+
+def pause(prompt: str | None = "Press Enter to continue...") -> None:
+    '''
+Pauses execution until Enter is pressed.
+
+prompt
+  the text to display as the prompt, default "Press Enter to continue..."
+    '''
+    getpass(prompt)
+
+def printd(*values: object, sep: str | None = " ", end: str | None = "\n", file: str | None = None, flush: bool = False) -> None:
+    '''
+Prints the values to a stream, or to sys.stdout by default.
+
+Text is shown usually in some sort of grey, technically BLACK.
+
+sep
+  string inserted between values, default a space.
+end
+  string appended after the last value, default a newline.
+file
+  a file-like object (stream); defaults to the current sys.stdout.
+flush
+  whether to forcibly flush the stream.
+    '''
+    content = Fore.BLACK
+    for value in values:
+        content += value
+        content += " " if values[::-1][0] != value else Fore.RESET
+    print(content, sep=sep, end=end, file=file, flush=flush)
+
+def printe(*values: object, sep: str | None = " ", end: str | None = "\n", file: str | None = None, flush: bool = False) -> None:
+    '''
+Prints the values to a stream, or to sys.stdout by default.
+
+Text is shown in RED.
+
+sep
+  string inserted between values, default a space.
+end
+  string appended after the last value, default a newline.
+file
+  a file-like object (stream); defaults to the current sys.stdout.
+flush
+  whether to forcibly flush the stream.
+    '''
+    content = Fore.RED
+    for value in values:
+        content += value
+        content += " " if values[::-1][0] != value else Fore.RESET
+    print(content, sep=sep, end=end, file=file, flush=flush)
+
+def printw(*values: object, sep: str | None = " ", end: str | None = "\n", file: str | None = None, flush: bool = False) -> None:
+    '''
+Prints the values to a stream, or to sys.stdout by default.
+
+Text is shown in YELLOW.
+
+sep
+  string inserted between values, default a space.
+end
+  string appended after the last value, default a newline.
+file
+  a file-like object (stream); defaults to the current sys.stdout.
+flush
+  whether to forcibly flush the stream.
+    '''
+    content = Fore.YELLOW
+    for value in values:
+        content += value
+        content += " " if values[::-1][0] != value else Fore.RESET
+    print(content, sep=sep, end=end, file=file, flush=flush)
+
+def prints(*values: object, sep: str | None = " ", end: str | None = "\n", file: str | None = None, flush: bool = False) -> None:
+    '''
+Prints the values to a stream, or to sys.stdout by default.
+
+Text is shown in GREEN.
+
+sep
+  string inserted between values, default a space.
+end
+  string appended after the last value, default a newline.
+file
+  a file-like object (stream); defaults to the current sys.stdout.
+flush
+  whether to forcibly flush the stream.
+    '''
+    content = Fore.GREEN
+    for value in values:
+        content += value
+        content += " " if values[::-1][0] != value else Fore.RESET
+    print(content, sep=sep, end=end, file=file, flush=flush)
+
+def load(count: int, block: str | None = f"{Back.WHITE} {Back.RESET}", nada: str | None = " ", beg: str | None = "", end: str | None = "", pause: float | None = 0.1):
+    '''
+Creates a loading bar with specified text.
+
+count
+  number of blocks to load, required parameter
+block
+  the character to use as the block, default solid white box
+nada
+  the character to use as the lack of a block, default whitespace
+beg
+  what to put before the loading bar, e.g. "loading [", default nothing
+end
+  what to put after the loading bar, e.g. "]", default nothing
+pause
+  how long to wait in seconds before loading the next block, default 0.1
+    '''
+    blocks = ""
+    nadacount = count
+    blockcount = 0
+    for _ in range(count):
+        blocks += nada
+    print(f"{beg}{blocks}{end}", end="\r")
+    for _ in range(count):
+        blocks = ""
+        nadacount -= 1
+        blockcount += 1
+        for _ in range(blockcount): 
+            blocks += block
+        for _ in range(nadacount): 
+            blocks += nada
+        print(f"{beg}{blocks}{end}", end="\r")
+        time.sleep(pause)
+    print(f"{beg}{blocks}{end}")
+
+def spinner(duration: int, symbols: list[str] | None = ["|", "/", "-", "\\"], finished_symbol: str | None = "✓", before_text: str | None = "", after_text: str | None = ""):
+    '''
+Creates a loading spinner that lasts for as long as you want.
+
+duration
+  how long the spinner lasts, no default, required
+symbols
+  the symbols that the spinner goes through to animate, default spinner
+finished_symbol
+  the symbol that shows up when the spinner is done, default ✓
+before_text
+  what to put before the spinner, e.g. "loading... [", default nothing
+after_text
+  what to put after the spinner, e.g. "]", default nothing
+    '''
+    elapsed = 0
+
+    while True:
+        elapsed += 0.1
+        time.sleep(0.1)
+        if elapsed >= duration:
+            break
+        else:
+            print(f"{before_text}{symbols[int((elapsed * 10) % 4)]}{after_text}", end="\r")
+    print(f"{before_text}{finished_symbol}{after_text}")
