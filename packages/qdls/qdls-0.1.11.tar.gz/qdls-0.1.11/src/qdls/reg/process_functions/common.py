c4235bd57824ec74d4917dcfeb444252c55e24a5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-08 16:05:02 +0000

    Add package qdls/0.1.11/qdls-0.1.11.tar.gz

---
+++ packages/qdls/qdls-0.1.11.tar.gz/qdls-0.1.11/src/qdls/reg/process_functions/common.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+# @File    :   common.py
+# @Time    :   2023/05/22 16:09:01
+# @Author  :   Qing 
+# @Email   :   aqsz2526@outlook.com
+######################### docstring ########################
+'''
+    通用的数据集分词处理函数
+'''
+from ..register import registers
+
