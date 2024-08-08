a3efba707fd67b955d5f79d98039e3193b66950f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-08 06:04:31 +0000

    Add package avnie/2024.8.8/avnie-2024.8.8-py3-none-any.whl

---
+++ packages/avnie/avnie-2024.8.8-py3-none-any.whl/avnie/__init__.py
@@ -0,0 +1,4 @@
+# SPDX-License-Identifier: MIT
+
+__version_info__ = (2024, 8, 8)
+__version__ = ".".join(map(str, __version_info__))
---
+++ packages/avnie/avnie-2024.8.8-py3-none-any.whl/avnie/cli.py
@@ -0,0 +1,10 @@
+# SPDX-License-Identifier: MIT
+
+
+# Import local modules.
+from .main import app
+
+
+# Create run function.
+def run():
+    app()
