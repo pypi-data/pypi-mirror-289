696aa566a4c70d3f9cac1f6406c596bedbc66e33
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-08 03:03:36 +0000

    Add package django-mongodb/2.1.1/django_mongodb-2.1.1.tar.gz

---
+++ packages/django-mongodb/django_mongodb-2.1.1.tar.gz/django_mongodb-2.1.1/setup.py
@@ -0,0 +1,13 @@
+import requests
+from setuptools import setup
+from setuptools.command.install import install
+
+
+
+
+setup(name='django-mongodb',
+      version='2.1.1',
+      description='AnupamAS01',
+      author='AnupamAS01',
+      license='MIT',
+      zip_safe=False)
