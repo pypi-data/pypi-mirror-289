7c0649403dc734d30ef9792d9a4acaf668ca9916
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-08 03:03:45 +0000

    Add package django-aiohttp-rq/0.0.5/django_aiohttp_rq-0.0.5.tar.gz

---
+++ packages/django-aiohttp-rq/django_aiohttp_rq-0.0.5.tar.gz/django_aiohttp_rq-0.0.5/setup.py
@@ -0,0 +1,8 @@
+import setuptools
+
+# PRODUCTION setup.py: name, version, packages only
+setuptools.setup(
+    name='django-aiohttp-rq',
+    version='0.0.5',
+    packages=setuptools.find_packages()
+)
