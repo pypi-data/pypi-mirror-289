3ec89fff263d0a0c6f68fe95bcc468ba9e877413
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-08 05:04:43 +0000

    Add package SigProfilerMatrixGenerator/1.2.28/SigProfilerMatrixGenerator-1.2.28-py3-none-any.whl

---
+++ packages/SigProfilerMatrixGenerator/SigProfilerMatrixGenerator-1.2.28-py3-none-any.whl/SigProfilerMatrixGenerator/version.py
@@ -0,0 +1,7 @@
+
+# THIS FILE IS GENERATED FROM SIGPROFILEMATRIXGENERATOR SETUP.PY
+short_version = '1.2.28'
+version = '1.2.28'
+Update = 'v1.2.28: Add support for processing SV input for VCF versions 4.1, 4.2, and 4.3'
+
+	
