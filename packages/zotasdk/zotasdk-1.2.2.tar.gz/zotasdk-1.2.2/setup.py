85ba1882e6c3b8e9bc7086fea5e475837e853e75
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-08 14:04:10 +0000

    Add package zotasdk/1.2.2/zotasdk-1.2.2.tar.gz

---
+++ packages/zotasdk/zotasdk-1.2.2.tar.gz/zotasdk-1.2.2/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+# Package version
+VERSION = "1.2.2"
+
+setup(
+    name='zotasdk',
+    version=VERSION,
+    description='Zota API SDK',
+    url='https://pypi.org/p/zotasdk',
+    author='Zota Technology Pte Ltd',
+    author_email='open-source@zota.com',
+    license='APACHE-2.0',
+    packages=find_packages(),
+    install_requires=['requests'],
+    zip_safe=False
+)
