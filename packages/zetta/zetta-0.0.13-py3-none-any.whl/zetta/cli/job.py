7e27abde8cfc4e706e0279ce77a4d47f93252fff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-08 07:03:45 +0000

    Add package zetta/0.0.13/zetta-0.0.13-py3-none-any.whl

---
+++ packages/zetta/zetta-0.0.13-py3-none-any.whl/zetta/cli/job.py
@@ -0,0 +1,25 @@
+# Copyright ZettaBlock Labs 2024
+import typer
+from zetta._utils.async_utils import synchronizer
+
+job_cli = typer.Typer(
+    name="job", help="Manage your jobs in Zetta Workspace.", no_args_is_help=True
+)
+
+
+@job_cli.command(name="list", help="List all jobs that are currently running.")
+@synchronizer.create_blocking
+async def list(json: bool = False):
+    pass
+
+
+@job_cli.command(name="get", help="get a job status.")
+@synchronizer.create_blocking
+async def get(json: bool = False):
+    pass
+
+
+@job_cli.command(name="update", help="update a job .")
+@synchronizer.create_blocking
+async def update(json: bool = False):
+    pass
