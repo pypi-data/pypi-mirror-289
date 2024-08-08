767d68f2907cabffa2e2791cc56eda5d09fdb64b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-08 15:04:33 +0000

    Add package fast-knifes/0.0.3/fast_knifes-0.0.3.tar.gz

---
+++ packages/fast-knifes/fast_knifes-0.0.3.tar.gz/fast_knifes-0.0.3/setup.py
@@ -0,0 +1,31 @@
+import setuptools
+
+
+setuptools.setup(
+    name="fast-knifes",
+    version="0.0.3",
+    author="knifes",
+    author_email="author@example.com",
+    description="Fast Swiss Army Knife",
+    long_description="`pip install fast-knifes --index-url https://pypi.python.org/simple -U`",
+    long_description_content_type="text/markdown",
+    url="https://github.com/",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    packages=setuptools.find_packages(),
+    python_requires=">=3.0",
+    install_requires=[
+        # "python-decouple",
+        # "prompt_toolkit",
+        # "attrs",
+        # "cattrs",
+        # "httpx[http2]",
+        # 'cryptography',                       #  required by aes
+        # 'tencentcloud-sdk-python==3.0.600',   #  required by sms
+        # 'Pillow',                             #  required by luban
+        # 'pillow-heif',                        #  required by luban
+    ],
+)
