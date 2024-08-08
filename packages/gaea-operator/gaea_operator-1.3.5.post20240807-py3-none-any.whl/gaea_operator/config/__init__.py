38a7c2da0b05bddbc59a07df7cf200e67009e731
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-08 15:04:51 +0000

    Add package gaea-operator/1.3.5.post20240807/gaea_operator-1.3.5.post20240807-py3-none-any.whl

---
+++ packages/gaea-operator/gaea_operator-1.3.5.post20240807-py3-none-any.whl/gaea_operator/config/__init__.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+# @Time    : 2024/2/26
+# @Author  : yanxiaodong
+# @File    : __init__.py.py
+"""
+from gaea_operator.config.config import Config, KEY_NETWORK_ARCHITECTURE
+from gaea_operator.config.ppyoloe_plus.ppyoloeplus_config import PPYOLOEPLUSMConfig
+from gaea_operator.config.resnet.resnet_config import ResNetConfig
+from gaea_operator.config.ocrnet.ocrnet_config import OCRNetConfig
+from gaea_operator.config.convnext.convnext_config import ConvNextConfig
+from gaea_operator.config.codetr.codetr_config import CoDETRConfig
+from gaea_operator.config.repvit.repvit_config import RepViTConfig 
+
+__all__ = ["PPYOLOEPLUSMConfig",
+           "KEY_NETWORK_ARCHITECTURE",
+           "Config",
+           "ResNetConfig",
+           "OCRNetConfig",
+           "ConvNextConfig", "CoDETRConfig", "RepViTConfig"]
