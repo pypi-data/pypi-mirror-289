350fd94821ccb58ba46765fc59ebfd6f9bd8e9d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-08 06:05:10 +0000

    Add package roboto/0.6.0/roboto-0.6.0.tar.gz

---
+++ packages/roboto/roboto-0.6.0.tar.gz/roboto-0.6.0/roboto/domain/topics/__init__.py
@@ -0,0 +1,39 @@
+# Copyright (c) 2024 Roboto Technologies, Inc.
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+from .operations import (
+    AddMessagePathRepresentationRequest,
+    AddMessagePathRequest,
+    CreateTopicRequest,
+    MessagePathRepresentationMapping,
+    SetDefaultRepresentationRequest,
+    UpdateMessagePathRequest,
+    UpdateTopicRequest,
+)
+from .record import (
+    CanonicalDataType,
+    MessagePathRecord,
+    RepresentationRecord,
+    RepresentationStorageFormat,
+    TopicRecord,
+)
+from .topic import Topic
+
+__all__ = (
+    "AddMessagePathRequest",
+    "AddMessagePathRepresentationRequest",
+    "CreateTopicRequest",
+    "CanonicalDataType",
+    "MessagePathRepresentationMapping",
+    "MessagePathRecord",
+    "RepresentationRecord",
+    "RepresentationStorageFormat",
+    "SetDefaultRepresentationRequest",
+    "Topic",
+    "TopicRecord",
+    "UpdateMessagePathRequest",
+    "UpdateTopicRequest",
+)
