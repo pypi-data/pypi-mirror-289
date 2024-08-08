3aeef42ed74ad197fbcdb8cda65baaee3197e526
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-08 04:03:45 +0000

    Add package adc-compliance-checker/0.1/adc-compliance-checker-0.1.tar.gz

---
+++ packages/adc-compliance-checker/adc-compliance-checker-0.1.tar.gz/adc-compliance-checker-0.1/README.md
@@ -0,0 +1,11 @@
+# ADC-compliance-checker
+Arctic Data Centre compliance checker is a python based tool for those wanting to submit data to the Artic Data Centre. The python module can be used to check if files fullfill the rquirements listed [here](https://adc.met.no/node/4).
+
+The compliance checker can be used in combination with CF and ACDD chekcers to enusre NetCDF files adhere to the FAIR principles for data management.
+
+## Usage
+
+### Command Line
+
+
+### As a Python Package
---
+++ packages/adc-compliance-checker/adc-compliance-checker-0.1.tar.gz/adc-compliance-checker-0.1/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='adc-compliance-checker',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[
+        # List package dependencies
+        'xarray',
+        'pathlib',
+        'netCDF4',
+        'h5netcdf'
+    ],
+    author='Alessio Canclini',
+    author_email='alessioc@met.no',
+    description='ADC Compliance Checker',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/Alessimc/adc-compliance-checker',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+    entry_points={
+        'console_scripts': [
+            'adc-compliance-checker=adc_compliance_checker.cli:main',
+        ],
+    },
+    license='GPLv3',
+)
