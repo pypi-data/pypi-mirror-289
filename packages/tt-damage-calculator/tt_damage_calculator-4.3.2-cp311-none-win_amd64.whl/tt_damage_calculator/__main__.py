9df16ee62c87e1584b12989f41839e0a026cce0c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-08 06:05:02 +0000

    Add package tt-damage-calculator/4.3.2/tt_damage_calculator-4.3.2-cp311-none-win_amd64.whl

---
+++ packages/tt-damage-calculator/tt_damage_calculator-4.3.2-cp311-none-win_amd64.whl/tt_damage_calculator/__main__.py
@@ -0,0 +1,15 @@
+"""
+TT-Damage-Calculator
+Copyright (C) 2022-2024 Vhou-Atroph
+
+TT-Damage-Calculator is a gag damage calculator for Toontown Rewritten. It has an interface built with Python's tkinter library, and is supplemented with modules written in Rust.
+
+CONTRIBUTORS:
+- Vhou-Atroph
+- BoggTech
+"""
+
+from .widgets import App
+
+app = App()
+app.run()
