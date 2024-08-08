466a58e8a4d92b04b69a4885bac6bd2ccce1b57f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-08 06:04:56 +0000

    Add package aniworld/1.2.0/aniworld-1.2.0-py3-none-any.whl

---
+++ packages/aniworld/aniworld-1.2.0-py3-none-any.whl/aniworld/__init__.py
@@ -0,0 +1,8 @@
+from .extractors.doodstream import doodstream_get_direct_link
+from .extractors.streamtape import streamtape_get_direct_link
+from .extractors.vidoza import vidoza_get_direct_link
+from .extractors.voe import voe_get_direct_link
+
+from .aniskip.ani_skip import aniskip
+
+from .common import make_request
