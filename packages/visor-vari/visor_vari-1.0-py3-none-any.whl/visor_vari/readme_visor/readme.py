d231aaa80ff4665ae0745a81a74038427ff3bf03
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-08 14:04:12 +0000

    Add package visor-vari/1.0/visor_vari-1.0-py3-none-any.whl

---
+++ packages/visor-vari/visor_vari-1.0-py3-none-any.whl/visor_vari/readme_visor/readme.py
@@ -0,0 +1,27 @@
+
+print("\
+Para que pueda emplear la libreria visor_vari \n\
+usted deberia traer la clase Super_tabla del \n\
+modulo 'variables_valores' que se encuentra en \n\
+la ruta: visor_vari.mas_bajo_nivel \n\
+o en su defecto traerse el objeto 'refer' de ese \n\
+modulo, que es un objeto ya formado alli. \n\
+\n\
+Con ese objeto usted podra guardar su variable \n\
+en cualquier instancia. A usted, la libreria le \n\
+proporciona (81) instancias para guardar diferentes \n\
+valores. \n\
+Comprendida desde 'self.selda_0' \n\
+hasta 'self.selda_80' \n\
+Sirvase usar la que usted prefiera \n\
+\n\
+Cuando quiera ver sus datos recogidos \n\
+(en cierto punto de la ejecucion de su programa) \n\
+deberá hacer la llamada a la funcion 'gentil' \n\
+que se encuentra en el modulo 'visorquipus' \n\
+del mismo paquete 'visor_vari'. \n\
+Por supuesto tambien deberá hacer \n\
+esta importacion antes en su propio modulo \n\
+la funcion 'gentil' no recibe agumentos. \n\
+")
+
