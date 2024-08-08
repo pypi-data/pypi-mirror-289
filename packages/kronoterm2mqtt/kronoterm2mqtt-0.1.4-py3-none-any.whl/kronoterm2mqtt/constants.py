433b4c5cc1b95be7c81c3f8cf467ff3f38ec0d9d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-08 03:03:44 +0000

    Add package kronoterm2mqtt/0.1.4/kronoterm2mqtt-0.1.4-py3-none-any.whl

---
+++ packages/kronoterm2mqtt/kronoterm2mqtt-0.1.4-py3-none-any.whl/kronoterm2mqtt/constants.py
@@ -0,0 +1,13 @@
+from pathlib import Path
+
+import kronoterm2mqtt
+
+
+CLI_EPILOG = 'Project Homepage: https://github.com/kosl/kronoterm2mqtt'
+
+BASE_PATH = Path(__file__).parent
+
+
+DEFAULT_DEVICE_MANUFACTURER = 'KRONOTERM'
+
+
