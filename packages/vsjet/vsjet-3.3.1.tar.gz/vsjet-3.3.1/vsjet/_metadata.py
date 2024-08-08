5751c108b4a6dd8cc7943f09090270007a722f07
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-08 04:05:36 +0000

    Add package vsjet/3.3.1/vsjet-3.3.1.tar.gz

---
+++ packages/vsjet/vsjet-3.3.1.tar.gz/vsjet-3.3.1/vsjet/_metadata.py
@@ -0,0 +1,12 @@
+"""VapourSynth packages from JET"""
+
+__version__ = '3.3.1'
+
+__author_name__, __author_email__ = 'Irrational Encoding Wizardry', 'wizards@encode.moe'
+__maintainer_name__, __maintainer_email__ = 'Setsugen no ao', 'setsugen@setsugen.dev'
+
+__author__ = f'{__author_name__} <{__author_email__}>'
+__maintainer__ = __author__
+
+if __name__ == '__github__':
+    print(__version__)
