46ecd318cc52da333c29a792b35c2161f80a4281
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-08 14:04:34 +0000

    Add package axeh/0.0.1/axeh-0.0.1-py3-none-any.whl

---
+++ packages/axeh/axeh-0.0.1-py3-none-any.whl/axeh/gitignore_api.py
@@ -0,0 +1,40 @@
+from urllib import request
+
+headers = {"User-Agent": "axe"}
+
+
+def get_template_list():
+    response = (
+        request.urlopen(
+            request.Request(url="https://www.gitignore.io/api/list", headers=headers)
+        )
+        .read()
+        .decode("utf-8")
+    )
+
+    template_list = []
+    for line in map(str, response.split("\n")):
+        template_list.extend(line.split(","))
+
+    return template_list
+
+
+def get_gitignore(gi_stack):
+    template_list = get_template_list()
+
+    for tech in gi_stack:
+        if tech.lower() not in template_list:
+            raise ValueError(tech)
+
+    comma_delimited = ",".join(gi_stack)
+    response = (
+        request.urlopen(
+            request.Request(
+                f"https://www.gitignore.io/api/{comma_delimited}", headers=headers
+            )
+        )
+        .read()
+        .decode("utf-8")
+    )
+
+    return response
