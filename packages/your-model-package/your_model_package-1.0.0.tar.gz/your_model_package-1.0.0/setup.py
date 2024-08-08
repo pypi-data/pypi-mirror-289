f32dfe5fda746ff3fa165aff544919054f1070c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-08 06:04:44 +0000

    Add package your-model-package/1.0.0/your_model_package-1.0.0.tar.gz

---
+++ packages/your-model-package/your_model_package-1.0.0.tar.gz/your_model_package-1.0.0/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='your_model_package',
+    version='1.0.0',
+    packages=find_packages(),
+    install_requires=[
+        'tensorflow',
+        'numpy',
+        'Pillow',
+    ],
+    author='Manav MM21B038',
+    author_email='mm21b038@smail.iitm.ac.in',
+    description='A package for your DL model',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/MM21B038/my_first_trial_model',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+)
