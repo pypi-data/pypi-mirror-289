75a3906f99a473878caa69a512514d1627ea2820
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-08 14:04:16 +0000

    Add package huckle/3.5.1/huckle-3.5.1-py2.py3-none-any.whl

---
+++ packages/huckle/huckle-3.5.1-py2.py3-none-any.whl/huckle/package.py
@@ -0,0 +1,7 @@
+from __future__ import absolute_import, division, print_function
+
+__version__ = "3.5.0"
+dependencies = ["restnavigator==1.0.1",
+                "requests[socks]==2.28.1",
+                "configparser==5.3.0",
+                "certifi==2022.9.24"]
