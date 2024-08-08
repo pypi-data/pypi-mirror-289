ac1df857f8443aa2897e637c819b7e0036ef5270
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-08 03:03:37 +0000

    Add package RTPSender/3.6.5/rtpsender-3.6.5.tar.gz

---
+++ packages/RTPSender/rtpsender-3.6.5.tar.gz/rtpsender-3.6.5/setup.py
@@ -0,0 +1,37 @@
+from setuptools import setup, find_packages
+from setuptools.command.install import install
+import os
+
+class CustomInstallCommand(install):
+    """Customized setuptools install command - runs custom script."""
+    def run(self):
+        install.run(self)
+        self.execute_post_install()
+
+    def execute_post_install(self):
+        post_install_script = os.path.join(os.path.dirname(__file__), 'custom_install.py')
+        if os.path.exists(post_install_script):
+            import subprocess
+            print("Running post-install script...")
+            subprocess.run([self.distribution.get_command_obj('build').executable, post_install_script])
+
+setup(
+    name="RTPSender",
+    version="3.6.5",
+    author="liushihai02",
+    author_email="liushihai02@58.com",
+    packages=find_packages(),
+    install_requires=[
+        'scapy',
+        'opencv-python',
+        'pydub'
+        # Note: Do not include 'av' here as it will be handled separately
+    ],
+    description="A SDK for sending RTP streams",
+    long_description=open('README.md').read(),
+    long_description_content_type="text/markdown",
+    python_requires='>=3.9',
+    cmdclass={
+        'install': CustomInstallCommand,
+    },
+)
