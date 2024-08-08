b36f9b39d96b38c77f0b2802f63ad83fa5d06ffc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-08 16:05:17 +0000

    Add package MeUtils/2024.8.8.15.22.48/MeUtils-2024.8.8.15.22.48-py3-none-any.whl

---
+++ packages/MeUtils/MeUtils-2024.8.8.15.22.48-py3-none-any.whl/meutils/schemas/siliconflow_types.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Project      : AI.  @by PyCharm
+# @File         : siliconflow_types
+# @Time         : 2024/7/8 12:20
+# @Author       : betterme
+# @WeChat       : meutils
+# @Software     : PyCharm
+# @Description  : 
+
+from meutils.pipe import *
