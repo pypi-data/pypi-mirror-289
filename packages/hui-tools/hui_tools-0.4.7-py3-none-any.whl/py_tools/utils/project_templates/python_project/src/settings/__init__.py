bf85ab6fe33ea1b87f297ec8ce4de8ba4f5667e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-08 16:04:55 +0000

    Add package hui-tools/0.4.7/hui_tools-0.4.7-py3-none-any.whl

---
+++ packages/hui-tools/hui_tools-0.4.7-py3-none-any.whl/py_tools/utils/project_templates/python_project/src/settings/__init__.py
@@ -0,0 +1,4 @@
+from .base_setting import server_host, server_log_level, server_port
+from .db_setting import (mysql_dbname, mysql_host, mysql_password, mysql_port, mysql_user, redis_db, redis_host,
+                         redis_password, redis_port)
+from .log_setting import console_log_level, logging_conf
