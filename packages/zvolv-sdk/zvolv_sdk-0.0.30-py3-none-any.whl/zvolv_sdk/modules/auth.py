4f37ea2d01ad7d9287132ae5156f87b627c0b659
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-08 04:03:32 +0000

    Add package zvolv-sdk/0.0.30/zvolv_sdk-0.0.30-py3-none-any.whl

---
+++ packages/zvolv-sdk/zvolv_sdk-0.0.30-py3-none-any.whl/zvolv_sdk/modules/auth.py
@@ -0,0 +1,49 @@
+import hashlib
+import requests
+
+class Auth:
+    def __init__(self, session, logger, base_url, workspace_instance):
+        self.session = session
+        self.logger = logger
+        self.base_url = base_url
+        self.workspace_instance = workspace_instance
+        self.user_instance = None
+
+    def login(self, email, password):
+        """Authenticate a user and store their auth token."""
+        try: 
+            if not email or not password:
+                raise ValueError("Email and Password are required to login")
+            
+            url = f"{self.base_url}/rest/v17/user/login"
+            headers = {
+                'Content-Type': 'application/json',
+                'jwt': 'true',
+                'businessDomain': self.workspace_instance['BUSINESS_DOMAIN'],
+                'businessTagId': self.workspace_instance['BUSINESS_TAG_ID'],
+            }
+            sha512pwd = hashlib.sha512(password.encode('utf-8')).hexdigest()
+            data = {'email': email, 'password': sha512pwd}
+            response = self.session.post(url, json=data, headers=headers)
+            response.raise_for_status()  # Raise an exception for HTTP errors
+            
+            resp = response.json()
+            if resp.get('error') == False:
+                token = resp['loginToken']
+                self.session.headers.update({'Authorization': f"Bearer {token}"})
+                self.user_instance = resp
+                self.logger.info(f"User {email} logged in")
+            else:
+                raise ValueError(resp.get('message'))
+            return resp
+        except requests.exceptions.RequestException as http_err:
+            error_response = response.json()
+            status_code = error_response.get('statusCode', response.status_code)
+            error_message = error_response.get('message', str(http_err))
+
+            error_message = f"{status_code} Error: {error_message}"
+            self.logger.error(f"An error occurred: {error_message}")
+            raise requests.exceptions.HTTPError(error_message)
+        except Exception as e:
+            self.logger.error(e)
+            raise e
