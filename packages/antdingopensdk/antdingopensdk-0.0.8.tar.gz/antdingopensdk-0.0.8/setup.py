2f2318b30025b8aa9598903ddedf23ee7f828a2e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-08 16:05:26 +0000

    Add package antdingopensdk/0.0.8/antdingopensdk-0.0.8.tar.gz

---
+++ packages/antdingopensdk/antdingopensdk-0.0.8.tar.gz/antdingopensdk-0.0.8/setup.py
@@ -0,0 +1,18 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="antdingopensdk",
+    version="0.0.8",
+    author="alipay ant ding open",
+    # url='xxx',
+    # author_email="xxx",
+    description="alipay ant ding open sdk for python",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.6",
+)
