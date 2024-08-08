76d12fdf4d9304fd3bba55fb3e3c17d4b314fbe4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-08 15:04:06 +0000

    Add package bode-logger/0.0.11/bode_logger-0.0.11-py3-none-any.whl

---
+++ packages/bode-logger/bode_logger-0.0.11-py3-none-any.whl/bode_logger/__init__.py
@@ -0,0 +1,6 @@
+from importlib.metadata import version
+from bode_logger.bode_logger import _update_root_logger
+from bode_logger.bode_logger import info, debug, warning, error, critical
+
+__version__ = version("bode_logger")
+_update_root_logger()
