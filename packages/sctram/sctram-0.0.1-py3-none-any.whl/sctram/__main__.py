f16ee6aec388cc689d4bb42f8789b279a45e95c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-08 05:05:09 +0000

    Add package sctram/0.0.1/sctram-0.0.1-py3-none-any.whl

---
+++ packages/sctram/sctram-0.0.1-py3-none-any.whl/sctram/__main__.py
@@ -0,0 +1,10 @@
+"""Command-line interface."""
+
+
+def main() -> None:
+    """sctram."""
+    pass
+
+
+if __name__ == "__main__":
+    main()
---
+++ packages/sctram/sctram-0.0.1-py3-none-any.whl/sctram/_doctest.py
@@ -0,0 +1,11 @@
+class DocTest:
+    """Test text."""
+
+    def a1(self, x1, x2):
+        """Helmhotz.
+
+        Args:
+            x1 (str): Kemal
+            x2 (int): Inecik
+        """
+        pass
