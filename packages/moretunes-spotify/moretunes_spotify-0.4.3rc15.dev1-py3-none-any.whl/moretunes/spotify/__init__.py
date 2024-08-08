b69127375d0042f778eaf0985b043034397bc4ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-08 14:04:23 +0000

    Add package moretunes-spotify/0.4.3rc15.dev1/moretunes_spotify-0.4.3rc15.dev1-py3-none-any.whl

---
+++ packages/moretunes-spotify/moretunes_spotify-0.4.3rc15.dev1-py3-none-any.whl/moretunes/spotify/__init__.py
@@ -0,0 +1,17 @@
+from .enhanced import Spotify, SuperCred, Tuned
+from .tokens import RefreshingAnonToken, RefreshingWebAccessToken, from_browser
+from .web import ClientWebSpotify, UserWebSpotify
+
+
+__all__ = [
+    'Spotify',
+    'SuperCred',
+    'Tuned',
+    'ClientWebSpotify',
+    'UserWebSpotify',
+    'RefreshingAnonToken',
+    'RefreshingWebAccessToken',
+    'from_browser'
+]
+__version__ = "0.4.3rc15.dev1"
+
