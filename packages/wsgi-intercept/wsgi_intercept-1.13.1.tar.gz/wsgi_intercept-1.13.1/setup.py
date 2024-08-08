bcc37cbb3222e8047e2f738a88d1cc82eda1b5a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-08 16:04:58 +0000

    Add package wsgi-intercept/1.13.1/wsgi_intercept-1.13.1.tar.gz

---
+++ packages/wsgi-intercept/wsgi_intercept-1.13.1.tar.gz/wsgi_intercept-1.13.1/setup.py
@@ -0,0 +1,56 @@
+
+from setuptools import setup, find_packages
+
+VERSION = '1.13.1'
+
+CLASSIFIERS = """
+Environment :: Web Environment
+Intended Audience :: Developers
+License :: OSI Approved :: MIT License
+Operating System :: OS Independent
+Programming Language :: Python :: 2
+Programming Language :: Python :: 2.7
+Programming Language :: Python :: 3
+Programming Language :: Python :: 3.7
+Programming Language :: Python :: 3.8
+Programming Language :: Python :: 3.9
+Programming Language :: Python :: 3.10
+Programming Language :: Python :: 3.11
+Programming Language :: Python :: 3.12
+Topic :: Internet :: WWW/HTTP :: WSGI
+Topic :: Software Development :: Testing
+""".strip().splitlines()
+
+
+META = {
+    'name': 'wsgi_intercept',
+    'version': VERSION,
+    'author': 'Titus Brown, Kumar McMillan, Chris Dent, Sasha Hart',
+    'author_email': 'cdent@peermore.com',
+    'description':
+        'wsgi_intercept installs a WSGI application in place of a '
+        'real URI for testing.',
+    # What will the name be?
+    'url': 'http://pypi.python.org/pypi/wsgi_intercept',
+    'long_description': open('README').read(),
+    'license': 'MIT License',
+    'classifiers': CLASSIFIERS,
+    'packages': find_packages(),
+    'install_requires': [
+        'six',
+    ],
+    'extras_require': {
+        'testing': [
+            'pytest>=2.4',
+            'httplib2',
+            'requests>=2.0.1',
+            'urllib3>=1.11.0,<2.0.0',
+        ],
+        'docs': [
+            'sphinx',
+        ],
+    },
+}
+
+if __name__ == '__main__':
+    setup(**META)
