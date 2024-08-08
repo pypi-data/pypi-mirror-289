e215697a3501daa672118d4021f89a251a6faad7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-08 05:04:57 +0000

    Add package jelver-cli/1.0.8/jelver_cli-1.0.8.tar.gz

---
+++ packages/jelver-cli/jelver_cli-1.0.8.tar.gz/jelver_cli-1.0.8/tests/test_local_tests.py
@@ -0,0 +1,10 @@
+""" File to test locally running tests """
+
+from src.local_tests import run_tests_locally
+
+
+def test__run_tests_locally__run_successfully():
+    """
+    Make sure tests can run locally.
+    """
+    # TODO: Make function properly
