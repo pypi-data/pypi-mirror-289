32faebeac50acd58000634f11bc798a7f527706b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-08 04:03:57 +0000

    Add package Overlap-BMS-1/0.5/overlap_bms_1-0.5.tar.gz

---
+++ packages/Overlap-BMS-1/overlap_bms_1-0.5.tar.gz/overlap_bms_1-0.5/setup.py
@@ -0,0 +1,17 @@
+
+# setup.py
+from setuptools import setup, find_packages
+
+setup(
+    name='Overlap_BMS_1',
+    version='0.5',
+    packages=find_packages(),
+    install_requires=[
+        'pandas',
+        'numpy',
+        'ipywidgets',
+        'IPython',
+    ],
+    include_package_data=True,
+    description='A library for interactive data analysis with widgets.',
+)
