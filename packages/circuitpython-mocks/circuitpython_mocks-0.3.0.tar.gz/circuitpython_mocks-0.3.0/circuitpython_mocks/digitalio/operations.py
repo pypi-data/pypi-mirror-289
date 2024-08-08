dde34e69b4478f6e8c4db8206608dbfbc3986f33
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-08 05:07:15 +0000

    Add package circuitpython-mocks/0.3.0/circuitpython_mocks-0.3.0.tar.gz

---
+++ packages/circuitpython-mocks/circuitpython_mocks-0.3.0.tar.gz/circuitpython_mocks-0.3.0/circuitpython_mocks/digitalio/operations.py
@@ -0,0 +1,24 @@
+class _State:
+    def __init__(self, state: bool | int) -> None:
+        self.state = bool(state)
+
+    def assert_state(self, value: bool | int):
+        assert self.state is bool(
+            value
+        ), "Expected pin state does not match given pin state"
+
+
+class SetState(_State):
+    """A class to represent setting the state of a
+    :py:class:`~circuitpython_mocks.digitalio.DigitalInOut` pin."""
+
+    def __repr__(self) -> str:
+        return f"<SetState value='{self.state}'>"
+
+
+class GetState(_State):
+    """A class to represent getting the state of a
+    :py:class:`~circuitpython_mocks.digitalio.DigitalInOut` pin."""
+
+    def __repr__(self) -> str:
+        return f"<GetState value='{self.state}'>"
