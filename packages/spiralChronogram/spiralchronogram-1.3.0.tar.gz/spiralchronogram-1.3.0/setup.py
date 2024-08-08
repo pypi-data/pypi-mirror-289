d1448bc2c6e569c8cae5251fc4bcae7c555456ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-08 06:04:52 +0000

    Add package spiralChronogram/1.3.0/spiralchronogram-1.3.0.tar.gz

---
+++ packages/spiralChronogram/spiralchronogram-1.3.0.tar.gz/spiralchronogram-1.3.0/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='spiralChronogram',   
+    version='1.3.0',
+    description='A package to generate various types of 2D representations of data and spiral chronograms.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/thushari93/Spiral_Chronogram',   
+    author='IMSI2024',
+    author_email='jcevall1@asu.edu',
+    license='CC BY-SA 4.0',   
+    install_requires=[
+        'numpy',
+        'pandas',
+        'matplotlib',
+        'seaborn',
+        'scipy',
+        'plotly',
+        'biopython',
+        'ipython'
+    ],
+    entry_points={
+        'console_scripts': [
+            'spiralChronogram=spiralChronogram.main:main',
+        ],
+    },
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
