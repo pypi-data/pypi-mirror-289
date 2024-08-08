01f615c857a1db0bb1beb02216ef041d0d40610f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-08 05:05:14 +0000

    Add package vIQA/1.4.0/viqa-1.4.0-cp311-cp311-win_amd64.whl

---
+++ packages/vIQA/viqa-1.4.0-cp311-cp311-win_amd64.whl/viqa/fr_metrics/__init__.py
@@ -0,0 +1,12 @@
+__all__ = ["FSIM", "GSM", "MAD", "MSSSIM", "PSNR", "UQI", "RMSE", "SSIM", "VIFp", "VSI"]
+
+from .fsim import FSIM
+from .gsm import GSM
+from .mad import MAD
+from .msssim import MSSSIM
+from .psnr import PSNR
+from .uqi import UQI
+from .rmse import RMSE
+from .ssim import SSIM
+from .vif import VIFp
+from .vsi import VSI
