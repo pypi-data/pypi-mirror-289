481096ea69c719b5f81b246f022741af4fe6f626
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-08 06:04:36 +0000

    Add package dicom4ortho/0.3.10.dev0/dicom4ortho-0.3.10.dev0-py3-none-any.whl

---
+++ packages/dicom4ortho/dicom4ortho-0.3.10.dev0-py3-none-any.whl/dicom4ortho/__init__.py
@@ -0,0 +1,11 @@
+""" A package to convert photographs stored in conventional formats (jpeg,
+png, etc) into DICOM.
+
+"""
+
+import dicom4ortho.defaults
+
+__version__ = dicom4ortho.defaults.VERSION
+__url__ = 'https://github.com/open-ortho/dicom4ortho'
+__author__ = 'Toni Magni'
+__email__ = 'amagni@open-ortho.org'
