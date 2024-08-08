e75eb6502c999b8dc234e7242a428b5954617aec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-08 06:04:29 +0000

    Add package itkwasm/1.0b177/itkwasm-1.0b177-py3-none-any.whl

---
+++ packages/itkwasm/itkwasm-1.0b177-py3-none-any.whl/itkwasm/interface_types.py
@@ -0,0 +1,12 @@
+from enum import Enum
+
+
+class InterfaceTypes(Enum):
+    TextFile = "InterfaceTextFile"
+    BinaryFile = "InterfaceBinaryFile"
+    TextStream = "InterfaceTextStream"
+    BinaryStream = "InterfaceBinaryStream"
+    Image = "InterfaceImage"
+    Mesh = "InterfaceMesh"
+    PolyData = "InterfacePolyData"
+    JsonCompatible = "InterfaceJsonCompatible"
