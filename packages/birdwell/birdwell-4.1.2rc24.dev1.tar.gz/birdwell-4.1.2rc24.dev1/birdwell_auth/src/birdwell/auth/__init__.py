e9ab72af74e13c0aca99ebf376172e6efe08975a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-08 14:04:35 +0000

    Add package birdwell/4.1.2rc24.dev1/birdwell-4.1.2rc24.dev1.tar.gz

---
+++ packages/birdwell/birdwell-4.1.2rc24.dev1.tar.gz/birdwell-4.1.2rc24.dev1/birdwell_auth/src/birdwell/auth/__init__.py
@@ -0,0 +1,8 @@
+from .client import AuthClient
+from .middleware import AuthMiddleware
+
+
+__all__ = ["AuthClient", "AuthMiddleware"]
+__version__ = "0.5.1"
+__name__ = "birdwell.auth"
+__title__ = "birdwell.auth"
