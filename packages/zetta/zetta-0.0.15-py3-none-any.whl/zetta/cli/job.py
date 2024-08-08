bad8cb7956f2bed6869d48b5610663743c20d9a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-08 14:03:56 +0000

    Add package zetta/0.0.15/zetta-0.0.15-py3-none-any.whl

---
+++ packages/zetta/zetta-0.0.15-py3-none-any.whl/zetta/cli/job.py
@@ -0,0 +1,56 @@
+# Copyright ZettaBlock Labs 2024
+import typer
+from zetta._utils.async_utils import synchronizer
+from zetta._utils.connections import check_api_status
+
+job_cli = typer.Typer(
+    name="job", help="Manage your jobs in Zetta Workspace.", no_args_is_help=True
+)
+
+@job_cli.command(name="list", help="List all jobs that are currently running.")
+@synchronizer.create_blocking
+async def list(json: bool = False):
+    pass
+
+
+@job_cli.command(name="get", help="get a job status.")
+@synchronizer.create_blocking
+async def get(json: bool = False):
+    pass
+
+
+@job_cli.command(name="update", help="update a job.")
+@synchronizer.create_blocking
+async def update(json: bool = False):
+    pass
+
+@job_cli.command(name="cancel", help="cancel a running job for current user.")
+@synchronizer.create_blocking
+async def cancel(json: bool = False):
+    pass
+
+@job_cli.command(name="status", help="check the status of the Zetta API.")
+@synchronizer.create_blocking
+async def status():
+    return check_api_status()
+
+@job_cli.command(name="init", help="Initialize a new fine-tune job at designated location.")
+@synchronizer.create_blocking
+async def init(framework: str = "llama-factory", location: str = "."):
+    """
+    Download a new fine-tune job config template at designated file location.
+    """
+    pass
+
+@job_cli.command(name="run", help="Create a new fine-tune job with given configuration.")
+@synchronizer.create_blocking
+async def run(config: str):
+    """
+    Submit a new fine-tune job with given config file. Return a job id.
+    """
+    pass
+
+@job_cli.command(name="logs", help="Show the logs of a job with given job id.")
+@synchronizer.create_blocking
+async def logs(job_id: str):
+    pass
