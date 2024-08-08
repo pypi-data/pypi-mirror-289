4550621be6a191f1a122f6ede1b0fdfdb81242c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-08 15:04:38 +0000

    Add package test2_in_august/0.0.0/test2_in_august-0.0.0-py3-none-any.whl

---
+++ packages/test2_in_august/test2_in_august-0.0.0-py3-none-any.whl/test2_in_august/__init__.py
@@ -0,0 +1,13 @@
+from importlib import metadata  # make sure to import metadata explicitly
+
+__version__ = metadata.version(__package__ or __name__)
+
+
+class BaseException(Exception):
+    """Base class for exceptions raised from test2_in_august"""
+
+    def __init__(self, message: str):
+        self._message = message
+
+    def __str__(self) -> str:
+        return f"{self._message}"
