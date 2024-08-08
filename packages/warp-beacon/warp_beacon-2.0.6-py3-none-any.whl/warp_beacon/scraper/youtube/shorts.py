31c93848b9376fe32a52838b30c94123f58fd5db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-08 14:04:11 +0000

    Add package warp-beacon/2.0.6/warp_beacon-2.0.6-py3-none-any.whl

---
+++ packages/warp-beacon/warp_beacon-2.0.6-py3-none-any.whl/warp_beacon/scraper/youtube/shorts.py
@@ -0,0 +1,42 @@
+from warp_beacon.jobs.types import JobType
+from warp_beacon.scraper.youtube.abstract import YoutubeAbstract
+
+from pytubefix import YouTube
+
+import logging
+
+class YoutubeShortsScraper(YoutubeAbstract):
+	YT_MAX_RETRIES_DEFAULT = 8
+	YT_PAUSE_BEFORE_RETRY_DEFAULT = 3
+	YT_TIMEOUT_DEFAULT = 2
+	YT_TIMEOUT_INCREMENT_DEFAULT = 60
+
+	def _download(self, url: str, timeout: int = 0) -> list:
+		res = []
+		thumbnail = None
+		yt = YouTube(url)
+		stream = yt.streams.get_highest_resolution()
+		if yt and yt.thumbnail_url:
+			logging.debug("Generation thumb for Shorts ...")
+			thumbnail = self.download_thumbnail(yt.thumbnail_url)
+		if stream:
+			local_file = stream.download(
+				output_path=self.DOWNLOAD_DIR,
+				max_retries=0,
+				timeout=timeout,
+				skip_existing=False,
+				filename_prefix="yt_download_"
+			)
+			logging.debug("Temp filename: '%s'", local_file)
+			res.append({
+				"local_media_path": self.rename_local_file(local_file),
+				"performer": yt.author,
+				"thumb": thumbnail,
+				"canonical_name": stream.title,
+				"media_type": JobType.VIDEO
+			})
+
+		return res
+
+	def download(self, url: str) -> list:
+		return self._download_hndlr(self._download, url)
