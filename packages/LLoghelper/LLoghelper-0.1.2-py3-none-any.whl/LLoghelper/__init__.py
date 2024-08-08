69ae2f574f967e0302a6aec166f596501a1195bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-08 14:04:11 +0000

    Add package LLoghelper/0.1.2/LLoghelper-0.1.2-py3-none-any.whl

---
+++ packages/LLoghelper/LLoghelper-0.1.2-py3-none-any.whl/LLoghelper/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+"""log_helper.py is a helper module for logging in Python. It provides a set of functions and classes for logging in Python."""
+
+__author__ = "atgczcl"
+__email__ = "atgczcl@163.com"
+# __all__ = ["log_helper"]
+# 导入模块
+# import log_helper
+# from pack import *
