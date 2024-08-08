9ca3e9f18cd13dce5792a21b87d4189c5582f9fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-08 05:04:15 +0000

    Add package whataspp-message-inforu-local/0.0.25/whataspp_message_inforu_local-0.0.25-py3-none-any.whl

---
+++ packages/whataspp-message-inforu-local/whataspp_message_inforu_local-0.0.25-py3-none-any.whl/whataspp_message_inforu_local/handler.py
@@ -0,0 +1,212 @@
+import json
+from logger_local.LoggerLocal import Logger
+from python_sdk_local.http_response import handler_decorator
+from python_sdk_local.utilities import DEFAULT_LANG_CODE_STR
+from message_local.MessageLocal import MessageLocal
+from database_mysql_local.generic_crud_ml import GenericCRUDML
+from message_local.CompoundMessage import CompoundMessage
+from phones_local.phones_local import PhonesLocal
+from profile_local.profiles_local import ProfilesLocal
+
+from .WhatsAppLocalConstants import get_logger_object
+from .WhatsAppMessageInforuLocal import WhatsAppMessageInforuLocal
+
+WHATSAPP_CHANNEL_ID = 11
+
+logger_object = get_logger_object()
+logger = Logger.create_logger(object=logger_object)
+
+#TODO Add calls to the logger
+
+#TODO What is the capabilities of this?
+# @handler_decorator(logger)
+#TODO insertMessageRecievedToTheDatabase
+#TODO request_parameters_dict
+def createHandler(request_parameters: dict) -> list[int]:
+    # In all serverless-com handlers' logger.start()- Add optional parameter
+    # to logger.start() which called api_call, so logger.start will call
+    # api_management to insert into api_call_table all fields including
+    # session_id.
+    """Read parameters from the payload and insert them"""
+    logger.start(object={"request_parameters": request_parameters})
+    generic_crud_ml_instance = GenericCRUDML(default_schema_name="message")
+    message_data_list: list[dict] = __extract_message_data(request_parameters)
+    messages_ids_list: list = []
+    for message_data in message_data_list:
+        message_id = generic_crud_ml_instance.insert(
+            schema_name="message", table_name="message_table",
+            data_dict=message_data)
+        if message_id is not None:
+            messages_ids_list.append(message_id)
+    if len(messages_ids_list) == 0:
+        raise Exception("Message/Messages were not created")
+    result_message = {"message": "Message/Messages created successfully", "messages_ids_list": messages_ids_list}
+    logger.end(object={"result_message": result_message})
+    return result_message
+
+"""
+Example of request_parameters:
+{
+    "CustomerId":28132,
+    "ProjectId":1042593,
+    "Data":[
+        {
+            "Channel":"SMS_MO",
+            "Type":"PhoneNumber",
+            "Value":"0522222229",
+            "Keyword":"wt1",
+            "Message":"wt1 ",
+            "Network":"WhatsApp",
+            "ShortCode":"+97233769182",
+            "ApplicationID":"11542",
+            "CustomerParam":"SMf14104210035723ecab943567b4ad4b5",
+            "MoSessionId":"SMf14104210035723ecab943567b4ad4b5"
+        }
+    ]
+}
+"""
+def __extract_message_data(request_parameters: dict) -> list[dict]:
+    """Extract message data from the payload"""
+    logger.start(object={"request_parameters": request_parameters})
+    message_data_list: list[dict] = []
+    message_customer_id = request_parameters.get("CustomerId")
+    message_project_id = request_parameters.get("ProjectId")
+    phones_local = PhonesLocal()
+    profiles_local = ProfilesLocal()
+
+    request_parameters_data: list[dict] = request_parameters.get("Data")
+    if not request_parameters_data:
+        logger.end(object={"message_data_list": message_data_list})
+        return []
+    for request_parameters_data_item in request_parameters_data:
+        message_data: dict = {}
+        message_data["inforu_customer_id"] = message_customer_id
+        message_data["inforu_project_id"] = message_project_id
+        message_data["inforu_type"] = request_parameters_data_item.get("Type")
+        message_data["body"] = request_parameters_data_item.get("Message")
+        message_data["inforu_channel"] = request_parameters_data_item.get("Channel")
+        message_data["actual_channel_id"] = WHATSAPP_CHANNEL_ID
+        message_data["inforu_network"] = request_parameters_data_item.get("Network")
+        message_data["inforu_incoming_json"] = json.dumps(request_parameters)
+        receiver_phone_number = request_parameters_data_item.get("ShortCode")
+        message_data["inforu_short_code"] = receiver_phone_number
+        message_data["inforu_application_id"] = request_parameters_data_item.get("ApplicationID")
+        message_data["inforu_customer_param"] = request_parameters_data_item.get("CustomerParam")
+        message_data["inforu_mo_session_id"] = request_parameters_data_item.get("MoSessionId")
+        message_data["inforu_keyword"] = request_parameters_data_item.get("Keyword")
+        sender_phone_number = request_parameters_data_item.get("Value")
+        message_data["inforu_value"] = sender_phone_number
+        number_info = PhonesLocal.normalize_phone_number(original_number=sender_phone_number, region="IL")
+        sender_normalized_phone_number = number_info.get("full_number_normalized")
+        # Replace the leading 0 in the phone number by 972 if there's a leading 0
+        if sender_normalized_phone_number.startswith("0"):
+            sender_normalized_phone_number = "972" + sender_normalized_phone_number[1:]
+        sender_phone_id = phones_local.get_phone_id_by_full_number_normalized(full_number_normalized=sender_normalized_phone_number)
+        # Get Sender profile id
+        sender_profile_id = profiles_local.select_one_value_by_column_and_value(
+            select_clause_value="profile_id", column_name="main_phone_id", column_value=sender_phone_id
+        )
+        if sender_profile_id is None:
+            # Create and insert a new profile
+            profile_dict = {
+                "profile.name":  sender_phone_number,
+            }
+            sender_profile_id = profiles_local.insert(profile_dict=profile_dict)
+        message_data["sender_profile_id"] = sender_profile_id
+        # Get receiver normalized phone number
+        number_info = PhonesLocal.normalize_phone_number(
+            original_number=receiver_phone_number, region="IL")
+        receiver_normalized_phone_number = number_info.get("full_number_normalized")
+        # Remove leading #
+        if receiver_normalized_phone_number.startswith("#"):
+            receiver_normalized_phone_number = receiver_normalized_phone_number[1:]
+
+        # Get receiver phone id
+        receiver_phone_id = phones_local.get_phone_id_by_full_number_normalized(full_number_normalized=receiver_phone_number)
+        # Get receiver profile id
+        receiver_profile_id = profiles_local.select_one_value_by_column_and_value(
+            select_clause_value="profile_id", column_name="main_phone_id", column_value=receiver_phone_id
+        )
+        message_data["to_profile_id"] = receiver_profile_id
+        message_data_list.append(message_data)
+
+    logger.end(object={"message_data_list": message_data_list})
+    return message_data_list
+
+# TODO: except ValueError as exception, BAD_REQUEST / INTERNAL_SERVER_ERROR...
+
+
+# @handler_decorator(logger)
+# def deleteHandler(request_parameters: dict) -> dict:
+#     """url/5   (5 is the eventId)"""
+
+#     message_id = request_parameters.get('eventId')
+#     is_test_data = request_parameters.get("isTestData", False)
+#     MessageLocal().delete_by_message_id(message_id=message_id)
+#     message = {"message": "Message deleted successfully"}
+#     return message
+
+
+# TODO This should be imported from entity-type-python-package (this file will be generated by Sql2Code)
+# @handler_decorator(logger)
+# def getAllHandler(request_parameters: dict) -> list:
+#     """url?langCode=en&limit=10"""
+
+#     lang_code_str = request_parameters.get("langCode") or DEFAULT_LANG_CODE_STR
+#     limit = request_parameters.get("limit")
+#     is_test_data = request_parameters.get("isTestData", False)
+#     messages_list = MessageLocal().select_all_messages(lang_code_str, limit=limit)
+#     return messages_list
+
+
+# @handler_decorator(logger)
+# def getMessageByProfileIdHandler(request_parameters: dict) -> list:
+#     """url/5?limit=10   (5 is the profileId)"""
+
+#     request_parameters = request_parameters
+#     profile_id = request_parameters.get("profileId")
+#     limit = request_parameters.get("limit")
+
+#     is_test_data = request_parameters.get("isTestData", False)
+#     messages_list = MessageLocal().select_messages_by_profile_id(profile_id, limit=limit)
+#     return messages_list
+
+
+# @handler_decorator(logger)
+# def getMessageByeventIdHandler(request_parameters: dict) -> dict:
+#     """url/5?isTestData=true   (eventId)"""
+
+#     message_id = request_parameters.get("eventId")
+
+#     is_test_data = request_parameters.get("isTestData", False)
+#     message = MessageLocal().select_by_message_id(message_id)
+#     if not message:
+#         raise Exception(f"Message not found with message_id: {message_id}")
+#     return message
+
+
+# @handler_decorator(logger)
+# def updateHandler(request_parameters: dict) -> dict:
+#     """url/5  (eventId)
+#     Read parameters from the payload and update them"""
+
+#     message_id = request_parameters.get('eventId')
+#     is_test_data = request_parameters.get("isTestData", False)
+#     if not message_id:
+#         raise Exception("message_id is required")
+
+#     MessageLocal().update_by_message_id(message_id, request_parameters)
+#     message = {"message": "Message updated successfully"}
+#     return message
+
+
+# @handler_decorator(logger)
+# def getMessageByMessageTitleHandler(request_parameters: dict) -> list:  # TODO: test
+#     """url/SomeTitle?limit=10"""
+
+#     title = request_parameters.get("title")
+
+#     limit = request_parameters.get("limit")
+#     is_test_data = request_parameters.get("isTestData", False)
+#     messages_list = MessageLocal().select_events_by_title(title, limit=limit)
+#     return messages_list
