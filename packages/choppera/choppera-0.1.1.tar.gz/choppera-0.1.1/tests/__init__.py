03e67dc6c2b664decc1ad1ce84953c4b55aa041c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-08 16:05:12 +0000

    Add package choppera/0.1.1/choppera-0.1.1.tar.gz

---
+++ packages/choppera/choppera-0.1.1.tar.gz/choppera-0.1.1/tests/__init__.py
@@ -0,0 +1,3 @@
+# SPDX-FileCopyrightText: 2024-present Gregory Tucker <gregory.tucker@ess.eu>
+#
+# SPDX-License-Identifier: BSD-3-Clause
---
+++ packages/choppera/choppera-0.1.1.tar.gz/choppera-0.1.1/tests/flightpaths_test.py
@@ -0,0 +1,26 @@
+def name_velocity_nominal(velocity=None):
+    from scipp import scalar, array
+    if velocity is None:
+        velocity = array(values=[100, 1e9], unit='m/s', dims=['wavelength'])
+    nominal = array(values=[3.14, 3.14], unit='m', dims=['wavelength'])
+    return 'Path', velocity, nominal
+
+
+def make_flightpath(velocity=None):
+    from choppera.flightpaths import FlightPath
+    return FlightPath(*name_velocity_nominal(velocity))
+
+
+def test_flightpath_creation():
+    from scipp import allclose
+    name, velocity, nominal = name_velocity_nominal()
+    flightpath = make_flightpath()
+    assert flightpath.name == name
+    assert allclose(flightpath.velocity, velocity)
+    assert allclose(flightpath.nominal, nominal)
+
+
+def test_flightpath_equivalence():
+    flightpath = make_flightpath()
+    flightpath2 = make_flightpath()
+    assert flightpath == flightpath2
