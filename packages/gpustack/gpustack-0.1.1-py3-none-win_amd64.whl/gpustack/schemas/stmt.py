7eb35dd60467676978a5ab40387ecfc281db904d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-08 17:04:04 +0000

    Add package gpustack/0.1.1/gpustack-0.1.1-py3-none-win_amd64.whl

---
+++ packages/gpustack/gpustack-0.1.1-py3-none-win_amd64.whl/gpustack/schemas/stmt.py
@@ -0,0 +1,21 @@
+worker_after_create_stmt = """
+CREATE VIEW IF NOT EXISTS gpu_devices_view AS
+SELECT
+    w.name || '-' || json_extract(value, '$.index') AS id,
+    w.id as worker_id,
+    w.name as worker_name,
+    w.ip as worker_ip,
+    w.created_at,
+    w.updated_at,
+    w.deleted_at,
+    json_extract(value, '$.uuid') AS uuid,
+    json_extract(value, '$.name') AS name,
+    json_extract(value, '$.vendor') AS vendor,
+    json_extract(value, '$.index') AS 'index',
+    json_extract(value, '$.core') AS core,
+    json_extract(value, '$.memory') AS memory,
+    json_extract(value, '$.temperature') AS temperature
+FROM
+    workers w,
+    json_each(w.status, '$.gpu_devices')
+"""
