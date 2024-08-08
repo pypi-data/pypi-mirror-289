2e17e24774909473e00df624c33ac065a593260f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-08 03:03:37 +0000

    Add package modfutugrpc/0.1/modfutugrpc-0.1.tar.gz

---
+++ packages/modfutugrpc/modfutugrpc-0.1.tar.gz/modfutugrpc-0.1/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+exclude_pkgs = [
+  'grpcio-tools',
+  'protobuf',
+  'pytest',
+  'setuptools',
+]
+# with open('./requirements.txt') as f:
+#   requirements = f.read().splitlines()
+# requirements = [x for x in requirements if not any([y for y in exclude_pkgs if y in x])]
+setup(
+  name='modfutugrpc',
+  version='0.1',
+  packages=find_packages('src', exclude=exclude_pkgs),
+  package_dir={'': 'src'},
+  # install_requires=requirements,
+)
