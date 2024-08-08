f85bd69e79510c7b6c8c1b45e057e5e9d7ddb2e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-08 14:04:05 +0000

    Add package Mensajes-gregory97/6.0/mensajes_gregory97-6.0.tar.gz

---
+++ packages/Mensajes-gregory97/mensajes_gregory97-6.0.tar.gz/mensajes_gregory97-6.0/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="Mensajes-gregory97",
+    version="6.0",
+    description="Un paquete para saludar y despedir",
+    long_description=open("README.md").read(),
+    long_description_content_type='text/markdown',
+    author= "Gregory Ramirez",
+    author_email="hola@gregory.dev",
+    url="https://www.gregory.dev",
+    license_files=['LICENSE'],
+    packages=find_packages(),
+    scripts=[],
+    test_suite='tests',
+    install_requires=[paquete.strip() 
+                      for paquete in open("requirements.txt").readlines()],
+    classifiers=[
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.12',
+        'Topic :: Utilities',
+    ]
+)
+
+
