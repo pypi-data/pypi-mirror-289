3c7c284c4c3984e71c176289ef4a9101da5507a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-08 03:04:09 +0000

    Add package spatialdata/0.2.2/spatialdata-0.2.2-py3-none-any.whl

---
+++ packages/spatialdata/spatialdata-0.2.2-py3-none-any.whl/spatialdata/_types.py
@@ -0,0 +1,19 @@
+from __future__ import annotations
+
+from typing import Union
+
+import numpy as np
+from datatree import DataTree
+from xarray import DataArray
+
+__all__ = ["ArrayLike", "DTypeLike", "Raster_T"]
+
+try:
+    from numpy.typing import DTypeLike, NDArray
+
+    ArrayLike = NDArray[np.float64]
+except (ImportError, TypeError):
+    ArrayLike = np.ndarray  # type: ignore[misc]
+    DTypeLike = np.dtype  # type: ignore[misc]
+
+Raster_T = Union[DataArray, DataTree]
