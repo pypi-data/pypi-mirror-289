7be9b278164faa8ed891ec344cebb6ae19a262da
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-08 14:04:11 +0000

    Add package alas-ce0-client/5.0.93/alas_ce0_client-5.0.93-py2.py3-none-any.whl

---
+++ packages/alas-ce0-client/alas_ce0_client-5.0.93-py2.py3-none-any.whl/alas_ce0/task/data_job.py
@@ -0,0 +1,13 @@
+from alas_ce0.common.client_base import EntityClientBase
+
+
+class DataJobClient(EntityClientBase):
+    entity_endpoint_base_url = '/task/data-jobs/'
+
+    def get_content(self, id):
+        return self.http_get(
+            self.entity_endpoint_base_url + "{0}/_content".format(id)
+        )
+
+    def task_handler(self, params):
+        return self.http_post_json(self.entity_endpoint_base_url + "_task-handler", params)
