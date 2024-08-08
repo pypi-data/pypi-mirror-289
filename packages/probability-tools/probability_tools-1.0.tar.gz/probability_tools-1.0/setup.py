9beb37dfacc5965d3ab6d807fbc65c064c8c54c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-08 03:04:01 +0000

    Add package probability-tools/1.0/probability_tools-1.0.tar.gz

---
+++ packages/probability-tools/probability_tools-1.0.tar.gz/probability_tools-1.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='probability_tools',
+      version='1.0',
+      description='Gaussian and binomial distributions',
+      packages=['probability_tools'],
+      author='Hesam Najafi',
+      author_email='hesamnajafi39@gmail.com',
+      zip_safe=False)
