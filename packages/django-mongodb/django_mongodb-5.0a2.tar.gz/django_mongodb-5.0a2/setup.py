52a91eb51260ef4af22fd006f3801e0773979642
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-08 03:04:01 +0000

    Add package django-mongodb/5.0a2/django_mongodb-5.0a2.tar.gz

---
+++ packages/django-mongodb/django_mongodb-5.0a2.tar.gz/django_mongodb-5.0a2/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+import os
+import codecs
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+    long_description = "\n" + fh.read()
+
+setup(
+    name="django-mongodb",
+    version="5.0a2",
+    author="AnupamAs0x1",
+    author_email="your.email@example.com",
+    description="MongoDB support for Django",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    install_requires=[
+        'requests',  # List other dependencies here
+    ],
+    classifiers=[
+        "Development Status :: 3 - Alpha"
+    ],
+    keywords="django mongodb",
+)
