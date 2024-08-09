5f7c9b5423ed514b39c5a1f386a122e0483484ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-09 07:04:47 +0000

    Add package aioopenexchangerates/0.4.16/aioopenexchangerates-0.4.16-py3-none-any.whl

---
+++ packages/aioopenexchangerates/aioopenexchangerates-0.4.16-py3-none-any.whl/aioopenexchangerates/model/latest.py
@@ -0,0 +1,7 @@
+"""Provide a model for endpoint latest.json."""
+
+from .response import BaseRatesResponse
+
+
+class Latest(BaseRatesResponse):
+    """Represent the model for endpoint latest.json."""
