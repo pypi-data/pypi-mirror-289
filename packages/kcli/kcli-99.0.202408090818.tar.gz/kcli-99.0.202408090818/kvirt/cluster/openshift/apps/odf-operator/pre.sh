f4c3444b779748f9d45d39936bd89d44280d7a17
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-09 15:05:24 +0000

    Add package kcli/99.0.202408090818/kcli-99.0.202408090818.tar.gz

---
+++ packages/kcli/kcli-99.0.202408090818.tar.gz/kcli-99.0.202408090818/kvirt/cluster/openshift/apps/odf-operator/pre.sh
@@ -0,0 +1,21 @@
+{{ "storagecluster" | wait_crd }}
+sleep 10
+{% if not odf_nodes %}
+{% set odf_nodes = odf_replicas|defaultnodes(cluster, domain, ctlplanes, workers) %}
+{% endif %}
+
+{% if odf_nodes|length < odf_replicas %}
+echo "Number of available nodes is lower than expected number of replicas"
+exit 1
+{% endif %}
+
+{% if odf_nodes|has_ctlplane %}
+echo "Marking all ctlplane nodes as schedulable since one of them will be used for storage"
+oc patch scheduler cluster -p '{"spec":{"mastersSchedulable": true}}' --type merge
+{% endif %}
+
+
+{% for node in odf_nodes %}
+oc label node {{ node }} cluster.ocs.openshift.io/openshift-storage=''
+oc label node {{ node }} topology.rook.io/rack=rack{{ loop.index }}
+{% endfor %}
