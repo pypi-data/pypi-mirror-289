4ae3ac9d06dcdc7a2fc1771fc0dc3cfdfe3f7e5b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-09 03:05:44 +0000

    Add package informatikunterricht/0.1.0/informatikunterricht-0.1.0.tar.gz

---
+++ packages/informatikunterricht/informatikunterricht-0.1.0.tar.gz/informatikunterricht-0.1.0/setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='informatikunterricht',
+    version='0.1.0',
+    author='Henning Mattes',
+    author_email='henning_mattes@gmx.de',
+    description='Ein Paket für den Informatikunterricht, das Module zur Bildverarbeitung und Diagrammerstellung enthält.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/IhrGitHub/informatikunterricht',
+    packages=find_packages(),
+    install_requires=[
+        'matplotlib',  # Da beide Module matplotlib verwenden, nehmen wir es in die Installationsanforderungen auf.
+        'numpy',       # Wenn bildverarbeitung NumPy verwendet.
+        'Pillow'       # Wenn bildverarbeitung PIL/Pillow verwendet.
+    ],
+    license='MIT with additional terms',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Education',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Topic :: Education',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ],
+    keywords='bildung informatik bildverarbeitung diagramme informatikunterricht',
+    package_data={
+        '': ['LICENSE', 'README.md']
+    },
+    include_package_data=True,
+    python_requires='>=3.7',
+)
