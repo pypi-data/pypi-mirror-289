ba120ce1320681b7b647486c9e4ac7d7167aea05
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-09 07:04:49 +0000

    Add package obstechutils/0.1.32/obstechutils-0.1.32.tar.gz

---
+++ packages/obstechutils/obstechutils-0.1.32.tar.gz/obstechutils-0.1.32/src/obstechutils/db/weather.py
@@ -0,0 +1,185 @@
+from __future__ import annotations
+
+from obstechutils.db import DataBase
+from obstechutils.mqtt import MQTTClient
+from obstechutils.dataclasses import strictdataclass, Field
+from obstechutils import logging
+
+import numpy as np
+import json
+import argparse
+
+def data_as_sql(v):
+    """Ensure valid values are sent to SQL the database. Check tables
+    all accept NULL!!"""
+    if isinstance(v, str):
+        return f"'{v}'"
+    if v is None or not np.isfinite(v):
+        return "NULL"
+    return f"{v}"
+
+@strictdataclass
+class WeatherDataBaseWriter(MQTTClient):
+
+    database: DataBase | None = None
+    table_name: str = 'weather'
+    
+    @classmethod
+    def from_credentials(
+            cls, 
+            mqtt_user: str, 
+            database_user: str, 
+            database_name: str,
+            topics: str,
+            table_name: str,
+            **kwargs,
+    ) -> WeatherDataBaseWriter:
+
+        database = None
+        if database_name:
+            database = DataBase.from_credentials(
+                user=database_user,
+                database=database_name,    
+            )
+        
+        writer = super().from_credentials(
+            user=mqtt_user, topics=topics, 
+            database=database, table_name=table_name,
+            **kwargs,
+        )
+
+        return writer
+
+    def write(self, data: dict[str, object]) -> bool:
+        
+        names = list(data.keys())
+        values = [data_as_sql(v) for v in data.values()]
+        command = f"""
+            INSERT INTO {self.table_name} ({', '.join(names)})
+            VALUES 
+            ({', '.join(values)})
+        """
+       
+        try:  
+
+            if self.database is not None:
+                
+                if self.table_name == 'weather_test':   
+                    print(command)
+                
+                with self.database.connect() as conn:
+                    cursor = conn.cursor()
+                    cursor.execute(command)
+                    conn.commit()
+             
+            else:
+                print(command)
+
+        except Exception as e:
+
+            logger = logging.getLogger()
+            logger.error(f'error writing to table {self.table_name}: {e}')
+            return False 
+
+        else: 
+
+            return True
+
+    def on_message(self, client, data, message) -> None: 
+
+        logger = logging.getLogger()
+        print('message')
+
+        topic = message.topic
+
+        try:
+            payload = message.payload.decode()
+            data = json.loads(payload)
+        except Exception as e:
+            logger.error(f'could not process MQTT message payload: ignored')
+            logger.error(f'error is: {e}')
+            return
+
+        ok = self.write(data)
+        
+        if ok and self.default_publish_topic:
+            self.publish(payload='OK', qos=1)
+        
+        logger.debug(f'received message on {topic}: {payload}') 
+
+def update_database(
+    database_user='generic_obstech',
+    database_name='',
+    topic='weatherdata',
+    publish_topic='update_weather_database',
+    table='weather',
+):
+    
+    writer = WeatherDataBaseWriter.from_credentials(
+        database_user=database_user,
+        database_name=database_name,
+        table_name=table,
+        mqtt_user='generic_obstech',
+        topics=[f'/ElSauce/Weather/{topic}'],
+        default_publish_topic=f'/ElSauce/Weather/{publish_topic}'
+    )
+    print(f"{writer=}")
+    writer.connect()
+    writer.loop_forever()
+    
+# ENTRY points for the four tables and two databases
+  
+def test_update():
+    update_database( 
+        database_user='generic_obstech',
+        database_name='ElSauceWeather', table='weather_test',
+        topic='weatherdata', publish_topic='test_update'
+    )
+def update_weather_sql_table():
+    update_database(
+        database_user='generic_obstech',
+        database_name='ElSauceWeather', table='weather',
+        topic='weatherdata', publish_topic='update_weather_sql_table'
+    )
+def update_dimm_sql_table():
+    update_database(
+        database_user='generic_obstech',
+        database_name='ElSauceWeather', table='dimm',
+        topic='Dimm', publish_topic='update_dimm_sql_table'
+    )
+def update_ninox_sql_table():
+    update_database(
+        database_user='generic_obstech',
+        database_name='ElSauceWeather', table='ninox',
+        topic='Ninox', publish_topic='update_ninox_sql_table'
+    )
+def update_solar_seeing_sql_table():
+    update_database(
+        database_user='generic_obstech',
+        database_name='ElSauceWeather', table='Solar_seeing',
+        topic='SolarSeeing', publish_topic='update_solar_seeing_sql_table'
+    )
+def update_weather_sql_rtable():
+    update_database(
+        database_user='remote_obstech',
+        database_name='obstecho_test', table='obstecho_test.weather',
+        topic='weatherdata', publish_topic='update_weather_sql_rtable'
+    )
+def update_dimm_sql_rtable():
+    update_database(
+        database_user='remote_obstech',
+        database_name='obstecho_test', table='obstecho_test.dimm',
+        topic='Dimm', publish_topic='update_dimm_sql_rtable'
+    )
+def update_ninox_sql_rtable():
+    update_database(
+        database_user='remote_obstech',
+        database_name='obstecho_test', table='obstecho_testninox',
+        topic='Ninox', publish_topic='update_ninox_sql_rtable'
+    )
+def update_solar_seeing_sql_rtable():
+    update_database(
+        database_user='remote_obstech',
+        database_name='obstecho_test', table='obstecho_test.ninox',
+        topic='SolarSeeing', publish_topic='update_solar_seeing_sql_rtable'
+    )
