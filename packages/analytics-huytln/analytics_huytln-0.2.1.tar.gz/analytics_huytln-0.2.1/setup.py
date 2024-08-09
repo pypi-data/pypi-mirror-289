fdba2555aaaab9f4ad778a6fd86c3ef0c943c0e4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-09 15:05:17 +0000

    Add package analytics-huytln/0.2.1/analytics_huytln-0.2.1.tar.gz

---
+++ packages/analytics-huytln/analytics_huytln-0.2.1.tar.gz/analytics_huytln-0.2.1/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+# Đọc nội dung của README.md
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name='analytics_huytln',
+    version='0.2.1',
+    packages=find_packages(),
+    install_requires=[
+        'pandas',
+        'matplotlib',
+    ],
+    description='A simple library to plot Insightful charts',
+    long_description=long_description,  # Thêm mô tả dài
+    long_description_content_type="text/markdown",  # Định dạng của README.md
+    author='Huy Trịnh Lê Nhật',
+    author_email='trinhlenhathuy@gmail.com',
+    # url='https://github.com/trinhlenhathuy/huytln_visualization',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
+
