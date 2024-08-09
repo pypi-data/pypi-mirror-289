ad73714b2b395beb6463ef19832477081403749b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-09 03:04:31 +0000

    Add package PyADScopeControl/1.1.7/pyadscopecontrol-1.1.7.tar.gz

---
+++ packages/PyADScopeControl/pyadscopecontrol-1.1.7.tar.gz/pyadscopecontrol-1.1.7/src/ADScopeControl/controller/mp_AD2Capture/MPCaptDeviceSceleton.py
@@ -0,0 +1,7 @@
+class MPCaptDeviceSceleton():
+
+    def discover_connected_devices(self):
+        """
+        This function is called to discover the connected devices.
+        :return:
+        """
