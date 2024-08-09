48f589e11e14918f1f4b442998962f611cc8e404
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-09 16:04:46 +0000

    Add package quickex-sdk/1.0.9/quickex_sdk-1.0.9.tar.gz

---
+++ packages/quickex-sdk/quickex_sdk-1.0.9.tar.gz/quickex_sdk-1.0.9/publish.sh
@@ -0,0 +1,3 @@
+rm -rf dist/
+hatch build
+hatch publish
---
+++ packages/quickex-sdk/quickex_sdk-1.0.9.tar.gz/quickex_sdk-1.0.9/src/quickex_sdk/__init__.py
@@ -0,0 +1,5 @@
+# SPDX-FileCopyrightText: 2024-present hakerkiler <vinatorul2@gmail.com>
+#
+# SPDX-License-Identifier: MIT
+from .models import *
+from .sdk import QuickexSDK
