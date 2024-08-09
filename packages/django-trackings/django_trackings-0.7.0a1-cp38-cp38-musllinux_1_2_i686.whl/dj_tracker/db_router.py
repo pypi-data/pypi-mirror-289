56244de32af11f7bff9f9229a2f72cd953003684
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-09 04:04:10 +0000

    Add package django-trackings/0.7.0a1/django_trackings-0.7.0a1-cp38-cp38-musllinux_1_2_i686.whl

---
+++ packages/django-trackings/django_trackings-0.7.0a1-cp38-cp38-musllinux_1_2_i686.whl/dj_tracker/db_router.py
@@ -0,0 +1,26 @@
+from django.apps import apps
+
+from dj_tracker.cache_utils import lazy_attribute
+
+
+class DjTrackerRouter:
+    app_label = "dj_tracker"
+    db_alias = "trackings"
+
+    @lazy_attribute
+    def models(cls):
+        return frozenset(
+            apps.get_app_config(cls.app_label).get_models(include_auto_created=True)
+        )
+
+    def db_for_read(self, model, **hints):
+        if model in self.models:
+            return self.db_alias
+
+    db_for_write = db_for_read
+
+    def allow_migrate(self, db, app_label, **hints):
+        if app_label == self.app_label:
+            return db == self.db_alias
+        if db == self.db_alias:
+            return False
