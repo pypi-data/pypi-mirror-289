0643aa028b9c1aa07e9e0dfa520194f35e905122
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-09 03:05:56 +0000

    Add package fh-matplotlib/0.0.4/fh-matplotlib-0.0.4.tar.gz

---
+++ packages/fh-matplotlib/fh-matplotlib-0.0.4.tar.gz/fh-matplotlib-0.0.4/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+from pathlib import Path
+
+long_description = (Path(__file__).parent / "README.md").read_text()
+
+setup(
+    name="fh-matplotlib",
+    description="Make is easy to use matplotlib in FastHTML",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="Vincent D. Warmerdam",
+    version="0.0.4",
+    packages=find_packages(),
+    install_requires=["matplotlib", "python-fasthtml"],
+    extras_require={
+        "dev": ["pytest"],
+    },
+)
