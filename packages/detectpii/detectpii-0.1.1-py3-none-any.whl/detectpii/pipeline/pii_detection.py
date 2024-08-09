aefd47201bd05328a467fca698b4a563c1d5161f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-09 15:05:20 +0000

    Add package detectpii/0.1.1/detectpii-0.1.1-py3-none-any.whl

---
+++ packages/detectpii/detectpii-0.1.1-py3-none-any.whl/detectpii/pipeline/pii_detection.py
@@ -0,0 +1,20 @@
+from attr import define, Factory
+
+from detectpii.catalog import CatalogT
+from detectpii.model import PiiColumn
+from detectpii.scanner import ScannerT
+
+
+@define(kw_only=True)
+class PiiDetectionPipeline:
+    catalog: CatalogT
+    scanners: list[ScannerT] = Factory(list)
+
+    def scan(self) -> list[PiiColumn]:
+        self.catalog.detect_tables()
+        pii_columns = []
+
+        for scanner in self.scanners:
+            pii_columns.extend(scanner.scan(catalog=self.catalog))
+
+        return list(set(pii_columns))
