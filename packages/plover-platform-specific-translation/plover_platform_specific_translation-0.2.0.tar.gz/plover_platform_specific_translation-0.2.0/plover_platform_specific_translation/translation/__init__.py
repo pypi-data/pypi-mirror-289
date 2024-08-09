013abe2cd92092160ee3cb64322798adc99ecffe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-09 16:04:52 +0000

    Add package plover-platform-specific-translation/0.2.0/plover_platform_specific_translation-0.2.0.tar.gz

---
+++ packages/plover-platform-specific-translation/plover_platform_specific_translation-0.2.0.tar.gz/plover_platform_specific_translation-0.2.0/plover_platform_specific_translation/translation/__init__.py
@@ -0,0 +1,13 @@
+"""
+# Translation
+
+A package dealing with:
+    - resolving the correct translation from a platform-specific outline value
+"""
+from .resolver import COMBO, COMMAND, resolve
+
+__all__ = [
+    "COMBO",
+    "COMMAND",
+    "resolve"
+]
