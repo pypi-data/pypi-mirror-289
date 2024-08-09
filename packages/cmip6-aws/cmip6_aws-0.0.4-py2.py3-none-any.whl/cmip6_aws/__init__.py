ddb534d9bc18440be62c36ad1c62ceaffe1e9efb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-09 03:05:56 +0000

    Add package cmip6-aws/0.0.4/cmip6_aws-0.0.4-py2.py3-none-any.whl

---
+++ packages/cmip6-aws/cmip6_aws-0.0.4-py2.py3-none-any.whl/cmip6_aws/__init__.py
@@ -0,0 +1,13 @@
+"""Top-level package for cmip6_aws."""
+
+__author__ = """advancehs"""
+__email__ = "1019753743@qq.com"
+__version__ = "0.0.4"
+
+
+from . import cmip6_aws
+
+
+__all__ = [
+    'cmip6_aws',
+]
