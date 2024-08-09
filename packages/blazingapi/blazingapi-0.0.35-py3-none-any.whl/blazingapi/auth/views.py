aec7debc068b7a5d6dc380ab8690b7637207f5ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-09 05:05:02 +0000

    Add package blazingapi/0.0.35/blazingapi-0.0.35-py3-none-any.whl

---
+++ packages/blazingapi/blazingapi-0.0.35-py3-none-any.whl/blazingapi/auth/views.py
@@ -0,0 +1,35 @@
+from blazingapi.app import app
+from blazingapi.auth.models import User
+from blazingapi.auth.permissions import IsAuthenticated
+from blazingapi.response import Response
+from blazingapi.settings import settings
+
+
+@app.post(settings.LOGIN_ENDPOINT)
+def login(request):
+
+    if request.user.is_authenticated:
+        return Response(body=request.user, status=200)
+
+    username = request.data["username"]
+    password = request.data["password"]
+
+    user = User.manager.get(username=username)
+
+    if user.check_password(password):
+        return Response(body=user, status=200)
+
+    return Response(body={"error": "Invalid credentials"}, status=401)
+
+
+@app.post(settings.REGISTER_ENDPOINT)
+def register(request):
+    user = User(username=request.data['username'], email=request.data['email'])
+    user.set_password(request.data["password"])
+    user.save()
+    return Response(body=user, status=201)
+
+
+@app.get(settings.ME_ENDPOINT, permission_classes=[IsAuthenticated])
+def me(request):
+    return Response(body=request.user, status=200)
