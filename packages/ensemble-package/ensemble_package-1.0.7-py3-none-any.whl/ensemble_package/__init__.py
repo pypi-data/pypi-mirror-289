88ee2b9dadc33657654eca7e5aa9b1dbce330447
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-09 04:04:44 +0000

    Add package ensemble-package/1.0.7/ensemble_package-1.0.7-py3-none-any.whl

---
+++ packages/ensemble-package/ensemble_package-1.0.7-py3-none-any.whl/ensemble_package/__init__.py
@@ -0,0 +1,14 @@
+'''File for intializing the ensemble package'''
+
+
+__all__ = ['ensemble_regressor_class', 'regressor_class', 'unit_testing']
+
+# import the modules
+from . import ensemble_regressor_class
+from . import regressor_class
+from . import unit_testing
+
+# define the version
+
+version = '1.0'
+
