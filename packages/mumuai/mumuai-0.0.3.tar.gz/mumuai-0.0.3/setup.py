df3927b9f11afec338acef7283903203a61630c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-09 03:05:53 +0000

    Add package mumuai/0.0.3/mumuai-0.0.3.tar.gz

---
+++ packages/mumuai/mumuai-0.0.3.tar.gz/mumuai-0.0.3/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="mumuai", # Replace with your own username
+    version="0.0.3",
+    author="Example Author",
+    author_email="mumuai@goatfarm.ai",
+    description="A small example package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/dltdnfrk/psh",
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    keywords=['pyxing'],
+    package_data={},
+    zip_safe=False,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
