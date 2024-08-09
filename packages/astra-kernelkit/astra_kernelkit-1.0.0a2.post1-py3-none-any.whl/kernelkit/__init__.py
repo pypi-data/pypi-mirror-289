9093793d61bb112be258f991fb8066714898aa33
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-09 05:05:05 +0000

    Add package astra-kernelkit/1.0.0a2.post1/astra_kernelkit-1.0.0a2.post1-py3-none-any.whl

---
+++ packages/astra-kernelkit/astra_kernelkit-1.0.0a2.post1-py3-none-any.whl/kernelkit/__init__.py
@@ -0,0 +1,18 @@
+__version__ = "1.0.0-alpha.2.post1"
+
+KERNELKIT_VERSION = __version__
+KERNELKIT_CUDA_SOURCES = "kernelkit.cuda"
+
+from .operator import *
+from .algo import fp, bp, fdk, sirt
+from .data import *
+from .geom import resolve_volume_geometry
+from .geom.proj import *
+from .geom.vol import *
+from . import kernel
+from . import kernels
+from . import experimental
+from . import processing
+
+# don't import automatically to avoid ASTRA dependency
+# from . import toolbox_support
---
+++ packages/astra-kernelkit/astra_kernelkit-1.0.0a2.post1-py3-none-any.whl/kernelkit/kernels/__init__.py
@@ -0,0 +1,2 @@
+from .cone_fp import RayDrivenConeFP
+from .cone_bp import VoxelDrivenConeBP
