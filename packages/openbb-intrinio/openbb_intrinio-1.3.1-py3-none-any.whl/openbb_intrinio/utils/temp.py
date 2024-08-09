73f0d054dab5f57b8d0964ff3f75eec8b9c069f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-09 05:04:59 +0000

    Add package openbb-intrinio/1.3.1/openbb_intrinio-1.3.1-py3-none-any.whl

---
+++ packages/openbb-intrinio/openbb_intrinio-1.3.1-py3-none-any.whl/openbb_intrinio/utils/temp.py
@@ -0,0 +1,186 @@
+__alias_dict__ = {
+    "amortization_expense": "amortizationexpense",
+    "amortizationof_deferred_policy_acquisition_costs": "amortizationofdeferredpolicyacquisitioncosts",
+    "basic_and_diluted_earnings_per_share": "basicdilutedeps",
+    "basic_earnings_per_share": "basiceps",
+    "capitalized_lease_obligations_interest_expense": "capitalizedleaseobligationinterestexpense",
+    "cash_dividends_to_common_per_share": "cashdividendspershare",
+    "current_and_future_benefits": "currentandfuturebenefits",
+    "depletion_expense": "depletionexpense",
+    "deposits_interest_expense": "depositsinterestexpense",
+    "deposits_and_money_market_investments_interest_income": "depositsinterestincome",
+    "depreciation_expense": "depreciationexpense",
+    "diluted_earnings_per_share": "dilutedeps",
+    "exploration_expense": "explorationexpense",
+    "extraordinary_income": "extraordinaryincome",
+    "federal_funds_purchased_and_securities_sold_interest_expense": "fedfundsandrepointerestexpense",
+    "federal_funds_sold_and_securities_borrowed_interest_income": "fedfundsandrepointerestincome",
+    "impairment_charge": "impairmentexpense",
+    "income_tax_expense": "incometaxexpense",
+    "investment_banking_income": "investmentbankingincome",
+    "investment_securities_interest_income": "investmentsecuritiesinterestincome",
+    "loans_and_leases_interest_income": "loansandleaseinterestincome",
+    "long_term_debt_interest_expense": "longtermdebtinterestexpense",
+    "marketing_expense": "marketingexpense",
+    "consolidated_net_income": "netincome",
+    "net_income_continuing_operations": "netincomecontinuing",
+    "net_income_discontinued_operations": "netincomediscontinued",
+    "net_income_attributable_to_common_shareholders": "netincometocommon",
+    "net_income_attributable_to_noncontrolling_interest": "netincometononcontrollinginterest",
+    "net_interest_income": "netinterestincome",
+    "net_occupancy_and_equipment_expense": "netoccupancyequipmentexpense",
+    "net_realized_and_unrealized_capital_gainson_investments": "netrealizedcapitalgains",
+    "non_operating_income": "nonoperatingincome",
+    "operating_costof_revenue": "operatingcostofrevenue",
+    "operating_revenue": "operatingrevenue",
+    "other_adjustments_to_consolidated_net_income": "otheradjustmentstoconsolidatednetincome",
+    "other_adjustments_to_net_income_attributable_to_common_shareholders": "otheradjustmentstonetincometocommon",
+    "other_adjustment_to_net_income_attributable_to_common_shareholders": "otheradjustmentstonetincometocommon",
+    "other_cost_of_revenue": "othercostofrevenue",
+    "other_gains": "othergains",
+    "other_income": "otherincome",
+    "other_interest_expense": "otherinterestexpense",
+    "other_interest_income": "otherinterestincome",
+    "other_non_interest_income": "othernoninterestincome",
+    "other_operating_expenses": "otheroperatingexpenses",
+    "other_revenue": "otherrevenue",
+    "other_service_charges": "otherservicechargeincome",
+    "other_special_charges": "otherspecialcharges",
+    "insurance_policy_acquisition_costs": "policyacquisitioncosts",
+    "preferred_stock_dividends_declared": "preferreddividends",
+    "premiums_earned": "premiumsearned",
+    "property_and_liability_insurance_claims": "propertyliabilityinsuranceclaims",
+    "provision_for_credit_losses": "provisionforcreditlosses",
+    "research_and_development_expense": "rdexpense",
+    "restructuring_charge": "restructuringcharge",
+    "salaries_and_employee_benefits": "salariesandemployeebenefitsexpense",
+    "service_chargeson_deposit_accounts": "servicechargesondepositsincome",
+    "selling_general_and_admin_expense": "sgaexpense",
+    "short_term_borrowings_interest_expense": "shorttermborrowinginterestexpense",
+    "total_cost_of_revenue": "totalcostofrevenue",
+    "total_gross_profit": "totalgrossprofit",
+    "interest_expense": "totalinterestexpense",
+    "total_interest_expense": "totalinterestexpense",
+    "interest_and_investment_income": "totalinterestincome",
+    "total_interest_income": "totalinterestincome",
+    "total_non_interest_expense": "totalnoninterestexpense",
+    "total_non_interest_income": "totalnoninterestincome",
+    "total_operating_expenses": "totaloperatingexpenses",
+    "total_operating_income": "totaloperatingincome",
+    "total_other_income": "totalotherincome",
+    "total_pre_tax_income": "totalpretaxincome",
+    "total_revenue": "totalrevenue",
+    "trading_account_interest_income": "tradingaccountinterestincome",
+    "trust_fees_by_commissions": "trustfeeincome",
+    "weighted_average_basic_and_diluted_shares_outstanding": "weightedavebasicdilutedsharesos",
+    "weighted_average_basic_shares_outstanding": "weightedavebasicsharesos",
+    "weighted_average_diluted_shares_outstanding": "weightedavedilutedsharesos"
+}
+
+fields = ["amortization_expense", "amortizationof_deferred_policy_acquisition_costs", "basic_and_diluted_earnings_per_share", "basic_earnings_per_share", "capitalized_lease_obligations_interest_expense", "cash_dividends_to_common_per_share", "current_and_future_benefits", "depletion_expense", "deposits_interest_expense", "deposits_and_money_market_investments_interest_income", "depreciation_expense", "diluted_earnings_per_share", "exploration_expense", "extraordinary_income", "federal_funds_purchased_and_securities_sold_interest_expense", "federal_funds_sold_and_securities_borrowed_interest_income", "impairment_charge", "income_tax_expense", "investment_banking_income", "investment_securities_interest_income", "loans_and_leases_interest_income", "long_term_debt_interest_expense", "marketing_expense", "consolidated_net_income", "net_income_continuing_operations", "net_income_discontinued_operations", "net_income_attributable_to_common_shareholders", "net_income_attributable_to_noncontrolling_interest", "net_interest_income", "net_occupancy_and_equipment_expense", "net_realized_and_unrealized_capital_gainson_investments", "non_operating_income", "operating_costof_revenue", "operating_revenue", "other_adjustments_to_consolidated_net_income", "other_adjustments_to_net_income_attributable_to_common_shareholders", "other_adjustment_to_net_income_attributable_to_common_shareholders", "other_cost_of_revenue", "other_gains", "other_income", "other_interest_expense", "other_interest_income", "other_non_interest_income", "other_operating_expenses", "other_revenue", "other_service_charges", "other_special_charges", "insurance_policy_acquisition_costs", "preferred_stock_dividends_declared", "premiums_earned", "property_and_liability_insurance_claims", "provision_for_credit_losses", "research_and_development_expense", "restructuring_charge", "salaries_and_employee_benefits", "service_chargeson_deposit_accounts", "selling_general_and_admin_expense", "short_term_borrowings_interest_expense", "total_cost_of_revenue", "total_gross_profit", "interest_expense", "total_interest_expense", "interest_and_investment_income", "total_interest_income", "total_non_interest_expense", "total_non_interest_income", "total_operating_expenses", "total_operating_income", "total_other_income", "total_pre_tax_income", "total_revenue", "trading_account_interest_income", "trust_fees_by_commissions", "weighted_average_basic_and_diluted_shares_outstanding", "weighted_average_basic_shares_outstanding", "weighted_average_diluted_shares_outstanding"]
+
+
+
+    acquisitions: Optional[float] = Field(default=None, description="Acquisitions")
+    amortization_expense: Optional[float] = Field(default=None, description="Amortization Expense")
+    cash_income_taxes_paid: Optional[float] = Field(default=None, description="Cash Income Taxes Paid")
+    cash_interest_paid: Optional[float] = Field(default=None, description="Cash Interest Paid")
+    cash_interest_received: Optional[float] = Field(default=None, description="Cash Interest Received")
+    depreciation_expense: Optional[float] = Field(default=None, description="Depreciation Expense")
+    divestitures: Optional[float] = Field(default=None, description="Divestitures")
+    effect_of_exchange_rate_changes: Optional[float] = Field(
+        default=None, description="Effect of Exchange Rate Changes"
+    )
+    changes_in_operating_assets_and_liabilities: Optional[float] = Field(
+        default=None, description="Changes in Operating Assets and Liabilities (Net)"
+    )
+    issuance_of_common_equity: Optional[float] = Field(
+        default=None, description="Issuance of Common Equity"
+    )
+    issuance_of_debt: Optional[float] = Field(default=None, description="Issuance of Debt")
+    issuance_of_preferred_equity: Optional[float] = Field(
+        default=None, description="Issuance of Preferred Equity"
+    )
+    loans_held_for_sale: Optional[float] = Field(
+        default=None, description="Loans Held for Sale (Net)"
+    )
+    net_cash_from_continuing_financing_activities: Optional[float] = Field(
+        default=None, description="Net Cash from Continuing Financing Activities"
+    )
+    net_cash_from_continuing_investing_activities: Optional[float] = Field(
+        default=None, description="Net Cash from Continuing Investing Activities"
+    )
+    net_cash_from_continuing_operating_activities: Optional[float] = Field(
+        default=None, description="Net Cash from Continuing Operating Activities"
+    )
+    net_cash_from_discontinued_financing_activities: Optional[float] = Field(
+        default=None, description="Net Cash from Discontinued Financing Activities"
+    )
+    net_cash_from_discontinued_investing_activities: Optional[float] = Field(
+        default=None, description="Net Cash from Discontinued Investing Activities"
+    )
+    net_cash_from_discontinued_operating_activities: Optional[float] = Field(
+        default=None, description="Net Cash from Discontinued Operating Activities"
+    )
+    net_cash_from_financing_activities: Optional[float] = Field(
+        default=None, description="Net Cash from Financing Activities"
+    )
+    net_cash_from_investing_activities: Optional[float] = Field(
+        default=None, description="Net Cash from Investing Activities"
+    )
+    net_cash_from_operating_activities: Optional[float] = Field(
+        default=None, description="Net Cash from Operating Activities"
+    )
+    net_change_in_cash_and_equivalents: Optional[float] = Field(
+        default=None, description="Net Change in Cash and Equivalents"
+    )
+    net_change_in_deposits: Optional[float] = Field(default=None, description="Net Change in Deposits")
+    consolidated_net_income: Optional[float] = Field(default=None, description="Consolidated Net Income")
+    net_income_continuing_operations: Optional[float] = Field(
+        default=None, description="Net Income (Continuing Operations)"
+    )
+    net_income_discontinued_operations: Optional[float] = Field(
+        default=None, description="Net Income (Discontinued Operations)"
+    )
+    net_increase_in_fed_funds_sold: Optional[float] = Field(
+        default=None, description="Net Increase in Fed Funds Sold"
+    )
+    non_cash_adjustments_to_reconcile_net_income: Optional[float] = Field(
+        default=None, description="Non-Cash Adjustments to Reconcile Net Income"
+    )
+    non_cash_adjustmentsto_reconcile_net_income: Optional[float] = Field(
+        default=None, description="Non-Cash Adjustments to Reconcile Net Income"
+    )
+    other_financing_activities: Optional[float] = Field(
+        default=None, description="Other Financing Activities (Net)"
+    )
+    other_investing_activities: Optional[float] = Field(
+        default=None, description="Other Investing Activities (Net)"
+    )
+    other_net_changes_in_cash: Optional[float] = Field(
+        default=None, description="Other Net Changes in Cash"
+    )
+    payment_of_dividends: Optional[float] = Field(default=None, description="Payment of Dividends")
+    provision_for_loan_losses: Optional[float] = Field(
+        default=None, description="Provision for Loan Losses"
+    )
+    purchase_of_investments: Optional[float] = Field(default=None, description="Purchase of Investments")
+    purchase_of_investment_securities: Optional[float] = Field(
+        default=None, description="Purchase of Investment Securities"
+    )
+    purchase_of_property_plant_and_equipment: Optional[float] = Field(
+        default=None, description="Purchase of Property, Plant, and Equipment"
+    )
+    repayment_of_debt: Optional[float] = Field(default=None, description="Repayment of Debt")
+    repurchase_of_common_equity: Optional[float] = Field(
+        default=None, description="Repurchase of Common Equity"
+    )
+    repurchase_of_preferred_equity: Optional[float] = Field(
+        default=None, description="Repurchase of Preferred Equity"
+    )
+    sale_and_maturity_of_investments: Optional[float] = Field(
+        default=None, description="Sale and Maturity of Investments"
+    )
+    sale_of_property_plant_and_equipment: Optional[float] = Field(
+        default=None, description="Sale of Property, Plant, and Equipment"
+    )
