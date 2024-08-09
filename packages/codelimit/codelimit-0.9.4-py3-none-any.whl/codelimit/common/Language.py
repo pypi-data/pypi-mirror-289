c2b455de4fe2e001e374bf482db6f8b252ee1701
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-09 03:05:56 +0000

    Add package codelimit/0.9.4/codelimit-0.9.4-py3-none-any.whl

---
+++ packages/codelimit/codelimit-0.9.4-py3-none-any.whl/codelimit/common/Language.py
@@ -0,0 +1,20 @@
+from abc import ABC, abstractmethod
+
+from codelimit.common.Token import Token
+from codelimit.common.TokenRange import TokenRange
+from codelimit.common.scope.Header import Header
+
+
+class Language(ABC):
+    def __init__(self, allow_nested_functions=True):
+        self.allow_nested_functions = allow_nested_functions
+
+    @abstractmethod
+    def extract_headers(self, tokens: list[Token]) -> list[Header]:
+        pass
+
+    @abstractmethod
+    def extract_blocks(
+        self, tokens: list[Token], headers: list[Header]
+    ) -> list[TokenRange]:
+        pass
