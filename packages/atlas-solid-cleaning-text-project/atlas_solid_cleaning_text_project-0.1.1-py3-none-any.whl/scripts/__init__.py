dc2d18c0756a97760e8fbc4080832e7979b04d71
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-09 05:04:47 +0000

    Add package atlas-solid-cleaning-text-project/0.1.1/atlas_solid_cleaning_text_project-0.1.1-py3-none-any.whl

---
+++ packages/atlas-solid-cleaning-text-project/atlas_solid_cleaning_text_project-0.1.1-py3-none-any.whl/scripts/__init__.py
@@ -0,0 +1,15 @@
+# scripts/__init__.py
+
+from .remove_pnum_hilight_title import process_directory 
+# from .split_chapters import process_directory 
+# from .clean_text import process_directory 
+# from .fix_lines import process_directory 
+# from .replace_numbers import process_directory 
+
+__all__ = [
+    "process_directory",
+    # "process_directory_split_ch",
+    # "process_directory_clean_txt",
+    # "process_directory_line_fix",
+    # "process_directory_numbers2words",
+]
