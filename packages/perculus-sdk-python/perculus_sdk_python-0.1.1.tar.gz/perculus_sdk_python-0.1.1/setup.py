2f6582fff3795118acedc6abe30f6700d0570a75
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-09 07:04:53 +0000

    Add package perculus-sdk-python/0.1.1/perculus_sdk_python-0.1.1.tar.gz

---
+++ packages/perculus-sdk-python/perculus_sdk_python-0.1.1.tar.gz/perculus_sdk_python-0.1.1/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='perculus-sdk-python',
+    version='0.1.1',
+    author='Perculus Dev Team',
+    author_email='zafer@perculus.com',
+    description='Perculus Python Software Development Kit',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    include_package_data=True,
+    packages=find_packages(where='.', include=['perculus-sdk-python', 'perculus-sdk-python.*']),
+    url='https://github.com/perculus/perculus-sdk-python',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+    install_requires=[
+        'python-dotenv==1.0.1',
+        'requests==2.32.3',
+        'urllib3==2.2.2'
+    ],
+)
