95ed941949bce69a5968f8295a0537bbf6861a07
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-09 07:04:47 +0000

    Add package eventiq/1.0.0/eventiq-1.0.0.tar.gz

---
+++ packages/eventiq/eventiq-1.0.0.tar.gz/eventiq-1.0.0/eventiq/settings.py
@@ -0,0 +1,34 @@
+from typing import Any, Generic, Literal, Optional, TypeVar
+
+from pydantic.networks import AnyUrl
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+from .imports import ImportedType
+from .types import Decoder, Encoder
+
+
+class BrokerSettings(BaseSettings):
+    name: str = "default"
+    default_consumer_timeout: int = 300
+    description: Optional[str] = None
+    validate_error_delay: Optional[int] = 3600 * 12
+
+    model_config = SettingsConfigDict(env_prefix="BROKER_")
+
+
+Url = TypeVar("Url", bound=AnyUrl)
+
+
+class UrlBrokerSettings(BrokerSettings, Generic[Url]):
+    url: Url
+    connection_options: dict[str, Any] = {}
+
+
+class ServiceSettings(BaseSettings):
+    name: str
+    title: Optional[str] = None
+    version: str = "0.1.0"
+    description: str = ""
+    asyncapi: Literal["3.0.0"] = "3.0.0"
+    encoder: Optional[ImportedType[Encoder]] = None
+    decoder: Optional[ImportedType[Decoder]] = None
