16c9513d9f69d26115e3e86999d129ab1cd8fec0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-09 14:05:47 +0000

    Add package nonstdout/1.0.4/nonstdout-1.0.4.tar.gz

---
+++ packages/nonstdout/nonstdout-1.0.4.tar.gz/nonstdout-1.0.4/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='nonstdout',
+    version='1.0.4',
+    author='Sushii64',
+    packages=find_packages(),
+    description='A Python library for making printing cooler with colours and loading bars.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/Sushii64/nonstdout',
+    license='GNU GPL V3',
+    install_requires=[
+        "colorama==0.4.6"
+    ],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python :: 3',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Utilities',
+    ],
+)
