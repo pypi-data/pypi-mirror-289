3b118498843c3546cfa574d310bac86f2d74860e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-09 15:05:13 +0000

    Add package hikyuu/2.1.1/hikyuu-2.1.1-cp39-none-win_amd64.whl

---
+++ packages/hikyuu/hikyuu-2.1.1-cp39-none-win_amd64.whl/hikyuu/strategy/strategy.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python
+# -*- coding: utf8 -*-
+# cp936
+
+from hikyuu import StrategyBase, Query
+from hikyuu import StrategyContext, StockManager
+
+
+class TestStrategy(StrategyBase):
+    def __init__(self):
+        super(self.__class__, self).__init__()
+        self.stock_list = ['sh600000', 'sz000001']
+        self.ktype_list = [Query.MIN, Query.DAY]
+
+    def init(self):
+        print("strategy init")
+
+    def on_bar(self, ktype):
+        print("on bar {}".format(ktype))
+        print("{}".format(len(StockManager.instance())))
+        for s in self.sm:
+            print(s)
+
+
+if __name__ == '__main__':
+    s = TestStrategy()
+    s.run()
