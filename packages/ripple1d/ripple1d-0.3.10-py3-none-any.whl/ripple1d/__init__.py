ee99d1097a20f69769936a6161a588749a2a3d2d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-09 14:04:57 +0000

    Add package ripple1d/0.3.10/ripple1d-0.3.10-py3-none-any.whl

---
+++ packages/ripple1d/ripple1d-0.3.10-py3-none-any.whl/ripple1d/__init__.py
@@ -0,0 +1,20 @@
+"""
+Ripple1D Package.
+
+This package provides tools and utilities for managing 1-dimensional HEC-RAS simulations.
+"""
+
+import os
+
+
+def get_version():
+    """Get version info for build."""
+    pyproject_path = os.path.join(os.path.dirname(__file__), "__version__.py")
+    with open(pyproject_path, "r") as f:
+        return f.readline().strip()
+
+
+try:
+    __version__ = get_version()
+except Exception as e:
+    __version__ = ""
