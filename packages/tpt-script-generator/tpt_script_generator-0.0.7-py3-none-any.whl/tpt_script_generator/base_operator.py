a4a990d35986971c8552f84b04101ec190d89bcc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-09 05:05:04 +0000

    Add package tpt-script-generator/0.0.7/tpt_script_generator-0.0.7-py3-none-any.whl

---
+++ packages/tpt-script-generator/tpt_script_generator-0.0.7-py3-none-any.whl/tpt_script_generator/base_operator.py
@@ -0,0 +1,37 @@
+from abc import ABC
+from tpt_script_generator.enums import AttributeType
+
+
+class Operator(ABC):
+    def __init__(self, operator_name, operator_type):
+        self.operator_name = operator_name
+        self.operator_type = operator_type
+        self.schema_name = ""
+        self.attributes = {}
+
+    def with_schema(self, schema_name):
+        self.schema_name = schema_name
+        return self
+
+    def with_attribute(self, attr_type: AttributeType, attr_key, attr_value):
+        self.attributes[attr_key] = (attr_type.value, attr_value)
+        return self
+
+    def build(self):
+        definition = f"    DEFINE OPERATOR {self.operator_name}\n"
+        definition += f"    TYPE {self.operator_type}\n"
+        if self.schema_name:
+            definition += f"    SCHEMA {self.schema_name}\n"
+        if self.attributes:
+            definition += "    ATTRIBUTES\n    (\n"
+            for attr_key, (attr_type, attr_value) in self.attributes.items():
+                if attr_type != AttributeType.VARCHAR.value or attr_value.startswith(
+                    "@"
+                ):
+                    definition += f"        {attr_type} {attr_key} = {attr_value},\n"
+                else:
+                    definition += f"        {attr_type} {attr_key} = '{attr_value}',\n"
+
+            definition = definition.rstrip(",\n")
+            definition += "\n    );\n"
+        return definition
---
+++ packages/tpt-script-generator/tpt_script_generator-0.0.7-py3-none-any.whl/tpt_script_generator/enums/__init__.py
@@ -0,0 +1,9 @@
+from enum import Enum
+
+
+class AttributeType(Enum):
+    """Enum for attribute types used in TPT scripts."""
+
+    VARCHAR = "VARCHAR"
+    INTEGER = "INTEGER"
+    VARCHAR_ARRAY = "VARCHAR_ARRAY"
