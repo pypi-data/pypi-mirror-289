eed32c3f5d9e4944932b0ad069fa1dda3d58d7ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-09 16:04:59 +0000

    Add package Topsis-sanchitm-102218071/0.1/Topsis-sanchitm-102218071-0.1.tar.gz

---
+++ packages/Topsis-sanchitm-102218071/Topsis-sanchitm-102218071-0.1.tar.gz/Topsis-sanchitm-102218071-0.1/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='Topsis-sanchitm-102218071',
+    version='0.1',
+    author='Sanchit2004',
+    author_email='sanchitm990@gmail.com',
+    description='A Python package for implementing the TOPSIS method',
+    packages=find_packages(),
+    entry_points={
+        'console_scripts': [
+            'topsis=main:topsis',  # Ensure this matches the file name
+        ],
+    },
+    install_requires=[
+        'pandas',
+        'numpy',
+    ],
+)
