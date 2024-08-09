64ab0db67ffc54a1886bd77cbbf1b445f28f379c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-09 14:04:34 +0000

    Add package ytb2audiobot/2.448/ytb2audiobot-2.448-py3-none-any.whl

---
+++ packages/ytb2audiobot/ytb2audiobot-2.448-py3-none-any.whl/ytb2audiobot/pytube.py
@@ -0,0 +1,33 @@
+from pytube import YouTube
+from ytb2audiobot import config
+from ytb2audiobot.datadir import get_data_dir
+
+
+async def get_movie_meta(movie_id):
+    movie_meta = config.DEFAULT_MOVIE_META.copy()
+
+    movie_meta['id'] = movie_id
+    movie_meta['store'] = get_data_dir()
+
+    try:
+        yt = YouTube.from_id(movie_id)
+    except Exception as e:
+        movie_meta['error'] = f'🟠 Exception. Cant get pytube object. \n🟠 {e}\n\n Continue ... '
+        return movie_meta
+
+    if yt.title:
+        movie_meta['title'] = yt.title
+
+    if yt.description:
+        movie_meta['description'] = yt.description
+
+    if yt.author:
+        movie_meta['author'] = yt.author
+
+    if yt.thumbnail_url:
+        movie_meta['thumbnail_url'] = yt.thumbnail_url
+
+    if yt.length:
+        movie_meta['duration'] = yt.length
+
+    return movie_meta
