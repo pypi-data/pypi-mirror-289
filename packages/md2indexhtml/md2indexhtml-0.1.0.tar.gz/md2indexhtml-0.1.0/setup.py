3b7a5aa2121542fd52961e9b3620edd62283ec15
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-09 06:04:00 +0000

    Add package md2indexhtml/0.1.0/md2indexhtml-0.1.0.tar.gz

---
+++ packages/md2indexhtml/md2indexhtml-0.1.0.tar.gz/md2indexhtml-0.1.0/setup.py
@@ -0,0 +1,30 @@
+# setup.py
+from setuptools import setup, find_packages
+
+setup(
+    name='md2indexhtml',
+    version='0.1.0',
+    description='Convert Markdown files to index.html for Odoo modules',
+    author='Your Name',
+    author_email='fasilwdr@hotmail.com',
+    url='https://github.com/fasilwdr/md2indexhtml',
+    packages=find_packages(),
+    install_requires=[
+        'markdown',
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+    entry_points={
+        'console_scripts': [
+            'md2indexhtml=md2indexhtml.converter:main',
+        ],
+    },
+    package_data={
+        'md2indexhtml': ['templates/*.html'],
+    },
+    include_package_data=True,
+)
