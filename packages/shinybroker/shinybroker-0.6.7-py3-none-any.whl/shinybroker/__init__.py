29093ecb1e72f7eb3a862236f12e76397f1bac10
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-09 03:04:31 +0000

    Add package shinybroker/0.6.7/shinybroker-0.6.7-py3-none-any.whl

---
+++ packages/shinybroker/shinybroker-0.6.7-py3-none-any.whl/shinybroker/__init__.py
@@ -0,0 +1,8 @@
+VERSION = '0.6.7'
+
+from shinybroker.connection import (
+    create_ibkr_socket_conn, ib_msg_reader_run_loop
+)
+from shinybroker.obj_defs import Contract, ComboLeg, DeltaNeutralContract
+from shinybroker.msgs_to_ibkr import *
+from shinybroker.sb_app import sb_app
