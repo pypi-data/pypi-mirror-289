0f31a189461e9ab2ba043039fe93d8979f16d709
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-09 07:04:51 +0000

    Add package retrievelab/0.1/retrievelab-0.1.tar.gz

---
+++ packages/retrievelab/retrievelab-0.1.tar.gz/retrievelab-0.1/README.md
@@ -0,0 +1,4 @@
+
+https://www.youtube.com/watch?v=Kz6IlDCyOUY
+
+python setup.py sdist bdist_wheel
---
+++ packages/retrievelab/retrievelab-0.1.tar.gz/retrievelab-0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+setup(
+    name='retrievelab',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[
+        # dependencies here
+        # e.g. 'numpy>=1.11.1'
+    ]
+)
