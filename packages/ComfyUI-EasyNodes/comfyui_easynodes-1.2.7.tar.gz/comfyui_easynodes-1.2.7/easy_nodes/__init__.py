f2c7b5693453878c8afa4e02b320945f065f5818
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-09 05:05:05 +0000

    Add package ComfyUI-EasyNodes/1.2.7/comfyui_easynodes-1.2.7.tar.gz

---
+++ packages/ComfyUI-EasyNodes/comfyui_easynodes-1.2.7.tar.gz/comfyui_easynodes-1.2.7/easy_nodes/__init__.py
@@ -0,0 +1,32 @@
+from easy_nodes.comfy_types import (  # noqa: F401
+    ConditioningTensor,
+    ImageTensor,
+    LatentTensor,
+    MaskTensor,
+    ModelTensor,
+    NumberType,
+    PhotoMaker,
+    SigmasTensor,
+)
+from easy_nodes.easy_nodes import (  # noqa: F401
+    AnyType,
+    AutoDescriptionMode,
+    CheckSeverityMode,
+    Choice,
+    ComfyNode,
+    CustomVerifier,
+    NumberInput,
+    StringInput,
+    TensorVerifier,
+    TypeVerifier,
+    create_field_setter_node,
+    get_node_mappings,
+    initialize_easy_nodes,
+    register_type,
+    save_node_list,
+    show_image,
+    show_text,
+)
+
+# For backwards compatibility with the original comfy_annotations module.
+ComfyFunc = ComfyNode
