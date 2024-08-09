131b77c9d72ff6e927a3f540b0328d7d2e331d3a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-09 15:05:02 +0000

    Add package fusion-platform-python-sdk/1.13.5/fusion_platform_python_sdk-1.13.5-py3-none-any.whl

---
+++ packages/fusion-platform-python-sdk/fusion_platform_python_sdk-1.13.5-py3-none-any.whl/fusion_platform/command.py
@@ -0,0 +1,16 @@
+"""
+Main entry point for the command line.
+
+author: Matthew Casey
+
+&copy; [Digital Content Analysis Technology Ltd](https://www.d-cat.co.uk)
+"""
+
+from fusion_platform.base import Base
+
+
+def fusion_platform():
+    """
+    Main entry point for command line.
+    """
+    Base.main()
