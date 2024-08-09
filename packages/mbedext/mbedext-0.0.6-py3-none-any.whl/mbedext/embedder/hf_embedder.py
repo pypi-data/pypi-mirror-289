24ea3b2b409215b52c1c01783c1b340484b3877d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-09 15:05:29 +0000

    Add package mbedext/0.0.6/mbedext-0.0.6-py3-none-any.whl

---
+++ packages/mbedext/mbedext-0.0.6-py3-none-any.whl/mbedext/embedder/hf_embedder.py
@@ -0,0 +1,21 @@
+from typing import Union, Optional
+
+import torch
+from transformers import AutoTokenizer
+
+from .embedder import Embedder
+
+
+class HFEmbedder(Embedder):
+    def __init__(self, model, text, *args, **kwargs) -> None:
+        super().__init__(model, text)
+        self.model = self.model.to(self.dev)
+
+    def embed(self, batch: Union[list[str], str]):
+        tokenizer = AutoTokenizer.from_pretrained(self.model.name_or_path)
+        tokenize_batch = tokenizer(batch, return_tensors='pt', padding="max_length", truncation=True).to(self.dev)
+
+        with torch.no_grad():
+            outputs = self.model(**tokenize_batch)
+            return outputs.last_hidden_state.mean(dim=1)
+
