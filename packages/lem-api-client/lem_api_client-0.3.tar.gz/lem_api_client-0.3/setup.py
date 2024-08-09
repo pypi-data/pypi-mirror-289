fdb9b78d127aaafee21f98a3d539c5c1e7070377
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-09 04:05:35 +0000

    Add package lem-api-client/0.3/lem_api_client-0.3.tar.gz

---
+++ packages/lem-api-client/lem_api_client-0.3.tar.gz/lem_api_client-0.3/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='lem-api-client',
+    version='0.3',
+    packages=find_packages(),
+    install_requires=[
+        'requests',
+    ],
+    entry_points={
+        'console_scripts': [
+            'lem-api-client = LEMApiClient.api_client:main'
+        ],
+    },
+)
