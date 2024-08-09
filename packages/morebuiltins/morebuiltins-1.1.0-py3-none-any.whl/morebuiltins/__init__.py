737f87835fd0f32e62be433f5e551454242b34b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-09 05:04:51 +0000

    Add package morebuiltins/1.1.0/morebuiltins-1.1.0-py3-none-any.whl

---
+++ packages/morebuiltins/morebuiltins-1.1.0-py3-none-any.whl/morebuiltins/__init__.py
@@ -0,0 +1,11 @@
+__version__ = "1.1.0"
+__all__ = [
+    "morebuiltins.utils",
+    "morebuiltins.date",
+    "morebuiltins.functools",
+    "morebuiltins.ipc",
+    "morebuiltins.request",
+    "morebuiltins.download_python",
+    "morebuiltins.tk",
+    "morebuiltins.emails",
+]
