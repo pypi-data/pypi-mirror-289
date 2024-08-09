fbf1b86dd77acb655aabe1bb3b471bc82c4b795b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-09 14:05:27 +0000

    Add package crosstab/0.0.11/crosstab-0.0.11-py3-none-any.whl

---
+++ packages/crosstab/crosstab-0.0.11-py3-none-any.whl/crosstab/_version.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+
+__title__ = "crosstab"
+__author__ = "Caleb Grant"
+__url__ = "https://github.com/geocoug/crosstab"
+__author_email__ = "grantcaleb22@gmail.com"
+__license__ = "GNU GPLv3"
+__version__ = "0.0.11"
+__description__ = "Rearrange data from a normalized CSV format to a crosstabulated format, with styling."
