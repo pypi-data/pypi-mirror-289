bfbfa46d86aba2c7e1c371fe2b74195776fababb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-09 04:05:17 +0000

    Add package gori/0.1/gori-0.1.tar.gz

---
+++ packages/gori/gori-0.1.tar.gz/gori-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='gori',
+      version='0.1',
+      description='This is my first library',
+      packages=['gori'],
+      author="Gori Sankar Borah",
+      zip_safe=False)
