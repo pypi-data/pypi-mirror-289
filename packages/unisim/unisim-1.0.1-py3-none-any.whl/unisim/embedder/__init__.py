feab6d1f32ab0c18f80f905e12b5578db24f8c71
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-09 07:04:41 +0000

    Add package unisim/1.0.1/unisim-1.0.1-py3-none-any.whl

---
+++ packages/unisim/unisim-1.0.1-py3-none-any.whl/unisim/embedder/__init__.py
@@ -0,0 +1,8 @@
+# Copyright 2023 Google LLC
+#
+# Use of this source code is governed by an MIT-style
+# license that can be found in the LICENSE file or at
+# https://opensource.org/licenses/MIT.
+
+from .embedder import Embedder  # noqa: F401
+from .text.text_embedder import TextEmbedder  # noqa: F401
