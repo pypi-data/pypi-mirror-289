dba0f7a0b29fdcf01fe10be1b5883cbe57dcafb8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-09 05:04:25 +0000

    Add package tpt-script-generator/0.0.6/tpt_script_generator-0.0.6.tar.gz

---
+++ packages/tpt-script-generator/tpt_script_generator-0.0.6.tar.gz/tpt_script_generator-0.0.6/src/tpt_script_generator/enums/__init__.py
@@ -0,0 +1,35 @@
+from enum import Enum
+
+
+class AttributeType(Enum):
+    """Enum for attribute types used in TPT scripts."""
+
+    VARCHAR = "VARCHAR"
+    INTEGER = "INTEGER"
+    VARCHAR_ARRAY = "VARCHAR_ARRAY"
+
+
+from tpt_script_generator.enums.update_operator import (
+    LogonMech,
+    LogSQL,
+    NotifyLevel,
+    NotifyMethod,
+    PauseAcq,
+    ReplicationOverride,
+    RoleName,
+    TASMFASTFAIL,
+    TimeZoneSessInfo,
+    TraceLevel,
+    TransformGroup,
+    TreatDBSRestartAsFatal,
+    UnicodePassThrough,
+)
+
+from tpt_script_generator.enums.ddl_operator import (
+    TraceLevelOption,
+    RestartAtFirstDMLGroupOption,
+    DataEncryptionOption,
+    LogSQLOption,
+    TreatDBSRestartAsFatalOption,
+    UnicodePassThroughOption
+)
