2329602683e018d41d7af643dba351bce9be3c5d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-09 14:05:23 +0000

    Add package crosstab/0.0.10/crosstab-0.0.10.tar.gz

---
+++ packages/crosstab/crosstab-0.0.10.tar.gz/crosstab-0.0.10/crosstab/__init__.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+from ._version import (
+    __author__,
+    __author_email__,
+    __description__,
+    __license__,
+    __title__,
+    __url__,
+    __version__,
+)
+from .crosstab import Crosstab
+
+__all__ = ["Crosstab"]
