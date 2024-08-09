d407b01e9845b45656587c68bc3dabf6f3226995
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-09 16:04:54 +0000

    Add package routeagent/0.2.4/routeagent-0.2.4.tar.gz

---
+++ packages/routeagent/routeagent-0.2.4.tar.gz/routeagent-0.2.4/routeagent/prompts.py
@@ -0,0 +1,20 @@
+import dataclasses
+
+
+@dataclasses.dataclass
+class RouteAgentPrompts:
+    system_prompt: str = (
+        """
+        You are a task manager. Given a user prompt and descriptions of available agents, select the most suitable 
+        agent for the task. Respond only with the chosen agent's number.
+        Input format:
+        Prompt: [User's task description]
+        1: [Agent 1 capabilities]
+        2: [Agent 2 capabilities]
+        (etc.)
+        """
+    )
+    user_prompt: str = (
+        "Prompt: {prompt}\n{agent_data} \nRemember you must only output a number which corresponds to an agent "
+        "given above based on your understanding of the prompt"
+    )
