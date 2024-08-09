4be637de08049eaa11d52ef4dee01cd00c3879e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-09 03:05:39 +0000

    Add package armada-airflow/1.0.1/armada_airflow-1.0.1-py3-none-any.whl

---
+++ packages/armada-airflow/armada_airflow-1.0.1-py3-none-any.whl/armada/auth.py
@@ -0,0 +1,10 @@
+from typing import Any, Dict, Protocol, Tuple
+
+""" We use this interface for objects fetching Kubernetes auth tokens. Since
+    it's used within the Trigger, it must be serialisable."""
+
+
+class TokenRetriever(Protocol):
+    def get_token(self) -> str: ...
+
+    def serialize(self) -> Tuple[str, Dict[str, Any]]: ...
