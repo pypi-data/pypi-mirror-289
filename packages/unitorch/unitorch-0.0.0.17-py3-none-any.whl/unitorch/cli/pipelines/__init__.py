61fdc59396bf3ab012d7b009a52c7679fb0fa237
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-09 15:04:44 +0000

    Add package unitorch/0.0.0.17/unitorch-0.0.0.17-py3-none-any.whl

---
+++ packages/unitorch/unitorch-0.0.0.17-py3-none-any.whl/unitorch/cli/pipelines/__init__.py
@@ -0,0 +1,31 @@
+# Copyright (c) FULIUCANSHENG.
+# Licensed under the MIT License.
+
+from diffusers.schedulers import (
+    DPMSolverSDEScheduler,
+    UniPCMultistepScheduler,
+    EulerAncestralDiscreteScheduler,
+    EulerDiscreteScheduler,
+    FlowMatchEulerDiscreteScheduler,
+)
+
+Schedulers = {
+    "DPM++SDE": DPMSolverSDEScheduler,
+    "UniPC++": UniPCMultistepScheduler,
+    "FlowMatchEuler": FlowMatchEulerDiscreteScheduler,
+    "EulerAncestral": EulerAncestralDiscreteScheduler,
+    "Euler": EulerDiscreteScheduler,
+}
+
+import unitorch.cli.pipelines.blip
+import unitorch.cli.pipelines.bloom
+import unitorch.cli.pipelines.bria
+import unitorch.cli.pipelines.detr
+import unitorch.cli.pipelines.dpt
+import unitorch.cli.pipelines.stable
+import unitorch.cli.pipelines.stable_xl
+import unitorch.cli.pipelines.stable_3
+import unitorch.cli.pipelines.llama
+import unitorch.cli.pipelines.llava
+import unitorch.cli.pipelines.mistral
+import unitorch.cli.pipelines.sam
