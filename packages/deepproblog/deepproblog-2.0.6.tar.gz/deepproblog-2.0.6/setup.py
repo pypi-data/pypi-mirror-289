1a967bfdd4fcecf19b20c5ab287547c94068a083
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-09 15:05:01 +0000

    Add package deepproblog/2.0.6/deepproblog-2.0.6.tar.gz

---
+++ packages/deepproblog/deepproblog-2.0.6.tar.gz/deepproblog-2.0.6/setup.py
@@ -0,0 +1,40 @@
+#! /usr/bin/env python
+
+from setuptools import setup, find_packages
+from pathlib import Path
+
+readme = (Path(__file__).parent / 'README.md').read_text()
+
+setup(
+    name="deepproblog",
+    version="2.0.6",
+    description="DeepProbLog: Problog with neural networks",
+    long_description=readme,
+    long_description_content_type='text/markdown',
+    url="https://github.com/ML-KULeuven/deepproblog",
+    author="DeepProbLog team",
+    author_email="robin.manhaeve@cs.kuleuven.be",
+    license="Apache Software License",
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "License :: OSI Approved :: Apache Software License",
+        "Intended Audience :: Science/Research",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Prolog",
+        "Topic :: Scientific/Engineering :: Artificial Intelligence",
+    ],
+    keywords="prolog probabilistic logic neural-symbolic problog deepproblog",
+    package_dir={"": "src"},
+    packages=find_packages(where="src"),
+    zip_safe=False,
+    include_package_data=True,
+    install_requires=["pysdd",
+                      "problog",
+                      "torch",
+                      "torchvision"],
+    extras_require={
+#        "approximate": ["pyswip @ git+https://github.com/ML-KULeuven/pyswip.git#egg=pyswip"],
+        "examples": ["Pillow"],
+        "tests": ["pytest"],
+    },
+)
