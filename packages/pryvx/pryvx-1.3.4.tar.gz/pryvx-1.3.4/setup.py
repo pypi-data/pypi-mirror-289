7da22607a345eb8faa6c883d486012976e205d54
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-09 05:04:41 +0000

    Add package pryvx/1.3.4/pryvx-1.3.4.tar.gz

---
+++ packages/pryvx/pryvx-1.3.4.tar.gz/pryvx-1.3.4/setup.py
@@ -0,0 +1,38 @@
+from setuptools import setup, find_packages
+
+VERSION = '1.3.4'
+
+
+# Setting up
+setup(
+    name="pryvx",
+    version=VERSION,
+    author="PryvX (Jayesh Kenaudekar)",
+    author_email="<jayesh@pryvx.com>",
+    description="A comprehensive package for privacy-enhancing technologies",
+    long_description_content_type="text/markdown",
+    long_description=open('README.md').read(),
+    packages=find_packages(),
+    install_requires=[
+        'grpcio',
+        'scikit-learn',
+        'protobuf',
+        'sympy',
+        'numpy',
+    ],
+    keywords=['python', 
+              'privacy-preserving', 
+              'federated-learning', 
+              'machine-learning', 
+              'privacy-enhancing-technology', 
+              'smpc', 
+              'private-set-intersection'],
+
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        "Intended Audience :: Developers",
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    license='MIT'
+)
