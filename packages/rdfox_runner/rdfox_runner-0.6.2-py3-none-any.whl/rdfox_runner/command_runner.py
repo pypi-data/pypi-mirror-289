95f64b6017c0164d160dcdd4afb53aa7f29da4ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-09 15:05:24 +0000

    Add package rdfox_runner/0.6.2/rdfox_runner-0.6.2-py3-none-any.whl

---
+++ packages/rdfox_runner/rdfox_runner-0.6.2-py3-none-any.whl/rdfox_runner/command_runner.py
@@ -0,0 +1,313 @@
+"""This module defines a function to run a command in a temporary directory,
+with specified input files present, and captures the contents of specified
+output files.
+
+This is the simplest building block used as the base for rdfox_runner.
+
+"""
+
+
+from io import IOBase, TextIOBase
+import os
+import logging
+import subprocess
+import shutil
+from pathlib import Path
+from tempfile import mkdtemp
+import threading
+import time
+
+from typing import Any, Callable, List, Union, Optional, TextIO, Mapping
+
+StrPath = Union[str, Path]
+PathOrIO = Union[StrPath, TextIO]
+
+logger = logging.getLogger(__name__)
+
+
+class CommandRunner:
+    """Run a command in a temporary directory.
+
+    This can be used as a context manager, ensuring the temporary directory is
+    cleaned up and the subprocess is stopped when finished with.
+
+    :param input_files: mapping with keys being the target path and value the
+        source path.
+    :param command: command to run, as passed to :class:`subprocess.Popen`
+    :param shell: whether to run command within shell
+    :param wait_before_enter: whether to wait for command to complete before
+        continuing with context manager body.
+    :param wait_before_exit: whether to wait for command to complete by itself
+        before terminating it, when leaving context manager body.
+    :param timeout: timeout if `wait` is true.
+    :param working_dir: Path to setup command in, defaults to a temporary
+        directory
+    :param output_callback: Callback on output from command.
+    """
+
+    def __init__(
+            self,
+            input_files: Optional[Mapping[str, PathOrIO]] = None,
+            command: Optional[Union[List, str, Callable]] = None,
+            shell: bool = False,
+            wait_before_enter: bool = False,
+            wait_before_exit: bool = False,
+            timeout: Optional[float] = None,
+            working_dir: Optional[StrPath] = None,
+            output_callback: Optional[Callable] = None,
+        ):
+
+        if working_dir is not None:
+            working_dir = Path(working_dir)
+            if working_dir.exists() and any(working_dir.iterdir()):
+                logger.warning(f"Existing working directory not empty: {working_dir}")
+
+        self.input_files = input_files or {}
+        self.command = command
+        self.shell = shell
+        self.wait_before_enter = wait_before_enter
+        self.wait_before_exit = wait_before_exit
+        self.timeout = timeout
+        self.working_dir = working_dir
+        self.output_callback = output_callback
+
+        self._process = None
+        self._cleanup_working_dir = (working_dir is None)
+
+        if os.environ.get("RDFOX_RUNNER_KEEP_WORKING_DIR", ""):
+            self._cleanup_working_dir = False
+
+    def start(self):
+        """Setup files and start the command running.
+
+        This is a convenience method to run :meth:`setup_files` and
+        :meth:`start_subprocess` together, as needed.
+        """
+        self.setup_files()
+        if self.command:
+            self.start_subprocess()
+
+    def stop(self):
+        """Stop the command and clean up files.
+
+        This is a convenience method to run :meth:`stop_subprocess` and
+        :meth:`cleanup_files` together, as needed.
+
+        :raises subprocess.CalledProcessError: if the subprocess returns an
+            error exit code.
+        """
+        if self.command:
+            self.stop_subprocess()
+        if self._cleanup_working_dir:
+            self.cleanup_files()
+
+    def setup_files(self):
+        """Setup the files ready to run the command.
+
+        If :attr:`working_dir` has been specified, it is created if it does not
+        exist.  Otherwise, a new temporary directory is created.
+
+        The files listed in :attr:`input_files` are copied into the working
+        directory.
+        """
+        if self.working_dir is None:
+            self.working_dir = Path(mkdtemp())
+            logger.debug("Created temporary working directory %s", self.working_dir)
+        elif not self.working_dir.exists():
+            self.working_dir.mkdir(parents=True)
+            logger.debug("Created temporary working directory %s", self.working_dir)
+
+        logger.info("Setting up to run command in %s", self.working_dir)
+
+        for target, source in self.input_files.items():
+            copy_files(source, self.working_dir / target)
+
+    def start_subprocess(self):
+        """Start the subprocess running.
+
+        If :attr:`wait` is true, wait for up to :attr:`timeout` seconds before
+        continuing.
+        """
+        if self.command is None:
+            return
+        elif callable(self.command):
+            command = self.command(self.working_dir)
+        else:
+            command = self.command
+
+        logger.info("Running command '%s'", command)
+
+        self._process = subprocess.Popen(
+            command,
+            cwd=self.working_dir,
+            stdin=subprocess.PIPE,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.STDOUT,
+            # text=True,
+            # bufsize=1,  # line buffered
+            shell=self.shell,
+        )
+
+        self._output_thread = threading.Thread(target=output_reader,
+                                               args=(self._process, self.output_callback))
+        self._output_thread.daemon = True
+        self._output_thread.start()
+
+        logger.debug("finished starting")
+
+    def wait(self):
+        """Wait for subprocess to exit.
+
+        Waits up to :attr:`timeout` seconds.
+        """
+        if self.timeout is not None:
+            logger.debug("waiting %.1f s for subprocess to finish...", self.timeout)
+        else:
+            logger.debug("waiting for ever for subprocess to finish...")
+        try:
+            self._process.wait(timeout=self.timeout)
+            logger.info('Subprocess exited with returncode = %s', self._process.returncode)
+        except subprocess.TimeoutExpired:
+            logger.error('Subprocess did not terminate in time when waiting, continuing')
+        except KeyboardInterrupt:
+            logger.info('KeyboardInterrupt: stopping subprocess...')
+            self.stop_subprocess()
+            raise
+
+    def stop_subprocess(self):
+        """Stop the subprocess. """
+        if self.command is None:
+            return
+
+        if not self._process:
+            logger.warning("trying to stop process that wasn't started")
+            return
+
+        logger.debug("trying to terminate processs...")
+        try:
+            self._process.stdin.close()
+        except (OSError, BrokenPipeError):
+            # On Windows it's an OSError, see https://bugs.python.org/issue35754
+            pass
+        self._process.terminate()
+        logger.debug("...terminate returned.")
+        try:
+            logger.debug("waiting for process to exit...")
+            self._process.wait(timeout=5)
+            logger.info('Subprocess exited with returncode = %s', self._process.returncode)
+        except subprocess.TimeoutExpired:
+            logger.error('Subprocess did not terminate in time')
+            self._process.kill()
+
+        self._output_thread.join()
+
+        self._process.poll()  # update returncode
+        if self._process.returncode > 0:
+            logger.error("Error running command: %d", self._process.returncode)
+            # raise subprocess.CalledProcessError(self._process.returncode, cmd=self.command)
+        elif self._process.returncode < 0:
+            logger.warning("Process was killed: returncode=%d", self._process.returncode)
+
+    @property
+    def returncode(self):
+        if self._process:
+            return self._process.returncode
+        return None
+
+    def cleanup_files(self):
+        """Cleanup temporary working directory, if needed.
+
+        The directory is only removed if it was newly created, not if it was
+        passed in as :attr:`working_dir`.
+        """
+        if self.working_dir is not None:
+            if not self._cleanup_working_dir:
+                logger.warning("trying to cleanup working directory that wasn't created")
+                return
+
+            shutil.rmtree(self.working_dir, ignore_errors=True)
+            logger.debug(f"Removed temporary working directory {self.working_dir}")
+            self.working_dir = None
+
+    def __enter__(self):
+        self.start()
+        if self.wait_before_enter:
+            self.wait()
+        return self
+
+    def __exit__(self, exc, value, tb):
+        if self.wait_before_exit:
+            self.wait()
+        self.stop()
+
+    def files(self, path) -> Path:
+        """Return path to temporary directory.
+
+        :param path: path relative to the working directory
+        """
+        return self.working_dir / path
+
+
+def copy_files(src: PathOrIO, dst: Path):
+    if isinstance(src, (Path, str)):
+        src = Path(src)
+        if src.is_dir():
+            if dst.exists():
+                shutil.rmtree(dst)
+            logger.debug("Copying directory %s to %s", src, dst)
+            start_time = time.perf_counter()
+            shutil.copytree(src, dst)
+            logger.debug("Finished in %d ms", (time.perf_counter() - start_time) * 1000)
+        else:
+            dst.parent.mkdir(parents=True, exist_ok=True)
+            logger.debug("Copying file %s to %s", src, dst)
+            start_time = time.perf_counter()
+            shutil.copy(src, dst)
+            logger.debug("Finished in %d ms", (time.perf_counter() - start_time) * 1000)
+        return
+
+    # The Path-like objects returned by importlib_resources have read_text() and
+    # read_bytes() methods, but they are not real Paths (so cannot be copied as
+    # above) and they do not have read() methods, so check specifically.
+    if hasattr(src, "read_bytes"):
+        mode = "b"
+        read_func = src.read_bytes
+    elif hasattr(src, "read_text"):
+        mode = "t"
+        read_func = src.read_text
+    elif isinstance(src, IOBase):
+        mode = "t" if isinstance(src, TextIOBase) else "b"
+        read_func = src.read
+    else:
+        raise TypeError("Unrecognised file source %r" % src)
+
+    dst.parent.mkdir(parents=True, exist_ok=True)
+    logger.debug("Writing data %s", dst)
+    start_time = time.perf_counter()
+    with open(dst, "w" + mode) as f:
+        f.write(read_func())
+    logger.debug("Finished in %d ms", (time.perf_counter() - start_time) * 1000)
+
+
+def get_file_contents(root_path: Path, output_files: Mapping[Any, StrPath]):
+    """Read the contents of output_files.
+
+    :param root_path: Root path that output_files are relative to
+    :param output_files: Dict of {label: target path} to collect
+    :return: Dict of {label: result}
+    """
+    result = {}
+    for label, rel_path in output_files.items():
+        with open(root_path / rel_path, "rt") as f:
+            result[label] = f.read()
+    return result
+
+
+def output_reader(process, output_callback):
+    logger.debug("output reader started (output_callback=%s)", output_callback)
+    for line in iter(process.stdout.readline, b""):
+        line = line.decode("utf-8").rstrip()
+        # line = line.rstrip()
+        logger.debug("cmd> %s", line)
+        if output_callback:
+            output_callback(line)
---
+++ packages/rdfox_runner/rdfox_runner-0.6.2-py3-none-any.whl/rdfox_runner/run_rdfox.py
@@ -0,0 +1,330 @@
+"""Functions for running RDFox with necessary input files and scripts, and
+collecting results.
+
+This aims to hide the complexity of setting up RDFox, loading data, adding
+rules, answering queries, behind a simple function that maps data -> answers.
+"""
+
+import subprocess
+import threading
+import logging
+from io import StringIO
+import re
+from pathlib import Path
+
+from typing import List, Union, Optional, Mapping
+
+from packaging.version import Version, parse as parse_version
+from packaging.specifiers import SpecifierSet
+
+StrPath = Union[str, Path]
+
+from .rdfox_endpoint import RDFoxEndpoint
+from .command_runner import CommandRunner, PathOrIO
+
+logger = logging.getLogger(__name__)
+
+
+VERSION_PATTERN = re.compile(r"RDFox version: ([0-9.]+).*")
+
+ERROR_PATTERN = re.compile(r"Error: .*|"
+                           r"File with name '.*' cannot be found|"
+                           r"Name '.*' cannot be resolved to a file relative to either|"
+                           r"Script file .* cannot be found|"
+                           r"Unknown command '.*'|"
+                           r"The server could not start listening")
+
+MULTILINE_ERROR_PATTERN = re.compile(r"An error occurred while executing the command:")
+
+ENDPOINT_PATTERN = re.compile(r"The REST endpoint was successfully started at port number/service name (\S+)")
+
+
+class RDFoxVersionError(RuntimeError):
+    pass
+
+
+def get_rdfox_version(rdfox_executable=None):
+    """Return RDFox version as a `packaging.Version` instance."""
+    if rdfox_executable is None:
+        rdfox_executable = "RDFox"
+    command = [rdfox_executable, "sandbox", ".", "echo RDFox version: $(version)", "quit"]
+    output = subprocess.check_output(command).decode()
+    lines = output.splitlines()
+    match = VERSION_PATTERN.match(lines[-1])
+    if match:
+        return parse_version(match.group(1))
+    else:
+        raise ValueError("Unknown output from RDFox")
+
+
+def check_rdfox_version(specifier, rdfox_executable=None):
+    """Raise `RDFoxVersionError` if RDFox version out of range."""
+
+    spec = SpecifierSet(specifier)
+    version = get_rdfox_version(rdfox_executable)
+    if not version in spec:
+        raise RDFoxVersionError("Bad RDFox version: {} does not match '{}'".format(version, spec))
+
+
+class RDFoxRunner:
+    """Context manager to run RDFox in a temporary directory.
+
+    :param input_files: mapping of files {target path: source file} to set up in
+        temporary working directory.
+    :param script: RDFox commands to run, either as a list of strings or a
+        single string.
+    :param namespaces: dict of RDFlib namespaces to bind
+    :param wait: whether to wait for RDFox to start the endpoint or exit when
+        starting.  If None, look for the presence of an "endpoint start" command
+        in `script` and wait for the endpoint if found, wait for exit otherwise.
+    :param working_dir: Path to setup command in, defaults to a temporary
+        directory
+    :param rdfox_executable: Path RDFox executable (default "RDFox")
+    :param endpoint: RDFoxEndpoint instance to use (default None, meaning use the
+        built in class). This can be used to customise the endpoint interface.
+
+    When used as a context manager, the `RDFoxRunner` instance returns
+    `endpoint` for running queries etc. For more control a custom
+    `RDFoxEndpoint` can be passed in. When the RDFox endpoint is started, the
+    `connect()` method on the endpoint will be called with the connection
+    string. The endpoint is available at the attribute `endpoint`.
+
+    """
+
+    MASTER_KEY = "__master.rdfox"
+
+    def __init__(
+            self,
+            input_files: Mapping[str, PathOrIO],
+            script: Union[List[str], str],
+            namespaces: Optional[Mapping] = None,
+            wait: Optional[str] = None,
+            working_dir: Optional[StrPath] = None,
+            rdfox_executable: Optional[StrPath] = None,
+            endpoint: Optional[RDFoxEndpoint] = None,
+    ):
+
+        if self.MASTER_KEY in input_files:
+            raise ValueError(f'Cannot have an input file named "{self.MASTER_KEY}"')
+
+        if not isinstance(script, str):
+            script = "\n".join(script)
+
+        if rdfox_executable is None:
+            rdfox_executable = "RDFox"
+        self.rdfox_executable = rdfox_executable
+
+        if wait is None:
+            # Try to choose a good default
+            if "endpoint start" in script:
+                wait = "endpoint"
+            else:
+                wait = "exit"
+        elif wait not in ("endpoint", "exit", "nothing"):
+            raise ValueError("wait must be 'endpoint' or 'exit' or 'nothing'")
+        self.wait = wait
+
+        if endpoint is None:
+            # Could remove namespaces in future and require a custom endpoint
+            # to be passed in if namespaces need to be configured?
+            endpoint = RDFoxEndpoint(namespaces)
+        self.endpoint = endpoint
+
+        # Generate the master script
+        self.input_files = {
+            **input_files,
+            self.MASTER_KEY: StringIO(script),
+        }
+        self.working_dir = working_dir
+
+        # Accumulate multi-line error messages
+        self._multiline_error = False
+        self._critical_error = False
+        self._critical_error_message = ""
+
+        # Store errors
+        self.errors = []
+        self.stopped_on_error = False
+
+    def _command(self, working_dir):
+        """Generate RDFox command line.
+
+        It's important that we pass the actual full path to the working
+        directory to RDFox, so that $(dir.root) in an absolute path and can be
+        used to robustly locate files in scripts.
+
+        """
+        return [
+            self.rdfox_executable,
+            "sandbox",
+            Path(working_dir).resolve(),
+            f"exec {self.MASTER_KEY}"
+        ]
+
+    def _check_for_errors(self, line):
+
+        # Check mode: if a multi-line error message has already started, accumulate it
+        if self._multiline_error:
+            if line.startswith(" "):
+                if self._multiline_error is True:
+                    self._multiline_error = []
+                self._multiline_error.append(line.strip())
+                logger.error("RDFox error: %s", line.strip())
+                # Treat out-of-memory as a critical error?
+                if line.strip() == "The RDFox instance has run out of memory.":
+                    # XXX make this less messy
+                    self._critical_error_message = line.strip()
+                    self._critical_error = True
+                return
+            else:
+                # Error finished
+                if isinstance(self._multiline_error, list):
+                    msg = "\n".join(self._multiline_error)
+                    if msg:
+                        self.errors.append(msg)
+                self._multiline_error = False
+
+        elif self._critical_error and not self._critical_error_message:
+            # because this is running in a different thread, don't raise here
+            self._critical_error_message = line
+            return
+
+        match = ENDPOINT_PATTERN.match(line)
+        if match:
+            port = match.group(1)
+            logger.info("RDFox started on port %s", port)
+            self.endpoint.connect("http://localhost:%s" % port)
+            if self._endpoint_ready is not None:
+                logger.debug("Signalling that endpoint is ready...")
+                self._endpoint_ready.set()
+
+        elif ERROR_PATTERN.match(line):
+            logger.error("RDFox error: %s" % line)
+            self.errors.append(line)
+
+        elif MULTILINE_ERROR_PATTERN.match(line):
+            # The error is more than one line -- start accumulator mode
+            logger.debug("Starting multiline error message")
+            self._multiline_error = True
+
+        elif line == "A critical error occurred while running RDFox:":
+            logger.debug("Starting critical error message")
+            self._critical_error = True
+
+        elif line.startswith("Stopping shell evaluation due to 'on-error' policy"):
+            logger.error("RDFox error: %s", line)
+            self.stopped_on_error = True
+            self.send_quit()
+            if self._endpoint_ready is not None:
+                self._endpoint_ready.set()
+
+    def start(self):
+        """Start RDFox.
+
+        :param wait_secs: how many seconds to wait for RDFox to start.
+        """
+        wait_for_exit = (self.wait == "exit")
+        wait_for_endpoint = (self.wait == "endpoint")
+
+        logger.debug("Starting to run RDFox (wait_for_exit=%s, wait_for_endpoint=%s)",
+                     wait_for_exit, wait_for_endpoint)
+        self._runner = CommandRunner(
+            self.input_files,
+            self._command,
+            working_dir=self.working_dir,
+            output_callback=self._check_for_errors,
+        )
+
+        if wait_for_endpoint:
+            self._endpoint_ready = threading.Event()
+            self._runner.start()
+            logger.debug("CommandRunner started, waiting for endpoint...")
+            self._endpoint_ready.wait()
+            self.raise_for_errors()
+            logger.debug("...endpoint ready!")
+        else:
+            self._endpoint_ready = None
+            self._runner.start()
+
+            if wait_for_exit:
+                logger.debug("CommandRunner started, waiting for exit...")
+                self._runner.wait()
+                self.raise_for_errors()
+            else:
+                logger.debug("CommandRunner started.")
+
+    def send_quit(self):
+        """Send "quit" command to RDFox."""
+        logger.debug("Sending 'quit' command to RDFox")
+
+        if not (self._runner and self._runner._process and self._runner._process.stdin):
+            logger.debug("Could not send 'quit' command to RDFox")
+            return
+
+        try:
+            self._runner._process.stdin.write(b"quit\n")
+            self._runner._process.stdin.flush()
+        except (OSError, BrokenPipeError, ValueError) as err:
+            # On Windows it's an OSError, see https://bugs.python.org/issue35754
+            logger.debug("Error sending 'quit' command to RDFox: %s", err)
+            pass
+
+    def stop(self):
+        """Stop RDFox."""
+        logger.debug("Stopping RDFox")
+
+        # Try to exit gracefully first
+        self.send_quit()
+
+        self._runner.stop()
+        self.raise_for_errors()
+
+    def raise_for_errors(self):
+        """Raise an exception if RDFox has reported an error.
+
+        "Critical" errors are reported. If the error policy is set to "stop",
+        then errors that caused RDFox to stop are also reported.
+
+        """
+        logger.debug("raise_for_errors: %s, %s", self._critical_error, self._critical_error_message)
+        if self._critical_error:
+            raise RuntimeError(f"Critical RDFox error: {self._critical_error_message}")
+        if self.stopped_on_error and self.errors:
+            raise RuntimeError("RDFox errors:\n  " + "\n  ".join(self.errors))
+
+    def __enter__(self):
+        self.start()
+        return self.endpoint
+
+    def __exit__(self, exc, value, tb):
+        self.stop()
+
+    def files(self, path) -> Path:
+        """Return path to temporary directory.
+
+        :param path: path relative to the working directory
+        """
+        return self._runner.files(path)
+
+
+def run_rdfox_collecting_output(input_files: Mapping[str, PathOrIO],
+                                script: Union[List[str], str],
+                                output_files: Mapping[str, str],
+                                **kwargs) -> Mapping[str, str]:
+    """Run RDFox once and return requested output files' contents.
+
+    :param input_files: passed to :class:`RDFoxRunner`.
+    :param script: passed to :class:`RDFoxRunner`.
+    :param output_files: mapping of {key: filename} of files whose contents should be returned.
+    :param \\**kwargs: passed to :class:`RDFoxRunner`.
+    """
+
+    if "wait" not in kwargs:
+        kwargs["wait"] = "exit"
+
+    result = {}
+    runner = RDFoxRunner(input_files, script, **kwargs)
+    with runner:
+        for key, filename in output_files.items():
+            result[key] = runner.files(filename).read_text()
+    return result
