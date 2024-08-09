8b452bc7accf603a846f809fea6ece3635a0727b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-09 04:05:00 +0000

    Add package snowman-py/0.0.26/snowman_py-0.0.26-py3-none-win_amd64.whl

---
+++ packages/snowman-py/snowman_py-0.0.26-py3-none-win_amd64.whl/snowman/__init__.py
@@ -0,0 +1,17 @@
+"""
+# ⛄ [Snowman](https://github.com/yassun7010/snowman-py) ⛄
+
+Python model and query builder for [Snowflake](https://www.snowflake.com/).
+"""
+
+import importlib
+import importlib.metadata
+
+from . import datatype as datatype
+from . import pydantic as pydantic
+from . import query as query
+from .query.column import Column as Column
+from .relation.table import Table as Table
+from .relation.table import table as table
+
+__version__ = importlib.metadata.version("snowman-py")
