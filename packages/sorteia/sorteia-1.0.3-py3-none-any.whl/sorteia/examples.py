a79da26e35a287a24aa2a97d1236ef0c0b8c79a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-09 03:05:56 +0000

    Add package sorteia/1.0.3/sorteia-1.0.3-py3-none-any.whl

---
+++ packages/sorteia/sorteia-1.0.3-py3-none-any.whl/sorteia/examples.py
@@ -0,0 +1,34 @@
+from melting_schemas.utils import wrap, to_openapi_examples
+from fastapi.openapi.models import Example
+
+from .schemas import ReorderManyResourcesIn, ReorderManyResourcesInList
+from redbaby.pyobjectid import PyObjectId
+
+
+def reorder_many_examples() -> dict[str, Example]:
+    reorder_many = ReorderManyResourcesInList(
+        resources=[
+            ReorderManyResourcesIn(
+                resource_id=PyObjectId(),
+                resource_ref="resource.$ref",
+                position=0,
+            ),
+            ReorderManyResourcesIn(
+                resource_id=PyObjectId(),
+                resource_ref="resource.$ref",
+                position=1,
+            ),
+        ]
+    )
+    return to_openapi_examples(
+        [wrap(name="Reorder Many Resources", value=reorder_many)]
+    )
+
+
+def reorder_one_examples() -> dict[str, Example]:
+    reorder_one = ReorderManyResourcesIn(
+        resource_id=PyObjectId(),
+        resource_ref="resource.$ref",
+        position=0,
+    )
+    return to_openapi_examples([wrap(name="Reorder One Resource", value=reorder_one)])
