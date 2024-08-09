b005cc2fed07e750a9af3b6c2a5ed1a66f82030c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-09 03:05:44 +0000

    Add package scaleway/2.6.0/scaleway-2.6.0-py3-none-any.whl

---
+++ packages/scaleway/scaleway-2.6.0-py3-none-any.whl/scaleway/ipam/v1/__init__.py
@@ -0,0 +1,33 @@
+# This file was automatically generated. DO NOT EDIT.
+# If you have any remark or suggestion do not hesitate to open an issue.
+from .types import ListIPsRequestOrderBy
+from .types import ResourceType
+from .types import Resource
+from .types import Reverse
+from .types import Source
+from .types import IP
+from .types import BookIPRequest
+from .types import GetIPRequest
+from .types import ListIPsRequest
+from .types import ListIPsResponse
+from .types import ReleaseIPRequest
+from .types import ReleaseIPSetRequest
+from .types import UpdateIPRequest
+from .api import IpamV1API
+
+__all__ = [
+    "ListIPsRequestOrderBy",
+    "ResourceType",
+    "Resource",
+    "Reverse",
+    "Source",
+    "IP",
+    "BookIPRequest",
+    "GetIPRequest",
+    "ListIPsRequest",
+    "ListIPsResponse",
+    "ReleaseIPRequest",
+    "ReleaseIPSetRequest",
+    "UpdateIPRequest",
+    "IpamV1API",
+]
