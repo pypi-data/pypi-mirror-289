d25e8c0421eda8c525aef9a45b503983856c701f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-09 03:05:08 +0000

    Add package lilya/0.9.1/lilya-0.9.1-py3-none-any.whl

---
+++ packages/lilya/lilya-0.9.1-py3-none-any.whl/lilya/types.py
@@ -0,0 +1,35 @@
+from typing import (
+    Any,
+    AsyncContextManager,
+    Awaitable,
+    Callable,
+    Mapping,
+    MutableMapping,
+    Sequence,
+    TypeVar,
+    Union,
+)
+
+ApplicationType = TypeVar("ApplicationType")
+
+Scope = MutableMapping[str, Any]
+Message = MutableMapping[str, Any]
+
+Receive = Callable[[], Awaitable[Message]]
+Send = Callable[[Message], Awaitable[None]]
+
+ASGIApp = Callable[[Scope, Receive, Send], Awaitable[None]]
+
+StatelessLifespan = Callable[[ApplicationType], AsyncContextManager[None]]
+StatefulLifespan = Callable[[ApplicationType], AsyncContextManager[Mapping[str, Any]]]
+Lifespan = Union[StatelessLifespan[ApplicationType], StatefulLifespan[ApplicationType]]
+
+LifespanEvent = Sequence[Callable[[], Any]]
+
+HTTPExceptionHandler = Callable[[Any, Exception], Union[Any, Awaitable[Any]]]
+WebSocketExceptionHandler = Callable[[Any, Exception], Awaitable[None]]
+ExceptionHandler = Union[HTTPExceptionHandler, WebSocketExceptionHandler]
+CallableDecorator = TypeVar("CallableDecorator", bound=Callable[..., Any])
+
+
+class Empty: ...
