d64bc5fdc9edfe1b6e8a88fc7b3880ad033cd904
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-09 15:05:21 +0000

    Add package nonebot-plugin-wakatime/0.2.0/nonebot_plugin_wakatime-0.2.0-py3-none-any.whl

---
+++ packages/nonebot-plugin-wakatime/nonebot_plugin_wakatime-0.2.0-py3-none-any.whl/nonebot_plugin_wakatime/render_pic.py
@@ -0,0 +1,64 @@
+from datetime import datetime
+
+from nonebot_plugin_htmlrender import template_to_pic, template_to_html
+
+from .schema import WakaTime
+from .config import RESOURCES_DIR, TEMPLATES_DIR, CustomSource, config
+from .utils import image_to_base64, get_lolicon_image, calc_work_time_percentage
+
+
+async def render(data: WakaTime) -> bytes:
+
+    data["user"]["created_at"] = datetime.strptime(
+        data["user"]["created_at"], "%Y-%m-%dT%H:%M:%SZ"
+    ).strftime("%b %d %Y")
+
+    default_background = RESOURCES_DIR / "images" / "background.png"
+
+    match config.background_source:
+        case "default":
+            background_image = image_to_base64(default_background)
+        case "LoliAPI":
+            background_image = "https://www.loliapi.com/acg/pe/"
+        case "Lolicon":
+            background_image = await get_lolicon_image()
+        case CustomSource() as cs:
+            background_image = cs.to_uri()
+
+    return await template_to_pic(
+        template_path=str(TEMPLATES_DIR),
+        template_name="profile.html",
+        templates={
+            "user": data["user"],
+            "background_image": background_image,
+            "insights": {
+                "data": data["stats"],
+                "last_week": calc_work_time_percentage(
+                    data["stats"]["human_readable_total"]
+                ),
+                "daily_average": calc_work_time_percentage(
+                    data["stats"]["human_readable_daily_average"], duration="day"
+                ),
+            },
+            "operating_systems": data["stats"]["operating_systems"],
+            "editors": data["stats"]["editors"],
+            "languages": data["stats"]["languages"],
+            "all_time_since_today": data["all_time_since_today"],
+        },
+        pages={
+            "viewport": {"width": 550, "height": 10},
+            "base_url": f"file://{TEMPLATES_DIR}",
+        },
+    )
+
+
+async def render_bind_result(status_code: int, content: str) -> str:
+
+    result = "success" if status_code == 200 else "error"
+
+    return await template_to_html(
+        template_path=str(TEMPLATES_DIR),
+        template_name=f"{result}.html.jinja2",
+        status_code=status_code,
+        content=content,
+    )
