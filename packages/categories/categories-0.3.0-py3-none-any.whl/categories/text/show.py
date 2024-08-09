aba8f39927d11ba53b18b18d7a83bbbf608428aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-09 14:05:41 +0000

    Add package categories/0.3.0/categories-0.3.0-py3-none-any.whl

---
+++ packages/categories/categories-0.3.0-py3-none-any.whl/categories/text/show.py
@@ -0,0 +1,18 @@
+from __future__ import annotations
+
+from dataclasses import dataclass
+from typing import TypeVar
+
+from categories.type import typeclass
+
+__all__ = (
+    'Show',
+)
+
+
+a = TypeVar('a')
+
+
+@dataclass(frozen=True)
+class Show(typeclass[a]):
+    def show(self, x : a, /) -> str: ...
