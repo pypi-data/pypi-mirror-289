e3e4db1b31252ce217d9d05e42b8391e75b17ccc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-09 04:04:22 +0000

    Add package matplotlib-visual/0.9.9.3/matplotlib_visual-0.9.9.3-py3-none-any.whl

---
+++ packages/matplotlib-visual/matplotlib_visual-0.9.9.3-py3-none-any.whl/matplotlib_visual/graph.py
@@ -0,0 +1,33 @@
+import os
+import subprocess
+import sys
+
+
+def display(snippet_name):
+    snippet_path = os.path.join(
+        os.path.dirname(__file__), "code_snippets", f"{snippet_name}.py"
+    )
+
+    with open(snippet_path, "r") as file:
+        source_code = file.read()
+
+    # Function to copy text to clipboard based on platform
+    def copy_to_clipboard(text):
+        # Linux
+        if "linux" in sys.platform:
+            subprocess.run(
+                ["/usr/bin/xclip", "-selection", "clipboard"],
+                input=text.strip().encode(),
+                check=True,
+            )
+
+        # Windows
+        elif "win32" in sys.platform:
+            subprocess.run(
+                ["C:\\Windows\\System32\\clip.exe"],
+                input=text.strip().encode(),
+                check=True,
+            )
+
+        else:
+            raise OSError("Unsupported operating system")
