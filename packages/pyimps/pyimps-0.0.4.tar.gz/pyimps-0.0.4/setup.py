059a4f3b5ac9734ef202871fd4e88c1fd05e5b42
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-09 07:04:41 +0000

    Add package pyimps/0.0.4/pyimps-0.0.4.tar.gz

---
+++ packages/pyimps/pyimps-0.0.4.tar.gz/pyimps-0.0.4/setup.py
@@ -0,0 +1,31 @@
+# Copyright (c) 2023 bedbad
+from setuptools import setup, find_packages
+
+setup(
+    name='pyimps',
+    version='0.0.4',
+    packages=find_packages(),
+    install_requires=[
+        'requests',
+    ],
+    entry_points={
+        'console_scripts': [
+            'pyimps = pyimps.pyimps:main',
+        ],
+    },
+    author='bedbad',
+    author_email='antonyuk@bu.edu',
+    description='Unconfuse your Py imports',
+    long_description='''Resolve your Py imports - see entire listing \r\n'''
+                     '''Where all tha you have sit; \r\n'''
+                     '''Where all tha you don't, should be; \r\n'''
+                     '''See all transitives the way the were imported and defined. \r\n'''
+                     '''All of it - right in your terminal \r\n''',
+    long_description_content_type='text/markdown',
+    url='https://github.com/bedbad/pyimps',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Operating System :: OS Independent',
+    ],
+)
