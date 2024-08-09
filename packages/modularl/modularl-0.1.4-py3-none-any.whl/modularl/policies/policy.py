2dc8568c5630a7ba69069daef70498ba250148be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-09 14:05:21 +0000

    Add package modularl/0.1.4/modularl-0.1.4-py3-none-any.whl

---
+++ packages/modularl/modularl-0.1.4-py3-none-any.whl/modularl/policies/policy.py
@@ -0,0 +1,35 @@
+import torch
+import torch.nn as nn
+from abc import ABC, abstractmethod
+
+
+class AbstractPolicy(nn.Module, ABC):
+
+    def __init__(self, **kwargs):
+        super(AbstractPolicy, self).__init__()
+
+    @abstractmethod
+    def forward(self, batch_observation: torch.Tensor) -> torch.Tensor:
+        """
+        Forward pass of the policy network
+
+        Args:
+            batch_observation (torch.Tensor): Batch observation from the environment
+        """  # noqa
+
+    @abstractmethod
+    def get_action(self, batch_observation: torch.Tensor) -> torch.Tensor:
+        """
+        Get action from the policy
+
+        Args:
+            batch_observation (torch.Tensor): Batch observation from the environment
+        return:
+            batch_action (torch.Tensor): Batch action to be taken
+        """  # noqa
+
+    @abstractmethod
+    def _initialize_weights(self) -> None:
+        """
+        Initialize weights of the policy
+        """  # noqa
