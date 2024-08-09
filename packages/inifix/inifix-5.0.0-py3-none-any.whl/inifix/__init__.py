496272f319a24d4864eddcba457d536c482cbba4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-09 16:04:47 +0000

    Add package inifix/5.0.0/inifix-5.0.0-py3-none-any.whl

---
+++ packages/inifix/inifix-5.0.0-py3-none-any.whl/inifix/__init__.py
@@ -0,0 +1,8 @@
+from .io import dump
+from .io import dumps
+from .io import load
+from .io import loads
+from .validation import validate_inifile_schema
+from .format import format_string
+
+__version__ = "5.0.0"
