55cbb3ac7801a5b412a0c1a3adc4ed0fd95582fa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-09 05:04:20 +0000

    Add package ehrapy/0.9.0/ehrapy-0.9.0.tar.gz

---
+++ packages/ehrapy/ehrapy-0.9.0.tar.gz/ehrapy-0.9.0/README.md
@@ -0,0 +1,44 @@
+[![Black](https://img.shields.io/badge/code%20style-black-000000.svg)](https://github.com/psf/black)
+[![Build](https://github.com/theislab/ehrapy/actions/workflows/build.yml/badge.svg)](https://github.com/theislab/ehrapy/actions/workflows/build.yml)
+[![Codecov](https://codecov.io/gh/theislab/ehrapy/branch/master/graph/badge.svg)](https://codecov.io/gh/theislab/ehrapy)
+[![License](https://img.shields.io/github/license/theislab/ehrapy)](https://opensource.org/licenses/Apache2.0)
+[![PyPI](https://img.shields.io/pypi/v/ehrapy.svg)](https://pypi.org/project/ehrapy/)
+[![Python Version](https://img.shields.io/pypi/pyversions/ehrapy)](https://pypi.org/project/ehrapy)
+[![Read the Docs](https://img.shields.io/readthedocs/ehrapy/latest.svg?label=Read%20the%20Docs)](https://ehrapy.readthedocs.io/)
+[![Test](https://github.com/theislab/ehrapy/actions/workflows/test.yml/badge.svg)](https://github.com/theislab/ehrapy/actions/workflows/test.yml)
+[![pre-commit](https://img.shields.io/badge/pre--commit-enabled-brightgreen?logo=pre-commit&logoColor=white)](https://github.com/pre-commit/pre-commit)
+
+<img src="https://user-images.githubusercontent.com/21954664/156930990-0d668468-0cd9-496e-995a-96d2c2407cf5.png" alt="ehrapy logo">
+
+# ehrapy overview
+
+![fig1](https://github.com/user-attachments/assets/7927aa20-751c-4e73-8939-1e4b1c465570)
+
+## Features
+
+-   Exploratory and targeted analysis of Electronic Health Records
+-   Quality control & preprocessing
+-   Visualization & Exploration
+-   Clustering & trajectory inference
+
+## Installation
+
+You can install _ehrapy_ via [pip] from [PyPI]:
+
+```console
+$ pip install ehrapy
+```
+
+## Usage
+
+Please have a look at the [Usage documentation](https://ehrapy.readthedocs.io/en/latest/usage/usage.html) and the [tutorials](https://ehrapy.readthedocs.io/en/latest/tutorials/index.html).
+
+```python
+import ehrapy as ep
+```
+
+## Citation
+
+[ Exploratory electronic health record analysis with ehrapy
+Lukas Heumos, Philipp Ehmele, Tim Treis, Julius Upmeier zu Belzen, Altana Namsaraeva, Nastassya Horlava, Vladimir A. Shitov, Xinyue Zhang, Luke Zappia, Rainer Knoll, Niklas J. Lang, Leon Hetzel, Isaac Virshup, Lisa Sikkema, Eljas Roellin, Fabiola Curion, Roland Eils, Herbert B. Schiller, Anne Hilgendorff, Fabian J. Theis
+medRxiv 2023.12.11.23299816; doi: https://doi.org/10.1101/2023.12.11.23299816 ](https://www.medrxiv.org/content/10.1101/2023.12.11.23299816v1).
---
+++ packages/ehrapy/ehrapy-0.9.0.tar.gz/ehrapy-0.9.0/tests/preprocessing/test_imputation.py
@@ -0,0 +1,221 @@
+import os
+import warnings
+from pathlib import Path
+
+import numpy as np
+import pytest
+from sklearn.exceptions import ConvergenceWarning
+
+from ehrapy.preprocessing._imputation import (
+    _warn_imputation_threshold,
+    explicit_impute,
+    knn_impute,
+    mice_forest_impute,
+    miss_forest_impute,
+    simple_impute,
+)
+from tests.conftest import TEST_DATA_PATH
+
+CURRENT_DIR = Path(__file__).parent
+_TEST_PATH = f"{TEST_DATA_PATH}/imputation"
+
+
+def test_mean_impute_no_copy(impute_num_adata):
+    simple_impute(impute_num_adata)
+
+    assert not np.isnan(impute_num_adata.X).any()
+
+
+def test_mean_impute_copy(impute_num_adata):
+    adata_imputed = simple_impute(impute_num_adata, copy=True)
+
+    assert id(impute_num_adata) != id(adata_imputed)
+    assert not np.isnan(adata_imputed.X).any()
+
+
+def test_mean_impute_throws_error_non_numerical(impute_adata):
+    with pytest.raises(ValueError):
+        simple_impute(impute_adata)
+
+
+def test_mean_impute_subset(impute_adata):
+    adata_imputed = simple_impute(impute_adata, var_names=["intcol", "indexcol"], copy=True)
+
+    assert not np.all([item != item for item in adata_imputed.X[::, 1:2]])
+    assert np.any([item != item for item in adata_imputed.X[::, 3:4]])
+
+
+def test_median_impute_no_copy(impute_num_adata):
+    simple_impute(impute_num_adata, strategy="median")
+
+    assert not np.isnan(impute_num_adata.X).any()
+
+
+def test_median_impute_copy(impute_num_adata, impute_adata):
+    adata_imputed = simple_impute(impute_num_adata, strategy="median", copy=True)
+
+    assert id(impute_adata) != id(adata_imputed)
+    assert not np.isnan(adata_imputed.X).any()
+
+
+def test_median_impute_throws_error_non_numerical(impute_adata):
+    with pytest.raises(ValueError):
+        simple_impute(impute_adata, strategy="median")
+
+
+def test_median_impute_subset(impute_adata):
+    adata_imputed = simple_impute(impute_adata, var_names=["intcol", "indexcol"], strategy="median", copy=True)
+
+    assert not np.all([item != item for item in adata_imputed.X[::, 1:2]])
+    assert np.any([item != item for item in adata_imputed.X[::, 3:4]])
+
+
+def test_most_frequent_impute_no_copy(impute_adata):
+    simple_impute(impute_adata, strategy="most_frequent")
+
+    assert not (np.all([item != item for item in impute_adata.X]))
+
+
+def test_most_frequent_impute_copy(impute_adata):
+    adata_imputed = simple_impute(impute_adata, strategy="most_frequent", copy=True)
+
+    assert id(impute_adata) != id(adata_imputed)
+    assert not (np.all([item != item for item in adata_imputed.X]))
+
+
+def test_most_frequent_impute_subset(impute_adata):
+    adata_imputed = simple_impute(impute_adata, var_names=["intcol", "strcol"], strategy="most_frequent", copy=True)
+
+    assert not (np.all([item != item for item in adata_imputed.X[::, 1:3]]))
+
+
+def test_knn_impute_check_backend(impute_num_adata):
+    knn_impute(impute_num_adata, backend="faiss")
+    knn_impute(impute_num_adata, backend="scikit-learn")
+    with pytest.raises(
+        ValueError,
+        match="Unknown backend 'invalid_backend' for KNN imputation. Choose between 'scikit-learn' and 'faiss'.",
+    ):
+        knn_impute(impute_num_adata, backend="invalid_backend")
+
+
+def test_knn_impute_no_copy(impute_num_adata):
+    knn_impute(impute_num_adata)
+
+    assert not (np.all([item != item for item in impute_num_adata.X]))
+
+
+def test_knn_impute_copy(impute_num_adata):
+    adata_imputed = knn_impute(impute_num_adata, n_neighbors=3, copy=True)
+
+    assert id(impute_num_adata) != id(adata_imputed)
+    assert not (np.all([item != item for item in adata_imputed.X]))
+
+
+def test_knn_impute_non_numerical_data(impute_adata):
+    adata_imputed = knn_impute(impute_adata, n_neighbors=3, copy=True)
+
+    assert not (np.all([item != item for item in adata_imputed.X]))
+
+
+def test_knn_impute_numerical_data(impute_num_adata):
+    adata_imputed = knn_impute(impute_num_adata, copy=True)
+
+    assert not (np.all([item != item for item in adata_imputed.X]))
+
+
+def test_knn_impute_list_str(impute_adata):
+    adata_imputed = knn_impute(impute_adata, var_names=["intcol", "strcol", "boolcol"], copy=True)
+
+    assert not (np.all([item != item for item in adata_imputed.X]))
+
+
+def test_missforest_impute_non_numerical_data(impute_adata):
+    adata_imputed = miss_forest_impute(impute_adata, copy=True)
+
+    assert not (np.all([item != item for item in adata_imputed.X]))
+
+
+def test_missforest_impute_numerical_data(impute_num_adata):
+    warnings.filterwarnings("ignore", category=ConvergenceWarning)
+    adata_imputed = miss_forest_impute(impute_num_adata, copy=True)
+
+    assert not (np.all([item != item for item in adata_imputed.X]))
+
+
+def test_missforest_impute_subset(impute_num_adata):
+    adata_imputed = miss_forest_impute(
+        impute_num_adata, var_names={"non_numerical": ["intcol"], "numerical": ["strcol"]}, copy=True
+    )
+
+    assert not (np.all([item != item for item in adata_imputed.X]))
+
+
+def test_missforest_impute_list_str(impute_num_adata):
+    warnings.filterwarnings("ignore", category=ConvergenceWarning)
+    adata_imputed = miss_forest_impute(impute_num_adata, var_names=["col1", "col2", "col3"], copy=True)
+
+    assert not (np.all([item != item for item in adata_imputed.X]))
+
+
+def test_missforest_impute_dict(impute_adata):
+    warnings.filterwarnings("ignore", category=ConvergenceWarning)
+    adata_imputed = miss_forest_impute(
+        impute_adata, var_names={"numerical": ["intcol", "datetime"], "non_numerical": ["strcol", "boolcol"]}, copy=True
+    )
+
+    assert not (np.all([item != item for item in adata_imputed.X]))
+
+
+@pytest.mark.skipif(os.name == "posix", reason="miceforest Imputation not supported by MacOS.")
+def test_miceforest_impute_no_copy(impute_iris_adata_adata):
+    adata_imputed = mice_forest_impute(impute_iris_adata_adata)
+
+    assert id(impute_iris_adata_adata) == id(adata_imputed)
+
+
+@pytest.mark.skipif(os.name == "posix", reason="miceforest Imputation not supported by MacOS.")
+def test_miceforest_impute_copy(impute_iris_adata):
+    adata_imputed = mice_forest_impute(impute_iris_adata, copy=True)
+
+    assert id(impute_iris_adata) != id(adata_imputed)
+
+
+@pytest.mark.skipif(os.name == "posix", reason="miceforest Imputation not supported by MacOS.")
+def test_miceforest_impute_non_numerical_data(impute_titanic_adata):
+    adata_imputed = mice_forest_impute(impute_titanic_adata)
+
+    assert not (np.all([item != item for item in adata_imputed.X]))
+
+
+@pytest.mark.skipif(os.name == "posix", reason="miceforest Imputation not supported by MacOS.")
+def test_miceforest_impute_numerical_data(impute_iris_adata):
+    adata_imputed = mice_forest_impute(impute_iris_adata)
+
+    assert not (np.all([item != item for item in adata_imputed.X]))
+
+
+@pytest.mark.skipif(os.name == "posix", reason="miceforest Imputation not supported by MacOS.")
+def test_miceforest_impute_list_str(impute_titanic_adata):
+    adata_imputed = mice_forest_impute(impute_titanic_adata, var_names=["Cabin", "Age"])
+
+    assert not (np.all([item != item for item in adata_imputed.X]))
+
+
+def test_explicit_impute_all(impute_num_adata):
+    warnings.filterwarnings("ignore", category=FutureWarning)
+    adata_imputed = explicit_impute(impute_num_adata, replacement=1011, copy=True)
+
+    assert (adata_imputed.X == 1011).sum() == 3
+
+
+def test_explicit_impute_subset(impute_adata):
+    adata_imputed = explicit_impute(impute_adata, replacement={"strcol": "REPLACED", "intcol": 1011}, copy=True)
+
+    assert (adata_imputed.X == 1011).sum() == 1
+    assert (adata_imputed.X == "REPLACED").sum() == 1
+
+
+def test_warning(impute_num_adata):
+    warning_results = _warn_imputation_threshold(impute_num_adata, threshold=20, var_names=None)
+    assert warning_results == {"col1": 25, "col3": 50}
