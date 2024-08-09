926c50f265b41039a2deba534a67983d77762f11
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-09 04:06:01 +0000

    Add package dekdjtools/0.1.51/dekdjtools-0.1.51-py3-none-any.whl

---
+++ packages/dekdjtools/dekdjtools-0.1.51-py3-none-any.whl/dekdjtools/utils/migration.py
@@ -0,0 +1,26 @@
+import os
+from django.conf import settings
+from django.apps import apps
+from dektools.file import normal_path
+
+project_dir = normal_path(settings.BASE_DIR)
+
+project_dir_prefix = project_dir + os.path.sep
+
+env_list = [".venv", "venv", "env"]
+
+project_dir_env_list = [os.path.join(project_dir, env) + os.path.sep for env in env_list]
+
+
+def list_migrations():
+    result = set()
+    for app in apps.get_app_configs():
+        app_path = app.path
+        if app_path.startswith(project_dir_prefix) and all(not app_path.startswith(x) for x in project_dir_env_list):
+            migrations_path = os.path.join(app_path, 'migrations')
+            if os.path.isdir(migrations_path):
+                for item in os.listdir(migrations_path):
+                    item_path = os.path.join(migrations_path, item)
+                    if os.path.isfile(item_path) and item != '__init__.py' and os.path.splitext(item)[-1] == '.py':
+                        result.add(item_path)
+    return project_dir, result
