8c1e47b39568388a60c90bbd0b1b9fb167980ffd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-09 16:04:57 +0000

    Add package odoo14-addon-ssi-sale/14.0.5.4.0/odoo14_addon_ssi_sale-14.0.5.4.0-py3-none-any.whl

---
+++ packages/odoo14-addon-ssi-sale/odoo14_addon_ssi_sale-14.0.5.4.0-py3-none-any.whl/odoo/addons/ssi_sale/models/__init__.py
@@ -0,0 +1,9 @@
+# Copyright 2023 OpenSynergy Indonesia
+# Copyright 2023 PT. Simetri Sinergi Indonesia
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
+from . import (
+    sale_order_type,
+    sale_order,
+    sale_order_line,
+)
