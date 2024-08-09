67262976f93fafcdfc020d13eaae5a81666f7f1c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-09 06:04:08 +0000

    Add package hr-commons/0.1/hr_commons-0.1.tar.gz

---
+++ packages/hr-commons/hr_commons-0.1.tar.gz/hr_commons-0.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+  name='hr-commons',
+  packages=['hrcommons'],
+  version='0.1',
+  description=("Common code for HR"),
+  author='Joshua "jag" Ginsberg',
+  author_email='jag@flowtheory.net',
+  install_requires=[]
+)
+
+print("Don't install public packages from PyPI you don't know what they are!")
