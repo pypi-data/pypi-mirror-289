c6c08b5b467e2ccd9262aea18c9b20dbc8994022
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-09 04:04:11 +0000

    Add package view-selection-python/0.1.5/view_selection_python-0.1.5-py3-none-any.whl

---
+++ packages/view-selection-python/view_selection_python-0.1.5-py3-none-any.whl/view_selection_python/CostEstimatorSinglePlan.py
@@ -0,0 +1,88 @@
+"""CostEstimatorSinglePlan class."""
+
+from typing import Dict, List, Tuple
+
+
+def _check_for_subplans(plan: Dict) -> bool:
+    """Check if the plan contains subplans."""
+    return "Plans" in plan
+
+
+def _calculate_cost(expected_rows: float, expected_width: float) -> float:
+    """Calculate the cost based on expected rows and width."""
+    return expected_rows * expected_width
+
+
+def _read_plan_contents(plan: Dict) -> Tuple[float, float, List[Dict] | None]:
+    """Extract expected rows, width, and subplans from the plan."""
+    expected_rows = plan["Plan Rows"]
+    expected_width = plan["Plan Width"]
+
+    subplans = plan["Plans"] if _check_for_subplans(plan) else None
+
+    return expected_rows, expected_width, subplans
+
+
+def _extract_plan_from_list(list_with_plan: List[Dict]) -> Dict:
+    """Extract the plan from a list."""
+    return list_with_plan[0]["Plan"]
+
+
+class CostEstimatorSinglePlan:
+    """This class estimates the cost of a single model.
+
+    This is done by traversing through the entire query plan as obtained by calling
+    EXPLAIN in postgres
+    """
+
+    def __init__(self):
+        """Initialize the class."""
+        self._reset_costs()
+
+    def _reset_costs(self):
+        """Reset the storage and execution cost."""
+        self.storage_cost = 0
+        self.creation_cost = 0
+
+    def _update_storage_cost(self, cost: float):
+        """Update the storage cost."""
+        self.storage_cost += cost
+
+    def _update_creation_cost(self, cost: float):
+        """Update the creation cost."""
+        self.creation_cost += cost
+
+    def _update_costs(self, cost: float, is_root_of_plan: bool):
+        """Update both storage and creation costs.
+
+        However, storage cost should only be updated if it's the root of the plan.
+        """
+        if is_root_of_plan:
+            self._update_storage_cost(cost)
+
+        self._update_creation_cost(cost)
+
+    def estimate_costs(
+        self, plan: List[Dict] | Dict, is_root_of_plan: bool = True
+    ) -> Tuple[float, float]:
+        """Estimate costs based on the provided plan.
+
+        This is done by extracting the E[#rows] and the E[width] of each row.
+        Do this recursively over all subplans, to get all the relevant costs.
+        """
+        if is_root_of_plan:
+            self._reset_costs()
+
+        if isinstance(plan, List):
+            plan = _extract_plan_from_list(plan)
+
+        expected_rows, expected_width, subplans = _read_plan_contents(plan)
+        cost = _calculate_cost(expected_rows, expected_width)
+
+        self._update_costs(cost, is_root_of_plan=is_root_of_plan)
+
+        if subplans:
+            for subplan in subplans:
+                self.estimate_costs(subplan, is_root_of_plan=False)
+
+        return self.storage_cost, self.creation_cost
---
+++ packages/view-selection-python/view_selection_python-0.1.5-py3-none-any.whl/view_selection_python/ViewSelectionAdvisor.py
@@ -0,0 +1,140 @@
+"""ViewSelectionAdvisor class."""
+
+from math import inf
+from typing import Deque, Tuple
+
+from .ConfigCostEstimator import ConfigCostEstimator
+from .ConfigurationGenerator import MaterializationConfigurationGenerator
+from .CwdChecker import CwdChecker
+from .ModelInfoManager import ModelInfoManager
+from .PostgresHandler import PostgresHandler
+from ruamel.yaml.comments import CommentedMap
+from .ProfilesScraper import ProfilesScraper
+from .DbtProjectScraper import DbtProjectScraper
+from tqdm import tqdm
+from .CLI import CLI
+
+
+class ViewSelectionAdvisor:
+    """The ViewSelectionAdvisor class.
+
+    This class compares all possible configurations, and picks
+    the one with the lowest expected cost.
+    """
+
+    def __init__(self, n_mater_in_config: int = 2):
+        """Initialize, do checks to the environment, and create necessary objects."""
+        self.n_mater_in_config = n_mater_in_config
+        self.cwd_checker = CwdChecker()
+        self.dbt_project_scraper = None
+        self.profiles_scraper = None
+        self.postgres_handler = None
+        self.model_info_manager = None
+        self.config_cost_estimator = None
+        self._create_necessary_objects()
+
+    def _create_necessary_objects(self):
+        """Create objects necessary for providing the view selection advise."""
+        self._create_dbt_project_scraper()
+        self._create_profiles_scraper()
+        self._create_postgres_handler()
+        self._create_model_info_manager()
+        self._create_config_cost_estimator()
+
+    def _create_dbt_project_scraper(self):
+        """Create an instance of DbtProjectScraper which will read contents of dbt_project.yml."""
+        dbt_project_path = self.cwd_checker.get_dbt_project_path()
+        self.dbt_project_scraper = DbtProjectScraper(filepath=dbt_project_path)
+
+    def _get_profile_name(self) -> str:
+        """Return the profile which contains the relevant db credentials."""
+        # If the user manually specified a profile through cli, use that one
+        cli = CLI()
+        cli_profile = cli.get_profile()
+
+        if cli_profile:
+            return cli_profile
+
+        # If no specified profile in cli, use the profile specified in dbt_project.yml
+        else:
+            return self.dbt_project_scraper.get_profile()
+
+    def _create_profiles_scraper(self):
+        """Create an instance of ProfilesScraper which will read contents of profiles.yml."""
+        profiles_yml_path = self.cwd_checker.get_profiles_path()
+        profile_name = self._get_profile_name()
+        self.profiles_scraper = ProfilesScraper(
+            filepath=profiles_yml_path,
+            profile_name=profile_name
+        )
+
+    def _obtain_db_credentials(self) -> CommentedMap:
+        """Return the db credentials of the DB schema.
+
+        This is the schema where the relevant tables from the dbt
+        part of the ViewSelectionTool are stored
+        """
+        return self.profiles_scraper.get_db_creds(
+            schema_appendix=self.dbt_project_scraper.get_schema_appendix(
+                profile='view_selection_tool'
+            )
+        )
+
+    def _create_postgres_handler(self):
+        """Create an instance of PostgresHandler which will communicate with the DB."""
+        db_creds = self._obtain_db_credentials()
+        self.postgres_handler = PostgresHandler(db_creds=db_creds)
+
+    def _create_model_info_manager(self):
+        """Create an instance of ModelInfoManager.
+
+        This instance which will contain all potentially relevant info on the models.
+        """
+        self.model_info_manager = ModelInfoManager(
+            postgres_handler=self.postgres_handler
+        )
+
+    def _create_config_cost_estimator(self):
+        """Create an instance of ConfigCostEstimator.
+
+        This instance which will calculate the cost for each possible configuration.
+        """
+        self.config_cost_estimator = ConfigCostEstimator(
+            models_info_dict=self.model_info_manager.get_model_info_dict(),
+            destination_nodes=self.model_info_manager.get_list_of_destination_nodes(),
+        )
+
+    def _get_configs_to_check(self) -> Deque[None | Tuple[str]]:
+        """Create a deque of configurations to check the cost for."""
+        config_list_generator = MaterializationConfigurationGenerator(
+            all_intermediate_models=self.model_info_manager.get_all_intermediate_models(),  # noqa E501
+            max_materializations=self.n_mater_in_config
+        )
+        return config_list_generator.get_all_possible_configurations()
+
+    def advise(self) -> str | None:
+        """Find and return the best configuration.
+
+        This is done by looping over all possible configuration and keeping track of
+        which configuration result in the lowest cost.
+
+        When checking a configuration, also make sure it fits within the storage bound
+        """
+        configs_to_check = self._get_configs_to_check()
+
+        minimal_cost = inf
+        best_config = None
+
+        storage_bound = self.postgres_handler.get_storage_space_left()
+
+        for config in tqdm(configs_to_check):
+
+            total_config_cost, total_storage_cost = (
+                self.config_cost_estimator.estimate_cost_of_configuration(config)
+            )
+
+            if total_config_cost < minimal_cost and total_storage_cost < storage_bound:
+                minimal_cost = total_config_cost
+                best_config = config
+
+        return best_config
