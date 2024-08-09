6fd7753e079af1e5b98f3a0de2fb3a919c942ec9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-09 05:04:39 +0000

    Add package azrail/2.1.5/azrail-2.1.5.tar.gz

---
+++ packages/azrail/azrail-2.1.5.tar.gz/azrail-2.1.5/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='azrail',
+    version='2.1.5',
+    packages=find_packages(),
+    install_requires=[],
+    description='A package for generating various user agents',
+    author='Your Name',
+    author_email='your.email@example.com',
+    url='https://github.com/yourusername/azrail',  # GitHub veya diğer URL'nizi buraya koyun
+)
