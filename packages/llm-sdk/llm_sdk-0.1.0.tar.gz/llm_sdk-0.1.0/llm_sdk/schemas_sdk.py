0c932c1393af9dd0a502b87991adf57df212c0f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-09 03:04:59 +0000

    Add package llm-sdk/0.1.0/llm_sdk-0.1.0.tar.gz

---
+++ packages/llm-sdk/llm_sdk-0.1.0.tar.gz/llm_sdk-0.1.0/llm_sdk/schemas_sdk.py
@@ -0,0 +1,28 @@
+from pydantic import BaseModel
+from typing import Optional
+
+
+class ProviderModel(BaseModel):
+    name: str
+    description: Optional[str]
+    provider_id: int
+    req_integration_params: dict
+
+
+class ModelCreatorModel(BaseModel):
+    name: str
+    description: str
+    creator_id: int
+
+
+class AvailableModelModel(BaseModel):
+    provider: ProviderModel
+    creator: ModelCreatorModel
+    model_type: str
+    name: str
+    model_name: Optional[str]
+    model_version: Optional[str]
+    is_verified: bool
+    cost_input_token: float
+    cost_output_token: float
+
