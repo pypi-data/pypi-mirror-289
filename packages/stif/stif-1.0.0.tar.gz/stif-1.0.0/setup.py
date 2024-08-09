63a605daaed4960ce699e6c95f7d1ac8d162b952
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-09 03:05:27 +0000

    Add package stif/1.0.0/stif-1.0.0.tar.gz

---
+++ packages/stif/stif-1.0.0.tar.gz/stif-1.0.0/setup.py
@@ -0,0 +1,67 @@
+import os
+
+from setuptools import setup
+
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+
+setup(
+    name="stif",
+    version="1.0.0",
+    author="Johannes Kopton",
+    author_email="johannes@kopton.org",
+    description=("Forecasting and Interpolation in Space and Time"),
+    keywords="kriging variogram spacetime spatio-temporal",
+    url="https://github.com/johanneskopton/stif",
+    packages=["stif"],
+    long_description=read("README.md"),
+    long_description_content_type="text/markdown",
+    classifiers=[
+        "Topic :: Scientific/Engineering",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Framework :: tox",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.9",
+    install_requires=[
+        "numpy>=1.19,<2.0",
+        "matplotlib",
+        "pandas",
+        "scikit-learn",
+        "numba>=0.54,<=0.60",
+        "scipy",
+    ],
+    extras_require={
+        "tensorflow": [
+            "tensorflow",
+            "keras",
+        ],
+        "geo": [
+            "geopandas",
+            "shapely",
+            "rasterio",
+        ],
+        "interactive": [
+            "ipykernel",
+        ],
+        "dev": [
+            "pytest",
+            "pytest-cov",
+            "pytest-html",
+            "genbadge[all]",
+            "flake8",
+            "flake8-html",
+            "autopep8",
+            "pre-commit",
+            "sphinx",
+            "myst-parser",
+            "pydata-sphinx-theme",
+            "tox",
+        ],
+    },
+)
