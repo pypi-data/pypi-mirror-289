2560cbf3a38f1903840cc8d82e9c23450838690f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-09 15:04:44 +0000

    Add package random-forest-package/0.1.5/random_forest_package-0.1.5.tar.gz

---
+++ packages/random-forest-package/random_forest_package-0.1.5.tar.gz/random_forest_package-0.1.5/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='random_forest_package',
+    version='0.1.5',
+    packages=find_packages(),
+    install_requires=[
+        'scikit-learn',
+        'numpy',
+        'pandas',
+        'flake8',
+        'lint',
+        'matplotlib',
+        'seaborn'
+    ],
+    author='Karim Mirzaguliyev',
+    author_email='karimmirzaguliyev@gmail.com',
+    description='A package to facilitate random forest modeling',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/k4rimDev/Random-Forest-Modelling',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
