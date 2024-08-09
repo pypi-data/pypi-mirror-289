19c519f8b8218e58e56c6a44aebc86ad0a0fc94f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-09 15:05:01 +0000

    Add package lark-oapi/1.3.1/lark_oapi-1.3.1-py3-none-any.whl

---
+++ packages/lark-oapi/lark_oapi-1.3.1-py3-none-any.whl/lark_oapi/core/model/base_response.py
@@ -0,0 +1,23 @@
+from typing import *
+
+from lark_oapi.core.const import X_TT_LOGID
+from lark_oapi.core.construct import init
+from .raw_response import RawResponse
+
+
+class BaseResponse(object):
+    _types = {}
+
+    def __init__(self, d=None):
+        self.raw: Optional[RawResponse] = None
+        self.code: Optional[int] = None
+        self.msg: Optional[str] = None
+        init(self, d, self._types)
+
+    def success(self):
+        return self.code is not None and self.code == 0
+
+    def get_log_id(self) -> Optional[str]:
+        if self.raw is None or self.raw.headers is None:
+            return None
+        return self.raw.headers.get(X_TT_LOGID)
