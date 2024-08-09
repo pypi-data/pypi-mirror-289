7bac49705f4a196456de3c3910df14377683a9a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-09 14:06:09 +0000

    Add package knowledgekit/0.0.1/knowledgekit-0.0.1.tar.gz

---
+++ packages/knowledgekit/knowledgekit-0.0.1.tar.gz/knowledgekit-0.0.1/README.md
@@ -0,0 +1,2 @@
+# knowledgekit-python
+
---
+++ packages/knowledgekit/knowledgekit-0.0.1.tar.gz/knowledgekit-0.0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="knowledgekit",
+    version="0.0.1",
+    packages=["knowledgekit"],
+    install_requires=[]
+)
