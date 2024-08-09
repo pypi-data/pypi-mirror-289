f875d428f208694a4ac8c05fb58011fc1def5c67
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-09 04:04:10 +0000

    Add package freeGPTFix/1.0.7/freeGPTFix-1.0.7-py3-none-any.whl

---
+++ packages/freeGPTFix/freeGPTFix-1.0.7-py3-none-any.whl/freeGPTFix/__init__.py
@@ -0,0 +1,17 @@
+from freeGPTFix import *
+from lib.headers import headers
+import requests
+__author__ = "RedPiar"
+__Name__ = "freeGPTFix"
+__version__ = "1.0.7"
+
+url = f"https://pypi.org/pypi/{__Name__}/json"
+	
+response = requests.get(url)
+	
+if response.status_code == 200:
+	data = response.json()
+		
+	version = data['info']['version']
+if version != __version__:
+	print(f"[freeGPTFix] Your version {__version__}, New version {version}")
