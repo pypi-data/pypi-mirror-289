af2224d9277bebd6baa692e0731c5fb710f3b4e4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-10 03:06:51 +0000

    Add package kaniko-deploy/0.3/kaniko_deploy-0.3.tar.gz

---
+++ packages/kaniko-deploy/kaniko_deploy-0.3.tar.gz/kaniko_deploy-0.3/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+import kaniko_deploy
+
+def parse_requirements(filename):
+    with open(filename, 'r') as f:
+        return [line.strip() for line in f if line and not line.startswith("#")]
+
+setup(
+    name=kaniko_deploy.__title__,
+    version='0.3',
+    packages=find_packages(),
+    install_requires=parse_requirements('requirements.txt'),
+    python_requires='>=3.12',
+    entry_points={
+        'console_scripts': [
+            'kaniko_deploy=kaniko_deploy.main:main',  # Replace with your main entry point
+        ],
+    },
+    license='MIT'
+    # Include other metadata as needed
+)
