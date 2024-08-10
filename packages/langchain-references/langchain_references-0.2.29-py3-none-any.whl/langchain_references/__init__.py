425469658d90580f76f19a891109318e0c312a5c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-10 03:05:32 +0000

    Add package langchain-references/0.2.29/langchain_references-0.2.29-py3-none-any.whl

---
+++ packages/langchain-references/langchain_references-0.2.29-py3-none-any.whl/langchain_references/__init__.py
@@ -0,0 +1,17 @@
+from .references import (
+    EmptyReferenceStyle,
+    HTMLReferenceStyle,
+    MarkdownReferenceStyle,
+    ReferenceStyle,
+    TextReferenceStyle,
+    manage_references,
+)
+
+__all__ = [
+    "ReferenceStyle",
+    "EmptyReferenceStyle",
+    "TextReferenceStyle",
+    "HTMLReferenceStyle",
+    "MarkdownReferenceStyle",
+    "manage_references",
+]
