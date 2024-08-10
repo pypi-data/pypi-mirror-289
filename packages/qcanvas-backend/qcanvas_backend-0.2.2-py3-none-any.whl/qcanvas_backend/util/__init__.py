7efa89dde6e7a692835f9948c55d23b53aeed893
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-10 14:05:47 +0000

    Add package qcanvas-backend/0.2.2/qcanvas_backend-0.2.2-py3-none-any.whl

---
+++ packages/qcanvas-backend/qcanvas_backend-0.2.2-py3-none-any.whl/qcanvas_backend/util/__init__.py
@@ -0,0 +1,4 @@
+from .canvas_data_sanitiser import remove_unwanted_whitespaces
+from .collecting_task_group import CollectingTaskGroup
+from .flatten import flatten
+from .form_input_extractor import extract_form_inputs
