0df6b391840ed371f1063e09586c3e5310d138b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-10 04:04:30 +0000

    Add package brighteyes-ffs/0.0.1/brighteyes_ffs-0.0.1.tar.gz

---
+++ packages/brighteyes-ffs/brighteyes_ffs-0.0.1.tar.gz/brighteyes_ffs-0.0.1/src/brighteyes_ffs/fcs_gui/fitmodel_class.py
@@ -0,0 +1,44 @@
+"""
+Fitmodels fields:
+    model                   str                 name of the model
+    paramNames              list of str         names of the parameters
+    paramDefvalues          list of numbers     default values for the parameters
+    allparamDefvalues       list of numbers     default values for all parameters that the function requires
+    fitfunctionParamUsed    list of numbers     indices of the parameters of the fit function that are actually used
+    paramFactors10          list of numbers     powers of 10 with which the parameters have to be multiplied
+    paramMinbound           list of numbers     minimum values for all parameters (must already include factors of 10)
+    paramMaxbound           list of numbers     maximum values for all parameters (must already include factors of 10)
+    fitfunctionName         function            name of the fit function
+    
+    Example: a fit function f(a, b, c, d) has 4 input parameters but parameter c must always be set to 3.14 in a given model
+    Then:   paramNames  = ['a', 'b', 'd']
+            paramDefvalues = [3, 9.2, -4] # default values for parameters a, b, d
+            allparamDefvalues = [-1, -1, 3.14, -1] # set c to 3.14 and all other parameters to -1
+            fitfunctionParamUsed = [0, 1, 3] # only parameters 0, 1, and 3 are used for the fit model
+"""
+
+class FitModel: # model, get="all"
+    def __init__(self):
+        self.model = None
+        self.shortlabel = None
+        self.paramNames =None
+        self.paramFittable = None
+        self.paramDefvalues = None
+        self.allparamDefvalues = None
+        self.paramFactors10 = None
+        self.paramMinbound = None
+        self.paramMaxbound = None
+        self.fitfunctionName = None
+        self.fitfunctionParamUsed = None
+            
+    
+    def returnfitparam(self):
+        return self.model, self.paramNames, self.paramDefvalues, self.fitfunctionName, self.fitfunctionParamUsed, self.paramFactors10, self.paramMinbound, self.paramMaxbound
+
+    @property
+    def Nparam(self):
+        if self.fitfunctionName is None and self.allparamDefvalues is None:
+            return 1
+        if self.model == 'Free diffusion pair-correlation (global fit)':
+            return 26
+        return(len(self.paramMinbound))
