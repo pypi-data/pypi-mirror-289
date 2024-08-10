0ea5e5ab7de086564cecc84d75d21f7cc1039b1d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-10 03:05:21 +0000

    Add package dyson-equalizer/0.1.4/dyson_equalizer-0.1.4.tar.gz

---
+++ packages/dyson-equalizer/dyson_equalizer-0.1.4.tar.gz/dyson_equalizer-0.1.4/dyson_equalizer/__init__.py
@@ -0,0 +1,18 @@
+"""
+    The ``dyson_equalizer`` package provides a class functions implementing the algorithms needed
+    to compute the Dyson Equalizer [1]_ and related auxiliary functions.
+
+    The functions may be used to build specialized implementation of the Dyson Equalizer.
+
+    Examples
+    --------
+
+    See :ref:`examples`
+
+    References
+    ----------
+
+    .. [1] Landa B., Kluger Y., "The Dyson Equalizer: Adaptive Noise Stabilization for Low-Rank Signal
+       Detection and Recovery," arXiv, https://arxiv.org/abs/2306.11263
+
+"""
