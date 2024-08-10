1fee1056b6bdc345f41eb7b13e4d7a8c3ab64b9c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-10 05:04:32 +0000

    Add package saft-model/0.2.1/saft_model-0.2.1-py3-none-any.whl

---
+++ packages/saft-model/saft_model-0.2.1-py3-none-any.whl/saft_model/classes/PredictionResponse.py
@@ -0,0 +1,18 @@
+
+from datetime import datetime
+from classes.Sentiment import Sentiment
+
+class PredictionResponse():
+    def __init__(self, model_id: str, time_predicted_at: int, sentiment: Sentiment, confidence: float) -> None:
+        self.modelId = model_id
+        self.timePredictedAt = time_predicted_at
+        self.sentiment = sentiment
+        self.confidence = confidence
+
+    def to_dict(self) -> dict:
+        return {
+            "model_id": self.modelId,
+            "time_predicted_at": self.timePredictedAt,
+            "sentiment": self.sentiment._value_,
+            "confidence": self.confidence
+        }
---
+++ packages/saft-model/saft_model-0.2.1-py3-none-any.whl/saft_model/templates/model_implementation_file.py
@@ -0,0 +1,24 @@
+
+def format_template_file(project_name: str, model_name: str):
+    model_implementation_file_template = """
+# ------------------------------------------------------------------------------------------
+# SAFT MODEL PROJECT
+#
+# PROJECT NAME: {project_name}
+# MODEL NAME: {model_name}
+# ------------------------------------------------------------------------------------------
+
+
+from saft_model.classes import SAFTModelClass
+from saft_model.classes.InputData import InputData  
+from saft_model.classes.PredictionResponse import PredictionResponse
+
+class {model_name}(SAFTModelClass):
+    def predict(self, data_input: InputData) -> PredictionResponse:
+        return None
+
+""".format(
+        project_name=project_name,
+        model_name=model_name
+    )
+    return model_implementation_file_template
