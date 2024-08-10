c1f7e82f1b6a06e1ab01306cc49cbb2d7e960b4f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-10 14:05:13 +0000

    Add package odin-sdk/2.0.1/odin_sdk-2.0.1.tar.gz

---
+++ packages/odin-sdk/odin_sdk-2.0.1.tar.gz/odin_sdk-2.0.1/odin/odin/__init__.py
@@ -0,0 +1,12 @@
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from odin.odin.certificate_api import CertificateApi
+from odin.odin.exposed_buckets_api import ExposedBucketsApi
+from odin.odin.exposed_files_api import ExposedFilesApi
+from odin.odin.fields_api import FieldsApi
+from odin.odin.health_api import HealthApi
+from odin.odin.hosts_api import HostsApi
+from odin.odin.domain_api import DomainApi
