bf9f7b8c3a67a7568d4523de959a02b1b7b22a91
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-10 04:04:24 +0000

    Add package arbor/0.10.0/arbor-0.10.0-pp39-pypy39_pp73-macosx_12_0_x86_64.whl

---
+++ packages/arbor/arbor-0.10.0-pp39-pypy39_pp73-macosx_12_0_x86_64.whl/arbor/include/arbor/spike.hpp
@@ -0,0 +1,41 @@
+#pragma once
+
+#include <ostream>
+#include <type_traits>
+#include <functional>
+
+#include <arbor/common_types.hpp>
+#include <arbor/serdes.hpp>
+
+namespace arb {
+
+template <typename I>
+struct basic_spike {
+    using id_type = I;
+
+    id_type source = id_type{};
+    time_type time = -1;
+
+    basic_spike() = default;
+
+    basic_spike(id_type s, time_type t):
+        source(std::move(s)), time(t)
+    {}
+
+    ARB_SERDES_ENABLE(basic_spike<I>, source, time);
+};
+
+/// Standard specialization:
+using spike = basic_spike<cell_member_type>;
+
+using spike_predicate = std::function<bool(const spike&)>;
+
+ARB_DEFINE_LEXICOGRAPHIC_ORDERING(spike, (a.source, a.time), (b.source, b.time));
+
+// Custom stream operator for printing arb::spike<> values.
+template <typename I>
+std::ostream& operator<<(std::ostream& o, basic_spike<I> const& s) {
+    return o << "S[src " << s.source << ", t " << s.time << "]";
+}
+
+} // namespace arb
