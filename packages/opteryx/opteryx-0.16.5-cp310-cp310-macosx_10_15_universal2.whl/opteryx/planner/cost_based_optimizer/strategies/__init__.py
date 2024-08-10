11c1214f754d7519778cbfdf98c387c3f245dc30
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-10 05:04:56 +0000

    Add package opteryx/0.16.5/opteryx-0.16.5-cp310-cp310-macosx_10_15_universal2.whl

---
+++ packages/opteryx/opteryx-0.16.5-cp310-cp310-macosx_10_15_universal2.whl/opteryx/planner/cost_based_optimizer/strategies/__init__.py
@@ -0,0 +1,17 @@
+from .boolean_simplication import BooleanSimplificationStrategy
+from .constant_folding import ConstantFoldingStrategy
+from .operator_fusion import OperatorFusionStrategy
+from .predicate_pushdown import PredicatePushdownStrategy
+from .projection_pushdown import ProjectionPushdownStrategy
+from .redundant_operators import RedundantOperationsStrategy
+from .split_conjunctive_predicates import SplitConjunctivePredicatesStrategy
+
+__all__ = [
+    "BooleanSimplificationStrategy",
+    "ConstantFoldingStrategy",
+    "OperatorFusionStrategy",
+    "PredicatePushdownStrategy",
+    "ProjectionPushdownStrategy",
+    "RedundantOperationsStrategy",
+    "SplitConjunctivePredicatesStrategy",
+]
