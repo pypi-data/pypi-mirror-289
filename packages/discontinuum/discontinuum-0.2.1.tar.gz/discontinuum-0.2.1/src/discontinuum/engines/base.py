85760006e5d12f8093de1b73660c27d1476a6919
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-10 05:04:28 +0000

    Add package discontinuum/0.2.1/discontinuum-0.2.1.tar.gz

---
+++ packages/discontinuum/discontinuum-0.2.1.tar.gz/discontinuum-0.2.1/src/discontinuum/engines/base.py
@@ -0,0 +1,46 @@
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from typing import Dict, Optional
+
+import functools
+
+
+class BaseModel:
+    def __init__(self, model_config: Optional[Dict] = None):
+        """ """
+        if model_config is None:
+            model_config = {}
+
+        self.dm = None
+        self.model_config = model_config
+        self.is_fitted = False
+
+    def fit(self, X, y=None):
+        self.is_fitted = True
+        return self
+
+    def predict(self, X):
+        pass
+
+    def build_model(self, X, y):
+        pass
+
+    def build_datamanager(self):
+        pass
+
+
+def is_fitted(func):
+    """Decorator checks whether model has been fit."""
+
+    @functools.wraps(func)
+    def inner(self, *args, **kwargs):
+        if not self.is_fitted:
+            raise RuntimeError(
+                "The model hasn't been fitted yet, call .fit()."
+            )
+        return func(self, *args, **kwargs)
+
+    return inner
---
+++ packages/discontinuum/discontinuum-0.2.1.tar.gz/discontinuum-0.2.1/src/rating_gp/__init__.py
@@ -0,0 +1,10 @@
+from rating_gp.models.gpytorch import (
+    RatingGPMarginalGPyTorch,
+)
+
+__all__ = (
+    # Sub-packages
+    # "providers",
+    # Top-level functions
+    "RatingGPMarginalGPyTorch",
+)
