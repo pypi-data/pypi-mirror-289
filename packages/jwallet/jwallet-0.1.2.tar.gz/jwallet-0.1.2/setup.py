ae99f9e4818180e636458a160b521bc9349236cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-10 15:04:35 +0000

    Add package jwallet/0.1.2/jwallet-0.1.2.tar.gz

---
+++ packages/jwallet/jwallet-0.1.2.tar.gz/jwallet-0.1.2/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='jwallet',
+    version='0.1.2',
+    packages=find_packages(),
+    entry_points={
+        'console_scripts': [
+            'jwallet=jwallet.jwallet:main',
+        ],
+    },
+    install_requires=[
+        "web3",
+        "argparse",
+        "python-dotenv",
+        "bitcoinlib",
+        "eth-account"
+    ],
+    python_requires='>=3.8',  # Adjust based on your needs
+    author='Jason Knoll',
+    author_email='jknolldev@gmail.com',
+    description='A CLI crypto wallet generator',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/jasonknoll/jwallet',
+    license="MIT",
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+)
