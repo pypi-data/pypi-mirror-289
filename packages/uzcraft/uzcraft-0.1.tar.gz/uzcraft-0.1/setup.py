35f9147047edf959f113295110ff94f2d782c6dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-10 15:05:14 +0000

    Add package uzcraft/0.1/uzcraft-0.1.tar.gz

---
+++ packages/uzcraft/uzcraft-0.1.tar.gz/uzcraft-0.1/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='uzcraft',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[
+        'ursina',
+    ],
+    author='EsanbekovSalohiddin',
+    author_email='salohiddinsalohiddin345@gmail.com',
+    description='free 3d minecraft',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+)
