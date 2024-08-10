aa4607c208f3e959532b37c64973fe91d8b1424e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-10 04:05:01 +0000

    Add package tokysnatcher/0.1/tokysnatcher-0.1.tar.gz

---
+++ packages/tokysnatcher/tokysnatcher-0.1.tar.gz/tokysnatcher-0.1/README.md
@@ -0,0 +1,24 @@
+# TokySnatcher 🤫
+> Snatch audiobooks from Tokybook 😈
+
+
+## Installation
+1. Open terminal, then run:
+```sh
+pip install git+https://github.com/rahaaatul/TokySnatcher.git
+```
+2. Run TokySnatcher
+```sh
+tokysnatcher
+```
+
+
+> [!TIP]
+> After downloading convert it into a proper m4b ebook with [AudioBookBinder](https://github.com/gonzoua/AudioBookBinder).
+
+
+# ❤️ Thank You
+- [Tokybook](https://tokybook.com/)
+- [Adrian Castro](https://github.com/castdrian/)
+- [Eric Winter](https://github.com/xdf8/)
+- [Claude AI](https://claude.ai/)
---
+++ packages/tokysnatcher/tokysnatcher-0.1.tar.gz/tokysnatcher-0.1/tokysnatcher/chapters.py
@@ -0,0 +1,74 @@
+import os
+from os import makedirs, path
+from re import search
+from urllib.parse import urlparse
+
+from gazpacho import Soup, get
+from json5 import loads
+
+from .download import download
+
+SKIP_CHAPTER = "https://file.tokybook.com/upload/welcome-you-to-tokybook.mp3"
+MEDIA_URL = "https://files01.tokybook.com/audio/"
+MEDIA_FALLBACK_URL = "https://files02.tokybook.com/audio/"
+SLASH_REPLACE_STRING = " out of "
+
+
+def get_chapters(BOOK_URL):
+    html = get(BOOK_URL)
+
+    soup = Soup(html)
+    js = soup.find("script")
+
+    for crap in js:
+        match = search(r"tracks\s*=\s*(\[[^\]]+\])\s*", crap.text)
+        if match:
+            string = match.group(1)
+            break
+
+    json = loads(string)
+    chapters = [
+        {"name": x["name"], "url": x["chapter_link_dropbox"]}
+        for x in json
+        if x["chapter_link_dropbox"] != SKIP_CHAPTER
+    ]
+
+    o = urlparse(BOOK_URL)
+    current_directory = os.getcwd()
+    download_folder = os.path.join(current_directory, os.path.basename(o.path))
+
+    print(f"Download folder: {download_folder}")
+
+    try:
+        makedirs(download_folder, exist_ok=True)
+    except OSError as e:
+        print(f"Error creating download folder: {e}")
+        return
+
+    for item in chapters:
+        try:
+            download(
+                MEDIA_URL + item["url"],
+                path.join(download_folder, SLASH_REPLACE_STRING.join(item["name"].split("/")) + ".mp3"),
+            )
+            print()
+
+        except OSError as e:
+            print(f"Error creating download folder: {e}")
+            return
+
+        except RuntimeError:
+            try:
+                download(
+                    MEDIA_FALLBACK_URL + item["url"],
+                    path.join(download_folder, item["name"] + ".mp3"),
+                )
+                print()
+
+            except Exception as e:
+                print(f"An error occured: {e}")
+                break
+
+        except Exception as e:
+            print(f"An error occured: {e}")
+            break
