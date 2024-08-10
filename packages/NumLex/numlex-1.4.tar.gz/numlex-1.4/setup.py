2cd1fc11fd46c59f5979e75061bbf931850fbd5e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-10 05:04:41 +0000

    Add package NumLex/1.4/numlex-1.4.tar.gz

---
+++ packages/NumLex/numlex-1.4.tar.gz/numlex-1.4/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name='NumLex',
+    version='1.4',
+    packages=['NumLex'],
+    author='Jenil Desai',
+    author_email='jenildev91@gmail.com',
+    description='A blend of "Numerical" and "Lexical," indicating the dual focus on numbers and language.',
+    url='https://github.com/Jenil-Desai/NumLex',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+    ],
+)
