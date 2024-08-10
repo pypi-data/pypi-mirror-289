258428dbdc8e873b587422fabaa9ec9c54aff34c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-10 14:05:20 +0000

    Add package dagster-airlift/0.0.5/dagster_airlift-0.0.5.tar.gz

---
+++ packages/dagster-airlift/dagster_airlift-0.0.5.tar.gz/dagster_airlift-0.0.5/setup.py
@@ -0,0 +1,56 @@
+from pathlib import Path
+
+from setuptools import find_packages, setup
+
+airflow_dep_list = [
+    "apache-airflow>=2.0.0,<2.8",
+    # Flask-session 0.6 is incompatible with certain airflow-provided test
+    # utilities.
+    "flask-session<0.6.0",
+    "connexion<3.0.0",  # https://github.com/apache/airflow/issues/35234
+    "pendulum>=2.0.0,<3.0.0",
+]
+
+
+def get_long_description() -> str:
+    readme_path = Path(__file__).parent / "README.md"
+
+    assert readme_path.exists(), "README.md not found"
+
+    return readme_path.read_text(encoding="utf-8")
+
+
+setup(
+    name="dagster-airlift",
+    version="0.0.5",
+    author="Dagster Labs",
+    author_email="hello@dagsterlabs.com",
+    license="Apache-2.0",
+    description="Tooling to assist with migrating from Airflow to Dagster.",
+    long_description=get_long_description(),
+    long_description_content_type="text/markdown",
+    url=(
+        "https://github.com/dagster-io/dagster/tree/master/python_modules/libraries/"
+        "dagster-airlift"
+    ),
+    classifiers=[
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+    ],
+    packages=find_packages(exclude=["dagster_airlift_tests*", "examples*"]),
+    extras_require={
+        "core": [
+            "dagster",
+        ],
+        "in-airflow": airflow_dep_list,
+        "mwaa": ["boto3"],
+        "dbt": ["dagster-dbt"],
+        "test": ["pytest", "dagster-dbt", "dbt-duckdb", "boto3"],
+    },
+    zip_safe=False,
+)
