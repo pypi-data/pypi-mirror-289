f8d0e9e9ac739c6384a61f61ee88976cd5442359
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-10 03:06:42 +0000

    Add package PyTorchLabFlow/0.1.4/pytorchlabflow-0.1.4.tar.gz

---
+++ packages/PyTorchLabFlow/pytorchlabflow-0.1.4.tar.gz/pytorchlabflow-0.1.4/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='PyTorchLabFlow',
+    version='0.1.4',
+    packages=find_packages(),
+    install_requires=[
+        'torch',
+        'librosa',
+        'pandas',
+        'tqdm'
+    ],
+    author='BBEK-Anand',
+    author_email='',
+    description='A pipeline for managing PyTorch models, datasets, optimizers, and more.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/BBEK-Anand/PyTorchLabFlow',
+    license="MIT",
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.11',
+)
