ccc171af8b558b9d48ae901eb98aba9beee0f02e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-10 14:05:50 +0000

    Add package abcli/9.192.1/abcli-9.192.1.tar.gz

---
+++ packages/abcli/abcli-9.192.1.tar.gz/abcli-9.192.1/abcli/.abcli/plugins/git/create_branch.sh
@@ -0,0 +1,25 @@
+#! /usr/bin/env bash
+
+function abcli_git_create_branch() {
+    local branch_name=$1
+    if [[ -z "$branch_name" ]]; then
+        abcli_log_error "-@git: create_brach: branch name not found."
+        return 1
+    fi
+
+    local options=$2
+    local do_push=$(abcli_option_int "$options" push 1)
+    local do_increment_version=$(abcli_option_int "$options" increment_version $(abcli_not $do_push))
+
+    [[ "$do_increment_version" == 1 ]] &&
+        abcli_git_increment_version
+
+    git pull
+    git checkout -b $branch_name
+    git push origin $branch_name
+
+    [[ "$do_push" == 1 ]] &&
+        abcli_git_push "start of $branch_name 🪄" first
+
+    return 0
+}
