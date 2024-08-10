5dd50a7e9ba2f1cee1010468b5f44f8fee420d5b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-10 15:04:47 +0000

    Add package stashdb/0.0.1/stashdb-0.0.1.tar.gz

---
+++ packages/stashdb/stashdb-0.0.1.tar.gz/stashdb-0.0.1/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name="stashdb",
+    version="0.0.1",
+    author="Monday David Osakpawan",
+    description="An extremly lightweight json database for small Python projects.",
+    long_description="An extremly lightweight json database for small Python projects.",
+    long_description_content_type="text/x-rst",
+    packages=find_packages(),
+    install_requires=[
+
+    ]
+)
