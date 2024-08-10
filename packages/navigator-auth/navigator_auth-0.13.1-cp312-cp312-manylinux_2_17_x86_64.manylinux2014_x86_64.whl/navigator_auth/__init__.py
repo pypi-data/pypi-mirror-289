802fb33f1f6deb808a66e93c49d0c5951df0499c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-10 05:05:02 +0000

    Add package navigator-auth/0.13.1/navigator_auth-0.13.1-cp312-cp312-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/navigator-auth/navigator_auth-0.13.1-cp312-cp312-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/navigator_auth/__init__.py
@@ -0,0 +1,11 @@
+"""
+  Navigator Auth.
+
+  Basic Toolkit for authentication/Authorization for aiohttp, using AsyncDB for Storage access.
+"""
+from .version import __author__, __description__, __title__, __version__, get_version
+
+from .auth import AuthHandler
+
+
+__all__ = ("AuthHandler", )
