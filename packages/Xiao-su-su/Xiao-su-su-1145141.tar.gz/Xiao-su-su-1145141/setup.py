8b2c53f6b3d04dd492176843dc2775d5ed64a65e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-10 15:05:28 +0000

    Add package Xiao-su-su/1145141/Xiao-su-su-1145141.tar.gz

---
+++ packages/Xiao-su-su/Xiao-su-su-1145141.tar.gz/Xiao-su-su-1145141/setup.py
@@ -0,0 +1,14 @@
+# setup.py
+
+from setuptools import setup, find_packages
+
+setup(
+    name="Xiao-su-su",
+    version="1145141",
+    description="申必自解压",
+    author="Harcic",
+    author_email="harcic@outlook.com",
+    url="https://guppy.ltd",
+    packages=["Xiaosusu520"],
+    include_package_data=True,
+)
