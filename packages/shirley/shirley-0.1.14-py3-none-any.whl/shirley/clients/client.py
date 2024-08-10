0841436995ccb6c38b976dfbbba24612c2d6a826
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-10 15:05:21 +0000

    Add package shirley/0.1.14/shirley-0.1.14-py3-none-any.whl

---
+++ packages/shirley/shirley-0.1.14-py3-none-any.whl/shirley/clients/client.py
@@ -0,0 +1,21 @@
+import os
+import pathlib
+import tempfile
+from abc import ABC
+
+
+class Client(ABC):
+
+    def __init__(self, local: bool, *args, **kwargs) -> None:
+        self._local = local
+        self._tempdir = os.environ.get('GRADIO_TEMP_DIR') or str(pathlib.Path(tempfile.gettempdir()) / 'gradio')
+
+
+    @property
+    def local(self) -> bool:
+        return self._local
+
+
+    @property
+    def tempdir(self) -> str:
+        return self._tempdir
