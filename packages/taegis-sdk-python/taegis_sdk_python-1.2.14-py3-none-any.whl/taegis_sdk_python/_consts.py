ad63d5db8acb92a1c520196c3060a1ba09526f3b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-10 04:04:14 +0000

    Add package taegis-sdk-python/1.2.14/taegis_sdk_python-1.2.14-py3-none-any.whl

---
+++ packages/taegis-sdk-python/taegis_sdk_python-1.2.14-py3-none-any.whl/taegis_sdk_python/_consts.py
@@ -0,0 +1,26 @@
+"""Taegis Constants."""
+
+TAEGIS_ENVIRONMENT_URLS = {
+    "charlie": "https://api.ctpx.secureworks.com",
+    "production": "https://api.ctpx.secureworks.com",
+    "US1": "https://api.ctpx.secureworks.com",
+    "delta": "https://api.delta.taegis.secureworks.com",
+    "US2": "https://api.delta.taegis.secureworks.com",
+    "echo": "https://api.echo.taegis.secureworks.com",
+    "EU": "https://api.echo.taegis.secureworks.com",
+    "foxtrot": "https://api.foxtrot.taegis.secureworks.com",
+    "US3": "https://api.foxtrot.taegis.secureworks.com",
+}
+
+
+TAEGIS_TENANTS_URLS = {
+    "charlie": "https://api.tenants.ctpx.secureworks.com",
+    "production": "https://api.tenants.ctpx.secureworks.com",
+    "US1": "https://api.tenants.ctpx.secureworks.com",
+    "delta": "https://api-tenants.delta.taegis.secureworks.com",
+    "US2": "https://api-tenants.delta.taegis.secureworks.com",
+    "echo": "https://api-tenants.echo.taegis.secureworks.com",
+    "EU": "https://api-tenants.echo.taegis.secureworks.com",
+    "foxtrot": "https://api-tenants.foxtrot.taegis.secureworks.com",
+    "US3": "https://api-tenants.foxtrot.taegis.secureworks.com",
+}
