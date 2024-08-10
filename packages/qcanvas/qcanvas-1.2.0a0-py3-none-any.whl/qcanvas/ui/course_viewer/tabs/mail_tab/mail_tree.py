335c51314fe5f21d56a9726c5e35ca7749dfbb8f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-10 14:05:51 +0000

    Add package qcanvas/1.2.0a0/qcanvas-1.2.0a0-py3-none-any.whl

---
+++ packages/qcanvas/qcanvas-1.2.0a0-py3-none-any.whl/qcanvas/ui/course_viewer/tabs/mail_tab/mail_tree.py
@@ -0,0 +1,56 @@
+import logging
+from typing import *
+
+import qcanvas_backend.database.types as db
+from qcanvas_backend.net.sync.sync_receipt import SyncReceipt
+from qtpy.QtWidgets import *
+
+from qcanvas.ui.course_viewer.content_tree import ContentTree
+from qcanvas.ui.memory_tree import MemoryTreeWidgetItem
+
+_logger = logging.getLogger(__name__)
+
+
+class MailTree(ContentTree[db.Course]):
+
+    def __init__(self, course_id: str):
+        super().__init__(
+            tree_name=f"course.{course_id}.mail",
+            emit_selection_signal_for_type=db.CourseMessage,
+        )
+
+        self.ui_setup(
+            header_text=["Subject", "Sender"],
+            max_width=500,
+            min_width=300,
+            indentation=20,
+        )
+
+        self.set_columns_resize_mode(
+            [QHeaderView.ResizeMode.Stretch, QHeaderView.ResizeMode.ResizeToContents]
+        )
+
+    def create_tree_items(
+        self, course: db.Course, sync_receipt: SyncReceipt
+    ) -> Sequence[MemoryTreeWidgetItem]:
+        widgets = []
+
+        for message in course.messages:  # type: db.CourseMessage
+            message_widget = self._create_mail_widget(message, sync_receipt)
+            widgets.append(message_widget)
+
+        return widgets
+
+    def _create_mail_widget(
+        self, message: db.CourseMessage, sync_receipt: SyncReceipt
+    ) -> MemoryTreeWidgetItem:
+        message_widget = MemoryTreeWidgetItem(
+            id=message.id,
+            data=message,
+            strings=[message.name, message.sender_name],
+        )
+
+        if sync_receipt.was_updated(message):
+            self.mark_as_unseen(message_widget)
+
+        return message_widget
---
+++ packages/qcanvas/qcanvas-1.2.0a0-py3-none-any.whl/qcanvas/ui/course_viewer/tabs/page_tab/page_tree.py
@@ -0,0 +1,60 @@
+import logging
+from typing import Sequence
+
+import qcanvas_backend.database.types as db
+from qcanvas_backend.net.sync.sync_receipt import SyncReceipt
+from qtpy.QtCore import Qt
+
+from qcanvas.ui.course_viewer.content_tree import ContentTree
+from qcanvas.ui.memory_tree import MemoryTreeWidgetItem
+
+_logger = logging.getLogger(__name__)
+
+
+class PageTree(ContentTree[db.Course]):
+    def __init__(self, course_id: str):
+        super().__init__(
+            tree_name=f"course.{course_id}.modules",
+            emit_selection_signal_for_type=db.ModulePage,
+        )
+
+        self.ui_setup(
+            header_text="Content", indentation=15, max_width=300, min_width=150
+        )
+
+    def create_tree_items(
+        self, course: db.Course, sync_receipt: SyncReceipt
+    ) -> Sequence[MemoryTreeWidgetItem]:
+        widgets = []
+
+        for module in course.modules:  # type: db.Module
+            if len(module.pages) == 0:
+                continue
+
+            module_widget = self._create_module_widget(module)
+            widgets.append(module_widget)
+
+            for page in module.pages:  # type: db.ModulePage
+                page_widget = self._create_page_widget(page, sync_receipt)
+                module_widget.addChild(page_widget)
+
+        return widgets
+
+    def _create_module_widget(self, module: db.Module) -> MemoryTreeWidgetItem:
+        module_widget = MemoryTreeWidgetItem(
+            id=module.id, data=module, strings=[module.name]
+        )
+        module_widget.setFlags(Qt.ItemFlag.ItemIsEnabled)
+
+        return module_widget
+
+    def _create_page_widget(
+        self, page: db.ModulePage, sync_receipt: SyncReceipt
+    ) -> MemoryTreeWidgetItem:
+        page_widget = MemoryTreeWidgetItem(id=page.id, data=page, strings=[page.name])
+        page_widget.setFlags(Qt.ItemFlag.ItemIsEnabled | Qt.ItemFlag.ItemIsSelectable)
+
+        if sync_receipt.was_updated(page):
+            self.mark_as_unseen(page_widget)
+
+        return page_widget
