601c09087ae38393bc6a7c9d3e1a149eb2f8c47d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-10 03:05:55 +0000

    Add package thermolib/0.6.1/thermolib-0.6.1.tar.gz

---
+++ packages/thermolib/thermolib-0.6.1.tar.gz/thermolib-0.6.1/src/python.rs
@@ -0,0 +1,25 @@
+/// A Python module implemented in Rust.
+use pyo3::prelude::*;
+#[pyfunction]
+fn hello() -> PyResult<String> {
+    Ok(String::from("Hello, Rust And Python."))
+}
+use crate::Helmholtz;
+use crate::LiquidMetal;
+use crate::Pr;
+use crate::Rk;
+use crate::Srk;
+use crate::Vdw;
+/// A Python module implemented in Rust.
+#[pymodule]
+#[pyo3(name = "thermolib")] // Rename pymodule
+pub fn pylib(_py: Python, m: &PyModule) -> PyResult<()> {
+    m.add_function(wrap_pyfunction!(hello, m)?)?;
+    m.add_class::<Vdw>()?;
+    m.add_class::<Rk>()?;
+    m.add_class::<Srk>()?;
+    m.add_class::<Pr>()?;
+    m.add_class::<Helmholtz>()?;
+    m.add_class::<LiquidMetal>()?;
+    Ok(())
+}
