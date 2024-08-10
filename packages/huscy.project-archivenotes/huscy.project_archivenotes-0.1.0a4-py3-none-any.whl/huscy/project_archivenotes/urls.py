0d86b227f06b882a7d07b4b12c1cc830a384447a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-10 03:04:53 +0000

    Add package huscy.project-archivenotes/0.1.0a4/huscy.project_archivenotes-0.1.0a4-py3-none-any.whl

---
+++ packages/huscy.project-archivenotes/huscy.project_archivenotes-0.1.0a4-py3-none-any.whl/huscy/project_archivenotes/urls.py
@@ -0,0 +1,9 @@
+from rest_framework.routers import DefaultRouter
+
+#from huscy.project_archivenotes import views
+
+
+router = DefaultRouter()
+#router.register('archivenotes', views.ArchiveNoteViewSet)
+
+urlpatterns = router.urls
