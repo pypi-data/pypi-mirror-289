8bde03d6b93f9a19abe5f4166a3d3b2cdb8688ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-10 03:06:49 +0000

    Add package open-fleet/0.0.6/open_fleet-0.0.6-py3-none-any.whl

---
+++ packages/open-fleet/open_fleet-0.0.6-py3-none-any.whl/fleet/manager.py
@@ -0,0 +1,266 @@
+from typing import List, Any, Dict
+import time
+import json
+from multiprocessing import Process
+from datetime import datetime
+
+from pathlib import Path
+from rich.console import Console
+from rich.progress import Progress, BarColumn, TextColumn
+
+from fleet.utils.file_utils import safe_load_json
+from fleet.utils.time_tracker import TimeTracker
+from fleet.manager_utils.assign_jobs import loop_assignment
+
+
+class Manager:
+    def __init__(self, args, job_list: List[Any], info: Dict = {}):
+        base_dir = args.base_dir
+        self.base_dir = Path(base_dir)
+        self.console = Console()
+        self.base_dir.mkdir(parents=True, exist_ok=True)
+        self.nodes_dir = self.base_dir / 'nodes'
+
+        self.status_dir = self.base_dir / 'status'
+        self.working_dir = self.base_dir / 'working'
+
+        self.heart_dir = self.base_dir / 'heart'
+
+        # to store the available nodes
+        self.available_dir = self.base_dir / 'available'
+
+        self.finished_file = self.base_dir / 'finished'
+
+        self.nodes_dir.mkdir(parents=True, exist_ok=True)
+        print(f"nodes_dir: {self.nodes_dir}")
+        self.heart_dir.mkdir(parents=True, exist_ok=True)
+        print(f"heart_dir: {self.heart_dir}")
+        self.status_dir.mkdir(parents=True, exist_ok=True)
+        print(f"status_dir: {self.status_dir}")
+        self.available_dir.mkdir(parents=True, exist_ok=True)
+        print(f"available_dir: {self.available_dir}")
+        self.working_dir.mkdir(parents=True, exist_ok=True)
+
+        self.job_list = job_list
+        self.total_jobs = len(job_list)
+        self.info = info
+        # self.working_task_status = {}
+        self.working_num = 0
+        self.unassigned_task_status = {}
+
+        self.no_available_nodes_num = 0
+
+        self.success_num = 0
+        self.crashed_num = 0
+        self.failed_num = 0
+
+        self.dead_nodes = {}
+        self.available_nodes = {}
+
+        self.time_tracker = TimeTracker(total_tasks=self.total_jobs)
+
+        self.first_assigned = True
+
+        self.job_assign_process = None
+
+        self.previous_log_time = None
+        # self.new_finished_num = 0
+
+    def initialize_tasks(self):
+        self.finished_num = 0
+        for idx, job_input in enumerate(self.job_list):
+            task_name = f'task{idx + 1}'
+            task_status_path = self.status_dir / f'{task_name}.status'
+            if not task_status_path.exists():
+                status_info = {'status': 'unassigned', 'input': job_input, "task_status_path": str(task_status_path)}
+                task_status_path.write_text(json.dumps(status_info))
+            else:
+                status_info = safe_load_json(task_status_path)
+
+            if status_info is None:
+                continue
+
+            if status_info['status'] in ["success", "crashed", "failed"]:
+                self.finished_num += 1
+                self.time_tracker.update()
+                if status_info['status'] == 'success':
+                    self.success_num += 1
+                elif status_info['status'] == 'crashed':
+                    self.crashed_num += 1
+                elif status_info['status'] == 'failed':
+                    self.failed_num += 1
+
+                self.progress.update(self.task_id, advance=1)
+
+            else:
+                if status_info['status'] == 'unassigned':
+                    self.unassigned_task_status[task_name] = status_info
+                    self.unassigned_task_status[task_name]["task_status_path"] = str(task_status_path)
+                else:
+                    assert status_info['status'] == 'assigned'
+                    # self.working_task_status[task_name] = status_info
+                    # self.working_task_status[task_name]["task_status_path"] = str(task_status_path)
+                    working_file = self.working_dir / task_name
+                    if not working_file.exists():
+                        status_info["task_status_path"] = str(task_status_path)
+                        working_file.write_text(json.dumps(status_info))
+
+        if self.finished_num == 0:
+            success_rate = 0
+        else:
+            success_rate = self.success_num / self.finished_num * 100
+
+        self.progress.update(self.task_id,
+                             description=f"Success Rate: {success_rate:.2f}% Finished: {self.finished_num}/{self.total_jobs}")
+
+        if self.finished_num > 0:
+            self.first_assigned = False
+
+    def process_dead_nodes(self, dead_nodes):
+        # check if the task is assigned to a dead node
+        for node in dead_nodes:
+            node_file = self.nodes_dir / f"{node}.status"
+            node_info = safe_load_json(node_file)
+            if node_info and node_info["status"] == 'busy':
+                task_status_path = node_info["task_status_path"]
+
+                status_info = safe_load_json(Path(task_status_path))
+
+                if status_info and status_info['status'] == 'assigned':
+                    status_info['status'] = 'crashed'
+                    Path(task_status_path).write_text(json.dumps(status_info))
+                    node_info['status'] = 'dead'
+                    node_file.write_text(json.dumps(node_info))
+
+    def log_status(self):
+        current_time = time.time()
+        if self.previous_log_time is None or current_time - self.previous_log_time > 1 or self.finished_num == self.total_jobs:
+            self.previous_log_time = current_time
+        else:
+            return
+
+        if self.finished_num == 0:
+            success_rate = 0
+        else:
+            success_rate = self.success_num / self.finished_num * 100
+        time_summary = self.time_tracker.summary
+
+        self.progress.update(self.task_id,
+                             description=f"Success Rate: {success_rate:.2f}% Finished/Working: {self.finished_num}/{self.working_num}/{self.total_jobs} Nodes(Good/Dead): {len(self.available_nodes)}/{len(self.dead_nodes)} {time_summary}")
+
+    def check_task_status_and_assign(self):
+        self.monitor_heartbeats()
+        self.check_working_tasks()
+        self.log_status()
+
+    def check_working_tasks(self):
+        self.working_num = 0
+        # self.new_finished_num = 0
+        # Remove finished tasks in working_task_status
+        working_files = list(self.working_dir.glob("*"))
+
+        # for job_key in list(self.working_task_status.keys()):
+        for working_file in working_files:
+            # status_info = self.working_task_status[job_key]
+            status_info = safe_load_json(working_file)
+            task_status_file = Path(status_info["task_status_path"])
+
+            assert task_status_file.exists(), f"task_status_file {task_status_file} not exists"
+
+            status_info_in_file = safe_load_json(task_status_file)
+            if status_info_in_file is None:
+                continue
+
+            assert status_info_in_file['status'] in ["assigned", "success", "crashed", "failed"]
+
+            if status_info_in_file['status'] == "assigned":
+                self.working_num += 1
+            else:
+                if status_info_in_file['status'] == 'success':
+                    self.success_num += 1
+                elif status_info_in_file['status'] == 'crashed':
+                    self.crashed_num += 1
+                elif status_info_in_file['status'] == 'failed':
+                    self.failed_num += 1
+
+                # del self.working_task_status[job_key]
+                working_file.unlink()
+
+                self.finished_num += 1
+                # self.new_finished_num += 1
+                # self.console.log(f"{self.finished_num}/{self.new_finished_num}")
+
+                self.time_tracker.update()
+                self.progress.update(self.task_id, advance=1)
+
+    def loop_assignment(self):
+        loop_assignment(self.available_dir, self.nodes_dir, self.working_dir, self.unassigned_task_status, self.console)
+
+    def start_job_assignment(self):
+        self.job_assign_process = Process(target=self.loop_assignment)
+        self.job_assign_process.start()
+
+    def stop_job_assignment(self):
+        if self.job_assign_process:
+            self.job_assign_process.terminate()
+            self.job_assign_process.join()
+
+    def run(self):
+        with Progress(
+                TextColumn("[progress.description]{task.description}"),
+                BarColumn(),
+                TextColumn("{task.percentage:>3.0f}%"),
+                console=self.console
+        ) as progress:
+            self.task_id = progress.add_task("Processing Jobs", total=self.total_jobs)
+            self.progress = progress
+            # self.pbar = tqdm(total=len(self.job_list), desc="Processing Jobs")
+            self.initialize_tasks()
+
+            self.start_job_assignment()
+
+            try:
+                while True:
+                    if self.finished_num == self.total_jobs or self.working_num + self.finished_num == self.total_jobs:
+                        if not self.finished_file.exists():
+                            self.finished_file.touch()
+
+                    if self.finished_num == self.total_jobs:
+                        break
+
+                    self.check_task_status_and_assign()
+
+            finally:
+                self.stop_job_assignment()
+
+    def monitor_heartbeats(self, heartbeat_timeout: int = 120):
+        self.available_nodes = {}
+        new_dead_nodes = {}
+
+        current_time = int(time.time())
+        for node_file in self.heart_dir.iterdir():
+            if node_file.stem in self.dead_nodes:
+                continue
+
+            node_info = safe_load_json(node_file)
+
+            if node_info and node_info['status'] == "available" and current_time - node_info.get("last_heartbeat",
+                                                                                                 0) <= heartbeat_timeout:
+                self.available_nodes[node_file.stem] = node_info
+
+            else:
+                if node_info is None:
+                    dead_reason = f"can not load node info from {str(node_file)}"
+                    node_info = {}
+                else:
+                    if node_info['status'] == 'dead':
+                        dead_reason = f"worker sends dead"
+                    else:
+                        dead_reason = f"no heartbeat, last heartbeat: {datetime.fromtimestamp(node_info.get('last_heartbeat', 0)).strftime('%Y-%m-%d %H:%M:%S')}"
+                node_info['status'] = 'dead'
+                node_info['dead_reason'] = dead_reason
+
+                node_file.write_text(json.dumps(node_info))
+                self.dead_nodes[node_file.stem] = node_info
+                new_dead_nodes[node_file.stem] = node_info
+        self.process_dead_nodes(new_dead_nodes)
---
+++ packages/open-fleet/open_fleet-0.0.6-py3-none-any.whl/fleet/worker.py
@@ -0,0 +1,242 @@
+from typing import Callable, Dict, List
+import os
+import time
+import uuid
+import json
+from multiprocessing import Process, Queue
+import traceback
+
+from pathlib import Path
+
+from fleet.utils.file_utils import safe_load_json
+
+
+def run_job(job_func, job_input, info, output_queue):
+    try:
+        result = job_func(job_input, info)
+        output_queue.put(result)
+    except Exception as e:
+        error_message = traceback.format_exc()
+        print(error_message)
+        output_queue.put({"error": error_message, "status": "crashed"})
+    finally:
+        # 强制终止当前进程和所有子线程/子进程
+        os._exit(0)
+
+
+class Worker:
+    def __init__(self, args, job_func: Callable, info: Dict = {}):
+        self.base_dir = Path(args.base_dir)
+        self.timeout = args.timeout
+        self.wait_manager = args.wait_manager
+        self.max_job = args.max_job
+        self.max_work_time = args.max_work_time
+        unique_id = str(uuid.uuid4())
+        self.node_id = f"{args.node_id}_{unique_id}" if args.node_id else unique_id
+        self.nodes_dir = self.base_dir / 'nodes'
+        self.status_dir = self.base_dir / 'status'
+        self.heart_dir = self.base_dir / 'heart'
+        self.finished_file = self.base_dir / 'finished'
+        self.available_dir = self.base_dir / 'available'
+        self.available_file = self.available_dir / self.node_id
+
+        self.node_status_path = self.nodes_dir / f'{self.node_id}.status'
+        self.node_heart_status_path = self.heart_dir / f'{self.node_id}.heart'
+
+        self.job_func = job_func
+        self.info = info
+
+        # self.unassigned_task_status = {}
+        self.not_find_job_num = 0
+
+        self.heartbeat_process = None  # 添加一个属性来保存心跳进程的引用
+
+        if self.wait_manager:
+            wait_time = 0
+            missing_dirs = self.check_dirs()
+            while len(missing_dirs) > 0:
+                if wait_time % 30 == 0:
+                    print(f"Waiting for manager to create dirs...")
+                    for missing_dir in missing_dirs:
+                        print(f"Missing dir: {missing_dir}")
+                time.sleep(1)
+                wait_time += 1
+                missing_dirs = self.check_dirs()
+
+        else:
+            missing_dirs = self.check_dirs()
+            assert len(missing_dirs) == 0, f"Missing dirs: {missing_dirs}"
+
+        self.finished_job_num = 0
+        self.worker_start_time = time.time()
+
+    def check_dirs(self) -> List[str]:
+        missing_dirs = []
+        for dir in [self.nodes_dir, self.status_dir, self.heart_dir, self.available_dir]:
+            if not dir.exists():
+                missing_dirs.append(str(dir))
+        return missing_dirs
+
+    def create_available_file(self):
+        if not self.available_file.exists():
+            self.available_file.touch()
+
+    def heartbeat_daemon(self):
+        """这个函数将作为独立的进程运行，负责发送心跳信号。"""
+        try:
+            while True:
+                self.send_heartbeat()
+                time.sleep(10)  # 设定心跳频率，例如每10秒发送一次心跳
+        except KeyboardInterrupt:
+            pass  # 这里可以捕捉 KeyboardInterrupt 异常来优雅地处理进程终止
+
+    def send_heartbeat(self, status: str = 'available'):
+        current_time = int(time.time())
+        while True:
+            retry_time = 0
+            try:
+                retry_time += 1
+                self.node_heart_status_path.write_text(json.dumps({"status": status, "last_heartbeat": current_time}))
+                break
+            except:
+                print(f"Failed to write heart status file {self.node_heart_status_path}, tried {retry_time} times")
+                time.sleep(1)
+                if retry_time > 20:
+                    print(f"Failed to write heart status file {self.node_heart_status_path}")
+                    break
+
+    def check_heart(self):
+        heart_status = safe_load_json(self.node_heart_status_path)
+        if heart_status is None or heart_status['status'] == 'dead':
+            return False
+        return True
+
+    def start_heartbeat(self):
+        """启动心跳进程。"""
+        self.heartbeat_process = Process(target=self.heartbeat_daemon)
+        self.heartbeat_process.start()
+
+    def stop_heartbeat(self):
+        """停止心跳进程。"""
+        if self.heartbeat_process:
+            self.heartbeat_process.terminate()
+            self.heartbeat_process.join()
+            self.send_dead()  # 在进程终止时发送死亡信号
+
+    def send_dead(self):
+        self.send_heartbeat(status='dead')
+
+    def register_node(self):
+        self.start_heartbeat()  # 在任务开始时启动心跳进程
+
+        # for task_status_file in self.status_dir.iterdir():
+        # status_info = safe_load_json(task_status_file)
+        # if status_info and status_info["status"] == "unassigned":
+        #     self.unassigned_task_status[task_status_file.stem] = status_info
+        # print("Read task status done")
+        node_info = {
+            "status": "idle"
+        }
+        # self.node_status_path.write_text('idle')
+        self.node_status_path.write_text(json.dumps(node_info))
+        self.create_available_file()
+        print(f"Node {self.node_id} registered")
+
+    def process_job(self):
+        find_job = False
+        node_info = safe_load_json(self.node_status_path)
+        if node_info and node_info['status'] == 'busy':
+            task_status_path = node_info['task_status_path']
+            status_info = safe_load_json(Path(task_status_path))
+            task_status_file = Path(node_info["task_status_path"])
+
+            job_input = status_info.get('input')
+            print(f"Processing task: {job_input}")
+            find_job = True
+
+            if self.timeout is None:
+                result = self.job_func(job_input, self.info)
+            else:
+                output_queue = Queue()
+                job_process = Process(target=run_job, args=(self.job_func, job_input, self.info, output_queue))
+                job_process.start()
+                job_process.join(timeout=self.timeout)
+                if job_process.is_alive():
+                    job_process.terminate()
+                    job_process.join()
+                    result = {"error": "job timeout", "status": "crashed"}
+                else:
+                    result = output_queue.get()
+
+            print(f"Task {job_input} Done!")
+            self.finished_job_num += 1
+            if 'error' in result:
+                status_info['error'] = result['error']
+
+            # 更新任务状态为完成
+            status_info['status'] = result['status']
+
+            task_status_file.write_text(json.dumps(status_info))
+
+            # 标记节点为空闲
+            node_info = {
+                "status": "idle"
+            }
+            self.node_status_path.write_text(json.dumps(node_info))
+            worker_status = self.check_worker_status()
+            if worker_status == "running":
+                self.create_available_file()
+            return find_job
+        return find_job
+
+    def check_and_process_tasks(self):
+        find_job = self.process_job()
+
+        if not find_job:
+            if self.not_find_job_num % 100 == 20:
+                print("No task assigned...")
+
+            if self.not_find_job_num < 20:
+                time.sleep(0.1)
+            else:
+                time.sleep(0.5)
+
+            self.not_find_job_num += 1
+        else:
+            self.not_find_job_num = 0
+
+    def check_worker_status(self)->str:
+        if self.max_job is not None and self.finished_job_num >= self.max_job:
+            return "max_job_reached"
+        if self.max_work_time is not None and time.time() - self.worker_start_time > self.max_work_time:
+            return "max_work_time_reached"
+        if self.finished_file.exists():
+            return "finished_file_exists"
+        if not self.check_heart():
+            return "heart_dead"
+        return "running"
+
+    def run(self):
+        self.register_node()
+
+        try:
+            while True:
+                self.check_and_process_tasks()
+                worker_status = self.check_worker_status()
+                if worker_status == "finished_file_exists":
+                    node_info = safe_load_json(self.node_status_path)
+                    if node_info and node_info['status'] == 'busy':
+                        continue
+
+                    print("Finished file exists, exit!")
+                    break
+                if worker_status != "running":
+                    print(f"Worker finish reason: {worker_status}")
+                    break
+
+        except Exception as e:
+            # traceback.print_exc()  # 打印异常信息和堆栈跟踪
+            error_message = traceback.format_exc()
+            print(error_message)
+        finally:
+            self.stop_heartbeat()  # 在任何结束时确保心跳进程被终止
