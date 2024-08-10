6644b905aebcc496644dedc3c73e721caa0b5c08
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-10 03:04:52 +0000

    Add package Jarvislib/2.9.7/jarvislib-2.9.7.tar.gz

---
+++ packages/Jarvislib/jarvislib-2.9.7.tar.gz/jarvislib-2.9.7/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+  name='Jarvislib',
+  version='2.9.7',
+  author='PlatonPython',
+  author_email='babadey.pl@gmail.com',
+  description='no',
+  packages=['Jarvislib'],
+  classifiers=[
+    'Programming Language :: Python :: 3.10'
+  ],
+  keywords='example python',
+  python_requires='>=3.9'
+)
