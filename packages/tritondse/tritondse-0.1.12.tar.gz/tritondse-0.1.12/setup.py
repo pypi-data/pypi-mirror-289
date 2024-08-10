c867ab3bddd55affedd8e7651622a34914de5492
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-10 05:04:28 +0000

    Add package tritondse/0.1.12/tritondse-0.1.12.tar.gz

---
+++ packages/tritondse/tritondse-0.1.12.tar.gz/tritondse-0.1.12/setup.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""Installation of the tritondse module."""
+
+import sys
+from setuptools import setup, find_packages
+
+
+with open("README.md") as f:
+    README = f.read()
+
+
+setup(
+    name="tritondse",
+    version="0.1.12",
+    description="A library of Dynamic Symbolic Exploration based the Triton library",
+    packages=find_packages(),
+    long_description=README,
+    long_description_content_type='text/markdown',
+    url="https://github.com/quarkslab/tritondse",
+    project_urls={
+        "Documentation": "https://quarkslab.github.io/tritondse/",
+        "Bug Tracker": "https://github.com/quarkslab/tritondse/issues",
+        "Source": "https://github.com/quarkslab/tritondse"
+    },
+    setup_requires=[],
+    install_requires=[
+        "cle",
+        "enum_tools",
+        "lief>=v0.15.0",
+        "networkx",
+        "pyQBDI",
+        "quokka-project",
+        "triton-library>=1.0.0rc4",
+    ],
+    tests_require=[],
+    license="Apache License Version 2.0",
+    author="Quarkslab",
+    classifiers=[
+        'Topic :: Security',
+        'Environment :: Console',
+        'Operating System :: OS Independent',
+    ],
+    test_suite="",
+    scripts=[]
+)
