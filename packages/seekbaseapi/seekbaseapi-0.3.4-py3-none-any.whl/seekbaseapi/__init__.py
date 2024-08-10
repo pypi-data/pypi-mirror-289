ac8625ba02ba59956d44f17270837369114b374d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-10 14:05:20 +0000

    Add package seekbaseapi/0.3.4/seekbaseapi-0.3.4-py3-none-any.whl

---
+++ packages/seekbaseapi/seekbaseapi-0.3.4-py3-none-any.whl/seekbaseapi/__init__.py
@@ -0,0 +1,13 @@
+"""
+Modules:
+- client: Contains the E2hBaseApiClient class for interacting with e2hapi.
+
+Version: 1.0.1
+"""
+
+from .client import SeekApiClient
+from .utils import BLACKLIST
+
+__version__ = "1.0.0"
+__author__ = "E2H"
+__license__ = "MIT"
