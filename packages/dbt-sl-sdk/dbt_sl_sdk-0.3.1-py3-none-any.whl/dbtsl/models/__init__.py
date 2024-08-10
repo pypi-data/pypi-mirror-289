c38961a11055f6187475956e4f2747d7f980f5c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-10 04:04:26 +0000

    Add package dbt-sl-sdk/0.3.1/dbt_sl_sdk-0.3.1-py3-none-any.whl

---
+++ packages/dbt-sl-sdk/dbt_sl_sdk-0.3.1-py3-none-any.whl/dbtsl/models/__init__.py
@@ -0,0 +1,32 @@
+"""The models used for the SDK.
+
+NOTE: this will be deleted in the future and will be replaced by
+generated code from our GraphQL schema.
+"""
+
+from .base import BaseModel
+from .dimension import Dimension, DimensionType
+from .entity import Entity, EntityType
+from .measure import AggregationType, Measure
+from .metric import Metric, MetricType
+from .query import QueryResult
+from .saved_query import SavedQuery
+from .time_granularity import TimeGranularity
+
+# Only importing this so it registers aliases
+_ = QueryResult
+
+BaseModel._apply_aliases()
+
+__all__ = [
+    "AggregationType",
+    "Dimension",
+    "DimensionType",
+    "Entity",
+    "EntityType",
+    "Measure",
+    "Metric",
+    "MetricType",
+    "SavedQuery",
+    "TimeGranularity",
+]
