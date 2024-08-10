792e12925dff769ddc5f66103053bea9d5dc981b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-10 04:04:26 +0000

    Add package bode-testing/0.0.9/bode_testing-0.0.9.tar.gz

---
+++ packages/bode-testing/bode_testing-0.0.9.tar.gz/bode_testing-0.0.9/src/bode_testing/_bode_testing.py
@@ -0,0 +1,31 @@
+import os
+import pytest
+import uuid
+
+
+@pytest.fixture
+def url():
+    deploy_url = os.environ["DEPLOY_URL"]
+    if not deploy_url.startswith("http"):
+        raise ValueError(
+            f"incorrect deploy url value '{deploy_url}' does not start with http "
+        )
+    return deploy_url
+
+
+@pytest.fixture(scope="function")
+def request_headers(request) -> dict[str, str]:
+    test_id = request.node.nodeid.replace("/", ".").replace(".py", "")
+    headers = {
+        "X-Test-Id": test_id,
+        "X-Session-Id": os.environ["SESSION_ID"],
+    }
+    if os.environ["CLOUD"] == "true":
+        id_token = os.environ["IDTOKEN"]
+        auth_header = {"Authorization": f"Bearer {id_token}"}
+        headers = {**headers, **auth_header}
+    return headers
+
+
+def generate_request_id_header() -> dict[str, str]:
+    return {"X-Request-Id": str(uuid.uuid4()).replace("-", "")[:8]}
