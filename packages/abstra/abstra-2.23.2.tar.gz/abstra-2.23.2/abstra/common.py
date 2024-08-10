92690446f1990406dbb3ef4087a4eac2362f9e05
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-10 05:04:45 +0000

    Add package abstra/2.23.2/abstra-2.23.2.tar.gz

---
+++ packages/abstra/abstra-2.23.2.tar.gz/abstra-2.23.2/abstra/common.py
@@ -0,0 +1,17 @@
+from abstra_internals.interface.sdk.common import (
+    execute_js,
+    get_persistent_dir,
+    get_project_url,
+    get_query_params,
+    get_user,
+    redirect,
+)
+
+__all__ = [
+    "get_user",
+    "get_query_params",
+    "redirect",
+    "execute_js",
+    "get_persistent_dir",
+    "get_project_url",
+]
