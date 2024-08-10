a8d1ac36f52788ea7d35e16939f3dc9b6792a9b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-10 04:04:05 +0000

    Add package macrosynergy/0.1.28/macrosynergy-0.1.28.tar.gz

---
+++ packages/macrosynergy/macrosynergy-0.1.28.tar.gz/macrosynergy-0.1.28/macrosynergy/learning/__init__.py
@@ -0,0 +1,97 @@
+from . import predictors
+
+from .panel_time_series_split import (
+    ExpandingKFoldPanelSplit,
+    RollingKFoldPanelSplit,
+    ExpandingIncrementPanelSplit,
+    ExpandingFrequencyPanelSplit,
+    BasePanelSplit,
+)
+from .cv_tools import panel_cv_scores
+from .transformers import (
+    LassoSelector,
+    MapSelector,
+    ENetSelector,
+    ZnScoreAverager,
+    PanelMinMaxScaler,
+    PanelStandardScaler,
+    FeatureAverager,
+)
+from .metrics import (
+    neg_mean_abs_corr,
+    panel_significance_probability,
+    sharpe_ratio,
+    sortino_ratio,
+    regression_accuracy,
+    regression_balanced_accuracy,
+)
+from .signal_optimizer import SignalOptimizer
+
+from .predictors import (
+    NaivePredictor,
+    BaseWeightedRegressor,
+    WeightedLinearRegression,
+    SignWeightedLinearRegression,
+    TimeWeightedLinearRegression,
+    WeightedLADRegressor,
+    SignWeightedLADRegressor,
+    TimeWeightedLADRegressor,
+    LADRegressor,
+    BaseRegressionSystem,
+    LADRegressionSystem,
+    RidgeRegressionSystem,
+    LinearRegressionSystem,
+    CorrelationVolatilitySystem,
+    ModifiedLinearRegression,
+    BaseModifiedRegressor,
+)
+
+from .beta_estimator import BetaEstimator
+
+__all__ = [
+    # panel_time_series_split
+    "ExpandingKFoldPanelSplit",
+    "RollingKFoldPanelSplit",
+    "ExpandingIncrementPanelSplit",
+    "ExpandingFrequencyPanelSplit",
+    "BasePanelSplit",
+    # cv_tools
+    "panel_cv_scores",
+    # transformers
+    "FeatureAverager",
+    "LassoSelector",
+    "MapSelector",
+    "ENetSelector",
+    "PanelMinMaxScaler",
+    "PanelStandardScaler",
+    "ZnScoreAverager",
+    # metrics
+    "neg_mean_abs_corr",
+    "panel_significance_probability",
+    "sharpe_ratio",
+    "sortino_ratio",
+    "regression_accuracy",
+    "regression_balanced_accuracy",
+    # signal_optimizer
+    "SignalOptimizer",
+    # predictors
+    "LADRegressor",
+    "NaivePredictor",
+    "SignWeightedLADRegressor",
+    "BaseWeightedRegressor",
+    "TimeWeightedLADRegressor",
+    "SignWeightedLinearRegression",
+    "TimeWeightedLinearRegression",
+    "ModifiedLinearRegression",
+    "BaseModifiedRegressor",
+    # market beta estimation
+    "BetaEstimator",
+    "WeightedLinearRegression",
+    "WeightedLADRegressor",
+    # regression system
+    "BaseRegressionSystem",
+    "LADRegressionSystem",
+    "RidgeRegressionSystem",
+    "LinearRegressionSystem",
+    "CorrelationVolatilitySystem",
+]
