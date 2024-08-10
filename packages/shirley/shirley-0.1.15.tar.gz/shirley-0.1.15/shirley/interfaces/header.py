8007bf88e74fcadec17a2edc8db47ada690d44aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-10 15:04:57 +0000

    Add package shirley/0.1.15/shirley-0.1.15.tar.gz

---
+++ packages/shirley/shirley-0.1.15.tar.gz/shirley-0.1.15/shirley/interfaces/header.py
@@ -0,0 +1,49 @@
+import gradio as gr
+import logging
+import sys
+from .interface import Interface
+
+
+logger = logging.getLogger(__name__)
+logging.basicConfig(stream=sys.stdout, level=logging.INFO)
+
+
+class Header(Interface):
+
+    def __init__(self, *args, **kwargs) -> None:
+        super().__init__()
+
+        self._make_components(*args, **kwargs)
+
+
+    def _setup_dark_mode_button(self, *args, **kwargs) -> None:
+        dark_mode_button: gr.Button = kwargs['dark_mode_button']
+
+        dark_mode_button.click(
+            fn=None,
+            js='() => { document.body.classList.toggle(\'dark\'); }',
+            show_api=False,
+        )
+
+
+    def _setup(self, *args, **kwargs) -> None:
+        self._setup_dark_mode_button(*args, **kwargs)
+
+
+    def _make_components(self, *args, **kwargs) -> None:
+        title: str = kwargs.get('title', '🦈 Shirley WebUI')
+        description: str = kwargs.get(
+            'description',
+            'It is just doing some stuff intelligently. This WebUI is built by [luojiahai](https://luojiahai.com).',
+        )
+
+        with gr.Row():
+            with gr.Column(scale=4):
+                gr.Markdown(value=f'# {title}')
+                gr.Markdown(value=f'{description}')
+            with gr.Column(scale=1):
+                dark_mode_button = gr.Button(value='🌙 Dark Mode (深色模式)')
+
+        self._setup(
+            dark_mode_button=dark_mode_button,
+        )
