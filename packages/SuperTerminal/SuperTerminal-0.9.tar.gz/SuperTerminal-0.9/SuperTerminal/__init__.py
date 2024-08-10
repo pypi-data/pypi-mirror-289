5725ee080b27deafeeb860842df216a6a25c50e4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-10 15:04:19 +0000

    Add package SuperTerminal/0.9/SuperTerminal-0.9.tar.gz

---
+++ packages/SuperTerminal/SuperTerminal-0.9.tar.gz/SuperTerminal-0.9/SuperTerminal/__init__.py
@@ -0,0 +1,5 @@
+from .api_call import Command_Generator
+from .const_var import Color
+from .error_handler import *
+from .failed_command import Command_corrector
+from .system_run import System_Run
---
+++ packages/SuperTerminal/SuperTerminal-0.9.tar.gz/SuperTerminal-0.9/SuperTerminal/main.py
@@ -0,0 +1,30 @@
+def main():
+    import subprocess
+    from SuperTerminal.api_call import Command_Generator
+    from SuperTerminal.const_var import Color
+    from SuperTerminal.failed_command import Command_corrector
+    from SuperTerminal.system_run import System_Run
+
+
+
+    print('''Welcome to SuperTerminal!\n''')
+
+    User_prompt = input("$ ")
+
+
+    while User_prompt.lower() != "q":
+        Terminal_Command = str(Command_Generator(User_prompt))
+
+        result = subprocess.run(Terminal_Command , shell=True, capture_output=True, text=True)
+        New_Command = ""
+        while result.returncode != 0:
+            New_Command = str(Command_corrector(f"Command '{Terminal_Command}' failed with exit code {result.returncode}: {result.stderr}"))
+            result = subprocess.run(New_Command , shell=True, capture_output=True, text=True)
+
+        if New_Command == "":
+            print(Color.GRAY.value + "> " + Terminal_Command + Color.RESET.value)
+            System_Run(Terminal_Command)
+        else:
+            print(Color.GRAY.value + "> " + New_Command + Color.RESET.value)
+            System_Run(New_Command)
+        User_prompt = input("$ ")
