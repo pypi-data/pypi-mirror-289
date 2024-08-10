5154eb4b4c622e06508775a5c7f148af7272090d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-10 04:04:28 +0000

    Add package kaniko-deploy/0.5/kaniko_deploy-0.5.tar.gz

---
+++ packages/kaniko-deploy/kaniko_deploy-0.5.tar.gz/kaniko_deploy-0.5/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+import kaniko_deploy
+
+def parse_requirements(filename):
+    with open(filename, 'r') as f:
+        return [line.strip() for line in f if line and not line.startswith("#")]
+
+setup(
+    name=kaniko_deploy.__title__,
+    version='0.5',
+    packages=find_packages(),
+    install_requires=parse_requirements('requirements.txt'),
+    python_requires='>=3.12',
+    entry_points={
+        'console_scripts': [
+            'kaniko_deploy=kaniko_deploy.main:main',
+        ],
+    },
+    license='MIT',
+)
