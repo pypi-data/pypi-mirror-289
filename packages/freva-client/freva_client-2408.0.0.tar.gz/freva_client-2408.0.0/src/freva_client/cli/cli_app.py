fa16ca593ae66b7a1b97c4072250cde482e66ed2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-10 14:05:55 +0000

    Add package freva-client/2408.0.0/freva_client-2408.0.0.tar.gz

---
+++ packages/freva-client/freva_client-2408.0.0.tar.gz/freva_client-2408.0.0/src/freva_client/cli/cli_app.py
@@ -0,0 +1,35 @@
+"""Freva the Free Evaluation System command line interface."""
+
+from typing import Optional
+
+import typer
+from freva_client.utils import logger
+
+from .auth_cli import authenticate_cli
+from .cli_utils import APP_NAME, version_callback
+from .databrowser_cli import databrowser_app
+
+app = typer.Typer(
+    name=APP_NAME,
+    help=__doc__,
+    add_completion=True,
+    callback=logger.set_cli,
+)
+
+
+@app.callback()
+def main(
+    version: Optional[bool] = typer.Option(
+        None,
+        "--version",
+        "-V",
+        help="Show version and exit",
+        callback=version_callback,
+        is_eager=True,
+    ),
+) -> None:
+    """The main cli app."""
+
+
+app.add_typer(databrowser_app, name="databrowser")
+app.command(name="auth", help=authenticate_cli.__doc__)(authenticate_cli)
