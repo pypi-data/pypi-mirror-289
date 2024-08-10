d3be6f42464c2e37a290713c37fee9129b597708
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-10 15:04:17 +0000

    Add package SuperTerminal/0.1.6/SuperTerminal-0.1.6-py3-none-any.whl

---
+++ packages/SuperTerminal/SuperTerminal-0.1.6-py3-none-any.whl/SuperTerminal/__init__.py
@@ -0,0 +1,5 @@
+from api_call import Command_Generator
+from system_run import System_Run
+from api_call import Command_Generator
+from const_var import Color
+from failed_command import Command_corrector
