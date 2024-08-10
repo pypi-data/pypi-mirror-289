5d6afb0d46bfef659ffafcaa8edb7a0038ba4225
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-10 05:04:35 +0000

    Add package fortune-teller/0.0.1/fortune_teller-0.0.1.tar.gz

---
+++ packages/fortune-teller/fortune_teller-0.0.1.tar.gz/fortune_teller-0.0.1/setup.py
@@ -0,0 +1,39 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.0.1' 
+DESCRIPTION = 'Description of my first Python package'
+LONG_DESCRIPTION = 'Longer description of my first Python package'
+
+# Setting up
+setup(
+       # the name must match the folder name 'verysimplemodule'
+        name="fortune_teller", 
+        version=VERSION,
+        author="Chine Meston",
+        author_email="<cmeston@student.42.fr>",
+        description=DESCRIPTION,
+        long_description=LONG_DESCRIPTION,
+        packages=find_packages(),
+        install_requires=[], # add any additional packages that 
+        # needs to be installed along with your package. Eg: 'caer'
+        
+        keywords=['python', 'first package'],
+        classifiers= [
+            "Development Status :: 3 - Alpha",
+            "Intended Audience :: Education",
+            "Programming Language :: Python :: 2",
+            "Programming Language :: Python :: 3",
+            "Operating System :: MacOS :: MacOS X",
+            "Operating System :: Microsoft :: Windows",
+        ]
+)
+
+# hoqgaf-xukdik-6vUgxe
+# pipwex-xogxoj-peMqa4
+# rupcek-bywpah-ziWru6
+
+# mystic 8 ball
+# fortune cookie
+# compliments
+
+# pypi-AgEIcHlwaS5vcmcCJGU4NDVmYzIwLWQzOTMtNDY1OC04N2VlLTRmM2ViNmYyYWFjOAACKlszLCI2YjJjY2QyOC01M2VmLTRjNTQtYTE4OC04NmNjYzE1NDQ5NGIiXQAABiDnVWEJctbWC6pTImiSr0pVKG4aa9W7t32_Ti_1V01Oiw
