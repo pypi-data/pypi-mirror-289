e98d14b302aeda877085e9b2c429d254a8a0f471
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-10 05:04:31 +0000

    Add package craft-parts/2.0.0/craft_parts-2.0.0.tar.gz

---
+++ packages/craft-parts/craft_parts-2.0.0.tar.gz/craft_parts-2.0.0/docs/how-to/index.rst
@@ -0,0 +1,13 @@
+.. _howto:
+
+How-to guides
+*************
+
+.. toctree::
+   :maxdepth: 1
+
+   Use the craftctl tool </common/craft-parts/how-to/craftctl>
+   Use craft-parts from the command-line <cli_tool>
+   Override a part's default build behavior </common/craft-parts/how-to/override_build>
+   Document a plugin <document-plugin>
+   Including local files and remote resources </common/craft-parts/how-to/include_files>
---
+++ packages/craft-parts/craft_parts-2.0.0.tar.gz/craft_parts-2.0.0/tests/unit/plugins/test_base.py
@@ -0,0 +1,85 @@
+# -*- Mode:Python; indent-tabs-mode:nil; tab-width:4 -*-
+#
+# Copyright 2021-2023 Canonical Ltd.
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License version 3 as published by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+from typing import Literal, cast
+
+import pytest
+from craft_parts.infos import PartInfo, ProjectInfo
+from craft_parts.parts import Part
+from craft_parts.plugins import Plugin, PluginProperties
+
+
+class FooPluginProperties(PluginProperties, frozen=True):
+    """Test plugin properties."""
+
+    plugin: Literal["foo"] = "foo"
+
+    foo_name: str
+
+
+class FooPlugin(Plugin):
+    """A test plugin."""
+
+    properties_class = FooPluginProperties
+
+    def get_build_snaps(self) -> set[str]:
+        return {"build_snap"}
+
+    def get_build_packages(self) -> set[str]:
+        return {"build_package"}
+
+    def get_build_environment(self) -> dict[str, str]:
+        return {"ENV": "value"}
+
+    def get_build_commands(self) -> list[str]:
+        options = cast(FooPluginProperties, self._options)
+        return ["hello", options.foo_name]
+
+
+def test_plugin(new_dir):
+    part = Part("p1", {})
+    project_info = ProjectInfo(application_name="test", cache_dir=new_dir)
+    part_info = PartInfo(project_info=project_info, part=part)
+
+    props = FooPluginProperties.unmarshal({"foo-name": "world"})
+    plugin = FooPlugin(properties=props, part_info=part_info)
+
+    validator = FooPlugin.validator_class(part_name=part.name, env="", properties=props)
+    validator.validate_environment()
+
+    assert plugin.get_build_snaps() == {"build_snap"}
+    assert plugin.get_build_packages() == {"build_package"}
+    assert plugin.get_build_environment() == {"ENV": "value"}
+    assert plugin.get_out_of_source_build() is False
+    assert plugin.get_build_commands() == ["hello", "world"]
+
+
+def test_abstract_methods(new_dir):
+    class FaultyPlugin(Plugin):
+        """A plugin that doesn't implement abstract methods."""
+
+    part = Part("p1", {})
+    project_info = ProjectInfo(application_name="test", cache_dir=new_dir)
+    part_info = PartInfo(project_info=project_info, part=part)
+
+    expected = (
+        r"^Can't instantiate abstract class FaultyPlugin with(out an implementation "
+        r"for)? abstract methods '?get_build_commands'?, '?get_build_environment'?, "
+        r"'?get_build_packages'?, '?get_build_snaps'?$"
+    )
+
+    with pytest.raises(TypeError, match=expected):
+        FaultyPlugin(properties=None, part_info=part_info)  # type: ignore[reportGeneralTypeIssues]
