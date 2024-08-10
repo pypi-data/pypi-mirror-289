6b772a9673fcc68f872ff78cb434726b8bb5436b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-10 04:04:55 +0000

    Add package psqlpy/0.7.5/psqlpy-0.7.5-cp311-none-win_amd64.whl

---
+++ packages/psqlpy/psqlpy-0.7.5-cp311-none-win_amd64.whl/psqlpy/extra_types.py
@@ -0,0 +1,31 @@
+from ._internal.extra_types import (
+    BigInt,
+    Float32,
+    Float64,
+    Integer,
+    Money,
+    PyCustomType,
+    PyJSON,
+    PyJSONB,
+    PyMacAddr6,
+    PyMacAddr8,
+    PyText,
+    PyVarChar,
+    SmallInt,
+)
+
+__all__ = [
+    "SmallInt",
+    "Integer",
+    "BigInt",
+    "PyJSONB",
+    "PyJSON",
+    "PyMacAddr6",
+    "PyMacAddr8",
+    "PyVarChar",
+    "PyText",
+    "PyCustomType",
+    "Float32",
+    "Float64",
+    "Money",
+]
