ef078c231ab27b3c76844bbb725b25afb50710f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-10 03:06:23 +0000

    Add package lang-search-plugin/0.8.3/lang_search_plugin-0.8.3.tar.gz

---
+++ packages/lang-search-plugin/lang_search_plugin-0.8.3.tar.gz/lang_search_plugin-0.8.3/lang_search_plugin/lang_search_plugin.py
@@ -0,0 +1,27 @@
+import os
+from mkdocs.plugins import BasePlugin
+from mkdocs.config import config_options
+import logging
+
+logger = logging.getLogger("mkdocs.plugins.lang_search_plugin")
+
+class LangSearchPlugin(BasePlugin):
+
+    config_scheme = (
+        ('default_language', config_options.Type(str, default='en')),
+        ('languages', config_options.Type(list, default=['en'])),
+        ('paths', config_options.Type(dict, default={})),
+    )
+
+    def on_pre_build(self, config):
+        logger.info(f"config['plugins']")
+        if 'search' in config['plugins']:
+            logger.info("search found in plugins")
+
+
+    def on_post_build(self, config):
+        logger.info("post: looking for search plugins")
+        if 'search' in config['plugins']:
+            logger.info("search found in plugins")
+
+
