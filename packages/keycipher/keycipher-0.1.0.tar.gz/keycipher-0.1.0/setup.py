e1f14805be0d0025ce49cd44ecc1499ca75d4755
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-10 15:04:18 +0000

    Add package keycipher/0.1.0/keycipher-0.1.0.tar.gz

---
+++ packages/keycipher/keycipher-0.1.0.tar.gz/keycipher-0.1.0/setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="keycipher",
+    version="0.1.0",
+    description="A Python package for AES-GCM encryption and decryption with a key.",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    author="imparth",
+    url="https://github.com/imparth7/keycipher-py",
+    packages=find_packages(),
+    install_requires=[
+        "cryptography>=3.4.7",
+    ],
+    python_requires=">=3.6",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+        "Programming Language :: Python :: 3 :: Only",
+    ],
+    keywords=["encryption", "decryption", "AES-GCM", "cryptography"],
+    project_urls={
+        "Source": "https://github.com/imparth7/keycipher-py",
+        "Bug Reports": "https://github.com/imparth7/keycipher-py/issues",
+        "Homepage": "https://github.com/imparth7/keycipher-py#readme",
+    },
+)
