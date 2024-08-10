eb88d03b6cffdbdf23a5e8e46295d6c4f14e51c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-10 03:06:12 +0000

    Add package pagekey-semver/0.2.2/pagekey_semver-0.2.2-py3-none-any.whl

---
+++ packages/pagekey-semver/pagekey_semver-0.2.2-py3-none-any.whl/pagekey_semver/cli.py
@@ -0,0 +1,70 @@
+"""Module for CLI."""
+
+import argparse
+import sys
+from pathlib import Path
+
+from pagekey_semver.changelog import ChangelogWriter
+from pagekey_semver.file_replacer import FileReplacer
+from pagekey_semver.git import GitManager
+from pagekey_semver.release import SemverRelease, Tag
+from pagekey_semver.config import load_config
+
+
+def cli_entrypoint(args=sys.argv[1:]):
+    """Runs pagekey-semver when called from the command-line.
+    
+    Args:
+        args: List of command-line args passed by user, or fake args for testing purposes.
+    """
+    parser = argparse.ArgumentParser(description="PageKey Semver")
+    subparsers = parser.add_subparsers(title="Commands", dest="command")
+    subparsers.add_parser("plan", help="Compute which version would be created. (Dry-run)")
+    subparsers.add_parser("apply", help="Compute version, then commit, tag, and push.")
+    parsed_args = parser.parse_args(args)
+
+    if parsed_args.command == "plan":
+        dry_run = True
+    elif parsed_args.command == "apply":
+        dry_run = False
+    else:
+        parser.print_usage()
+        return
+
+    config = load_config(Path(".semver"))
+
+    # Init classes.
+    manager = GitManager(config)
+    release = SemverRelease(config)
+    writer = ChangelogWriter.from_config(config)
+
+    # Compute tags, commits
+    tags = manager.get_git_tags()
+    max_tag: Tag = release.get_biggest_tag(tags)
+    hash = None
+    if max_tag is not None:
+        hash = max_tag.name
+    commits = manager.get_commit_messages_since(hash)
+    release_type = release.compute_release_type(commits)
+    next_version: Tag = release.compute_next_version(release_type, tags)
+
+    print("Next version:", next_version, flush=True)
+    if max_tag == next_version:
+        print("No new release (nothing to do).")
+    else:
+        # Apply tag if appropriate.
+        if not dry_run:
+            print("Applying version.")
+            # Write to changelog.
+            writer.update_changelog(next_version, commits)
+            # Replace files
+            file_replacer = FileReplacer(config, next_version)
+            file_replacer.replace_all()
+            # Apply tag, commit, push
+            manager.apply_tag(tags, next_version)
+        else:
+            print("Dry run mode - not applying version.", flush=True)
+
+
+if __name__ == "__main__":
+    cli_entrypoint()
