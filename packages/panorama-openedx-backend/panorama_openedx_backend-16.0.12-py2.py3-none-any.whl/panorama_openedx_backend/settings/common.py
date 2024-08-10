8a3f57ead71b4e0056aba1ec7668aec7199075fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-10 04:04:31 +0000

    Add package panorama-openedx-backend/16.0.12/panorama_openedx_backend-16.0.12-py2.py3-none-any.whl

---
+++ packages/panorama-openedx-backend/panorama_openedx_backend-16.0.12-py2.py3-none-any.whl/panorama_openedx_backend/settings/common.py
@@ -0,0 +1,14 @@
+# coding=utf-8
+"""
+Common Pluggable Django App settings.
+
+"""
+
+
+def plugin_settings(settings):
+    """
+    Inject local settings into django settings.
+    """
+    print(settings)
+    # settings.EXAMPLE = value
+    # pass
