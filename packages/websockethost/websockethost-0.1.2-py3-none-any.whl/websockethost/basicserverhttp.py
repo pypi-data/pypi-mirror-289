10608fc607f99261cbab27450638965f3ec12c0d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-10 03:04:37 +0000

    Add package websockethost/0.1.2/websockethost-0.1.2-py3-none-any.whl

---
+++ packages/websockethost/websockethost-0.1.2-py3-none-any.whl/websockethost/basicserverhttp.py
@@ -0,0 +1,17 @@
+import http.server as server
+import socketserver
+import threading 
+
+def start_server(port: int, host: str):
+    adress = (host, port)
+
+    handler = server.SimpleHTTPRequestHandler
+    httpR = socketserver.TCPServer(adress, handler)
+    
+    try:        
+        print('Server lancer CTRL + C pour arreté le serveur...')
+        httpR.serve_forever()
+    except KeyboardInterrupt:
+        print('Serveur arrété...')
+
+start_server(8080, "localhost")
