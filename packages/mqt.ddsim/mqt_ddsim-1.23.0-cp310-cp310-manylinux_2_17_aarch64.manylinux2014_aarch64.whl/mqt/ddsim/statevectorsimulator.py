afcdcd6b3b752a46ed575407ab93416d7e75c98a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-10 14:05:23 +0000

    Add package mqt.ddsim/1.23.0/mqt_ddsim-1.23.0-cp310-cp310-manylinux_2_17_aarch64.manylinux2014_aarch64.whl

---
+++ packages/mqt.ddsim/mqt_ddsim-1.23.0-cp310-cp310-manylinux_2_17_aarch64.manylinux2014_aarch64.whl/mqt/ddsim/statevectorsimulator.py
@@ -0,0 +1,24 @@
+"""Backend for DDSIM."""
+
+from __future__ import annotations
+
+from qiskit.transpiler import Target
+
+from .qasmsimulator import QasmSimulatorBackend
+
+
+class StatevectorSimulatorBackend(QasmSimulatorBackend):
+    """Python interface to MQT DDSIM."""
+
+    _SHOW_STATE_VECTOR = True
+    _SV_TARGET = Target(
+        description="MQT DDSIM Statevector Simulator Target",
+        num_qubits=30,  # corresponds to 16GiB memory for storing the full statevector
+    )
+
+    def __init__(self) -> None:
+        super().__init__(name="statevector_simulator", description="MQT DDSIM Statevector Simulator")
+
+    @property
+    def target(self) -> Target:
+        return self._SV_TARGET
