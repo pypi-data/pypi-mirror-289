c895e70d94d2616559ff78736eb2b75ae3c960ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-10 14:05:36 +0000

    Add package parllama/0.3.6/parllama-0.3.6-py3-none-any.whl

---
+++ packages/parllama/parllama-0.3.6-py3-none-any.whl/parllama/messages/par_chat_messages.py
@@ -0,0 +1,20 @@
+"""Chat messages for par event system"""
+
+from __future__ import annotations
+
+from dataclasses import dataclass
+
+from parllama.par_event_system import ParEventBase
+
+
+@dataclass
+class ParChatMessage(ParEventBase):
+    """Chat message base class"""
+
+    parent_id: str
+    message_id: str
+
+
+@dataclass
+class ParChatUpdated(ParChatMessage):
+    """Chat message updated"""
