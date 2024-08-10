7e018c2e36475caed1a7bb3e8b27da0598e984ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-10 15:05:21 +0000

    Add package nonebot-plugin-ba-tools/0.0.2/nonebot_plugin_ba_tools-0.0.2.tar.gz

---
+++ packages/nonebot-plugin-ba-tools/nonebot_plugin_ba_tools-0.0.2.tar.gz/nonebot_plugin_ba_tools-0.0.2/nonebot_plugin_ba_tools/__init__.py
@@ -0,0 +1,27 @@
+from pathlib import Path
+from nonebot import get_plugin_config, load_plugins
+from nonebot.plugin import PluginMetadata
+
+from .config import Config
+from .send_birthday_info import send_birthday_info
+
+
+__version__ = "0.0.2"
+__plugin_meta__ = PluginMetadata(
+    name="ba-tools",
+    description="BlueArchive的工具箱",
+    usage="推送每日学生生日信息等",
+    type="application",
+    homepage="https://github.com/hanasa2023/ba-tools#readme",
+    config=Config,
+    supported_adapters={"~onebot.v11"},
+    extra={
+        "version": __version__,
+        "authors": [
+            "hanasa2023 <hanasakayui2022@gmail.com>",
+            "TheresaKugua <2277044081@qq.com>",
+        ],
+    },
+)
+
+config = get_plugin_config(Config)
