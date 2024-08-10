64eda5d06bcb987439c59cb367ad98c8a2d0ef32
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-10 14:05:20 +0000

    Add package digifoam/0.1/digifoam-0.1.tar.gz

---
+++ packages/digifoam/digifoam-0.1.tar.gz/digifoam-0.1/setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="digifoam",
+    version="0.1",
+    author="Michael",
+    author_email="michael@digitalcarbon.ai",
+    description="DigiFOAM CLI",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://digifoam.ai",
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        "click",
+        "python-dotenv",
+        "requests",
+        "chardet",
+        "typer",
+    ],
+    python_requires=">=3.7",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    entry_points={
+        "console_scripts": [
+            "digifoam=digifoam.cli:app",
+        ],
+    },
+)
