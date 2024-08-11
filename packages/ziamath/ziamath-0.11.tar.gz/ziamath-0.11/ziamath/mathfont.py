0dd24fcea060040ef2a16fb1e6ef30e3c8327bd9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-11 04:05:03 +0000

    Add package ziamath/0.11/ziamath-0.11.tar.gz

---
+++ packages/ziamath/ziamath-0.11.tar.gz/ziamath-0.11/ziamath/mathfont.py
@@ -0,0 +1,28 @@
+''' Math Font extends Ziafont with MATH table '''
+
+from __future__ import annotations
+from typing import Union
+from pathlib import Path
+
+from ziafont import Font
+
+from .mathtable import MathTable
+
+
+class MathFont(Font):
+    ''' Extend ziafont by reading MATH table and a base font size
+
+        Args:
+            fname: File name of font
+            basesize: Default font size
+    '''
+    def __init__(self, fname: Union[str, Path], basesize: float = 24):
+        super().__init__(fname)
+        self.basesize = basesize
+        if 'MATH' not in self.tables:
+            raise ValueError('Font has no MATH table!')
+
+        self.math = MathTable(self)
+        if 'math' in self.scripts():
+            self.language('math', '')
+        self.features['ssty'] = True  # Enable math script variants
---
+++ packages/ziamath/ziamath-0.11.tar.gz/ziamath-0.11/ziamath/tex.py
@@ -0,0 +1,38 @@
+''' Latex to MathML interface '''
+import re
+import latex2mathml.tokenizer
+import latex2mathml.commands
+from latex2mathml.converter import convert
+
+from .config import config
+
+
+def declareoperator(name: str) -> None:
+    r''' Declare a new operator name, similar to Latex ``\DeclareMathOperator`` command.
+
+        Args:
+            name: Name of operator, should start with a ``\``.
+                Example: ``declareoperator(r'\myfunc')``
+    '''
+    latex2mathml.commands.FUNCTIONS = latex2mathml.commands.FUNCTIONS + (name,)  # type: ignore
+
+
+def tex2mml(tex: str, inline: bool = False) -> str:
+    ''' Convert Latex to MathML. Do some hacky preprocessing to work around
+        some issues with generated MathML that ziamath doesn't support yet.
+    '''
+    tex = re.sub(r'\\binom{(.+?)}{(.+?)}', r'\\left( \1 \\atop \2 \\right)', tex)
+    # latex2mathml bug requires space after mathrm
+    tex = re.sub(r'\\mathrm{(.+?)}', r'\\mathrm {\1}', tex)
+    tex = tex.replace('||', '‖')
+    if config.decimal_separator == ',':
+        # Replace , with {,} to remove right space
+        # (must be surrounded by digits)
+        tex = re.sub(r'([0-9]),([0-9])', r'\1{,}\2', tex)
+
+    mml = convert(tex, display='inline' if inline else 'block')
+
+    # Replace some operators with "stretchy" variants
+    mml = re.sub(r'<mo>&#x0005E;', r'<mo>&#x00302;', mml)  # widehat
+    mml = re.sub(r'<mo>&#x0007E;', r'<mo>&#x00303;', mml)  # widetilde
+    return mml
