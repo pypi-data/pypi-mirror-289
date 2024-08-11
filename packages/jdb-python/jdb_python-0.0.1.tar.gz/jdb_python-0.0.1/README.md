59e46419c6c919286891543b51105e4078aa2740
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-11 05:05:06 +0000

    Add package jdb-python/0.0.1/jdb_python-0.0.1.tar.gz

---
+++ packages/jdb-python/jdb_python-0.0.1.tar.gz/jdb_python-0.0.1/README.md
@@ -0,0 +1,2 @@
+# json-data-base-python
+Json DATABASE Project attempt in python!
---
+++ packages/jdb-python/jdb_python-0.0.1.tar.gz/jdb_python-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup,find_packages
+
+setup(
+    name="jdb_python"
+    ,
+    version="0.0.1"
+    ,
+    packages=find_packages()
+    ,
+    description= "Package of mine to do a JSON based simple database!"
+)
