df9ad166f5cbbd904f4f599f6781a1ccedfc0b65
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-11 03:05:52 +0000

    Add package ASHTEAM/0.2/ASHTEAM-0.2.tar.gz

---
+++ packages/ASHTEAM/ASHTEAM-0.2.tar.gz/ASHTEAM-0.2/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup,find_packages
+
+
+setup(
+	version='0.2',
+	name='ASHTEAM',
+	author='ASHTEAM',
+	description='A powerful encryption libraries in Python created by the TC4 team',
+	packages=find_packages(),
+	classifiers=[
+	'Programming Language :: Python :: 3'
+
+	]
+
+)
+
+
+
+
+
