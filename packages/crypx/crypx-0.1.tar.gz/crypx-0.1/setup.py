1305873bf5e99dae8b8699250ada5e0b43bc1373
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-11 04:05:03 +0000

    Add package crypx/0.1/crypx-0.1.tar.gz

---
+++ packages/crypx/crypx-0.1.tar.gz/crypx-0.1/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup,find_packages
+
+
+setup(
+	version='0.1',
+	name='crypx',
+	author='ASH team',
+	description='A powerful encryption libraries in Python created by the ASH team',
+	packages=find_packages(),
+	
+
+)
+
+
+
+
