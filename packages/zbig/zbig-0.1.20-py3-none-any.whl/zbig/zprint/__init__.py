bd5c8826c9f34fc03734412b69a0ad57e2c1ddbd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-11 03:05:49 +0000

    Add package zbig/0.1.20/zbig-0.1.20-py3-none-any.whl

---
+++ packages/zbig/zbig-0.1.20-py3-none-any.whl/zbig/zprint/__init__.py
@@ -0,0 +1,12 @@
+"""
+Some tool
+
+Print python library.
+"""
+
+from .cn_now import cn_now
+from .table import table, curses_table
+from .json_print import json_print
+
+__version__ = "0.1.0"
+__author__ = "Big Zhu"
---
+++ packages/zbig/zbig-0.1.20-py3-none-any.whl/zbig/ztelegram/__init__.py
@@ -0,0 +1,7 @@
+"""
+wrap telegram bot
+need put .env file in the same folder and set TEL_TOKEN CHAT_ID
+"""
+
+__version__ = "0.1.0"
+__author__ = "Big Zhu"
