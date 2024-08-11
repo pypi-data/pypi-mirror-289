2c672f96fb68e5efa03e0941cd7787e80dbe68a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-11 04:04:35 +0000

    Add package omu/0.6.18/omu-0.6.18.tar.gz

---
+++ packages/omu/omu-0.6.18.tar.gz/omu-0.6.18/src/omu/extension/extension.py
@@ -0,0 +1,31 @@
+from __future__ import annotations
+
+import abc
+from collections.abc import Callable
+
+from omu.client import Client
+from omu.identifier import Identifier
+
+
+class Extension(abc.ABC):
+    pass
+
+
+class ExtensionType[T: Extension](Identifier):
+    name: str
+    create: Callable[[Client], T]
+    dependencies: Callable[[], list[ExtensionType]]
+
+    def __init__(
+        self,
+        name: str,
+        create: Callable[[Client], T],
+        dependencies: Callable[[], list[ExtensionType]],
+    ) -> None:
+        super().__init__("ext", name)
+        self.name = name
+        self.create = create
+        self.dependencies = dependencies
+
+    def key(self) -> str:
+        return self.name
