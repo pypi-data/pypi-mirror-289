e9f4cca1b868cdd0fb1b5be3997a72ae89a92789
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-11 03:05:33 +0000

    Add package syncall/1.8.6/syncall-1.8.6-py3-none-any.whl

---
+++ packages/syncall/syncall-1.8.6-py3-none-any.whl/syncall/__init__.py
@@ -0,0 +1,7 @@
+"""__init__"""
+
+# global imports ------------------------------------------------------------------------------
+from syncall.__version__ import __version__ as version
+from syncall.__version__ import __version_tuple__ as version_tuple
+
+__all__ = ["version", "version_tuple"]
