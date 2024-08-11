cd1af0d5137b96e7c2a594bd6106055a183ccef6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-11 04:04:45 +0000

    Add package dreamfinetune/0.1.0/dreamfinetune-0.1.0.tar.gz

---
+++ packages/dreamfinetune/dreamfinetune-0.1.0.tar.gz/dreamfinetune-0.1.0/src/dreamfinetune/__init__.py
@@ -0,0 +1,36 @@
+__version__ = "1.0"
+
+from diffusers import _LazyModule
+
+from .trainings import StableDiffusionInpaintingFineTune
+from .trainings import StableDiffusionTextToImageFineTune
+
+from .utils import (
+    DreamBoothDataset, PromptDataset
+)
+
+_import_structure = {
+    "configuration_utils": ["ConfigMixin"],
+    "loaders": ["FromOriginalModelMixin"],
+    "models": [],
+    "pipelines": [],
+    "schedulers": [],
+    "utils": [
+        "DreamBoothDataset",
+        "PromptDataset"
+    ],
+    "training": [
+        "StableDiffusionInpaintingFineTune",
+        "StableDiffusionTextToImageFineTune"
+    ]
+}
+
+import sys
+
+sys.modules[__name__] = _LazyModule(
+    __name__,
+    globals()["__file__"],
+    _import_structure,
+    module_spec="",
+    extra_objects={"__version__": __version__},
+)
