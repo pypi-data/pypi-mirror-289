2faa364c8465d19db04cb5f1528666681ef22727
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-11 15:04:36 +0000

    Add package scyllaft/1.6.3/scyllaft-1.6.3.tar.gz

---
+++ packages/scyllaft/scyllaft-1.6.3.tar.gz/scyllaft-1.6.3/src/query_builder/utils.rs
@@ -0,0 +1,43 @@
+use pyo3::{FromPyObject, Py, PyAny};
+
+#[derive(FromPyObject, Debug, Clone)]
+pub enum Timeout {
+    #[pyo3(transparent)]
+    Int(i32),
+    #[pyo3(transparent)]
+    Str(String),
+}
+
+#[derive(Clone, Debug)]
+pub enum IfCluase {
+    Exists,
+    Condition {
+        clauses: Vec<String>,
+        values: Vec<Py<PyAny>>,
+    },
+}
+
+impl IfCluase {
+    #[must_use]
+    pub fn extend_values(&self, query_values: Vec<Py<PyAny>>) -> Vec<Py<PyAny>> {
+        match self {
+            IfCluase::Exists => query_values,
+            IfCluase::Condition { clauses: _, values } => {
+                query_values.iter().chain(values.iter()).cloned().collect()
+            }
+        }
+    }
+}
+
+/// Function for building
+/// pretty queries.
+///
+/// It assembles all query parts in one string,
+/// removing empty string and joining all query parts with space character.
+pub fn pretty_build<'a>(query_parts: impl IntoIterator<Item = &'a str>) -> String {
+    query_parts
+        .into_iter()
+        .filter(|s| !s.is_empty())
+        .collect::<Vec<_>>()
+        .join(" ")
+}
