26568d4cc5f127a5630614694ac914abc9b4077e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-11 04:04:33 +0000

    Add package networklab/1.9.0.dev2/networklab-1.9.0.dev2.tar.gz

---
+++ packages/networklab/networklab-1.9.0.dev2.tar.gz/networklab-1.9.0.dev2/netsim/devices/arubacx.py
@@ -0,0 +1,32 @@
+#
+# Aruba AOS-CX quirks
+# # 
+# # OSPF Process ID can only be 1-63.
+# # When using VRFs, the process ID is taken from the vrfidx, which usually is > 100.
+# # Here we are mapping every VRF to a specific ospfidx
+#
+from box import Box
+
+from . import _Quirks,need_ansible_collection
+from ..augment import devices
+from ..utils import log
+
+class ARUBACX(_Quirks):
+
+  @classmethod
+  def device_quirks(self, node: Box, topology: Box) -> None:
+    mods = node.get('module',[])
+    if 'ospf' in mods and 'vrf' in mods:
+        ospfidx = 2
+        for vrf in node.get('vrfs', {}).keys():
+            if ospfidx > 63:
+                log.error(
+                    f'Too many VRFs with OSPF in ({node.name}).\n',
+                    log.IncorrectType,
+                    'quirks')
+                return
+            node.vrfs[vrf]['ospfidx'] = ospfidx
+            ospfidx = ospfidx + 1
+
+  def check_config_sw(self, node: Box, topology: Box) -> None:
+    need_ansible_collection(node,'arubanetworks.aoscx')
---
+++ packages/networklab/networklab-1.9.0.dev2.tar.gz/networklab-1.9.0.dev2/netsim/modules/eigrp.py
@@ -0,0 +1,19 @@
+#
+# IS-IS transformation module
+#
+from box import Box
+
+from . import _Module,_routing
+
+class EIGRP(_Module):
+
+  def node_post_transform(self, node: Box, topology: Box) -> None:
+    _routing.router_id(node,'eigrp',topology.pools)
+    for intf in node.get('interfaces',[]):
+      if not _routing.external(intf,'eigrp'):
+        _routing.passive(intf,'eigrp',topology)
+
+    _routing.remove_unaddressed_intf(node,'eigrp')
+    _routing.remove_vrf_interfaces(node,'eigrp')
+    _routing.routing_af(node,'eigrp')
+    _routing.remove_unused_igp(node,'eigrp',topology.defaults.get('eigrp.warnings.inactive',False))
