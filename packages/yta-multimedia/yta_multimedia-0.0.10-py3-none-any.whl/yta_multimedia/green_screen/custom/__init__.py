785fdf1a201024bbd17463ce6230afc740c81baa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-11 05:05:07 +0000

    Add package yta-multimedia/0.0.10/yta_multimedia-0.0.10-py3-none-any.whl

---
+++ packages/yta-multimedia/yta_multimedia-0.0.10-py3-none-any.whl/yta_multimedia/green_screen/custom/__init__.py
@@ -0,0 +1,5 @@
+# TODO: This custom green screens need work, refactor and 
+# make them work better and simplify te code.
+# Consider this part as experimental.
+# Was working in the first stage but it is not clear how 
+# to handle it in code (x.x)
---
+++ packages/yta-multimedia/yta_multimedia-0.0.10-py3-none-any.whl/yta_multimedia/video/generation/google/__init__.py
@@ -0,0 +1,37 @@
+from yta_multimedia.video.utils import generate_videoclip_from_image
+from yta_multimedia.video.generation.constants import GOOGLE_SEARCH_IMAGE_ABSOLUTE_PATH
+from yta_multimedia.audio.sound.generation import create_typing_audio_clip
+from moviepy.editor import TextClip, CompositeVideoClip
+
+class GoogleSearch():
+    __EXTENDED_DURATION = 0.5
+    __DURATION = 3
+
+    def __init__(self, text):
+        self.text = text
+
+    def generate(self):
+        clip = generate_videoclip_from_image(GOOGLE_SEARCH_IMAGE_ABSOLUTE_PATH)
+
+        # Calculate each char duration and set texts according to this
+        text_len = len(self.text)
+        each_char_duration = self.__DURATION / text_len
+
+        clips = []
+        for i in range(text_len):
+            # Generate a text clip for each text writing part
+            txt_clip = TextClip(self.text[:i + 1], font = 'Arial', fontsize = 40, color = 'black')
+            clip_duration = each_char_duration
+            if i == (text_len - 1):
+                clip_duration = each_char_duration + self.__EXTENDED_DURATION
+            
+            txt_clip = txt_clip.set_position([380, 465]).set_duration(clip_duration).set_start(i * each_char_duration)
+
+            clips.append(txt_clip)
+
+        video = CompositeVideoClip([clip] + clips)
+        # Here we have the text being written, we need the sound
+        audio = create_typing_audio_clip()
+        video = video.set_audio(audio)
+
+        return video
