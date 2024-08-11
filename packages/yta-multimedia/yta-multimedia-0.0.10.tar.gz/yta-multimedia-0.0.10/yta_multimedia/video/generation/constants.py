c92f9f63c8bd21853a030b9af65fbb0079423ab9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-11 05:05:07 +0000

    Add package yta-multimedia/0.0.10/yta-multimedia-0.0.10.tar.gz

---
+++ packages/yta-multimedia/yta-multimedia-0.0.10.tar.gz/yta-multimedia-0.0.10/yta_multimedia/video/generation/constants.py
@@ -0,0 +1,9 @@
+from pathlib import Path
+
+RESOURCES_ABSOLUTE_PATH = (Path(__file__).parent.parent.__str__() + '/resources/video/generation/').replace('\\', '/')
+
+# TODO: This resources should be urls to make a download when generating
+# because the project will become very large, or we should generate a 
+# a method that downloads the resources package and stores it in a local
+# folder
+GOOGLE_SEARCH_IMAGE_ABSOLUTE_PATH = RESOURCES_ABSOLUTE_PATH + 'google_search.jpg'
