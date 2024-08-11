a8c1dc4d7b6575a2b484d0f796edf0490c3b6d6a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-11 05:05:07 +0000

    Add package rratelimiter/0.1.1/rratelimiter-0.1.1.tar.gz

---
+++ packages/rratelimiter/rratelimiter-0.1.1.tar.gz/rratelimiter-0.1.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="rratelimiter",
+    version="0.1.1",
+    packages=find_packages(),
+    install_requires=[
+        # list your dependencies here
+    ],
+    author="Rishabh Prajapati",
+    author_email="prajapatirishabh04@gmail.com",
+    description="A simple example Python library",
+)
