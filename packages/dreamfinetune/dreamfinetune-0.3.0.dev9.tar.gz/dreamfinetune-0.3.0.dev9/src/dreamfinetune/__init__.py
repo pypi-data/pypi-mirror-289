73138f1155a8cd958715566d9924f921a0c70ad4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-11 05:04:58 +0000

    Add package dreamfinetune/0.3.0.dev9/dreamfinetune-0.3.0.dev9.tar.gz

---
+++ packages/dreamfinetune/dreamfinetune-0.3.0.dev9.tar.gz/dreamfinetune-0.3.0.dev9/src/dreamfinetune/__init__.py
@@ -0,0 +1,53 @@
+__version__ = "0.1"
+
+import importlib
+import sys
+
+from trainings.Inpaint import StableDiffusionInpaintingFineTune
+from trainings.TextToImage import StableDiffusionTextToImageFineTune
+from utils.DreamBoothDataset import DreamBoothInpaintDataset, DreamBoothTextToImageDataset
+from utils.PromptDataset import PromptDataset
+
+_import_structure = {
+    "configuration_utils": ["ConfigMixin"],
+    "loaders": ["FromOriginalModelMixin"],
+    "models": [],
+    "pipelines": [],
+    "schedulers": [],
+    "utils": [
+        "DreamBoothInpaintDataset",
+        "DreamBoothTextToImageDataset",
+        "PromptDataset"
+    ],
+    "trainings": [
+        "StableDiffusionInpaintingFineTune",
+        "StableDiffusionTextToImageFineTune"
+    ]
+}
+
+
+def _load_module(name):
+    """Load a module and return it."""
+    return importlib.import_module(name)
+
+
+class LazyLoader:
+    def __init__(self, import_structure, extra_objects=None):
+        self._import_structure = import_structure
+        self._loaded_modules = {}
+        self._extra_objects = extra_objects or {}
+
+    def __getattr__(self, item):
+        if item in self._extra_objects:
+            return self._extra_objects[item]
+
+        for module_name, objects in self._import_structure.items():
+            if item in objects:
+                if module_name not in self._loaded_modules:
+                    self._loaded_modules[module_name] = _load_module(f".{module_name}")
+                return getattr(self._loaded_modules[module_name], item)
+
+        raise AttributeError(f"Module {__name__} has no attribute {item}")
+
+
+sys.modules[__name__] = LazyLoader(_import_structure, extra_objects={"__version__": __version__})
