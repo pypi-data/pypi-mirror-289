8464dc65fc045a71b52da34561e81a68968f102a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-11 04:05:01 +0000

    Add package DreamBoothFineTune/0.1/dreamboothfinetune-0.1.tar.gz

---
+++ packages/DreamBoothFineTune/dreamboothfinetune-0.1.tar.gz/dreamboothfinetune-0.1/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='DreamBoothFineTune',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[
+        "pillow~=10.4.0",
+        "torchvision~=0.19.0",
+        "diffusers~=0.30.0",
+        "transformers~=4.44.0",
+        "tqdm~=4.66.4",
+        "datasets~=2.20.0",
+        "bitsandbytes"
+    ],
+    author='Alex',
+    long_description=open('README.md').read(),
+    long_description_content='text/markdown',
+    url='https://github.com/skillfi/fine-tuning',
+    classifiers=[
+
+    ],
+    python_requires='>=3.12'
+)
