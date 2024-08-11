6d26b08843a6d4c7ad006029a37a20f8faac5f85
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-11 04:04:49 +0000

    Add package microbiome-analysis/0.1.2/microbiome_analysis-0.1.2.tar.gz

---
+++ packages/microbiome-analysis/microbiome_analysis-0.1.2.tar.gz/microbiome_analysis-0.1.2/setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="microbiome_analysis",
+    version="0.1.2",
+    description="A comprehensive Python library for microbiome data analysis",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="Jaemin Kim",
+    author_email="jaemin52577441@gmail.com",
+    packages=find_packages(),
+    install_requires=[
+        "biopython",
+        "numpy",
+        "scipy",
+        "pandas",
+        "matplotlib",
+        "seaborn",
+        "scikit-learn",
+        "plotly",
+        "streamlit",
+        "networkx",
+        "statsmodels",
+        "kaleido"
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
