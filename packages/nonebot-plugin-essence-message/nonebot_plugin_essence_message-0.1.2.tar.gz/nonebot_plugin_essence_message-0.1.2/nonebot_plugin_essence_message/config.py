726369cb07768e1ee0f0f11cae172b9474c801d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-11 15:04:28 +0000

    Add package nonebot-plugin-essence-message/0.1.2/nonebot_plugin_essence_message-0.1.2.tar.gz

---
+++ packages/nonebot-plugin-essence-message/nonebot_plugin_essence_message-0.1.2.tar.gz/nonebot_plugin_essence_message-0.1.2/nonebot_plugin_essence_message/config.py
@@ -0,0 +1,14 @@
+from pathlib import Path
+from pydantic import BaseModel
+
+PATH = Path().absolute()
+PATH_DATA = PATH / "data" / "essence_message"
+
+
+class config(BaseModel):
+    essence_random_limit: int = 5
+    essence_enable_groups: list = ["all"]
+
+    def db():
+        (PATH_DATA).mkdir(parents=True, exist_ok=True)
+        return PATH_DATA / "essence_message.db"
