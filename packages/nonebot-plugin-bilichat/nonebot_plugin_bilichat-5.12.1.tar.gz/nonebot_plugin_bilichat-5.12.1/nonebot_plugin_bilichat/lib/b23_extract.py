a4100862d9ac0aadd66b1e3f84f86c2bd750f1ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-11 03:05:41 +0000

    Add package nonebot-plugin-bilichat/5.12.1/nonebot_plugin_bilichat-5.12.1.tar.gz

---
+++ packages/nonebot-plugin-bilichat/nonebot_plugin_bilichat-5.12.1.tar.gz/nonebot_plugin_bilichat-5.12.1/nonebot_plugin_bilichat/lib/b23_extract.py
@@ -0,0 +1,20 @@
+import contextlib
+
+from nonebot.log import logger
+
+from .bilibili_request import hc
+
+
+async def b23_extract(b23: list[str]):
+    try:
+        url = f"https://b23.tv/{b23[1]}"
+        for _ in range(3):
+            with contextlib.suppress(Exception):
+                resp = await hc.get(url, follow_redirects=True)
+                break
+        else:
+            return None
+        logger.debug(f"b23.tv url: {resp.url}")
+        return str(resp.url)
+    except TypeError:
+        return None
