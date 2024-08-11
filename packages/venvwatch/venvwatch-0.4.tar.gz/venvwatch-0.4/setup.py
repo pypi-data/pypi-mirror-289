75bfaa33f58d409d3028cfa74e07a681c13a2592
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-11 15:04:41 +0000

    Add package venvwatch/0.4/venvwatch-0.4.tar.gz

---
+++ packages/venvwatch/venvwatch-0.4.tar.gz/venvwatch-0.4/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='venvwatch',
+    version='0.4',
+    description='Automatically updates requirements.txt by watching your virtual environment for changes',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author='Your Name',
+    author_email='your.email@example.com',
+    packages=find_packages(),
+    install_requires=[
+        'watchdog',
+    ],
+    entry_points={
+        'console_scripts': [
+            'venvwatch = venvwatch.main:main',
+        ],
+    },
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+)
