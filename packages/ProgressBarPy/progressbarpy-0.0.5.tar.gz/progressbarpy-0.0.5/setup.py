b29fc3db987018c5bd2b1ec389018f613b5ef56a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-11 03:06:09 +0000

    Add package ProgressBarPy/0.0.5/progressbarpy-0.0.5.tar.gz

---
+++ packages/ProgressBarPy/progressbarpy-0.0.5.tar.gz/progressbarpy-0.0.5/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+import pathlib
+
+__version__ = "0.0.5"
+
+HERE = pathlib.Path(__file__).parent
+
+README = (HERE / "README.md").read_text(encoding='utf-8')
+
+setup(
+    name='ProgressBarPy',
+    version=__version__,
+
+    url='https://github.com/Vladislavus1/ProgressBarPy',
+    author='Vladislavus1',
+    author_email='vlydgames@gmail.com',
+
+    description='This package adds simple windowed progressbar.',
+    long_description=README,
+    long_description_content_type='text/markdown',
+
+    packages=find_packages(),
+)
