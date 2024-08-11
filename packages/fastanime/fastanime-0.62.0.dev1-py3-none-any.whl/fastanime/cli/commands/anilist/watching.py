7f90adffc11eb1c57d70d988819bfd8f7441e296
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-11 15:04:32 +0000

    Add package fastanime/0.62.0.dev1/fastanime-0.62.0.dev1-py3-none-any.whl

---
+++ packages/fastanime/fastanime-0.62.0.dev1-py3-none-any.whl/fastanime/cli/commands/anilist/watching.py
@@ -0,0 +1,32 @@
+from typing import TYPE_CHECKING
+
+import click
+
+if TYPE_CHECKING:
+    from ...config import Config
+
+
+@click.command(help="View anime you are watching")
+@click.pass_obj
+def watching(config: "Config"):
+    from ....anilist import AniList
+    from ...interfaces import anilist_interfaces
+    from ...utils.tools import QueryDict, exit_app
+
+    if not config.user:
+        print("Not authenticated")
+        print("Please run: fastanime anilist loggin")
+        exit_app()
+    anime_list = AniList.get_anime_list("CURRENT")
+    if not anime_list:
+        return
+    if not anime_list[0] or not anime_list[1]:
+        return
+    media = [
+        mediaListItem["media"]
+        for mediaListItem in anime_list[1]["data"]["Page"]["mediaList"]
+    ]  # pyright:ignore
+    anime_list[1]["data"]["Page"]["media"] = media  # pyright:ignore
+    anilist_config = QueryDict()
+    anilist_config.data = anime_list[1]
+    anilist_interfaces.select_anime(config, anilist_config)
