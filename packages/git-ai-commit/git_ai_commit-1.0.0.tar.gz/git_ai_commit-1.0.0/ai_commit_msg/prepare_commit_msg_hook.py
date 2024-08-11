777f8b1144d8979083c4d2773642ed34b77e48cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-11 03:06:06 +0000

    Add package git-ai-commit/1.0.0/git_ai_commit-1.0.0.tar.gz

---
+++ packages/git-ai-commit/git_ai_commit-1.0.0.tar.gz/git_ai_commit-1.0.0/ai_commit_msg/prepare_commit_msg_hook.py
@@ -0,0 +1,19 @@
+from ai_commit_msg.core.gen_commit_msg import generate_commit_message
+from ai_commit_msg.services.git_service import GitService
+from ai_commit_msg.utils.logger import Logger
+
+def prepare_commit_msg_hook():
+  existing_content = GitService.read_commit_editmsg_file()
+
+  # filter out any lines that start with a "#"
+  filtered_content = "\n".join([line for line in existing_content.splitlines() if not line.strip().startswith('#')])
+
+  if(filtered_content != ""):
+    Logger().log("Commit message already exists, skipping AI commit message generation")
+    return
+
+  commit_message = "✨" + generate_commit_message()
+
+  GitService.update_commit_message(commit_message)
+
+  return
---
+++ packages/git-ai-commit/git_ai_commit-1.0.0.tar.gz/git_ai_commit-1.0.0/ai_commit_msg/services/git_service.py
@@ -0,0 +1,84 @@
+from ai_commit_msg.utils.utils import execute_cli_command
+
+class GitService:
+  def __init__(self):
+    return
+
+  @staticmethod
+  def get_staged_diff():
+    return execute_cli_command(['git', 'diff', '--staged'],
+                               cwd=GitService.get_repo_root_directory(), output=False)
+
+  @staticmethod
+  def get_repo_root_directory():
+    git_directory = execute_cli_command(['git', 'rev-parse', '--show-toplevel']).stdout.rstrip()
+
+    return git_directory
+
+  @staticmethod
+  def get_git_directory():
+    script_directory = execute_cli_command(['git', 'rev-parse', '--git-dir']).stdout.rstrip()
+    return script_directory
+
+  @staticmethod
+  def get_commit_editmsg_file_path():
+    git_directory = GitService.get_repo_root_directory()
+    commit_editmsg_file = git_directory + '/.git/COMMIT_EDITMSG'
+    return commit_editmsg_file
+
+  @staticmethod
+  def read_commit_editmsg_file():
+    existing_content = ""
+
+    try:
+      with open(GitService.get_commit_editmsg_file_path(), 'r') as file:
+        existing_content = file.read()
+    except FileNotFoundError:
+      pass
+
+    return existing_content
+
+  @staticmethod
+  def update_commit_message(commit_message):
+    # open COMMIT_EDITMSG file to add the generated commit message
+    existing_content = GitService.read_commit_editmsg_file()
+
+    new_content = commit_message + '\n' + existing_content
+
+    with open(GitService.get_commit_editmsg_file_path(), 'w') as file:
+      file.write(new_content)
+
+    return 0
+
+  @staticmethod
+  def get_staged_files():
+    staged_files = execute_cli_command(['git', 'diff', '--name-only', '--cached']).stdout.splitlines()
+    return staged_files
+
+  @staticmethod
+  def get_current_branch():
+    return execute_cli_command(['git', 'rev-parse', '--abbrev-ref', 'HEAD']).stdout.strip()
+
+  @staticmethod
+  def has_upstream_branch(branch_name):
+    try:
+        execute_cli_command(['git', 'rev-parse', '--abbrev-ref', f'{branch_name}@{{u}}'])
+        return True
+    except Exception:
+        return False
+
+  @staticmethod
+  def is_git_installed():
+    try:
+        execute_cli_command(['git', '--version'])
+        return True
+    except Exception:
+        return False
+
+  @staticmethod
+  def is_git_repository():
+    try:
+        execute_cli_command(['git', 'rev-parse', '--is-inside-work-tree'])
+        return True
+    except Exception:
+        return False
