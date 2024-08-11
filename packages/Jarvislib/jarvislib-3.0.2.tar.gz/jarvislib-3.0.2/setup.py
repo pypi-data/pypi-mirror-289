1e6be26b99b2164ddbbbc92546f501b816b79be9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-11 03:05:33 +0000

    Add package Jarvislib/3.0.2/jarvislib-3.0.2.tar.gz

---
+++ packages/Jarvislib/jarvislib-3.0.2.tar.gz/jarvislib-3.0.2/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+  name='Jarvislib',
+  version='3.0.2',
+  author='Platon',
+  author_email='babadey.pl@gmail.com',
+  packages=['Jarvislib'],
+  classifiers=['Programming Language :: Python :: 3.10'])
