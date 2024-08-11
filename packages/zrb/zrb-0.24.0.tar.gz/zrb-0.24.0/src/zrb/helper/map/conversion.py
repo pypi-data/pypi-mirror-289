c3d92f3adb9c9e0abb2b3c08dd62a7a3d57add4d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-11 15:04:43 +0000

    Add package zrb/0.24.0/zrb-0.24.0.tar.gz

---
+++ packages/zrb/zrb-0.24.0.tar.gz/zrb-0.24.0/src/zrb/helper/map/conversion.py
@@ -0,0 +1,21 @@
+from zrb.helper.accessories.color import colored
+from zrb.helper.log import logger
+from zrb.helper.typecheck import typechecked
+from zrb.helper.typing import Any, Mapping
+
+logger.debug(colored("Loading zrb.helper.map.conversion", attrs=["dark"]))
+
+
+@typechecked
+def to_str(
+    str_map: Mapping[str, Any],
+    item_separator: str = "\n",
+    item_prefix: str = "",
+    keyval_separator: str = " : ",
+) -> str:
+    keys = list(str_map.keys())
+    keys.sort()
+    str_list = [
+        item_prefix + key + keyval_separator + str(str_map[key]) for key in keys
+    ]
+    return item_separator.join(str_list)
