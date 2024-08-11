5d1a44101206f6aba492359d7cc78e46d3f8ffb3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-11 15:04:33 +0000

    Add package digistore24-client/0.2.1/digistore24_client-0.2.1.tar.gz

---
+++ packages/digistore24-client/digistore24_client-0.2.1.tar.gz/digistore24_client-0.2.1/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+import os
+
+own_dir = os.path.dirname(__file__)
+
+
+def version():
+    with open(os.path.join('version')) as f:
+        return f.read()
+
+
+setuptools.setup(
+    name='digistore24-client',
+    version=version(),
+    descriptions='an API client for API exposed by digistore24.com',
+    python_requires='>=3.11',
+    packages=('digistore',),
+    install_request=(
+        'dacite',
+        'requests',
+    ),
+)
