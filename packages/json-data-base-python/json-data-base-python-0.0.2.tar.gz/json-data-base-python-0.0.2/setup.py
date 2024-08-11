67d0529125340b249d3eb04cc9f5f5105d6d150f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-11 04:04:51 +0000

    Add package json-data-base-python/0.0.2/json-data-base-python-0.0.2.tar.gz

---
+++ packages/json-data-base-python/json-data-base-python-0.0.2.tar.gz/json-data-base-python-0.0.2/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup,find_packages
+
+setup(
+    name="json-data-base-python"
+    ,
+    version="0.0.2"
+    ,
+    packages=find_packages()
+    ,
+    description= "Package of mine to do a JSON based simple database!"
+)
