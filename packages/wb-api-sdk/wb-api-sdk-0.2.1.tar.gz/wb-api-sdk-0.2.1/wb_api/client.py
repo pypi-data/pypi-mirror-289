a2ac4f19b5a1cccf570f4963ebbfdb86adca5ae3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-11 14:04:40 +0000

    Add package wb-api-sdk/0.2.1/wb-api-sdk-0.2.1.tar.gz

---
+++ packages/wb-api-sdk/wb-api-sdk-0.2.1.tar.gz/wb-api-sdk-0.2.1/wb_api/client.py
@@ -0,0 +1,16 @@
+from wb_api.content import Content
+from wb_api.statistics import Statistics
+
+
+class WBApi:
+    def __init__(self, api_key: str, test_mode: bool = False) -> None:
+        self.api_key = api_key
+        self.test_mode = test_mode
+
+    @property
+    def statistics(self) -> Statistics:
+        return Statistics(self)
+
+    @property
+    def content(self) -> Content:
+        return Content(self)
