adf40e8062cc53157e61032fe516c72e80f83ed9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-11 05:04:58 +0000

    Add package anoteai/0.20/anoteai-0.20.tar.gz

---
+++ packages/anoteai/anoteai-0.20.tar.gz/anoteai-0.20/anoteai/__init__.py
@@ -0,0 +1,2 @@
+from .core import Anote
+# from .constants import *
---
+++ packages/anoteai/anoteai-0.20.tar.gz/anoteai-0.20/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+# Function to read the requirements.txt file
+def read_requirements():
+    with open('/Users/natanvidra/Workspace/Anote-Product/server/requirements.txt') as f:
+        return f.read().splitlines()
+
+setup(
+    name='anoteai',
+    version='0.20',
+    packages=find_packages(),
+    install_requires=read_requirements(),
+    description='An SDK for interacting with the Anote API',
+    author='Natan Vidra',
+    author_email='nvidra@anote.ai',
+    url='https://github.com/nv78/Anote',
+    license='MIT',
+)
