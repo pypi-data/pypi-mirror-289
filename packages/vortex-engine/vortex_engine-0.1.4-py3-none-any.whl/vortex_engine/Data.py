3d1f4e084199e513a3c553b61c4a75f251d7459e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-11 15:04:45 +0000

    Add package vortex-engine/0.1.4/vortex_engine-0.1.4-py3-none-any.whl

---
+++ packages/vortex-engine/vortex_engine-0.1.4-py3-none-any.whl/vortex_engine/Data.py
@@ -0,0 +1,12 @@
+from ursina import color
+
+
+color_dict = {
+    'red': color.red,
+    'green': color.green,
+    'blue': color.blue,
+    'yellow': color.yellow,
+    'white': color.white,
+    'black': color.black,
+    'orange': color.orange,
+}
