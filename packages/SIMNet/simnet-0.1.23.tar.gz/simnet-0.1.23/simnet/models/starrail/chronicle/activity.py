6a74763027de6fa951dd4290ee013fce11a6c508
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-11 03:05:33 +0000

    Add package SIMNet/0.1.23/simnet-0.1.23.tar.gz

---
+++ packages/SIMNet/simnet-0.1.23.tar.gz/simnet-0.1.23/simnet/models/starrail/chronicle/activity.py
@@ -0,0 +1,11 @@
+"""Starrail chronicle activity."""
+
+from typing import List
+
+from simnet.models.base import APIModel
+
+
+class StarRailActivity(APIModel):
+    """Starrail chronicle activity."""
+
+    activities: List
