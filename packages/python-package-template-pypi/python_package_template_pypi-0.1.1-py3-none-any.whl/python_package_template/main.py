112c0c859e01fd82320720f865199b8e313c702c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-11 03:06:11 +0000

    Add package python-package-template-pypi/0.1.1/python_package_template_pypi-0.1.1-py3-none-any.whl

---
+++ packages/python-package-template-pypi/python_package_template_pypi-0.1.1-py3-none-any.whl/python_package_template/main.py
@@ -0,0 +1,29 @@
+"""
+Main module of the package.
+
+@author "Daniel Mizsak" <info@pythonvilag.hu>
+"""
+
+
+def add_five(x: int) -> int:
+    """Increment the input by 5.
+
+    Args:
+        x (int): The input number.
+
+    Returns:
+        int: The input incremented by 5.
+    """
+    return x + 5
+
+
+def subtract_three(x: int) -> int:
+    """Decrement the input by 3.
+
+    Args:
+        x (int): The input number.
+
+    Returns:
+        int: The input decremented by 3.
+    """
+    return x - 3
