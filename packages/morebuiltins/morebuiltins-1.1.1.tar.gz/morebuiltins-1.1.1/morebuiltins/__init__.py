790c4e1c3fa581f913a3aed524ab0250a533a842
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-11 03:06:01 +0000

    Add package morebuiltins/1.1.1/morebuiltins-1.1.1.tar.gz

---
+++ packages/morebuiltins/morebuiltins-1.1.1.tar.gz/morebuiltins-1.1.1/morebuiltins/__init__.py
@@ -0,0 +1,12 @@
+__version__ = "1.1.1"
+__all__ = [
+    "morebuiltins.utils",
+    "morebuiltins.date",
+    "morebuiltins.functools",
+    "morebuiltins.ipc",
+    "morebuiltins.request",
+    "morebuiltins.download_python",
+    "morebuiltins.tk",
+    "morebuiltins.emails",
+    "morebuiltins.cmd.log_server",
+]
