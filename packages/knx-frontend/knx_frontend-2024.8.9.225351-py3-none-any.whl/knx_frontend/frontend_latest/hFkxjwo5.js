fdab434a0e184e7c42bf6f5aec21382d513a2ba7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-11 05:05:04 +0000

    Add package knx-frontend/2024.8.9.225351/knx_frontend-2024.8.9.225351-py3-none-any.whl

---
+++ packages/knx-frontend/knx_frontend-2024.8.9.225351-py3-none-any.whl/knx_frontend/frontend_latest/hFkxjwo5.js
@@ -0,0 +1,11 @@
+/*! For license information please see hFkxjwo5.js.LICENSE.txt */
+export const id=7150;export const ids=[7150];export const modules={47150:(e,t,d)=>{d.r(t),d.d(t,{HaFormBoolean:()=>u});var a=d(85461),o=d(76513),i=d(196),l=d(80487),n=d(4258);let r=class extends l.M{};r.styles=[n.R],r=(0,o.Cg)([(0,i.EM)("mwc-formfield")],r);var c=d(98597),s=d(33167);d(19887);let u=(0,a.A)([(0,i.EM)("ha-form-boolean")],(function(e,t){return{F:class extends t{constructor(...t){super(...t),e(this)}},d:[{kind:"field",decorators:[(0,i.MZ)({attribute:!1})],key:"schema",value:void 0},{kind:"field",decorators:[(0,i.MZ)({attribute:!1})],key:"data",value:void 0},{kind:"field",decorators:[(0,i.MZ)()],key:"label",value:void 0},{kind:"field",decorators:[(0,i.MZ)({type:Boolean})],key:"disabled",value(){return!1}},{kind:"field",decorators:[(0,i.P)("ha-checkbox",!0)],key:"_input",value:void 0},{kind:"method",key:"focus",value:function(){this._input&&this._input.focus()}},{kind:"method",key:"render",value:function(){return c.qy`
+      <mwc-formfield .label=${this.label}>
+        <ha-checkbox
+          .checked=${this.data}
+          .disabled=${this.disabled}
+          @change=${this._valueChanged}
+        ></ha-checkbox>
+      </mwc-formfield>
+    `}},{kind:"method",key:"_valueChanged",value:function(e){(0,s.r)(this,"value-changed",{value:e.target.checked})}}]}}),c.WF)}};
+//# sourceMappingURL=hFkxjwo5.js.map
