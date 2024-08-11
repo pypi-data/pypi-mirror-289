61543ecc910c64186568168eb3fcebda7cfc9010
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-11 14:04:42 +0000

    Add package depyf/0.17.0/depyf-0.17.0.tar.gz

---
+++ packages/depyf/depyf-0.17.0.tar.gz/depyf-0.17.0/depyf/explain/__init__.py
@@ -0,0 +1,33 @@
+from depyf.explain.utils import DynamoOptimizationResult
+
+from torch._dynamo.eval_frame import innermost_fn
+
+from typing import List, Callable, Dict, Union, Set
+from types import CodeType
+
+
+def _extract_artifacts(original_code: CodeType, module):
+    result = DynamoOptimizationResult(original_code, None, module)
+    return result
+
+
+def _collect_compiled_subgraphs(result: DynamoOptimizationResult):
+    compiled_subgraphs = {
+        entry.compiled_subgraph_proxy.name: entry.compiled_subgraph for entry in result.compiled_code_entries}
+    for entry in result.compiled_code_entries:
+        for func in entry.referenced_global_functions.values():
+            ans = _collect_compiled_subgraphs(func)
+            compiled_subgraphs.update(ans)
+    return compiled_subgraphs
+
+
+def interactive_explain(fn: Callable):
+    artifacts = _extract_artifacts(fn)
+    return artifacts
+
+
+def dump_src(original_code: CodeType, module):
+    from depyf.explain.global_variables import data
+    assert data["is_inside_prepare_debug"], "`dump_src` must be used inside `depyf.prepare_debug`."
+    artifacts = _extract_artifacts(original_code, module)
+    return artifacts.to_src()
---
+++ packages/depyf/depyf-0.17.0.tar.gz/depyf-0.17.0/depyf/explain/patched_lazy_format_graph_code.py
@@ -0,0 +1,60 @@
+def patched_lazy_format_graph_code(name, gm, maybe_id=None, **kwargs):
+    from depyf.explain.utils import get_current_compiled_fn_name, get_code_owner, write_code_to_file_template
+    func_name = get_current_compiled_fn_name()
+    file_name = name if name != func_name else "Captured Graph"
+    file_name = func_name + " " + file_name
+    import inspect
+    import os
+
+    # https://github.com/pytorch/pytorch/pull/117911 introduces LazyGraphModule
+    # whose `forward` method is mangled and cannot be manipulated.
+    # We need to get rid of the laziness by calling `str` on it.
+    gm_s = str(gm)
+
+    fn = gm.forward
+
+    fn = get_code_owner(fn)
+
+    # update file path
+    filepath = inspect.getsourcefile(fn)
+    # try to use verbose code with type and shape annotations
+    src = "from __future__ import annotations\n" + \
+        gm._graph.python_code(root_module="self", verbose=True).src
+    try:
+        compile(src, "noname", "exec")
+    except Exception as e:
+        # the pytorch version is before this PR: https://github.com/pytorch/pytorch/pull/113345
+        # Verbose code contains syntax error, it is recommended to use new
+        # version of PyTorch to get runnable code with shape and type
+        # annotations.
+        simple_code = gm._graph.python_code(root_module="self", verbose=False).src
+        commented_src = "\n# code below is commented out due to syntax error. You can refer to the code for shape and dtype annotation.\n"
+        commented_src += "".join(["# " + line +
+                                 "\n" for line in src.splitlines()])
+        src = simple_code + commented_src
+    if filepath is not None:
+        new_filepath = write_code_to_file_template(
+            src, os.path.dirname(filepath) + "/" + file_name + " " + "%s" + ".py")
+        scope = fn.__globals__
+        exec(compile(src, filename=new_filepath, mode="exec"), scope)
+        fn.__code__ = scope[fn.__name__].__code__
+        del scope[fn.__name__]
+
+    # =========================================
+    # original code of `lazy_format_graph_code`
+    def format_name():
+        if maybe_id is not None:
+            return f"{name} {maybe_id}"
+        else:
+            return name
+
+    if "print_output" not in kwargs:
+        kwargs["print_output"] = False
+
+    return LazyString(
+        lambda: _format_graph_code(
+            f"===== {format_name()} =====\n",
+            gm.forward.__code__.co_filename,
+            gm.print_readable(**kwargs),
+        )
+    )
