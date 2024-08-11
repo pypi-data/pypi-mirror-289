711cb7229966d23683ecc604a36d72ae6b8bb4ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-11 03:05:35 +0000

    Add package logged/0.3/logged-0.3.tar.gz

---
+++ packages/logged/logged-0.3.tar.gz/logged-0.3/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='logged',
+    version='0.3',
+    packages=find_packages(),
+    install_requires=[
+        "datetime"
+    ],
+    entry_points={
+        'console_scripts': [
+            # If you have command-line scripts
+        ],
+    },
+    author='hypixeloffical',
+    author_email='hypixel.offical.tr@gmail.com',
+    description='A custom logging package with advanced features',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/hypixeloffical/logged-py',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+)
