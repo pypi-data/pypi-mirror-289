8a658a7f88a6a6c9c59ff5ce7cdae9f2c27711ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-11 14:04:53 +0000

    Add package creme-nn/0.2.4/creme_nn-0.2.4.tar.gz

---
+++ packages/creme-nn/creme_nn-0.2.4.tar.gz/creme_nn-0.2.4/setup.py
@@ -0,0 +1,45 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="creme-nn",
+    # TODO: Consider using https://github.com/python-versioneer/python-versioneer to
+    # get version information from git.
+    version="0.2.4",
+    description="An in silico perturbation framework to interpret large-scale genomic deep learning",
+    # author='A. Random Developer',
+    # author_email='author@example.com',
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3 :: Only",
+        "Topic :: Scientific/Engineering :: Bio-Informatics",
+    ],
+    packages=find_packages(),
+    python_requires=">=3.6, <4",
+    # Do not install tensorflow here, because might want to use tensorflow or
+    # tensorflow-cpu.
+    install_requires=[
+        "pyranges",
+        "numpy",
+        "pandas",
+        "kipoiseq",
+        "tensorflow-hub",
+        "pyfaidx",
+        "logomaker",
+        "matplotlib",
+        "seaborn",
+        "protobuf==3.20",
+    ],
+    extras_require={
+        "dev": [
+            "black",  # styler
+            "flake8",  # linter
+        ],
+    },
+)
