ead0911dc9bcd37a7f323f9dd9ba6c51c42aa7b0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-11 15:04:30 +0000

    Add package zenlite-DEV/1.0.2024.post8/zenlite_dev-1.0.2024.post8.tar.gz

---
+++ packages/zenlite-DEV/zenlite_dev-1.0.2024.post8.tar.gz/zenlite_dev-1.0.2024.post8/zenlite/core/globs.py
@@ -0,0 +1,6 @@
+import os, sys;    os.environ["PYGAME_HIDE_SUPPORT_PROMPT"] = str(True)
+import glm, glfw
+import pygame as pg, numpy as np
+from numba import njit as bytec
+
+ZEN_ASSET_DIR = __file__.removesuffix("globs.py")+"..\\assets\\"
