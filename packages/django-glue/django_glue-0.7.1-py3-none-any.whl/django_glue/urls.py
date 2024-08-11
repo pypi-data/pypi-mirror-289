188c30e66122b5e33fc7c238839a1cf3747c7648
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-11 04:04:32 +0000

    Add package django-glue/0.7.1/django_glue-0.7.1-py3-none-any.whl

---
+++ packages/django-glue/django_glue-0.7.1-py3-none-any.whl/django_glue/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+
+from django_glue import views
+
+app_name = 'django_glue'
+
+urlpatterns = [
+    # These url path names are used in our middleware to avoid cleaning session data.
+    path("", views.glue_data_ajax_handler_view, name="django_glue_data_handler"),
+    path("keep_live/", views.glue_keep_live_handler_view, name="django_glue_keep_live_handler"),
+]
