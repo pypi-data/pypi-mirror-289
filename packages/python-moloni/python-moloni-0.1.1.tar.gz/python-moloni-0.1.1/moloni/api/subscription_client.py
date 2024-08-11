cc810f9432efa58a4d1578c75577f7770df6d5ba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-11 05:04:58 +0000

    Add package python-moloni/0.1.1/python-moloni-0.1.1.tar.gz

---
+++ packages/python-moloni/python-moloni-0.1.1.tar.gz/python-moloni-0.1.1/moloni/api/subscription_client.py
@@ -0,0 +1,52 @@
+from pydantic import BaseModel, ValidationError
+from typing import Union, Optional, List
+
+from moloni.base.client import MoloniBaseClient
+from moloni.base.helpers import endpoint, fill_query_params, validate_data
+
+
+class Suppliers(BaseModel):
+    cost_price: Optional[Any] = None
+    supplier_id: Optional[Any] = None
+
+
+class Taxes(BaseModel):
+    cumulative: Optional[Any] = None
+    order: Optional[Any] = None
+    tax_id: Optional[Any] = None
+    value: Optional[Any] = None
+
+
+class Warehouses(BaseModel):
+    stock: Optional[Any] = None
+    warehouse_id: Optional[Any] = None
+
+
+class SubscriptionGetOneModel(BaseModel):
+    company_id: Union[str, int]
+
+
+class SubscriptionClient(MoloniBaseClient):
+
+    @endpoint("/<version>/subscription/getOne/", method="post")
+    def get_one(self, data: Union[SubscriptionGetOneModel, dict], **kwargs):
+        """
+        get_one(self, data: Union[SubscriptionGetOneModel, dict], **kwargs)
+
+        Args:
+
+            data (Union[SubscriptionGetOneModel, dict]): A model instance or dictionary containing the following fields:
+
+                - company_id (Union[str, int]): company_id of the SubscriptionGetOneModel.
+
+
+
+        Returns:
+            ApiResponse: The response from the API.
+        """
+
+        data = validate_data(data, self.validate, SubscriptionGetOneModel)
+
+        return self._request(
+            fill_query_params(kwargs.pop("path"), self.version), data={**data, **kwargs}
+        )
