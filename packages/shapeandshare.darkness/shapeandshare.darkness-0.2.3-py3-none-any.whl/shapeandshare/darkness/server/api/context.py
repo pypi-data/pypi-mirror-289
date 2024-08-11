94662ebb1ea60a090b8c42ecc03234fb50a8de17
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-11 14:04:33 +0000

    Add package shapeandshare.darkness/0.2.3/shapeandshare.darkness-0.2.3-py3-none-any.whl

---
+++ packages/shapeandshare.darkness/shapeandshare.darkness-0.2.3-py3-none-any.whl/shapeandshare/darkness/server/api/context.py
@@ -0,0 +1,36 @@
+import logging
+import sys
+from pathlib import Path
+
+from ...sdk.contracts.errors.server.service import ServiceError
+from ..dao.island import IslandDao
+from ..dao.tile import TileDao
+from ..dao.world import WorldDao
+from ..services.state import StateService
+
+logger = logging.getLogger()
+
+STORAGE_BASE_PATH: Path = Path(".") / "data"
+
+
+class ContextManager:
+    # Application Level Services
+    state_service: StateService | None = None
+
+    def __init__(self):
+        if ContextManager.state_service is None:
+            ContextManager.state_service = StateService(
+                worlddao=WorldDao(storage_base_path=STORAGE_BASE_PATH),
+                islanddao=IslandDao(storage_base_path=STORAGE_BASE_PATH),
+                tiledao=TileDao(storage_base_path=STORAGE_BASE_PATH),
+            )
+            logger.debug("[ContextManager] assigned new state service to context manager")
+        else:
+            logger.debug("[ContextManager] state service already assigned")
+
+
+try:
+    ContextManager()
+except ServiceError as error:
+    logger.error(error)
+    sys.exit(1)
