b6af7eb49c9e795b8a9fc7317aea49d5a577af6f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-11 15:04:31 +0000

    Add package 0xdirsan/0.0.5/0xdirsan-0.0.5.tar.gz

---
+++ packages/0xdirsan/0xdirsan-0.0.5.tar.gz/0xdirsan-0.0.5/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='0xdirsan',
+    version='0.0.5',
+    author='x-projetion',
+    author_email='lutfifakee@proton.me',
+    description='0xdirsan is a simple program designed to search for directories within the file system using brute-force with a given list of words.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    packages=find_packages(),
+    package_data={
+        'xdirsan': ['wordlist/*.txt'],
+    },
+    entry_points={
+        'console_scripts': [
+            'xdirsan=xdirsan.main:main',
+        ],
+    },
+    install_requires=[
+        'requests',
+        'colorama',
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
