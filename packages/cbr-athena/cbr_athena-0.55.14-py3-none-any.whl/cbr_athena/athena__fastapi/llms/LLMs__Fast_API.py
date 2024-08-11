82884f41c6ac74711a2d4ba9cee81e5d1c294853
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-11 04:05:01 +0000

    Add package cbr-athena/0.55.14/cbr_athena-0.55.14-py3-none-any.whl

---
+++ packages/cbr-athena/cbr_athena-0.55.14-py3-none-any.whl/cbr_athena/athena__fastapi/llms/LLMs__Fast_API.py
@@ -0,0 +1,12 @@
+from cbr_athena.athena__fastapi.llms.routes.Routes__Chat import Routes__Chat
+from osbot_fast_api.api.Fast_API import Fast_API
+
+
+class LLMs__Fast_API(Fast_API):
+    base_path : str  = '/llms'
+
+    def mount(self, parent_app):
+        parent_app.mount(self.base_path, self.app())
+
+    def setup_routes(self):
+        self.add_routes(Routes__Chat)
