027638fe03c67c28a5a27103a5c19ac3fc888b1e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-11 03:06:06 +0000

    Add package django-pdf-view/1.0.6/django_pdf_view-1.0.6.tar.gz

---
+++ packages/django-pdf-view/django_pdf_view-1.0.6.tar.gz/django_pdf_view-1.0.6/django_pdf_view/views/__init__.py
@@ -0,0 +1,8 @@
+from .pdf_view import PDFView
+from .examples import MultipageExamplePDFView, SinglepageExamplePDFView
+
+__all__ = [
+    'PDFView',
+    'MultipageExamplePDFView',
+    'SinglepageExamplePDFView',
+]
