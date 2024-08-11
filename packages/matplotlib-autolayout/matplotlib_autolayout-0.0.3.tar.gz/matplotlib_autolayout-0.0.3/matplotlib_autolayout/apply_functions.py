343feb232bda7eb26cb915e4140f4120ba69d316
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-11 15:04:49 +0000

    Add package matplotlib-autolayout/0.0.3/matplotlib_autolayout-0.0.3.tar.gz

---
+++ packages/matplotlib-autolayout/matplotlib_autolayout-0.0.3.tar.gz/matplotlib_autolayout-0.0.3/matplotlib_autolayout/apply_functions.py
@@ -0,0 +1,57 @@
+import matplotlib.pyplot as plt
+import math
+
+def apply_direction_count(figure: plt.Figure, direction: str, count: int):
+    if direction == "row":
+        total_width = figure.get_figwidth()
+        total_height = figure.get_figheight()
+
+        # count is the number in each column for row direction
+        columns = math.ceil(len(figure.axes) / count)
+
+        # calculate the width of each plot
+        plot_height = total_height / count 
+        plot_width = total_width / columns
+
+        for i, ax in enumerate(figure.axes):
+            x = (i // count) * plot_width
+            y = total_height - ((i % count) * plot_height)
+
+            ax.set_position([x, y, plot_width, plot_height])
+
+    elif direction == "column":
+        total_width = figure.get_figwidth()
+        total_height = figure.get_figheight()
+
+
+        # count is the number in each row for column direction
+        rows = math.ceil(len(figure.axes) / count)
+
+        # calculate the height of each plot
+        plot_height = total_height / rows
+        plot_width = total_width / count
+
+        for i, ax in enumerate(figure.axes):
+            x = (i % count) * plot_width
+            y = total_height - ((i // count) * plot_height)
+
+            ax.set_position([x, y, plot_width, plot_height])
+    else:
+        return ValueError("Flex direction must be either row or column")
+
+    return figure
+
+
+def apply_margin(figure: plt.Figure, margin: int):
+    for ax in figure.axes:
+        # get the position of the plot
+        position = ax.get_position()
+        x, y, width, height = position.x0, position.y0, position.width, position.height
+        # calculate the new position
+        x = x + margin
+        y = y + margin
+        width = width - margin
+        height = height - margin
+        ax.set_position([x, y, width, height])
+
+    return figure
