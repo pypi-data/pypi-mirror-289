fea48443e35de40e4351357662c6cdedb80d7f2e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-11 15:04:51 +0000

    Add package thoughtdb/0.1.2/thoughtdb-0.1.2.tar.gz

---
+++ packages/thoughtdb/thoughtdb-0.1.2.tar.gz/thoughtdb-0.1.2/thoughtdb/Document.py
@@ -0,0 +1,13 @@
+#
+# Tina4 - This is not a 4ramework.
+# Copy-right 2007 - current Tina4
+# License: MIT https://opensource.org/licenses/MIT
+#
+from thoughtdb.Core import Core
+
+
+class Document(Core):
+
+    def __init__(self, vector_store):
+        self.id = 0
+        super(Document, self).__init__(vector_store)
