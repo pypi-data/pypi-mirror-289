62429bb5fbe141eef5d80ecfbe4bee0fbe9ecfa5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-11 04:04:26 +0000

    Add package autopwlf/0.4.0/autopwlf-0.4.0.tar.gz

---
+++ packages/autopwlf/autopwlf-0.4.0.tar.gz/autopwlf-0.4.0/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="autopwlf",
+    version="0.4.0",
+    author="Nedeesha Weerasuriya",
+    author_email="nedeeshawork@gmail.com",
+    description="Automated piecewise linear fitting",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/nweerasuriya/auto_pwlf",
+    packages=find_packages(where="src"),
+    package_dir={"": "src"},
+    install_requires=[
+        "numpy>=1.24.0",
+        "scipy>=1.11.4",
+        "pwlf>=2.2.1",
+    ],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "License :: OSI Approved :: MIT License",
+    ],
+    extras_require={
+        "docs": ["sphinx>=3.0", "sphinx_rtd_theme"],
+    },
+    python_requires=">=3.7",
+)
