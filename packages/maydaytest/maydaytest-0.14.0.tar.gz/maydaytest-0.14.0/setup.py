f278e1d8cd3e853de7dfe60dcf461635883fa731
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-11 04:04:45 +0000

    Add package maydaytest/0.14.0/maydaytest-0.14.0.tar.gz

---
+++ packages/maydaytest/maydaytest-0.14.0.tar.gz/maydaytest-0.14.0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="maydaytest",
+    version="0.14.0",
+    description="A short description of your project",
+    long_description="A longer description of your project",
+    long_description_content_type="text/markdown",
+    author="Your Name",
+    author_email="your_email@example.com",
+    packages=find_packages(),
+)
