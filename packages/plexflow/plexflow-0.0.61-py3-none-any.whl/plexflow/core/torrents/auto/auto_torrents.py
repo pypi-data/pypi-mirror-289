620930c5e4e627cb933d162946c874edd868d6e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-11 15:04:44 +0000

    Add package plexflow/0.0.61/plexflow-0.0.61-py3-none-any.whl

---
+++ packages/plexflow/plexflow-0.0.61-py3-none-any.whl/plexflow/core/torrents/auto/auto_torrents.py
@@ -0,0 +1,17 @@
+from plexflow.core.torrents.providers.tpb.tpb import TPB
+from plexflow.core.torrents.providers.yts.yts import YTS
+from plexflow.core.torrents.providers.torrentquest.torrentquest import TorrentQuest
+from typing import List
+from plexflow.core.torrents.results.torrent import Torrent
+
+class AutoTorrents:
+    @staticmethod
+    def movie(imdb_id: str = None, query: str = None, source: str = 'yts') -> List[Torrent]:
+        if source == 'tpb':
+            return TPB().search(query=imdb_id)
+        elif source == 'yts':
+            return YTS().search(query=imdb_id)
+        elif source == 'torrentquest':
+            return TorrentQuest().search(query=query)
+        else:
+            raise ValueError(f"Invalid source: {source}")
