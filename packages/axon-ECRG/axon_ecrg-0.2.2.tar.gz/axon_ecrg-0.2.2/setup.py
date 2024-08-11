a8b07bf4228f0998a334840bfb4f19f527c58a81
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-11 03:05:58 +0000

    Add package axon-ECRG/0.2.2/axon_ecrg-0.2.2.tar.gz

---
+++ packages/axon-ECRG/axon_ecrg-0.2.2.tar.gz/axon_ecrg-0.2.2/setup.py
@@ -0,0 +1,33 @@
+import pathlib
+from setuptools import setup, find_packages
+# from axon.config import version
+
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
+
+# The text of the README file
+README = (HERE / "README.md").read_text()
+
+# This call to setup() does all the work
+setup(
+	name="axon-ECRG",
+	version="0.2.2",
+	description="RPC proxy framework",
+	long_description=README,
+	long_description_content_type="text/markdown",
+	url="https://github.com/DuncanMays/axon-ECRG",
+	packages=['axon'],
+	install_requires=[
+		'pytest',
+		'pytest-asyncio',
+		'flask', 
+		'uuid', 
+		'urllib3',
+		'requests', 
+		'aiohttp', 
+		'python-socketio[client]', 
+		'psutil', 
+		'cloudpickle'
+	],
+)
+
