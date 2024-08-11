99d726ed6556079b15c7ff2fee0789779484177e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-11 05:04:58 +0000

    Add package maydaytest/0.19.0/maydaytest-0.19.0.tar.gz

---
+++ packages/maydaytest/maydaytest-0.19.0.tar.gz/maydaytest-0.19.0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="maydaytest",
+    version="0.19.0",
+    description="A short description of your project",
+    long_description="A longer description of your project",
+    long_description_content_type="text/markdown",
+    author="Bruhahahah",
+    author_email="bruhahah@example.com",
+    packages=find_packages(),
+)
