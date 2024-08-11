34fc64336d878ffa442ebc93a1277603deec14ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-11 14:04:33 +0000

    Add package python-moloni/0.1.12/python-moloni-0.1.12.tar.gz

---
+++ packages/python-moloni/python-moloni-0.1.12.tar.gz/python-moloni-0.1.12/moloni/api/users_client.py
@@ -0,0 +1,52 @@
+from pydantic import BaseModel, ValidationError
+from typing import Union, Optional, List, Any
+
+from moloni.base.client import MoloniBaseClient
+from moloni.base.helpers import endpoint, fill_query_params, validate_data
+
+
+class Suppliers(BaseModel):
+    cost_price: Optional[Any] = None
+    supplier_id: Optional[Any] = None
+
+
+class Taxes(BaseModel):
+    cumulative: Optional[Any] = None
+    order: Optional[Any] = None
+    tax_id: Optional[Any] = None
+    value: Optional[Any] = None
+
+
+class Warehouses(BaseModel):
+    stock: Optional[Any] = None
+    warehouse_id: Optional[Any] = None
+
+
+class UsersGetAllModel(BaseModel):
+    company_id: Union[str, int]
+
+
+class UsersClient(MoloniBaseClient):
+
+    @endpoint("/<version>/users/getAll/", method="post")
+    def get_all(self, data: Union[UsersGetAllModel, dict], **kwargs):
+        """
+        get_all(self, data: Union[UsersGetAllModel, dict], **kwargs)
+
+        Args:
+
+            data (Union[UsersGetAllModel, dict]): A model instance or dictionary containing the following fields:
+
+                - company_id (Union[str, int]): company_id of the UsersGetAllModel.
+
+
+
+        Returns:
+            ApiResponse: The response from the API.
+        """
+
+        data = validate_data(data, self.validate, UsersGetAllModel)
+
+        return self._request(
+            fill_query_params(kwargs.pop("path"), self.version), data={**data, **kwargs}
+        )
