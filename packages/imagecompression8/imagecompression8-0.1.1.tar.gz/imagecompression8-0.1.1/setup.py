5b9fe36f6ad8b5bed89a45b774c2a08f56426db5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-11 14:04:42 +0000

    Add package imagecompression8/0.1.1/imagecompression8-0.1.1.tar.gz

---
+++ packages/imagecompression8/imagecompression8-0.1.1.tar.gz/imagecompression8-0.1.1/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='imagecompression8',
+    version='0.1.1',
+    packages=find_packages(),
+    description='Image compression using REIS algorithms',
+    install_requires=[
+        'numpy',
+        'pillow',
+        'scipy',
+    ],
+    author='Katie Wen-Ling Kuo',
+    author_email='katie20030705@gmail.com',
+    license='MIT',
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3'
+    ],
+    python_requires='>=3.6',
+)
