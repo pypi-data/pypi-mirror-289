15338ad41182b06b2ce0471723fbd9e829e69070
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-11 04:04:48 +0000

    Add package aiohttp-toolkit/0.1.1/aiohttp_toolkit-0.1.1-py3-none-any.whl

---
+++ packages/aiohttp-toolkit/aiohttp_toolkit-0.1.1-py3-none-any.whl/aiohttp_toolkit/response_callbacks.py
@@ -0,0 +1,95 @@
+from ._handler import CallbackBuilder as _cbuilder
+from . import callback_builders as _cbb
+
+
+__all__ = (
+    "status",
+    "properties",
+    "read",
+    "text",
+    "json",
+)
+
+
+status = _cbuilder.build(
+    _cbb.status,
+)
+"""
+`status` - return response status code and it's boolean representation
+
+Returns:
+    status (str): response status code
+    ok (bool): response status code boolean representation.
+    `True` if status is less than 400; otherwise, `False`.
+"""
+
+properties = _cbuilder.build(
+    _cbb.status,
+    _cbb.headers,
+    _cbb.cookies,
+)
+"""
+`properties` - return all property-like response fields: response status code,
+it's boolean representation, headers and cookies
+
+Returns:
+    status (str): response status code
+    ok (bool): response status code boolean representation.
+    `True` if status is less than 400; otherwise, `False`.
+    headers (CIMultiDictProxy[str]): inmutable case-insensitive multidict with
+    HTTP response headers.
+    cookies (http.cookies.SimpleCookie): container with HTTP response cookies.
+"""
+
+read = _cbuilder.develop(
+    properties,
+    _cbb.read,
+)
+"""
+`read` - return response body `bytes` and all property-like response fields
+(listed in `properties` response callback)
+
+Returns:
+    status (str): response status code
+    ok (bool): response status code boolean representation.
+    `True` if status is less than 400; otherwise, `False`.
+    headers (CIMultiDictProxy[str]): inmutable case-insensitive multidict with
+    HTTP response headers.
+    cookies (http.cookies.SimpleCookie): container with HTTP response cookies.
+    read (bytes): response body bytes
+"""
+
+text = _cbuilder.develop(
+    properties,
+    _cbb.text,
+)
+"""
+`text` - return response body decoded as `str` and all property-like response
+fields (listed in `properties` response callback)
+
+Arguments:
+    text (**kwargs): kwargs to be unpacked as
+    `aiohttp.ClientResponse.text(**kwargs)`
+
+Returns:
+    status (str): response status code
+    ok (bool): response status code boolean representation.
+    `True` if status is less than 400; otherwise, `False`.
+    headers (CIMultiDictProxy[str]): inmutable case-insensitive multidict with
+    HTTP response headers.
+    cookies (http.cookies.SimpleCookie): container with HTTP response cookies.
+    text (str): response body decoded
+"""
+
+json = _cbuilder.develop(
+    properties,
+    _cbb.json,
+)
+"""
+`json` - return response body with json, deserialized as a Python object, also
+all property-like response fields (listed in `properties` response callback)
+
+Arguments:
+    json (**kwargs): kwargs to be unpacked as
+    `aiohttp.ClientResponse.json(**kwargs)`
+"""
---
+++ packages/aiohttp-toolkit/aiohttp_toolkit-0.1.1-py3-none-any.whl/aiohttp_toolkit/response_models.py
@@ -0,0 +1,108 @@
+from typing import Any
+import dataclasses
+import http.cookies
+
+import multidict
+
+
+__all__ = (
+    "Status", "Properties", "Read", "Text", "JsonObj", "JsonList", "JsonAny",
+)
+
+
+@dataclasses.dataclass
+class Status:
+    """
+    `Status` - used to be applied from `status` callback.
+    """
+
+    status: int
+    ok: bool
+
+    def __repr__(self) -> str:
+        return f"<{self.__class__.__name__}({self.status})>"
+
+
+@dataclasses.dataclass
+class Properties(Status):
+    """
+    `Properties` - used to be applied from `properties` callback.
+    Inherits `Status`.
+    """
+
+    headers: multidict.CIMultiDictProxy[str]
+    cookies: http.cookies.BaseCookie
+
+    def __repr__(self) -> str:
+        return (
+            f"<{self.__class__.__name__}({self.status}): "
+            "{}>".format(repr(self.headers).strip("<>"))
+        )
+
+
+@dataclasses.dataclass
+class Read(Properties):
+    """
+    `Read` - used to be applied from `read` callback.
+    Inherits `Properties`.
+    """
+
+    read: bytes
+
+    def __repr__(self) -> str:
+        return super().__repr__()
+
+
+@dataclasses.dataclass
+class Text(Properties):
+    """
+    `Text` - used to be applied from `text` callback.
+    Inherits `Properties`.
+    """
+
+    text: str
+
+    def __repr__(self) -> str:
+        return super().__repr__()
+
+
+@dataclasses.dataclass
+class JsonObj(Properties):
+    """
+    `JsonObj` - used to be applied from `json` callback, when JSON
+    `object` is expected to be returned.
+    Inherits `Properties`.
+    """
+
+    json: dict[str, Any]
+
+    def __repr__(self) -> str:
+        return super().__repr__()
+
+
+@dataclasses.dataclass
+class JsonList(Properties):
+    """
+    `JsonList` - used to be applied from `json` callback, when JSON
+    `list` is expected to be returned.
+    Inherits `Properties`.
+    """
+
+    json: list[Any]
+
+    def __repr__(self) -> str:
+        return super().__repr__()
+
+
+@dataclasses.dataclass
+class JsonAny(Properties):
+    """
+    `JsonAny` - used to be applied from `json` callback, when `any` JSON
+    structure is expected to be returned, for example `number` or `null`.
+    Inherits `Properties`.
+    """
+
+    json: Any
+
+    def __repr__(self) -> str:
+        return super().__repr__()
