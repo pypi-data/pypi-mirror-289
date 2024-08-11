db9fd7efe0c64efc586dd429adef69af0e65158c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-11 03:05:51 +0000

    Add package wordle-solverGG/0.0.3/wordle_solvergg-0.0.3.tar.gz

---
+++ packages/wordle-solverGG/wordle_solvergg-0.0.3.tar.gz/wordle_solvergg-0.0.3/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="wordle_solverGG",
+    version="0.0.3",
+    packages=find_packages(),
+    install_requires=[
+        'selenium',
+        'pyautogui',
+    ],
+    author="Your Name",
+    author_email="firi8228@gmail.com",
+    description="A package to automatically solve Wordle puzzles",
+    long_description="This package provides functionality to automatically solve Wordle puzzles using Selenium and PyAutoGUI.",
+    url="http://example.com/wordle_solver",
+)
