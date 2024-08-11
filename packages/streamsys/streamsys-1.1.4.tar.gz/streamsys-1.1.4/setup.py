3b70b04765a1de94479de9e187ff5c0a5cd14045
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-11 04:04:32 +0000

    Add package streamsys/1.1.4/streamsys-1.1.4.tar.gz

---
+++ packages/streamsys/streamsys-1.1.4.tar.gz/streamsys-1.1.4/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='streamsys',
+    version='1.1.4',  # Asegúrate de que esta versión no esté ya en PyPI
+    description='Advanced Streaming System | Available with 24/7 Stream',
+    author='Javi17mod',
+    author_email='javi17mod@gmail.com',
+    packages=find_packages(),  # Encuentra todos los paquetes automáticamente
+    install_requires=[],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
