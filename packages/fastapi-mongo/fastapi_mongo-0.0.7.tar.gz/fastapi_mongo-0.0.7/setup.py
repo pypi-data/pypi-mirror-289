14717c9b872959d19630a1e807fa6c68d2a33b5b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-11 15:04:30 +0000

    Add package fastapi-mongo/0.0.7/fastapi_mongo-0.0.7.tar.gz

---
+++ packages/fastapi-mongo/fastapi_mongo-0.0.7.tar.gz/fastapi_mongo-0.0.7/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='fastapi-mongo',
+    version='0.0.7',
+    long_description='This is a simple package that helps you to create a FastAPI project with MongoDB it comes with default crud operations and database setup just configure your environment variables and you are good to go.',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+    ],
+    packages=find_packages(),
+    data_files=[
+        ('', ['package/folder_structure.json']),
+    ],
+    include_package_data=True,
+    install_requires=["fastapi[standard]", "pymongo", "python-dotenv",
+                       "passlib", "python-jose", "python-multipart", 'setuptools',"motor"],
+    entry_points={
+        'console_scripts': [
+            'build=package.build_structure:create_files_and_dirs_from_json',
+        ],
+    },
+)
