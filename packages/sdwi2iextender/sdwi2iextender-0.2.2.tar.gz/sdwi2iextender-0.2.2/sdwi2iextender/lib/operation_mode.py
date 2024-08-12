40e39cb6cdc99394e351fc8d6c6ec1d4ffbcde08
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-12 14:06:15 +0000

    Add package sdwi2iextender/0.2.2/sdwi2iextender-0.2.2.tar.gz

---
+++ packages/sdwi2iextender/sdwi2iextender-0.2.2.tar.gz/sdwi2iextender-0.2.2/sdwi2iextender/lib/operation_mode.py
@@ -0,0 +1,21 @@
+import gradio as gr
+
+
+class OperationMode:
+    show_inpaint_params = True
+    requested_elem_ids = []
+
+    def __init__(self):
+        pass
+
+    def image_components(self):
+        return gr.Image(visible=False), gr.Image(visible=False)
+
+    def tab(self):
+        gr.TabItem(label="", visible=False)
+
+    def section(self, components: list):
+        pass
+
+    def gradio_events(self, selected: gr.Checkbox):
+        pass
