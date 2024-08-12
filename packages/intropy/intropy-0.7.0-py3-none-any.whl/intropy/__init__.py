90cd5602423dd6b53039e2c103d42cbb536dcbfb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-12 14:06:02 +0000

    Add package intropy/0.7.0/intropy-0.7.0-py3-none-any.whl

---
+++ packages/intropy/intropy-0.7.0-py3-none-any.whl/intropy/__init__.py
@@ -0,0 +1,53 @@
+__version__ = "0.7.0"
+
+import logging
+import logging.config
+import time
+
+INTROPY_ROOT_LOGGER = logging.getLogger(__name__)
+
+
+class UTCFormatter(logging.Formatter):
+    converter = time.gmtime
+
+
+LOGGING_CONFIG = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "formatters": {
+        "simple": {"format": "%(levelname)s: %(message)s"},
+        "standard_utc": {
+            "()": UTCFormatter,
+            "format": "[%(levelname)s|%(name)s|L%(lineno)d] %(asctime)s: %(message)s",
+            "datefmt": "%Y-%m-%dT%H:%M:%S%z",
+        },
+    },
+    "handlers": {
+        "default": {
+            "level": "WARNING",
+            "formatter": "simple",
+            "class": "logging.StreamHandler",
+        },
+        "detailed": {
+            "level": "DEBUG",
+            "formatter": "standard_utc",
+            "class": "logging.StreamHandler",
+            "stream": "ext://sys.stderr",
+        },
+    },
+    "loggers": {
+        "": {  # root logger
+            "handlers": ["default"],
+            "level": "DEBUG",
+        },
+        "__main__": {  # if __name__ == '__main__'
+            "handlers": ["detailed"],
+            "level": "DEBUG",
+            "propagate": False,
+        },
+    },
+}
+
+
+def setup_logging():
+    logging.config.dictConfig(LOGGING_CONFIG)
