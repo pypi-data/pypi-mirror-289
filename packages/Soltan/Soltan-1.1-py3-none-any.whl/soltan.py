b8324d471373ebdee13357a6c5c8f6dbd462c01d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-12 03:05:26 +0000

    Add package Soltan/1.1/Soltan-1.1-py3-none-any.whl

---
+++ packages/Soltan/Soltan-1.1-py3-none-any.whl/soltan.py
@@ -0,0 +1,20 @@
+__author__ = 'Mohammad Parsa Mortazavi'
+__version__ = '1.0.0'
+#Soltan_Hashemi_Module
+#Channel: ble.ir/fanofheshemi
+def sortlist(l):
+    l.sort()
+def texttolist(text):
+    n = len(text)
+    b = n*['']
+    for i in range(n):
+        b[i] = text[i]
+    return b
+SOLTAN_ID = 651290248
+SOLTAN_USERNAME = '@doreh_15'
+def listtotext(list):
+    s = ''
+    for i in range(len(list)):
+        s += list[i]
+    return s
+#Thanks For Using😁
