c14cea7d79e916593c723abb3b683943097c0474
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-12 04:06:08 +0000

    Add package timestep/12.0.67/timestep-12.0.67.tar.gz

---
+++ packages/timestep/timestep-12.0.67.tar.gz/timestep-12.0.67/timestep/api/openai/v1/controllers/embeddings_controller.py
@@ -0,0 +1,11 @@
+async def create_embedding(create_embedding_request):  # noqa: E501
+    """Creates an embedding vector representing the input text.
+
+     # noqa: E501
+
+    :param create_embedding_request:
+    :type create_embedding_request: dict | bytes
+
+    :rtype: Union[CreateEmbeddingResponse, Tuple[CreateEmbeddingResponse, int], Tuple[CreateEmbeddingResponse, int, Dict[str, str]]
+    """
+    raise NotImplementedError
