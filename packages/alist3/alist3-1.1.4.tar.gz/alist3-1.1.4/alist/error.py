1ecc937f46da83ff982f582d2d8986ce2e5771d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-12 03:05:29 +0000

    Add package alist3/1.1.4/alist3-1.1.4.tar.gz

---
+++ packages/alist3/alist3-1.1.4.tar.gz/alist3-1.1.4/alist/error.py
@@ -0,0 +1,12 @@
+class AListError(Exception):
+    pass
+
+
+class AuthenticationError(AListError):
+    pass
+
+class ServerError(AListError):
+    pass
+
+class SecurityWarning(Warning):
+    pass
