2eb90aa5fab0906db9bddf82f7622e9253b5a967
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-12 03:05:46 +0000

    Add package ensemble-package/1.0.13/ensemble_package-1.0.13.tar.gz

---
+++ packages/ensemble-package/ensemble_package-1.0.13.tar.gz/ensemble_package-1.0.13/setup.py
@@ -0,0 +1,11 @@
+# project\setup.py
+# Author: Florian Meier
+
+# Dieses File wird verwendet, um das Setup der Distribution des Pakcages zu erstellen.
+
+from setuptools import setup, find_packages
+
+setup(
+    name='ensemble_package',
+    version='1.0.13',
+    packages=find_packages())
