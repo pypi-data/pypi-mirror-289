6209694e4084340a69f1f9a792a6c91b0511b177
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-12 14:06:29 +0000

    Add package torch-npu/2.4.0rc1/torch_npu-2.4.0rc1-cp38-cp38-manylinux_2_17_aarch64.manylinux2014_aarch64.whl

---
+++ packages/torch-npu/torch_npu-2.4.0rc1-cp38-cp38-manylinux_2_17_aarch64.manylinux2014_aarch64.whl/torch_npu/distributed/__init__.py
@@ -0,0 +1,29 @@
+import torch_npu
+from torch_npu.utils._error_code import ErrCode, dist_error
+
+__all__ = [
+    "batch_isend_irecv", "gather", "gather_object", "is_hccl_available", "_verify_params_across_processes"
+]
+
+
+def is_available():
+    """
+    Returns ``True`` if the distributed package is available. Otherwise,
+    ``torch.distributed`` does not expose any other APIs. Currently,
+    ``torch.distributed`` is available on Linux, MacOS and Windows. Set
+    ``USE_DISTRIBUTED=1`` to enable it when building PyTorch from source.
+    Currently, the default value is ``USE_DISTRIBUTED=1`` for Linux and Windows,
+    ``USE_DISTRIBUTED=0`` for MacOS.
+    """
+    return hasattr(torch_npu._C, "_c10d_npu_init")
+
+
+if is_available() and not torch_npu._C._c10d_npu_init():
+    raise RuntimeError("Failed to initialize torch_npu.distributed" + dist_error(ErrCode.INTERNAL))
+
+
+from torch_npu._C._distributed_c10d import (
+    _verify_params_across_processes,
+)
+
+from .distributed_c10d import batch_isend_irecv, gather, gather_object, is_hccl_available
---
+++ packages/torch-npu/torch_npu-2.4.0rc1-cp38-cp38-manylinux_2_17_aarch64.manylinux2014_aarch64.whl/torch_npu/version.py
@@ -0,0 +1,2 @@
+__version__ = '2.4.0rc1'
+git_version = '6e1b5d0298941bc13fd216397439790b912ba7d5'
