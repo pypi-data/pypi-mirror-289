43834da29720707bd7d1c3bf19d9056ccc3e2d8f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-12 05:04:35 +0000

    Add package jsify/0.9.7/jsify-0.9.7-py3-none-any.whl

---
+++ packages/jsify/jsify-0.9.7-py3-none-any.whl/jsify/__init__.py
@@ -0,0 +1,5 @@
+from .jsify import Object, Dict, Tuple, List, Iterator, jsify, unjsify, deep_unjsify, jsified_copy, \
+    jsified_get, jsified_update, jsified_keys, jsified_values, jsified_items
+from .undefined import Undefined
+from .encoder import ObjectEncoder
+from .assertion import Assert
