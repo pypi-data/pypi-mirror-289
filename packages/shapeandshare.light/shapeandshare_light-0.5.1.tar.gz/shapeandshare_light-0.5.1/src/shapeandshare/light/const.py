e0840b7e14d733217e44f249c5c5b6bf56004b66
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-12 05:04:38 +0000

    Add package shapeandshare.light/0.5.1/shapeandshare_light-0.5.1.tar.gz

---
+++ packages/shapeandshare.light/shapeandshare_light-0.5.1.tar.gz/shapeandshare_light-0.5.1/src/shapeandshare/light/const.py
@@ -0,0 +1,27 @@
+import pygame
+
+# Predefined some colors
+BLUE: tuple[int, int, int] = (0, 0, 255)
+RED: tuple[int, int, int] = (255, 0, 0)
+GREEN: tuple[int, int, int] = (0, 255, 0)
+BLACK: tuple[int, int, int] = (0, 0, 0)
+WHITE: tuple[int, int, int] = (255, 255, 255)
+
+# dimensions=(20, 20) 20x15=300, 20x15=300
+
+# WINDOW_WIDTH: int = 75
+# WINDOW_HEIGHT: int = 75
+
+# WINDOW_WIDTH: int = 300
+# WINDOW_HEIGHT: int = 300
+
+# 30x15=450
+WINDOW_WIDTH: int = 450
+WINDOW_HEIGHT: int = 450
+
+
+FPS: int = 60
+FramePerSec = pygame.time.Clock()
+
+
+PRODUCT_NAME: str = "light"
---
+++ packages/shapeandshare.light/shapeandshare_light-0.5.1.tar.gz/shapeandshare_light-0.5.1/src/shapeandshare/light/loop.py
@@ -0,0 +1,57 @@
+import sys
+
+import pygame
+from pygame import QUIT, Surface, SurfaceType
+
+from shapeandshare.darkness import Client, CommandOptions, Island, TileType
+
+from .const import FPS, WHITE, FramePerSec
+from .contracts.sprites.island import SpriteIsland
+
+
+async def loop(display_surface: Surface | SurfaceType):
+    options: CommandOptions = CommandOptions(sleep_time=5, retry_count=3, tld="127.0.0.1:8000", timeout=60)
+    client: Client = Client(options=options)
+
+    # create a new world
+    world_id: str = await client.world_create(name="darkness")
+
+    # create a new island
+    island_id: str = await client.island_create(
+        world_id=world_id, name="roshar", dimensions=(30, 30), biome=TileType.GRASS
+    )
+    island: Island = await client.island_get(world_id=world_id, island_id=island_id, full=True)
+
+    sprite_island: SpriteIsland = SpriteIsland.model_validate(
+        {**island.model_dump(exclude={"contents"}), "offset": (0, 0)}
+    )
+    sprite_island.load_tiles(tiles=island.contents)
+
+    while True:
+        # Review game events (of interest)
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                pygame.quit()
+                sys.exit()
+
+            if event.type in (pygame.MOUSEMOTION, pygame.MOUSEBUTTONDOWN):
+                pointer_pos: tuple[int, int] = pygame.mouse.get_pos()
+                # print(pointer_pos)
+
+                # Determine action type
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                    # print("MOUSEBUTTONDOWN")
+                    sprite_island.update_tile_selection(position=pointer_pos)
+
+                elif event.type == pygame.MOUSEMOTION:
+                    # print("MOUSEMOTION")
+                    sprite_island.update_tile_hover(position=pointer_pos)
+
+        # Apply game logic updates
+        # Redraw the surface
+        display_surface.fill(WHITE)
+        for _, tile in sprite_island.tiles.items():
+            tile.draw(display_surface)
+
+        pygame.display.update()
+        FramePerSec.tick(FPS)
