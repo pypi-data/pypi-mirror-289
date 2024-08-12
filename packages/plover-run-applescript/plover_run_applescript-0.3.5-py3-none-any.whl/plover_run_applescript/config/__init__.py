79461c705d3304cb0a1744fe244f15a81c274595
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-12 03:05:26 +0000

    Add package plover-run-applescript/0.3.5/plover_run_applescript-0.3.5-py3-none-any.whl

---
+++ packages/plover-run-applescript/plover_run_applescript-0.3.5-py3-none-any.whl/plover_run_applescript/config/__init__.py
@@ -0,0 +1,13 @@
+"""
+# Config
+
+A package dealing with:
+    - loading and saving config containing filepath pointers to compiled
+      AppleScript files
+"""
+from .actions import load, save
+
+__all__ = [
+    "load",
+    "save",
+]
