3f8d67f890b83688c5db3cd3a564a30658655342
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-12 03:05:17 +0000

    Add package efficient-linear-decoding/0.0.3/efficient_linear_decoding-0.0.3.tar.gz

---
+++ packages/efficient-linear-decoding/efficient_linear_decoding-0.0.3.tar.gz/efficient_linear_decoding-0.0.3/setup.py
@@ -0,0 +1,25 @@
+from setuptools import find_packages,setup
+
+
+setup(
+    name="efficient_linear_decoding",
+    version = "0.0.3",
+    keywords = ["pip", "efficient_linear_decoding"],
+    description = "Efficient computation library for linear attention.",
+    license = "MIT Licence",
+    url = "https://github.com/Computational-Machine-Intelligence/efficient_linear_decoding",
+    
+    author = "Jiaping Wang",
+    author_email = "wjp666.s@gmail.com",
+    packages=find_packages(
+        exclude=[
+            "tests"
+        ]
+    ),
+    package_data={
+        'efficient_linear_decoding': ['methods/*'],
+    },
+    platforms = "any",
+    install_requires=["torch==2.1.0", "pycuda", "triton==2.1.0","pynvml","numpy"], 
+    include_package_data=True,
+)
