e9bf898cb7bb070ba177758912a10fd8b656f49b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-12 15:05:40 +0000

    Add package py-modelrunner/0.18.2/py_modelrunner-0.18.2.tar.gz

---
+++ packages/py-modelrunner/py_modelrunner-0.18.2.tar.gz/py_modelrunner-0.18.2/scripts/format_code.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+# This script formats the code of this package
+
+echo "Upgrading python syntax..."
+pushd .. > /dev/null
+find . -name '*.py' -exec pyupgrade --py38-plus {} +
+popd > /dev/null
+
+echo "Formating import statements..."
+isort ..
+
+echo "Formating docstrings..."
+docformatter --in-place --black --recursive ..
+
+echo "Formating source code..."
+black ..
