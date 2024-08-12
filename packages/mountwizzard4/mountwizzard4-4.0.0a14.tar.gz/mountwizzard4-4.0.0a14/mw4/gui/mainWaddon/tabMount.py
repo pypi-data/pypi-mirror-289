d4d1068e4273f3b0fd6f9bbc697a2dd5f4b22b1a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-12 03:05:58 +0000

    Add package mountwizzard4/4.0.0a14/mountwizzard4-4.0.0a14.tar.gz

---
+++ packages/mountwizzard4/mountwizzard4-4.0.0a14.tar.gz/mountwizzard4-4.0.0a14/mw4/gui/mainWaddon/tabMount.py
@@ -0,0 +1,174 @@
+############################################################
+# -*- coding: utf-8 -*-
+#
+#       #   #  #   #   #    #
+#      ##  ##  #  ##  #    #
+#     # # # #  # # # #    #  #
+#    #  ##  #  ##  ##    ######
+#   #   #   #  #   #       #
+#
+# Python-based Tool for interaction with the 10micron mounts
+# GUI with PyQT5 for python
+#
+# written in python3, (c) 2019-2024 by mworion
+# Licence APL2.0
+#
+###########################################################
+# standard libraries
+
+# external packages
+
+# local import
+from gui.utilities.toolsQtWidget import MWidget
+
+
+class Mount(MWidget):
+    """
+    """
+
+    def __init__(self, mainW):
+        super().__init__()
+
+        self.mainW = mainW
+        self.app = mainW.app
+        self.msg = mainW.app.msg
+        self.ui = mainW.ui
+
+        self.app.gameABXY.connect(self.changeParkGameController)
+        self.app.gameABXY.connect(self.stopGameController)
+        self.app.gameABXY.connect(self.changeTrackingGameController)
+        self.app.gameABXY.connect(self.flipMountGameController)
+        self.ui.flipMount.clicked.connect(self.flipMount)
+        self.ui.tracking.clicked.connect(self.changeTracking)
+        self.ui.setLunarTracking.clicked.connect(self.setLunarTracking)
+        self.ui.setSiderealTracking.clicked.connect(self.setSiderealTracking)
+        self.ui.setSolarTracking.clicked.connect(self.setSolarTracking)
+        self.ui.park.clicked.connect(self.changePark)
+        self.ui.stop.clicked.connect(self.stop)
+
+    def initConfig(self):
+        """
+        """
+        config = self.app.config.get('mainW', {})
+        self.ui.coordsJ2000.setChecked(config.get('coordsJ2000', False))
+        self.ui.coordsJNow.setChecked(config.get('coordsJNow', False))
+
+    def storeConfig(self):
+        """
+        """
+        config = self.app.config['mainW']
+        config['coordsJ2000'] = self.ui.coordsJ2000.isChecked()
+        config['coordsJNow'] = self.ui.coordsJNow.isChecked()
+
+    def changeTrackingGameController(self, value):
+        """
+        """
+        if value == 0b00000100:
+            self.changeTracking()
+
+    def changeTracking(self):
+        """
+        """
+        obs = self.app.mount.obsSite
+        if obs.status == 0:
+            suc = obs.stopTracking()
+            if not suc:
+                self.msg.emit(2, 'Mount', 'Command', 'Cannot stop tracking')
+            else:
+                self.msg.emit(0, 'Mount', 'Command', 'Stopped tracking')
+        else:
+            suc = obs.startTracking()
+            if not suc:
+                self.msg.emit(2, 'Mount', 'Command', 'Cannot start tracking')
+            else:
+                self.msg.emit(0, 'Mount', 'Command', 'Started tracking')
+
+    def changeParkGameController(self, value):
+        """
+        """
+        if value == 0b00000001:
+            self.changePark()
+
+    def changePark(self):
+        """
+        """
+        obs = self.app.mount.obsSite
+        if obs.status == 5:
+            suc = obs.unpark()
+            if not suc:
+                self.msg.emit(2, 'Mount', 'Command', 'Cannot unpark mount')
+            else:
+                self.msg.emit(0, 'Mount', 'Command', 'Mount unparked')
+        else:
+            suc = obs.park()
+            if not suc:
+                self.msg.emit(2, 'Mount', 'Command', 'Cannot park mount')
+            else:
+                self.msg.emit(0, 'Mount', 'Command', 'Mount parked')
+
+    def setLunarTracking(self):
+        """
+        """
+        sett = self.app.mount.setting
+        suc = sett.setLunarTracking()
+        if not suc:
+            self.msg.emit(2, 'Mount', 'Command', 'Cannot set tracking to Lunar')
+        else:
+            self.msg.emit(0, 'Mount', 'Command', 'Tracking set to Lunar')
+        return suc
+
+    def setSiderealTracking(self):
+        """
+        """
+        sett = self.app.mount.setting
+        suc = sett.setSiderealTracking()
+        if not suc:
+            self.msg.emit(2, 'Mount', 'Command', 'Cannot set tracking to Sidereal')
+        else:
+            self.msg.emit(0, 'Mount', 'Command', 'Tracking set to Sidereal')
+        return suc
+
+    def setSolarTracking(self):
+        """
+        """
+        sett = self.app.mount.setting
+        suc = sett.setSolarTracking()
+        if not suc:
+            self.msg.emit(2, 'Mount', 'Command', 'Cannot set tracking to Solar')
+        else:
+            self.msg.emit(0, 'Mount', 'Command', 'Tracking set to Solar')
+        return suc
+
+    def flipMountGameController(self, value):
+        """
+        """
+        if value == 0b00000010:
+            self.flipMount()
+
+    def flipMount(self):
+        """
+        """
+        obs = self.app.mount.obsSite
+        suc = obs.flip()
+        if not suc:
+            self.msg.emit(2, 'Mount', 'Command', 'Cannot flip mount')
+        else:
+            self.msg.emit(0, 'Mount', 'Command', 'Mount flipped')
+        return suc
+
+    def stopGameController(self, value):
+        """
+        """
+        if value == 0b00001000:
+            self.stop()
+
+    def stop(self):
+        """
+        """
+        obs = self.app.mount.obsSite
+        suc = obs.stop()
+        if not suc:
+            self.msg.emit(2, 'Mount', 'Command', 'Cannot stop mount')
+        else:
+            self.msg.emit(0, 'Mount', 'Command', 'Mount stopped')
+        return suc
