7f7409ec685c06c8f7dde598ee041b6ac003d325
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-12 14:06:11 +0000

    Add package openai-commands/3.138.1/openai_commands-3.138.1.tar.gz

---
+++ packages/openai-commands/openai_commands-3.138.1.tar.gz/openai_commands-3.138.1/setup.py
@@ -0,0 +1,17 @@
+from openai_commands import NAME, VERSION, DESCRIPTION, REPO_NAME
+from blueness.pypi import setup
+
+setup(
+    filename=__file__,
+    repo_name=REPO_NAME,
+    name=NAME,
+    version=VERSION,
+    description=DESCRIPTION,
+    packages=[NAME],
+    include_package_data=True,
+    package_data={
+        NAME: [
+            ".abcli/**/*.sh",
+        ],
+    },
+)
