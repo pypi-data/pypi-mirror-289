7bf5570a823295a0088431e117b12c72d254f74f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-12 03:05:16 +0000

    Add package meme-generator/0.1.0/meme_generator-0.1.0.tar.gz

---
+++ packages/meme-generator/meme_generator-0.1.0.tar.gz/meme_generator-0.1.0/meme_generator/memes/high_EQ/__init__.py
@@ -0,0 +1,51 @@
+from datetime import datetime
+from pathlib import Path
+
+from pil_utils import BuildImage
+
+from meme_generator import CommandShortcut, add_meme
+from meme_generator.exception import TextOverLength
+
+img_dir = Path(__file__).parent / "images"
+
+
+def high_EQ(images, texts: list[str], args):
+    frame = BuildImage.open(img_dir / "0.jpg")
+
+    def draw(pos: tuple[float, float, float, float], text: str):
+        try:
+            frame.draw_text(
+                pos,
+                text,
+                max_fontsize=100,
+                min_fontsize=50,
+                allow_wrap=True,
+                fill="white",
+                stroke_fill="black",
+                stroke_ratio=0.05,
+            )
+        except ValueError:
+            raise TextOverLength(text)
+
+    draw((40, 540, 602, 1140), texts[0])
+    draw((682, 540, 1244, 1140), texts[1])
+    return frame.save_jpg()
+
+
+add_meme(
+    "high_EQ",
+    high_EQ,
+    min_texts=2,
+    max_texts=2,
+    default_texts=["高情商", "低情商"],
+    keywords=["低情商", "高情商"],
+    shortcuts=[
+        CommandShortcut(
+            key=r"低情商[\s:：]*(?P<left>\S+)\s*高情商[\s:：]*(?P<right>\S+)",
+            args=["{low}", "{high}"],
+            humanized="低情商xx高情商xx",
+        )
+    ],
+    date_created=datetime(2022, 6, 12),
+    date_modified=datetime(2024, 8, 9),
+)
