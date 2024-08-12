ce102050ef08b2f2496b60053ef1ad2ab4cddee6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-12 05:04:39 +0000

    Add package yhttp-dbmanager/2.6.2/yhttp_dbmanager-2.6.2.tar.gz

---
+++ packages/yhttp-dbmanager/yhttp_dbmanager-2.6.2.tar.gz/yhttp_dbmanager-2.6.2/tests/test_dburi.py
@@ -0,0 +1,20 @@
+from yhttp.ext.dbmanager import DatabaseURI
+
+import pytest
+
+
+def test_dburi():
+    uristr = 'foo://bar:baz@qux/quux.thud'
+    uri = DatabaseURI.loads(uristr)
+
+    assert uri.provider == 'foo'
+    assert uri.user == 'bar'
+    assert uri.password == 'baz'
+    assert uri.host == 'qux'
+    assert uri.dbname == 'quux.thud'
+    assert uri.dumps() == uristr
+
+    # Invalid uri
+    uristr = 'baduri'
+    with pytest.raises(ValueError):
+        DatabaseURI.loads(uristr)
