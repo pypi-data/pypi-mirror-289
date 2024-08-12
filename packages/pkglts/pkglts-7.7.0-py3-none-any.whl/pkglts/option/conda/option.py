3ea6d18c90a9d8a5c39ba30f50beb0b1b1e1683e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-12 15:06:25 +0000

    Add package pkglts/7.7.0/pkglts-7.7.0-py3-none-any.whl

---
+++ packages/pkglts/pkglts-7.7.0-py3-none-any.whl/pkglts/option/conda/option.py
@@ -0,0 +1,44 @@
+import logging
+from pathlib import Path
+
+from pkglts.option.reqs.option import requirements
+from pkglts.option_object import Option
+from pkglts.version import __version__
+
+LOGGER = logging.getLogger(__name__)
+
+
+class OptionConda(Option):
+    def version(self):
+        return __version__
+
+    def root_dir(self):
+        return Path(__file__).parent
+
+    def update_parameters(self, cfg):
+        LOGGER.info("update parameters %s", self._name)
+        sec = dict(
+            env_name="{{ base.pkgname }}",
+        )
+        cfg[self._name] = sec
+
+    def check(self, cfg):
+        invalids = []
+        env_name = cfg[self._name]['env_name']
+
+        if not env_name:
+            invalids.append("conda.env_name")
+
+        if len(env_name) != len(env_name.strip()) or "-" in env_name:
+            invalids.append("conda.env_name")
+
+        return invalids
+
+    def require_option(self, cfg):
+        return ['pyproject']
+
+    def environment_extensions(self, cfg):
+        channels = set(dep.channel for dep in requirements(cfg)) - {None}
+        channels_minimal = set(dep.channel for dep in requirements(cfg) if 'install' in dep.intents) - {None}
+
+        return {"channels": channels, "channels_minimal": channels_minimal}
