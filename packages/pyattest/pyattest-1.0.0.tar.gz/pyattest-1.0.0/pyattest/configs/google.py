43f7fc2b2b8fd7edff488e567ce7b90043f4c92f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-12 15:05:50 +0000

    Add package pyattest/1.0.0/pyattest-1.0.0.tar.gz

---
+++ packages/pyattest/pyattest-1.0.0.tar.gz/pyattest-1.0.0/pyattest/configs/google.py
@@ -0,0 +1,29 @@
+from typing import Optional, List
+
+from pyattest.configs.config import Config
+from pyattest.verifiers.google_assertion import GoogleAssertionVerifier
+from pyattest.verifiers.google_attestation import GoogleAttestationVerifier
+
+
+class GoogleConfig(Config):
+    attestation_verifier_class = GoogleAttestationVerifier
+    assertion_verifier_class = GoogleAssertionVerifier
+
+    def __init__(
+        self,
+        key_ids: List[bytes],
+        apk_package_name: str,
+        production: bool,
+        root_cn: str = "attest.android.com",
+        root_ca: bytes = None,
+    ):
+        self.key_ids = key_ids
+        self.apk_package_name = apk_package_name
+        self.production = production
+        self.root_cn = root_cn
+        self._custom_root_ca = root_ca
+
+    @property
+    def root_ca(self) -> Optional[bytes]:
+        """This is only used for simplified unit testing."""
+        return self._custom_root_ca
