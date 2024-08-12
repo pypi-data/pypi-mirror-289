864803db5bcf677c82f88237ef1000fbf2e53eac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-12 15:05:27 +0000

    Add package tspoon/0.1/tspoon-0.1.tar.gz

---
+++ packages/tspoon/tspoon-0.1.tar.gz/tspoon-0.1/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+	name='tspoon',
+	version='0.1',
+	packages=find_packages(),
+	install_requires=[
+		# 'os',
+		# 'time',
+		# 'sys',
+		# 'pickle',
+		# 'pandas',
+		# 'numpy',
+		# 'regex',
+		# 'datetime',
+		# 'matplotlib',
+		# 'statsmodels',
+		# 'copy'
+	]
+)
