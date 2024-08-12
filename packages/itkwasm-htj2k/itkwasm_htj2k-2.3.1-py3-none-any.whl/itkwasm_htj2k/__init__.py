25bcdbf7bc34477e3752ab0ea7560b9ba6386a73
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-12 03:05:13 +0000

    Add package itkwasm-htj2k/2.3.1/itkwasm_htj2k-2.3.1-py3-none-any.whl

---
+++ packages/itkwasm-htj2k/itkwasm_htj2k-2.3.1-py3-none-any.whl/itkwasm_htj2k/__init__.py
@@ -0,0 +1,10 @@
+# Generated file. To retain edits, remove this comment.
+
+"""itkwasm-htj2k: Wasm-SIMD accelerated decoding and encoding of High-throughput JPEG2000 images."""
+
+from .decode_async import decode_async
+from .decode import decode
+from .encode_async import encode_async
+from .encode import encode
+
+from ._version import __version__
