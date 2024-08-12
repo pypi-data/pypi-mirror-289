9efc4ab2092c8caac077ec449c6e88238e0e5033
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-12 14:06:04 +0000

    Add package backstepping-controller/0.1.2/backstepping_controller-0.1.2.tar.gz

---
+++ packages/backstepping-controller/backstepping_controller-0.1.2.tar.gz/backstepping_controller-0.1.2/backstepping_controller/plotting.py
@@ -0,0 +1,25 @@
+import matplotlib.pyplot as plt
+
+def plot_responses(time, state_values, control_inputs, refs=None):
+    # Plot states
+    plt.figure(figsize=(12, 8))
+    for i, state in enumerate(state_values):
+        plt.plot(time, state, label=f'Actual State {i+1}')
+        if refs is not None:
+            plt.plot(time, refs[i], '--', label=f'Reference State {i+1}')
+    plt.xlabel('Time [s]')
+    plt.ylabel('States')
+    plt.legend()
+    plt.title('System States Over Time')
+    plt.grid(True)
+    plt.show()
+    
+    # Plot control inputs
+    plt.figure(figsize=(12, 6))
+    plt.plot(time[:-1], control_inputs, label='Control Input')
+    plt.xlabel('Time [s]')
+    plt.ylabel('Control Input')
+    plt.legend()
+    plt.title('Control Input Over Time')
+    plt.grid(True)
+    plt.show()
