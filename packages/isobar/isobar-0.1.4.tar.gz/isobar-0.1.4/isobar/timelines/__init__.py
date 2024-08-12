a997591f64f0ce0bae405b245d990996c7044bc9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-12 03:05:54 +0000

    Add package isobar/0.1.4/isobar-0.1.4.tar.gz

---
+++ packages/isobar/isobar-0.1.4.tar.gz/isobar-0.1.4/isobar/timelines/__init__.py
@@ -0,0 +1,7 @@
+from .timeline import Timeline
+from .track import Track
+from .lfo import LFO
+from .clock import Clock, DummyClock
+from .clock_link import AbletonLinkClock
+
+__all__ = ["Timeline", "Track", "LFO", "Clock", "DummyClock", "AbletonLinkClock"]
---
+++ packages/isobar/isobar-0.1.4.tar.gz/isobar-0.1.4/tests/test_timeline_lfo.py
@@ -0,0 +1,11 @@
+""" Unit tests for LFO """
+
+import isobar as iso
+from . import dummy_timeline
+
+def test_lfo(dummy_timeline):
+    lfo = iso.LFO(dummy_timeline, "sine", 1.0)
+    assert lfo.value == 0.5
+    for n in range(dummy_timeline.ticks_per_beat):
+        lfo.tick()
+    assert lfo.value == 0.5
