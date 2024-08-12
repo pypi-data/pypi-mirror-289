b973dfad2733535aa435e26f2f4557c16c81463d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-12 14:06:03 +0000

    Add package settlers/0.0.1/settlers-0.0.1.tar.gz

---
+++ packages/settlers/settlers-0.0.1.tar.gz/settlers-0.0.1/settlers/ascii_display.py
@@ -0,0 +1,16 @@
+'''A module for displaying the Settlers game-state in ASCII'''
+
+def display_board():
+    return 'ASCII Board'
+
+# Build road
+# Build settlement
+# Build City
+# Add Player
+# Add Resource Card(Player)
+# Add Devlopment Card(Player)
+# Change Intersection
+# Change Road
+# Robber
+# Display Tile Letters
+# Display Dice(Number)
