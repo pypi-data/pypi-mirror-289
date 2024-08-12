475004623023eae426c96360a04a2bf3c17af7fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-12 03:05:27 +0000

    Add package torch-operation-counter/0.3.5/torch_operation_counter-0.3.5.tar.gz

---
+++ packages/torch-operation-counter/torch_operation_counter-0.3.5.tar.gz/torch_operation_counter-0.3.5/setup.py
@@ -0,0 +1,18 @@
+from setuptools import find_packages, setup
+
+__version__ = "0.3.5"
+
+install_requires = [
+    "torch>=1.13.1",
+]
+
+pkg_name = "torch_operation_counter"
+setup(
+    name=pkg_name,
+    version=__version__,
+    install_requires=install_requires,
+    packages=find_packages(),
+    author="Samir Moustafa",
+    author_email="samir.moustafa.97@gmail.com",
+    url="https://github.com/SamirMoustafa/torch-operation-counter/",
+)
