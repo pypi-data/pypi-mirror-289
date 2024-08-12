3a325c87d8ceba10e355d9b194763e032f19d68b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-12 04:06:21 +0000

    Add package altmindpy/0.3.0/altmindpy-0.3.0.tar.gz

---
+++ packages/altmindpy/altmindpy-0.3.0.tar.gz/altmindpy-0.3.0/CHANGELOG.md
@@ -0,0 +1,146 @@
+# Changelog
+
+## 0.3.0 (2024-08-11)
+
+Full Changelog: [v0.2.0...v0.3.0](https://github.com/AltermindLabs/altmind-python/compare/v0.2.0...v0.3.0)
+
+### Features
+
+* **api:** update via SDK Studio ([#47](https://github.com/AltermindLabs/altmind-python/issues/47)) ([26f7af9](https://github.com/AltermindLabs/altmind-python/commit/26f7af91bee25bca540e023b0245255ee9727882))
+
+## 0.2.0 (2024-08-11)
+
+Full Changelog: [v0.1.0...v0.2.0](https://github.com/AltermindLabs/altmind-python/compare/v0.1.0...v0.2.0)
+
+### Features
+
+* **api:** update via SDK Studio ([#44](https://github.com/AltermindLabs/altmind-python/issues/44)) ([a276861](https://github.com/AltermindLabs/altmind-python/commit/a276861d1332f30dd11deda4c790451a3d633585))
+
+## 0.1.0 (2024-08-11)
+
+Full Changelog: [v0.0.6...v0.1.0](https://github.com/AltermindLabs/altmind-python/compare/v0.0.6...v0.1.0)
+
+### Features
+
+* **api:** update via SDK Studio ([#38](https://github.com/AltermindLabs/altmind-python/issues/38)) ([ba3077f](https://github.com/AltermindLabs/altmind-python/commit/ba3077f1dde86e21ca369cdcb2536d8641c19670))
+
+
+### Chores
+
+* **ci:** bump prism mock server version ([#41](https://github.com/AltermindLabs/altmind-python/issues/41)) ([b6ba34d](https://github.com/AltermindLabs/altmind-python/commit/b6ba34d73436b45f8c3d2e5ec724f9d84f7eb1d4))
+* configure new SDK language ([#33](https://github.com/AltermindLabs/altmind-python/issues/33)) ([2b1f9b3](https://github.com/AltermindLabs/altmind-python/commit/2b1f9b335f7fd042380cfece7539d5f5e28f9d0b))
+* **internal:** add type construction helper ([#37](https://github.com/AltermindLabs/altmind-python/issues/37)) ([c7c478d](https://github.com/AltermindLabs/altmind-python/commit/c7c478d78fc4085a0f1222856f8107ef36ffd413))
+* **internal:** codegen related update ([#35](https://github.com/AltermindLabs/altmind-python/issues/35)) ([f105876](https://github.com/AltermindLabs/altmind-python/commit/f105876ac84bbd1482c0f9be09bf53e6e72517ae))
+* **internal:** codegen related update ([#39](https://github.com/AltermindLabs/altmind-python/issues/39)) ([b1f70ef](https://github.com/AltermindLabs/altmind-python/commit/b1f70ef9e25745d73a1e327af51ce3829060bee7))
+* **internal:** ensure package is importable in lint cmd ([#42](https://github.com/AltermindLabs/altmind-python/issues/42)) ([f935c9d](https://github.com/AltermindLabs/altmind-python/commit/f935c9d2a0095527c28f2e5a324e697e4a6cd4f3))
+* **internal:** remove deprecated ruff config ([#40](https://github.com/AltermindLabs/altmind-python/issues/40)) ([dd1b220](https://github.com/AltermindLabs/altmind-python/commit/dd1b220b82a9f89b862527ecf09a84d9dfb75063))
+* **tests:** update prism version ([#36](https://github.com/AltermindLabs/altmind-python/issues/36)) ([c7346d9](https://github.com/AltermindLabs/altmind-python/commit/c7346d9879333ebe1aaa6775761c16b7f0b6e559))
+
+## 0.0.6 (2024-05-28)
+
+Full Changelog: [v0.0.5...v0.0.6](https://github.com/AltermindLabs/altmind-python/compare/v0.0.5...v0.0.6)
+
+### Features
+
+* **api:** update via SDK Studio ([#31](https://github.com/AltermindLabs/altmind-python/issues/31)) ([ce50867](https://github.com/AltermindLabs/altmind-python/commit/ce508670eef53ea1b6106b973d416ce02d6c1467))
+
+
+### Chores
+
+* **internal:** version bump ([#29](https://github.com/AltermindLabs/altmind-python/issues/29)) ([a8097bb](https://github.com/AltermindLabs/altmind-python/commit/a8097bb5238732bde6edf7b24b13511043a668c4))
+
+## 0.0.5 (2024-05-28)
+
+Full Changelog: [v0.0.4-alpha...v0.0.5](https://github.com/AltermindLabs/altmind-python/compare/v0.0.4-alpha...v0.0.5)
+
+### Features
+
+* **api:** update via SDK Studio ([#28](https://github.com/AltermindLabs/altmind-python/issues/28)) ([65ec8a6](https://github.com/AltermindLabs/altmind-python/commit/65ec8a6e3718a0ec24c2aca19bd4aee3023bd32d))
+
+
+### Chores
+
+* update SDK settings ([#26](https://github.com/AltermindLabs/altmind-python/issues/26)) ([92ffeb0](https://github.com/AltermindLabs/altmind-python/commit/92ffeb00512a8bc71e2e7ff4702a4bde3684c98b))
+
+## 0.0.4-alpha (2024-05-27)
+
+Full Changelog: [v0.0.3-alpha...v0.0.4-alpha](https://github.com/AltermindLabs/altmind-python/compare/v0.0.3-alpha...v0.0.4-alpha)
+
+### Chores
+
+* update SDK settings ([#23](https://github.com/AltermindLabs/altmind-python/issues/23)) ([cc20380](https://github.com/AltermindLabs/altmind-python/commit/cc2038091729cd63078c8ab26e1cfef348e6f43e))
+
+## 0.0.3-alpha (2024-05-27)
+
+Full Changelog: [v0.0.2-alpha...v0.0.3-alpha](https://github.com/AltermindLabs/altmind-python/compare/v0.0.2-alpha...v0.0.3-alpha)
+
+### Chores
+
+* **internal:** version bump ([#20](https://github.com/AltermindLabs/altmind-python/issues/20)) ([8672393](https://github.com/AltermindLabs/altmind-python/commit/86723935282517fc332757b26a63f815b587ceb4))
+
+## 0.0.2-alpha (2024-05-27)
+
+Full Changelog: [v0.2.0...v0.0.2-alpha](https://github.com/AltermindLabs/altmind-python/compare/v0.2.0...v0.0.2-alpha)
+
+### Features
+
+* **api:** update via SDK Studio ([f05ae85](https://github.com/AltermindLabs/altmind-python/commit/f05ae85883ced71f384979a346095978900b43c6))
+* **api:** update via SDK Studio ([#19](https://github.com/AltermindLabs/altmind-python/issues/19)) ([7e6d755](https://github.com/AltermindLabs/altmind-python/commit/7e6d755336989d39d5fababa2eee328f775b879c))
+
+
+### Chores
+
+* go live ([#17](https://github.com/AltermindLabs/altmind-python/issues/17)) ([86995de](https://github.com/AltermindLabs/altmind-python/commit/86995de77004877380a3728c2678a9be2940c6a3))
+
+## 0.2.0 (2024-05-27)
+
+Full Changelog: [v0.1.0...v0.2.0](https://github.com/AltermindLabs/altmind-python/compare/v0.1.0...v0.2.0)
+
+### Features
+
+* **api:** update via SDK Studio ([#14](https://github.com/AltermindLabs/altmind-python/issues/14)) ([82ce474](https://github.com/AltermindLabs/altmind-python/commit/82ce474c587fa94b40ff40ca62b319debf63b14e))
+
+## 0.1.0 (2024-05-20)
+
+Full Changelog: [v0.0.3...v0.1.0](https://github.com/AltermindLabs/altmind-python/compare/v0.0.3...v0.1.0)
+
+### Features
+
+* **api:** update via SDK Studio ([#11](https://github.com/AltermindLabs/altmind-python/issues/11)) ([6b77d0d](https://github.com/AltermindLabs/altmind-python/commit/6b77d0dd3b27b8b0072dcefee2d66f8d1fcba6d7))
+
+## 0.0.3 (2024-05-19)
+
+Full Changelog: [v0.0.2...v0.0.3](https://github.com/AltermindLabs/altmind-python/compare/v0.0.2...v0.0.3)
+
+### Chores
+
+* **internal:** version bump ([#8](https://github.com/AltermindLabs/altmind-python/issues/8)) ([879d67f](https://github.com/AltermindLabs/altmind-python/commit/879d67f801169a5b259db00dc242abae94b7a3ab))
+
+## 0.0.2 (2024-05-19)
+
+Full Changelog: [v0.0.1-alpha...v0.0.2](https://github.com/AltermindLabs/altmind-python/compare/v0.0.1-alpha...v0.0.2)
+
+### Features
+
+* **api:** update via SDK Studio ([b9feb63](https://github.com/AltermindLabs/altmind-python/commit/b9feb63fcce10999369dbafac5355a1d9ca44149))
+* **api:** update via SDK Studio ([9b74f14](https://github.com/AltermindLabs/altmind-python/commit/9b74f14c9624e0a5a8f9ccc2a0baacc77dc79ea0))
+* **api:** update via SDK Studio ([7fc6fde](https://github.com/AltermindLabs/altmind-python/commit/7fc6fde731c3bfa3dcb5e7a688c2f4efcc92928e))
+* **api:** update via SDK Studio ([87dd84e](https://github.com/AltermindLabs/altmind-python/commit/87dd84e6eae8f343494a304ef76e9850be23d1a6))
+* **api:** update via SDK Studio ([b776ee3](https://github.com/AltermindLabs/altmind-python/commit/b776ee34f0a225e2de00b575bdc78cc93070f202))
+* **api:** update via SDK Studio ([f0c5cae](https://github.com/AltermindLabs/altmind-python/commit/f0c5cae4ed6e8bd671863b7e8ad3bec018aff425))
+
+
+### Chores
+
+* go live ([#5](https://github.com/AltermindLabs/altmind-python/issues/5)) ([159b70c](https://github.com/AltermindLabs/altmind-python/commit/159b70ca469df0160e10020a5cf3028f25ca9ad8))
+* update SDK settings ([#7](https://github.com/AltermindLabs/altmind-python/issues/7)) ([77fd5b0](https://github.com/AltermindLabs/altmind-python/commit/77fd5b0554f9d4db7fd084dcccefd4f6633dd4ad))
+
+## 0.0.1-alpha (2024-05-18)
+
+Full Changelog: [v0.0.1-alpha.0...v0.0.1-alpha](https://github.com/AltermindLabs/altmind-python/compare/v0.0.1-alpha.0...v0.0.1-alpha)
+
+### Chores
+
+* configure new SDK language ([002a198](https://github.com/AltermindLabs/altmind-python/commit/002a1984a136a46bc716002537d4df8de9beb121))
+* go live ([#1](https://github.com/AltermindLabs/altmind-python/issues/1)) ([c4774f6](https://github.com/AltermindLabs/altmind-python/commit/c4774f6c665041b281933d1d44caae2a108ab521))
+* update SDK settings ([#3](https://github.com/AltermindLabs/altmind-python/issues/3)) ([1a0885a](https://github.com/AltermindLabs/altmind-python/commit/1a0885a451badbd871740e93333067193f3100bb))
---
+++ packages/altmindpy/altmindpy-0.3.0.tar.gz/altmindpy-0.3.0/tests/api_resources/test_threads.py
@@ -0,0 +1,362 @@
+# File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
+
+from __future__ import annotations
+
+import os
+from typing import Any, cast
+
+import pytest
+
+from altmindpy import Altmind, AsyncAltmind
+from tests.utils import assert_matches_type
+from altmindpy.types import (
+    ThreadDelete,
+    ThreadResponse,
+    ThreadsResponse,
+)
+
+base_url = os.environ.get("TEST_API_BASE_URL", "http://127.0.0.1:4010")
+
+
+class TestThreads:
+    parametrize = pytest.mark.parametrize("client", [False, True], indirect=True, ids=["loose", "strict"])
+
+    @parametrize
+    def test_method_create(self, client: Altmind) -> None:
+        thread = client.threads.create()
+        assert_matches_type(ThreadResponse, thread, path=["response"])
+
+    @parametrize
+    def test_method_create_with_all_params(self, client: Altmind) -> None:
+        thread = client.threads.create(
+            thread_metadata={},
+            title="title",
+        )
+        assert_matches_type(ThreadResponse, thread, path=["response"])
+
+    @parametrize
+    def test_raw_response_create(self, client: Altmind) -> None:
+        response = client.threads.with_raw_response.create()
+
+        assert response.is_closed is True
+        assert response.http_request.headers.get("X-Stainless-Lang") == "python"
+        thread = response.parse()
+        assert_matches_type(ThreadResponse, thread, path=["response"])
+
+    @parametrize
+    def test_streaming_response_create(self, client: Altmind) -> None:
+        with client.threads.with_streaming_response.create() as response:
+            assert not response.is_closed
+            assert response.http_request.headers.get("X-Stainless-Lang") == "python"
+
+            thread = response.parse()
+            assert_matches_type(ThreadResponse, thread, path=["response"])
+
+        assert cast(Any, response.is_closed) is True
+
+    @parametrize
+    def test_method_retrieve(self, client: Altmind) -> None:
+        thread = client.threads.retrieve(
+            0,
+        )
+        assert_matches_type(ThreadResponse, thread, path=["response"])
+
+    @parametrize
+    def test_raw_response_retrieve(self, client: Altmind) -> None:
+        response = client.threads.with_raw_response.retrieve(
+            0,
+        )
+
+        assert response.is_closed is True
+        assert response.http_request.headers.get("X-Stainless-Lang") == "python"
+        thread = response.parse()
+        assert_matches_type(ThreadResponse, thread, path=["response"])
+
+    @parametrize
+    def test_streaming_response_retrieve(self, client: Altmind) -> None:
+        with client.threads.with_streaming_response.retrieve(
+            0,
+        ) as response:
+            assert not response.is_closed
+            assert response.http_request.headers.get("X-Stainless-Lang") == "python"
+
+            thread = response.parse()
+            assert_matches_type(ThreadResponse, thread, path=["response"])
+
+        assert cast(Any, response.is_closed) is True
+
+    @parametrize
+    def test_method_update(self, client: Altmind) -> None:
+        thread = client.threads.update(
+            thread_id=0,
+        )
+        assert_matches_type(ThreadResponse, thread, path=["response"])
+
+    @parametrize
+    def test_method_update_with_all_params(self, client: Altmind) -> None:
+        thread = client.threads.update(
+            thread_id=0,
+            thread_metadata={},
+            title="title",
+        )
+        assert_matches_type(ThreadResponse, thread, path=["response"])
+
+    @parametrize
+    def test_raw_response_update(self, client: Altmind) -> None:
+        response = client.threads.with_raw_response.update(
+            thread_id=0,
+        )
+
+        assert response.is_closed is True
+        assert response.http_request.headers.get("X-Stainless-Lang") == "python"
+        thread = response.parse()
+        assert_matches_type(ThreadResponse, thread, path=["response"])
+
+    @parametrize
+    def test_streaming_response_update(self, client: Altmind) -> None:
+        with client.threads.with_streaming_response.update(
+            thread_id=0,
+        ) as response:
+            assert not response.is_closed
+            assert response.http_request.headers.get("X-Stainless-Lang") == "python"
+
+            thread = response.parse()
+            assert_matches_type(ThreadResponse, thread, path=["response"])
+
+        assert cast(Any, response.is_closed) is True
+
+    @parametrize
+    def test_method_list(self, client: Altmind) -> None:
+        thread = client.threads.list()
+        assert_matches_type(ThreadsResponse, thread, path=["response"])
+
+    @parametrize
+    def test_method_list_with_all_params(self, client: Altmind) -> None:
+        thread = client.threads.list(
+            limit=0,
+            offset=0,
+        )
+        assert_matches_type(ThreadsResponse, thread, path=["response"])
+
+    @parametrize
+    def test_raw_response_list(self, client: Altmind) -> None:
+        response = client.threads.with_raw_response.list()
+
+        assert response.is_closed is True
+        assert response.http_request.headers.get("X-Stainless-Lang") == "python"
+        thread = response.parse()
+        assert_matches_type(ThreadsResponse, thread, path=["response"])
+
+    @parametrize
+    def test_streaming_response_list(self, client: Altmind) -> None:
+        with client.threads.with_streaming_response.list() as response:
+            assert not response.is_closed
+            assert response.http_request.headers.get("X-Stainless-Lang") == "python"
+
+            thread = response.parse()
+            assert_matches_type(ThreadsResponse, thread, path=["response"])
+
+        assert cast(Any, response.is_closed) is True
+
+    @parametrize
+    def test_method_delete(self, client: Altmind) -> None:
+        thread = client.threads.delete(
+            0,
+        )
+        assert_matches_type(ThreadDelete, thread, path=["response"])
+
+    @parametrize
+    def test_raw_response_delete(self, client: Altmind) -> None:
+        response = client.threads.with_raw_response.delete(
+            0,
+        )
+
+        assert response.is_closed is True
+        assert response.http_request.headers.get("X-Stainless-Lang") == "python"
+        thread = response.parse()
+        assert_matches_type(ThreadDelete, thread, path=["response"])
+
+    @parametrize
+    def test_streaming_response_delete(self, client: Altmind) -> None:
+        with client.threads.with_streaming_response.delete(
+            0,
+        ) as response:
+            assert not response.is_closed
+            assert response.http_request.headers.get("X-Stainless-Lang") == "python"
+
+            thread = response.parse()
+            assert_matches_type(ThreadDelete, thread, path=["response"])
+
+        assert cast(Any, response.is_closed) is True
+
+
+class TestAsyncThreads:
+    parametrize = pytest.mark.parametrize("async_client", [False, True], indirect=True, ids=["loose", "strict"])
+
+    @parametrize
+    async def test_method_create(self, async_client: AsyncAltmind) -> None:
+        thread = await async_client.threads.create()
+        assert_matches_type(ThreadResponse, thread, path=["response"])
+
+    @parametrize
+    async def test_method_create_with_all_params(self, async_client: AsyncAltmind) -> None:
+        thread = await async_client.threads.create(
+            thread_metadata={},
+            title="title",
+        )
+        assert_matches_type(ThreadResponse, thread, path=["response"])
+
+    @parametrize
+    async def test_raw_response_create(self, async_client: AsyncAltmind) -> None:
+        response = await async_client.threads.with_raw_response.create()
+
+        assert response.is_closed is True
+        assert response.http_request.headers.get("X-Stainless-Lang") == "python"
+        thread = await response.parse()
+        assert_matches_type(ThreadResponse, thread, path=["response"])
+
+    @parametrize
+    async def test_streaming_response_create(self, async_client: AsyncAltmind) -> None:
+        async with async_client.threads.with_streaming_response.create() as response:
+            assert not response.is_closed
+            assert response.http_request.headers.get("X-Stainless-Lang") == "python"
+
+            thread = await response.parse()
+            assert_matches_type(ThreadResponse, thread, path=["response"])
+
+        assert cast(Any, response.is_closed) is True
+
+    @parametrize
+    async def test_method_retrieve(self, async_client: AsyncAltmind) -> None:
+        thread = await async_client.threads.retrieve(
+            0,
+        )
+        assert_matches_type(ThreadResponse, thread, path=["response"])
+
+    @parametrize
+    async def test_raw_response_retrieve(self, async_client: AsyncAltmind) -> None:
+        response = await async_client.threads.with_raw_response.retrieve(
+            0,
+        )
+
+        assert response.is_closed is True
+        assert response.http_request.headers.get("X-Stainless-Lang") == "python"
+        thread = await response.parse()
+        assert_matches_type(ThreadResponse, thread, path=["response"])
+
+    @parametrize
+    async def test_streaming_response_retrieve(self, async_client: AsyncAltmind) -> None:
+        async with async_client.threads.with_streaming_response.retrieve(
+            0,
+        ) as response:
+            assert not response.is_closed
+            assert response.http_request.headers.get("X-Stainless-Lang") == "python"
+
+            thread = await response.parse()
+            assert_matches_type(ThreadResponse, thread, path=["response"])
+
+        assert cast(Any, response.is_closed) is True
+
+    @parametrize
+    async def test_method_update(self, async_client: AsyncAltmind) -> None:
+        thread = await async_client.threads.update(
+            thread_id=0,
+        )
+        assert_matches_type(ThreadResponse, thread, path=["response"])
+
+    @parametrize
+    async def test_method_update_with_all_params(self, async_client: AsyncAltmind) -> None:
+        thread = await async_client.threads.update(
+            thread_id=0,
+            thread_metadata={},
+            title="title",
+        )
+        assert_matches_type(ThreadResponse, thread, path=["response"])
+
+    @parametrize
+    async def test_raw_response_update(self, async_client: AsyncAltmind) -> None:
+        response = await async_client.threads.with_raw_response.update(
+            thread_id=0,
+        )
+
+        assert response.is_closed is True
+        assert response.http_request.headers.get("X-Stainless-Lang") == "python"
+        thread = await response.parse()
+        assert_matches_type(ThreadResponse, thread, path=["response"])
+
+    @parametrize
+    async def test_streaming_response_update(self, async_client: AsyncAltmind) -> None:
+        async with async_client.threads.with_streaming_response.update(
+            thread_id=0,
+        ) as response:
+            assert not response.is_closed
+            assert response.http_request.headers.get("X-Stainless-Lang") == "python"
+
+            thread = await response.parse()
+            assert_matches_type(ThreadResponse, thread, path=["response"])
+
+        assert cast(Any, response.is_closed) is True
+
+    @parametrize
+    async def test_method_list(self, async_client: AsyncAltmind) -> None:
+        thread = await async_client.threads.list()
+        assert_matches_type(ThreadsResponse, thread, path=["response"])
+
+    @parametrize
+    async def test_method_list_with_all_params(self, async_client: AsyncAltmind) -> None:
+        thread = await async_client.threads.list(
+            limit=0,
+            offset=0,
+        )
+        assert_matches_type(ThreadsResponse, thread, path=["response"])
+
+    @parametrize
+    async def test_raw_response_list(self, async_client: AsyncAltmind) -> None:
+        response = await async_client.threads.with_raw_response.list()
+
+        assert response.is_closed is True
+        assert response.http_request.headers.get("X-Stainless-Lang") == "python"
+        thread = await response.parse()
+        assert_matches_type(ThreadsResponse, thread, path=["response"])
+
+    @parametrize
+    async def test_streaming_response_list(self, async_client: AsyncAltmind) -> None:
+        async with async_client.threads.with_streaming_response.list() as response:
+            assert not response.is_closed
+            assert response.http_request.headers.get("X-Stainless-Lang") == "python"
+
+            thread = await response.parse()
+            assert_matches_type(ThreadsResponse, thread, path=["response"])
+
+        assert cast(Any, response.is_closed) is True
+
+    @parametrize
+    async def test_method_delete(self, async_client: AsyncAltmind) -> None:
+        thread = await async_client.threads.delete(
+            0,
+        )
+        assert_matches_type(ThreadDelete, thread, path=["response"])
+
+    @parametrize
+    async def test_raw_response_delete(self, async_client: AsyncAltmind) -> None:
+        response = await async_client.threads.with_raw_response.delete(
+            0,
+        )
+
+        assert response.is_closed is True
+        assert response.http_request.headers.get("X-Stainless-Lang") == "python"
+        thread = await response.parse()
+        assert_matches_type(ThreadDelete, thread, path=["response"])
+
+    @parametrize
+    async def test_streaming_response_delete(self, async_client: AsyncAltmind) -> None:
+        async with async_client.threads.with_streaming_response.delete(
+            0,
+        ) as response:
+            assert not response.is_closed
+            assert response.http_request.headers.get("X-Stainless-Lang") == "python"
+
+            thread = await response.parse()
+            assert_matches_type(ThreadDelete, thread, path=["response"])
+
+        assert cast(Any, response.is_closed) is True
