972389806cf15f77c1564328eedcba2b72a3857d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-12 14:06:11 +0000

    Add package halfedge/0.5.1/halfedge-0.5.1.tar.gz

---
+++ packages/halfedge/halfedge-0.5.1.tar.gz/halfedge-0.5.1/src/halfedge/__init__.py
@@ -0,0 +1,30 @@
+"""Allow modules to be imported from top-level."""
+
+from halfedge.half_edge_constructors import BlindHalfEdges
+from halfedge.half_edge_elements import Edge, Face, MeshElementBase, Vert
+from halfedge.half_edge_object import HalfEdges
+from halfedge.type_attrib import (
+    Attrib,
+    ContagionAttrib,
+    IncompatibleAttrib,
+    NumericAttrib,
+    StaticAttrib,
+    Vector2Attrib,
+    Vector3Attrib,
+)
+
+__all__ = [
+    "Attrib",
+    "BlindHalfEdges",
+    "ContagionAttrib",
+    "Edge",
+    "Face",
+    "HalfEdges",
+    "IncompatibleAttrib",
+    "MeshElementBase",
+    "NumericAttrib",
+    "StaticAttrib",
+    "Vector2Attrib",
+    "Vector3Attrib",
+    "Vert",
+]
