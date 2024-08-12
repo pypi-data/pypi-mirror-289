49dfeeffc6e6b29664331160d4793abe0f021441
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-12 03:05:23 +0000

    Add package oarepo-model-builder-communities/5.0.0/oarepo_model_builder_communities-5.0.0-py3-none-any.whl

---
+++ packages/oarepo-model-builder-communities/oarepo_model_builder_communities-5.0.0-py3-none-any.whl/oarepo_model_builder_communities/datatypes/components/communities_model/service.py
@@ -0,0 +1,12 @@
+from oarepo_model_builder.datatypes.components import ServiceModelComponent
+from oarepo_model_builder.datatypes.components.model.utils import set_default
+from oarepo_model_builder.datatypes import ModelDataType
+
+
+class RecordCommunitiesServiceModelComponent(ServiceModelComponent):
+    eligible_datatypes = [ModelDataType]
+    affects = [ServiceModelComponent] # must be before workflow component
+
+    def before_model_prepare(self, datatype, *, context, **kwargs):
+        config = set_default(datatype, "service-config", {})
+        config.setdefault("components", []).append("{{oarepo_communities.services.components.include.CommunityInclusionComponent}}")
