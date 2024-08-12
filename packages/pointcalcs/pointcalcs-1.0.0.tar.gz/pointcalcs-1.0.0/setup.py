f9f26fef0cb8de836ac3c64a9324a97605ade1ba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-12 04:06:24 +0000

    Add package pointcalcs/1.0.0/pointcalcs-1.0.0.tar.gz

---
+++ packages/pointcalcs/pointcalcs-1.0.0.tar.gz/pointcalcs-1.0.0/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+setup(
+    name='pointcalcs',
+    version='1.0.0',
+    author='WK',
+    author_email='wiktor20122709@gmail.com',
+    description='Includes a point calss and point operations',
+    packages=find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+)
