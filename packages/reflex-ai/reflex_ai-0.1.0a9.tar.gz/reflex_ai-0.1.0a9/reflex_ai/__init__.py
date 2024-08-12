1c3cff85260792517467cf3c9aaf83d4bb946f6c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-12 15:06:10 +0000

    Add package reflex-ai/0.1.0a9/reflex_ai-0.1.0a9.tar.gz

---
+++ packages/reflex-ai/reflex_ai-0.1.0a9.tar.gz/reflex_ai-0.1.0a9/reflex_ai/__init__.py
@@ -0,0 +1,37 @@
+"""The local Reflex agent."""
+
+import inspect
+import os
+import types
+import reflex as rx
+
+
+def enable(app: rx.App):
+    """Enable the agent on an app.
+
+    Args:
+        app: The app to enable the agent on.
+
+    Note:
+        For now, this must be called before add_page is called as
+        we override the add_page method.
+    """
+
+    from .selection import clickable
+    from .toolbar import playground
+
+    # The base path is the directory where the app is defined.
+    caller_frame = inspect.stack()[1]
+    caller_path = caller_frame.filename
+    base_paths = [os.path.dirname(caller_path)]
+
+    def add_page(self, component, *args, **kwargs):
+        route = kwargs.pop("route", rx.utils.format.format_route(component.__name__))
+        rx.App.add_page(self, component, *args, route=route, **kwargs)
+        rx.App.add_page(
+            self,
+            clickable(base_paths=base_paths)(lambda: playground(component)),
+            route=f"/{route}/edit",
+        )
+
+    app.add_page = types.MethodType(add_page, app)
