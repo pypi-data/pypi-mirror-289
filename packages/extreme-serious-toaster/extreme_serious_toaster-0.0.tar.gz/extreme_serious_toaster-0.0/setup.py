7e9e598f2ca4c5f5df785e02f533692176ed99d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-12 04:06:07 +0000

    Add package extreme-serious-toaster/0.0/extreme_serious_toaster-0.0.tar.gz

---
+++ packages/extreme-serious-toaster/extreme_serious_toaster-0.0.tar.gz/extreme_serious_toaster-0.0/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='extreme_serious_toaster',
+      version='0.0',
+      description='Extreme serious toaster for streamlit',
+      packages=['extreme_serious_toaster'],
+      author_email='toupitsyn@gmail.com',
+      zip_safe=False)
