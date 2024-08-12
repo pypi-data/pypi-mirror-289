6a0beb1bd1262786349573d7e7529b243a9855d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-12 04:05:59 +0000

    Add package alogamous/1.0.0/alogamous-1.0.0-py3-none-any.whl

---
+++ packages/alogamous/alogamous-1.0.0-py3-none-any.whl/alogamous/analyzer.py
@@ -0,0 +1,21 @@
+from abc import ABC, abstractmethod
+
+
+class Analyzer(ABC):
+    @abstractmethod
+    def read_log_line(self, line):
+        pass
+
+    @abstractmethod
+    def report(self, out_stream):
+        pass
+
+
+def analyze_log_stream(analyzers, in_stream, out_stream):
+    for line in in_stream:
+        for analyzer in analyzers:
+            analyzer.read_log_line(line.rstrip())
+    for analyzer in analyzers:
+        analyzer.report(out_stream)
+        out_stream.write("\n\n")
+    out_stream.write("------ a report has been reported ------\n\n")
