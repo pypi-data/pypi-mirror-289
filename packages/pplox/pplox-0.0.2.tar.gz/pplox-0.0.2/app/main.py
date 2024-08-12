edd118335bbf2d9710978114b64995c20e1bda70
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-12 05:04:37 +0000

    Add package pplox/0.0.2/pplox-0.0.2.tar.gz

---
+++ packages/pplox/pplox-0.0.2.tar.gz/pplox-0.0.2/app/main.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+import sys
+from pplox.scanner import Scanner
+from pplox.error_reporter import ErrorReporter
+
+def main():
+    if len(sys.argv) < 3:
+        print("Usage: ./your_program.sh tokenize <filename>", file=sys.stderr)
+        exit(1)
+
+    command = sys.argv[1]
+    filename = sys.argv[2]
+
+    if command != "tokenize":
+        print(f"Unknown command: {command}", file=sys.stderr)
+        exit(1)
+
+    with open(filename) as file:
+        file_contents = file.read()
+
+    scanner = Scanner(file_contents)
+    tokens = scanner.scan_tokens()
+    for token in tokens:
+        print(token.to_string())
+
+    if ErrorReporter.had_error:
+        exit(65)
+
+if __name__ == "__main__":
+    main()
