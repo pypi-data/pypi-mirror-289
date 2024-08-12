ee89890774486d68827fbe3ed22dac29906ead4f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-12 04:05:14 +0000

    Add package dduk-core/0.0.18/dduk_core-0.0.18.tar.gz

---
+++ packages/dduk-core/dduk_core-0.0.18.tar.gz/dduk_core-0.0.18/src/dduk/core/__init__.py
@@ -0,0 +1,34 @@
+#--------------------------------------------------------------------------------
+# 참조 모듈 목록.
+#--------------------------------------------------------------------------------
+from __future__ import annotations
+from typing import Any, Final, Optional, Type, TypeVar, Union, Tuple, List, Dict, Set, cast
+import builtins
+from .constant import Constant
+from .platform import PlatformType, GetPlatformType
+from .repository import Repository
+from .sharedclass import SharedClass
+from .singleton import Singleton, SingletonException
+
+
+#--------------------------------------------------------------------------------
+# 공개 인터페이스 목록.
+#--------------------------------------------------------------------------------
+__all__ = [
+	# constant.
+	"Constant",
+
+	# platform.
+	"PlatformType",
+	"GetPlatformType",
+
+	# repository.
+	"Repository",
+
+	# sharedclass.
+	"SharedClass",
+
+	# singleton.
+	"Singleton",
+	"SingletonException"
+]
