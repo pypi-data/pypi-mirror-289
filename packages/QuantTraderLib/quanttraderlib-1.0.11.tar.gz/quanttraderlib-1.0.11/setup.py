b38a46589ed5119646d83842542403d949bb72ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-12 15:05:25 +0000

    Add package QuantTraderLib/1.0.11/quanttraderlib-1.0.11.tar.gz

---
+++ packages/QuantTraderLib/quanttraderlib-1.0.11.tar.gz/quanttraderlib-1.0.11/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='QuantTraderLib',
+    version='1.0.11',
+    author='Hephaestus Tech',
+    author_email='heptech2023@gmail.com',
+    description='QuantTraderLib là một thư viện Python hỗ trợ những vấn đề về quant trading.',
+    url='https://github.com/Gnosis-Tech/PyQuantTrader_Dev',  # Project homepage URL
+    packages=find_packages(),
+    include_package_data=True,
+    package_data={
+        '': ['static/autoscale_cb.js'],  # Ensure this path matches your directory structure
+    },
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+    ],
+    install_requires=[
+        'numpy',
+        'yfinance',
+        'vnstock',
+        'pandas_datareader',
+        'bokeh',
+        'backtesting',
+    ],
+    python_requires='>=3.7, <3.11',
+)
