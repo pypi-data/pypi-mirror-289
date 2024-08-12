abbd8e8783b73f26305e138fe0ac900cff5964e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-12 15:05:35 +0000

    Add package pip-sdk-test/0.5/pip-sdk-test-0.5.tar.gz

---
+++ packages/pip-sdk-test/pip-sdk-test-0.5.tar.gz/pip-sdk-test-0.5/setup.py
@@ -0,0 +1,18 @@
+# setup.py
+
+from setuptools import setup, find_packages
+
+setup(
+    name='pip-sdk-test',
+    version='0.5',
+    packages=find_packages(),
+    entry_points={
+        'console_scripts': [
+            'pip-sdk-test=pip_sdk_test.main:main',
+        ],
+    },
+    description='A simple package that prints Hello, World!',
+    author='Mahesh Karre',
+    author_email='your.email@example.com',
+)
+
