5482aa721c009dcc3c7d1ba52980d110b5d0b71c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-12 04:05:55 +0000

    Add package anandhu-api/0.1/anandhu_api-0.1.tar.gz

---
+++ packages/anandhu-api/anandhu_api-0.1.tar.gz/anandhu_api-0.1/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='anandhu_api',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[
+        'requests',  # Required for making HTTP requests
+    ],
+    description='A simple API client library with API key support by anandhu',
+    author='anandhuaskkkk',
+    author_email='wedalev767@eixdeal.com',
+    url='https://github.com/anandhu/anandhu_api',
+)
