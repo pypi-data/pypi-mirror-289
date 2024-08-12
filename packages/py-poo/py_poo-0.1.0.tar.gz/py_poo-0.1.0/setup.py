73cdcdfa0c21968329a6743c7d03cf2fbfec1d9c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-12 04:06:10 +0000

    Add package py-poo/0.1.0/py_poo-0.1.0.tar.gz

---
+++ packages/py-poo/py_poo-0.1.0.tar.gz/py_poo-0.1.0/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='py-poo',  
+    version='0.1.0',
+    packages=find_packages(),  
+    install_requires=[], 
+    author='Gabriel Carvalho',
+    author_email='contatotrabalhogab@gmail.com',
+    description='Python poo non native features',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/iamgabs/python-poo',  # URL do repositório
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6', 
+)
