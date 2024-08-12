3272972d6e5ba8ac354ea730d85f7bc2346e124c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-12 15:06:24 +0000

    Add package ibdpainting/0.1/ibdpainting-0.1.tar.gz

---
+++ packages/ibdpainting/ibdpainting-0.1.tar.gz/ibdpainting-0.1/README.md
@@ -0,0 +1,2 @@
+# ibdpainting
+Identify parents of a crossed individual by comparing identity in windows across their genomes
---
+++ packages/ibdpainting/ibdpainting-0.1.tar.gz/ibdpainting-0.1/ibdpainting/__init__.py
@@ -0,0 +1,8 @@
+"""Top-level package for methlab."""
+
+__author__ = """Tom Ellis"""
+__email__ = 'thomas.ellis@gmi.oeaw.ac.at'
+__version__ = '0.1'
+
+
+from ibdpainting import *
