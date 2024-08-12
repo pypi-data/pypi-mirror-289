138c6817f446eaea173e61d650d3423cbc87701d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-12 05:04:41 +0000

    Add package mehanika-robota/2024.8.1/mehanika_robota-2024.8.1-py3-none-any.whl

---
+++ packages/mehanika-robota/mehanika_robota-2024.8.1-py3-none-any.whl/mehanika_robota/__init__.py
@@ -0,0 +1,6 @@
+from mehanika_robota import __version__
+from mehanika_robota import mehanika
+from mehanika_robota import roboti
+# from .__version__ import __version__
+# from . import mehanika
+# from . import roboti
