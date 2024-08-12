9eabc059ede0c47f27e46347ba2b2b1fc5cf50a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-12 15:05:23 +0000

    Add package bamboost/0.7.2/bamboost-0.7.2.tar.gz

---
+++ packages/bamboost/bamboost-0.7.2.tar.gz/bamboost-0.7.2/bamboost/__init__.py
@@ -0,0 +1,35 @@
+__author__ = "florez@ethz.ch"
+__copyright__ = ""
+__license__ = "LGPLv3"
+__version__ = "0.7.2"
+
+import logging
+from typing import Literal
+
+
+def add_stream_handler(logger: logging.Logger) -> None:
+    stream_handler = logging.StreamHandler()
+    formatter = logging.Formatter(
+        "[%(asctime)s] %(name)s: %(levelname)s - %(message)s",
+        style="%",
+        datefmt="%Y-%m-%d %H:%M:%S",
+    )
+    stream_handler.setFormatter(formatter)
+    logger.addHandler(stream_handler)
+
+
+BAMBOOST_LOGGER = logging.getLogger("bamboost")
+add_stream_handler(BAMBOOST_LOGGER)
+
+
+def set_log_level(
+    level: Literal["DEBUG", "INFO", "WARNING", "ERROR", "CRITICAL"],
+) -> None:
+    BAMBOOST_LOGGER.setLevel(level)
+
+
+from bamboost._config import config  # noqa: E402, F401
+from bamboost.extensions import extensions  # noqa: E402, F401
+from bamboost.manager import Manager  # noqa: E402, F401
+from bamboost.simulation import Simulation  # noqa: E402, F401
+from bamboost.simulation_writer import SimulationWriter  # noqa: E402, F401
---
+++ packages/bamboost/bamboost-0.7.2.tar.gz/bamboost-0.7.2/bamboost/common/__init__.py
@@ -0,0 +1,10 @@
+from . import file_handler, git_utility, hdf_pointer, job, utilities
+
+__all__ = [
+    "file_handler",
+    "job",
+    "utilities",
+    "git_utility",
+    "hdf_pointer",
+    "mpi",
+]
