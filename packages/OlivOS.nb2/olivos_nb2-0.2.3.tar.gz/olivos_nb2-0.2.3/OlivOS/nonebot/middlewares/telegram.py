5dd198c12ffe4863510875554fb0511f1311d5cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-12 03:05:45 +0000

    Add package OlivOS.nb2/0.2.3/olivos_nb2-0.2.3.tar.gz

---
+++ packages/OlivOS.nb2/olivos_nb2-0.2.3.tar.gz/olivos_nb2-0.2.3/OlivOS/nonebot/middlewares/telegram.py
@@ -0,0 +1,74 @@
+"""
+https://github.com/OlivOS-Team/OlivOS/blob/main/OlivOS/telegramSDK.py
+"""
+
+from datetime import datetime
+from typing import Dict, Type, Optional
+
+from OlivOS.messageAPI import Message_templet
+
+from nonebot import get_bot
+from nonebot.adapters.telegram.bot import Bot
+from nonebot.adapters.telegram.message import Message
+from nonebot.adapters.telegram.event import (
+    Event,
+    MessageEvent,
+    GroupMessageEvent,
+    PrivateMessageEvent,
+)
+
+from . import MSG, BotInfo
+from . import OlivOSEvent as BaseOlivOSEvent
+
+
+class OlivOSEvent(BaseOlivOSEvent):
+    def process_bot(self, bot: Bot):
+        self.platform = {"sdk": "telegram", "platform": "telegram", "model": "nonebot"}
+        self.bot_info = BotInfo(bot)
+        self.base_info["self_id"] = bot.self_id
+
+    def process_event(self, event: Event):
+        self.data = self.Data(**event.dict())
+
+        self.base_info["time"] = (
+            getattr(event, "time", None) or datetime.now().timestamp()
+        )
+        self.base_info["type"] = event.get_type()
+
+        self.plugin_info["message_mode_rx"] = "old_string"
+
+        func_type_map: Dict[Type[Event], Optional[str]] = {
+            PrivateMessageEvent: "private_message",
+            GroupMessageEvent: "group_message",
+        }
+        if type(event) in func_type_map:
+            self.active = True
+            self.plugin_info["func_type"] = func_type_map[type(event)]
+        if isinstance(event, GroupMessageEvent) or isinstance(
+            event, PrivateMessageEvent
+        ):
+            self.data.sender = {
+                "nickname": event.from_.first_name,
+                "user_id": event.from_.id,
+                "name": event.from_.first_name,
+                "id": event.from_.id,
+            }
+
+            self.data.extend = {}
+            message = Message_templet("old_string", str(event.message))
+            self.data.raw_message = message
+            self.data.message_sdk = message
+            self.data.raw_message_sdk = message
+
+    def reply(self, message: MSG):
+        if self.data:
+            self.run_async(
+                get_bot(str(self.bot_info.id)).send(
+                    MessageEvent(**self.data.dict()), Message(message)
+                )
+            )
+
+    def send(self, message: MSG):
+        self.call_api(
+            "sendMessage", chat_id=self.data.chat.id, text=message  # type:ignore
+        )
---
+++ packages/OlivOS.nb2/olivos_nb2-0.2.3.tar.gz/olivos_nb2-0.2.3/README.md
@@ -0,0 +1,71 @@
+<div align="center">
+    <img width="200" src="docs/logo.png" alt="logo"></br>
+
+# OlivOS.nb2
+
+[NoneBot2](https://github.com/nonebot/nonebot2) 的 [OlivOS](https://github.com/OlivOS-Team/OlivOS) 兼容层插件
+
+**注意，本兼容层无法获得 API 的返回值！**
+
+[![License](https://img.shields.io/github/license/nonepkg/OlivOS.nb2)](LICENSE)
+![Python Version](https://img.shields.io/badge/python-3.7.3+-blue.svg)
+![NoneBot Version](https://img.shields.io/badge/nonebot-2.0.0a13+-red.svg)
+![PyPI Version](https://img.shields.io/pypi/v/OlivOS.nb2.svg)
+
+</div>
+
+## 安装
+
+### 从 PyPI 安装（推荐）
+
+- 使用 nb-cli  
+
+```sh
+nb plugin install OlivOS.nb2
+```
+
+- 使用 poetry
+
+```sh
+poetry add OlivOS.nb2
+```
+
+- 使用 pip
+
+```sh
+pip install OlivOS.nb2
+```
+
+### 从 GitHub 安装（不推荐）
+
+- 使用 poetry
+
+```sh
+poetry add git+https://github.com/nonepkg/OlivOS.nb2.git
+```
+
+- 使用 pip
+
+```sh
+pip install git+https://github.com/nonepkg/OlivOS.nb2.git
+```
+
+## 使用
+
+目前只有 OneBot V11（CQHTTP）平台的兼容层，其他平台待添加（Telegram 平台实验中）。
+
+OlivOS 插件请放入`./data/OlivOS/app/`。
+
+## 插件兼容名单
+
+**这里的插件指 OlivOS 插件。** 未在此名单并不意味着无法加载，只是加载后可能出现未知问题。
+
+- [OlivOS-Team/OlivaDiceCore](https://github.com/OlivOS-Team/OlivaDiceCore)
+- [OlivOS-Team/OlivaDiceJoy](https://github.com/OlivOS-Team/OlivaDiceJoy)
+- [OlivOS-Team/OlivaDiceLogger](https://github.com/OlivOS-Team/OlivaDiceLogger)
+- [Fishroud/OlivaBilibiliPlugin](https://github.com/Fishroud/OlivaBilibiliPlugin)
+
+### 已证实不兼容
+
+- [lunzhiPenxil/OlivOSOnebotV11](https://github.com/lunzhiPenxil/OlivOSOnebotV11) 使用 Thread
+- [OlivOS-Team/OlivaDiceMaster](https://github.com/OlivOS-Team/OlivaDiceMaster) 使用 Thread
