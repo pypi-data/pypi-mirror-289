33662a1a30ffdee1f5480a59892b29324516ca33
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-12 03:05:40 +0000

    Add package fake-profile/0.2.0/fake_profile-0.2.0.tar.gz

---
+++ packages/fake-profile/fake_profile-0.2.0.tar.gz/fake_profile-0.2.0/setup.py
@@ -0,0 +1,22 @@
+
+from setuptools import setup, find_packages
+
+setup(
+    name='fake_profile',
+    version='0.2.0',
+    author='Collins O. Odhiambo',
+    description = 'This is a python package that helps developers to generate fake social media user profiles and interactions for development purposes',
+    author_email='comon928@gmail.com',
+    url='https://github.com/C-o-m-o-n/fake_profile',
+    packages=find_packages(),
+    install_requires=[
+        'Faker',
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
+
+
