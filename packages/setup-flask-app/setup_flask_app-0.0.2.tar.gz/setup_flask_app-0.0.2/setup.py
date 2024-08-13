a259dc9cfd3fe417dae7f0c0ef521d5a101e8e60
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-13 03:05:07 +0000

    Add package setup-flask-app/0.0.2/setup_flask_app-0.0.2.tar.gz

---
+++ packages/setup-flask-app/setup_flask_app-0.0.2.tar.gz/setup_flask_app-0.0.2/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.0.2'
+DESCRIPTION = 'A CLI to setup a flask app with just one command'
+LONG_DESCRIPTION = 'This is a CLI tool which setup a flask app with just one command. This is to ease devs. They just have to write setup-flask-app and every thing require to stat adding value to their app will be setup automatically.'
+
+# Setting up
+setup(
+    name="setup-flask-app",
+    version=VERSION,
+    author="Saqib Tanveer",
+    author_email="<elven5055@gmail.com>",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=LONG_DESCRIPTION,
+    packages=find_packages(),
+    entry_points={
+        'console_scripts': [
+            'setup-flask-app=setup_flask_app.cli:app',  # Command to run your CLI
+        ],
+    },
+    keywords=['python', 'flask', 'setup-flask-app', 'auto setup'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
