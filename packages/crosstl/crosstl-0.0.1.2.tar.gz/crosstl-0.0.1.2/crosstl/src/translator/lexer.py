7b296a55595425bf258dc271fe663e53c834819f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-13 03:05:20 +0000

    Add package crosstl/0.0.1.2/crosstl-0.0.1.2.tar.gz

---
+++ packages/crosstl/crosstl-0.0.1.2.tar.gz/crosstl-0.0.1.2/crosstl/src/translator/lexer.py
@@ -0,0 +1,109 @@
+import re
+
+TOKENS = [
+    ("COMMENT_SINGLE", r"//.*"),
+    ("COMMENT_MULTI", r"/\*[\s\S]*?\*/"),
+    ("SHADER", r"\bshader\b"),
+    ("INPUT", r"\binput\b"),
+    ("OUTPUT", r"\boutput\b"),
+    ("VOID", r"\bvoid\b"),
+    ("MAIN", r"\bmain\b"),
+    ("UNIFORM", r"\buniform\b"),
+    ("VECTOR", r"\bvec[2-4]\b"),
+    ("MATRIX", r"\bmat[2-4]\b"),
+    ("BOOL", r"\bbool\b"),
+    ("VERTEX", r"\bvertex\b"),
+    ("FRAGMENT", r"\bfragment\b"),
+    # ("INCREMENT_DECREMENT", r"\b[a-zA-Z_][a-zA-Z_0-9]*\s*(\+\+|--)\b"),
+    ("FLOAT", r"\bfloat\b"),
+    ("INT", r"\bint\b"),
+    ("SAMPLER2D", r"\bsampler2D\b"),
+    ("IDENTIFIER", r"[a-zA-Z_][a-zA-Z_0-9]*"),
+    ("NUMBER", r"\d+(\.\d+)?"),
+    ("LBRACE", r"\{"),
+    ("RBRACE", r"\}"),
+    ("LPAREN", r"\("),
+    ("RPAREN", r"\)"),
+    ("SEMICOLON", r";"),
+    ("COMMA", r","),
+    ("ASSIGN_ADD", r"\+="),
+    ("ASSIGN_SUB", r"-="),
+    ("ASSIGN_MUL", r"\*="),
+    ("ASSIGN_DIV", r"/="),
+    ("WHITESPACE", r"\s+"),
+    ("IF", r"\bif\b"),
+    ("ELSE", r"\belse\b"),
+    ("FOR", r"\bfor\b"),
+    ("RETURN", r"\breturn\b"),
+    ("LESS_EQUAL", r"<="),
+    ("GREATER_EQUAL", r">="),
+    ("LESS_THAN", r"<"),
+    ("GREATER_THAN", r">"),
+    ("INCREMENT", r"\+\+"),
+    ("DECREMENT", r"--"),
+    ("EQUAL", r"=="),
+    ("NOT_EQUAL", r"!="),
+    ("AND", r"&&"),
+    ("OR", r"\|\|"),
+    ("NOT", r"!"),
+    ("INCREMENT", r"\+\+"),
+    ("DECREMENT", r"\-\-"),
+    ("PLUS", r"\+"),
+    ("MINUS", r"-"),
+    ("MULTIPLY", r"\*"),
+    ("DIVIDE", r"/"),
+    ("DOT", r"\."),
+    ("EQUALS", r"="),
+    ("QUESTION", r"\?"),
+    ("COLON", r":"),
+]
+
+KEYWORDS = {
+    "shader": "SHADER",
+    "input": "INPUT",
+    "output": "OUTPUT",
+    "void": "VOID",
+    "main": "MAIN",
+    "vertex": "VERTEX",
+    "fragment": "FRAGMENT",
+    "if": "IF",
+    "else": "ELSE",
+    "for": "FOR",
+    "return": "RETURN",
+}
+
+
+class Lexer:
+    def __init__(self, code):
+        self.code = code
+        self.tokens = []
+        self.tokenize()
+
+    def tokenize(self):
+        pos = 0
+        while pos < len(self.code):
+            match = None
+            for token_type, pattern in TOKENS:
+                regex = re.compile(pattern)
+                match = regex.match(self.code, pos)
+                if match:
+                    text = match.group(0)
+                    if token_type == "IDENTIFIER" and text in KEYWORDS:
+                        token_type = KEYWORDS[text]
+                    if token_type != "WHITESPACE":  # Ignore whitespace tokens
+
+                        token = (token_type, text)
+
+                        self.tokens.append(token)
+                    pos = match.end(0)
+                    break
+            if not match:
+                unmatched_char = self.code[pos]
+                highlighted_code = (
+                    self.code[:pos] + "[" + self.code[pos] + "]" + self.code[pos + 1 :]
+                )
+                raise SyntaxError(
+                    f"Illegal character '{unmatched_char}' at position {pos}\n{highlighted_code}"
+                )
+
+        self.tokens.append(("EOF", None))  # End of file token
