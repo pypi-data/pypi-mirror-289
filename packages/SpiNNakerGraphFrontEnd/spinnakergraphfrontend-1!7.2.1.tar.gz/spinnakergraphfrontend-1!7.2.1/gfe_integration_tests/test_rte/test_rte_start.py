f7a460af7584fcca30897254323c3f83aba301da
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-13 03:05:35 +0000

    Add package SpiNNakerGraphFrontEnd/1!7.2.1/spinnakergraphfrontend-1!7.2.1.tar.gz

---
+++ packages/SpiNNakerGraphFrontEnd/spinnakergraphfrontend-1!7.2.1.tar.gz/spinnakergraphfrontend-1!7.2.1/gfe_integration_tests/test_rte/test_rte_start.py
@@ -0,0 +1,30 @@
+# Copyright (c) 2017 The University of Manchester
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import os
+import pytest
+from spinnman.exceptions import SpinnmanException
+from spinnman.model.enums import ExecutableType
+import spinnaker_graph_front_end as s
+from gfe_integration_tests.test_rte.run_vertex import RunVertex
+
+
+def test_rte_at_start():
+    s.setup(model_binary_folder=os.path.dirname(__file__))
+    s.add_machine_vertex_instance(
+        RunVertex(
+            "test_rte_start.aplx",
+            ExecutableType.USES_SIMULATION_INTERFACE))
+    with pytest.raises(SpinnmanException):
+        s.run(1000)
