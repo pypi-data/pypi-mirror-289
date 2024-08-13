734cbaf347732c6fea90c7d1600431fd3bdcef7f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-13 03:05:31 +0000

    Add package python-injection/0.10.1/python_injection-0.10.1.tar.gz

---
+++ packages/python-injection/python_injection-0.10.1.tar.gz/python_injection-0.10.1/injection/testing/__init__.pyi
@@ -0,0 +1,20 @@
+from contextlib import ContextDecorator
+from typing import ContextManager
+
+import injection as _
+from injection import Module
+
+set_test_constant = _.set_constant
+should_be_test_injectable = _.should_be_injectable
+test_constant = _.constant
+test_injectable = _.injectable
+test_singleton = _.singleton
+
+def use_test_injectables(
+    *,
+    module: Module = ...,
+    test_module: Module = ...,
+) -> ContextManager[None] | ContextDecorator:
+    """
+    Context manager or decorator for temporary use test module.
+    """
