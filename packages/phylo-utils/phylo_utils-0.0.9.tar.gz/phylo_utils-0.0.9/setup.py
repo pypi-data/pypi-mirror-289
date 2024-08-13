93a83ecddd6c745504c3fe8459ad187d0bc07c40
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-13 03:05:32 +0000

    Add package phylo-utils/0.0.9/phylo_utils-0.0.9.tar.gz

---
+++ packages/phylo-utils/phylo_utils-0.0.9.tar.gz/phylo_utils-0.0.9/setup.py
@@ -0,0 +1,26 @@
+try:
+    from setuptools import setup, find_packages, Extension
+except ImportError:
+    from distutils.core import setup, Extension
+
+from Cython.Distutils import build_ext
+
+import numpy
+
+ext = Extension("phylo_utils.likcalc",
+                sources = ['extensions/likcalc.pyx',
+                           'extensions/discrete_gamma.c'],
+                include_dirs = [numpy.get_include()],
+               )
+
+setup(cmdclass={'build_ext':build_ext},
+      name="phylo_utils",
+      author='Kevin Gori',
+      author_email='kgori@ebi.ac.uk',
+      description='Phylogenetics calculations in python',
+      url='',
+      version="0.0.9",
+      ext_modules = [ext],
+      install_requires = ['cython', 'numpy', 'dendropy'],
+      packages=find_packages(),
+     )
