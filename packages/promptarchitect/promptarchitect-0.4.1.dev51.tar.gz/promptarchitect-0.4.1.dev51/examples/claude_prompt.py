341b934cea2662e1dc8f7a5d19733c71ff5f9691
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-13 03:05:21 +0000

    Add package promptarchitect/0.4.1.dev51/promptarchitect-0.4.1.dev51.tar.gz

---
+++ packages/promptarchitect/promptarchitect-0.4.1.dev51.tar.gz/promptarchitect-0.4.1.dev51/examples/claude_prompt.py
@@ -0,0 +1,21 @@
+from pathlib import Path
+
+from promptarchitect import EngineeredPrompt
+
+prompt_path = Path(__file__).parent / "prompts" / "claude.prompt"
+input_file_path = (
+    Path(__file__).parent / "inputs" / "podcast_titels" / "beschrijving.txt"
+)
+
+# Initialize the EngineeredPrompt
+prompt = EngineeredPrompt(
+    prompt_file_path=str(prompt_path), output_path="output_directory"
+)
+
+print(prompt)
+
+# Execute the prompt
+# response = prompt.execute(input_file=str(input_file_path), cached=False)
+
+
+# print(response)
