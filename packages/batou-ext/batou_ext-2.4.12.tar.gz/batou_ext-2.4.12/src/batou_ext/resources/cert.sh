e71778342c53ef3b1d6df7aef925f936427fd33c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-13 03:05:25 +0000

    Add package batou-ext/2.4.12/batou_ext-2.4.12.tar.gz

---
+++ packages/batou-ext/batou_ext-2.4.12.tar.gz/batou_ext-2.4.12/src/batou_ext/resources/cert.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+set -e
+
+{% if component.host.platform == "nixos" -%}
+source /etc/profile
+{% endif -%}
+
+{{component.workdir}}/dehydrated \
+    --config {{component.config.path}} \
+    --register --accept-terms
+
+{{component.workdir}}/dehydrated \
+    --config {{component.config.path}} \
+    --out {{component.workdir}} \
+{%- if component.letsencrypt_alternative_chain %}
+    --preferred-chain "{{component.letsencrypt_alternative_chain}}" \
+{%- endif %}
+    -c "$@"
+setfacl -Rm u:{{component.granted_user}}:rX {{component.workdir}}/"$@"
+{% if component.extracommand %}
+{{component.extracommand}}
+{% endif %}
